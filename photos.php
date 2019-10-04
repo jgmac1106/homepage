ae3c1936a053d7906649d949426f1745aad54463
Greg McVerry <jgregmcverry@gmail.com>
2019-10-04 14:03:31 -0400

    Update photos.php
    adding h-feed photos

--- photos.php
+++ photos.php
@@ -1,242 +1,81 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>INTERTEXTrEVOLUTION</title>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/reset.css">
-    
-  <link rel="stylesheet" href="styles.css">
-      <link rel="stylesheet" href="css/gallery.css">
-     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-9ralMzdK1QYsk4yBY680hmsb4/hJ98xK3w0TIaJ3ll4POWpWUYaA2bRjGGujGT8w" crossorigin="anonymous">
-
-    <script src="/script.js" defer></script>
-    <link rel="webmention" href="https://webmention.io/jgmac1106homepage.glitch.me/webmention" />
-<link rel="pingback" href="https://webmention.io/jgmac1106homepage.glitch.me/xmlrpc" />
-    <link rel="authorization_endpoint" href="https://indieauth.com/auth">
-<link rel="token_endpoint" href="https://tokens.indieauth.com/token">
-    <link rel="webmention" href="https://webmention.io/jgregorymcverry.com/webmention" />
-<link rel="pingback" href="https://webmention.io/jgregorymcverry.com/xmlrpc" />
-  </head>
-  <body>
-    <header>
-       <a href="index.html">
-   <div class="leading">
-     <h1 class="leading-bigtext">
-       INTERTEXTrEVOLUTION
-     </h1>
-     <h2 class="bigtext">
-       Make.Hack.Play.Learn
-     </h2>
-      </div>
-     </a>  
-       <nav class="Navbar__Items">
-    <div class="Navbar__Link">
-    
-    <a href="/aboutme.html" >About</a>
+<?php include 'head.php'; ?>
+<?php include 'header.php'; ?>
+  <main class="h-feed">
+  
+     <div class="h-card">
+  <a class="p-name u-url" href="http://jgregorymcverry.com" rel="me">Greg McVerry</a><br>
     </div>
-    <div class="Navbar__Link">
-    <a href="/articles.html" >Articles</a>
-    </div>
-        <div class="Navbar__Link">
-    <a href="https://quickthoughts.jgregorymcverry.com" >Blog</a>
-    </div>
-     
-      <div class="Navbar__Link">
-    <a href="/contact.html">Contact</a>
-    </div>
-     <div class="Navbar__Link">
-    <a href="/resume.html" class="navLinks">Vita</a>
-    </div>
-      <div class="Navbar__Link">
-    <a href="https://consulting.jgregorymcverry.com/" class="navLinks">Consulting</a>
-    </div>
-        <div class="Navbar__Link">
-    <a href="/2toPonder.html" class="navLinks">Podcasts</a>
-    </div>
-        <div class="Navbar__Link">
-    <a href="/following.html" class="navLinks">Following</a>
-    </div>
-            <div class="Navbar__Link">
-    <a href="/annotations.html" class="navLinks">Annotations</a>
-    </div>
-            <div class="Navbar__Link">
-    <a href="/imagecredits.html" class="navLinks">Img Credits</a>
-    </div>
-  </nav>
-    </header>
-
-     
-      
-    
-
-  <main>
-  <h1 class="p-name">My Photos</h1>
-    <h2>
-      Enjoy a trip down <span class="p-author h-card">Greg</span>'s Memory Lane
-      </h2>
-
-	<div class="grid">
-
-	<figure class=" h-entry grid-item grid-item-lg">
-			<a class="u-url" href="https://quickthoughts.jgregorymcverry.com/2019/02/23/meet-flamethrower-and-rainbow-rocket">
+<section class="card-container">
+  <article class="h-entry card">
+        <header class="card__title">
+            <h3 class="p-name">My Photos</h3>
+        </header>
+      <span class="authorInfo">
+         <span class="h-card"> 
+           <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
+         </span>
+        
+         <time class="dt-published" datetime="2019-09-01 10:41:00">2019-09-01 </time>
+      </span>
+        <figure class="card__thumbnail">
+            <img class="u-featured" src="http://jgregorymcverry.com/photos/2019-08-21/MVIMG_20190820_184049.jpg" alt="three boys on a golf course" >
          
-        <img class="u-photo" src="https://quickthoughts.jgregorymcverry.com/file/35b4e80cd2b43dc833bd282f4615c3f6/thumb.jpg" alt="Flamethrower and Rocket">
-      </a>
-        <figcaption class="p-name">Flamethrower and the Driver</figcaption>
-       </figure>
-      <figure class=" h-entry grid-item grid-item-db">
-			<a class="u-url" href="https://quickthoughts.jgregorymcverry.com/2019/02/23/meet-flamethrower-and-rainbow-rocket">
+        </figure>
+        <div class="p-summary card__description">
+          <p>
+            Final Scramble of the 2019 Season
+          </p>
+        </div>
+        <a  href="/finalscramble" class="u-url button">View</a>
+
+    </article> 
+     <article class="h-entry card">
+        <header class="card__title">
+            <h3 class="p-name">Ocean City 2019</h3>
+        </header>
+      <span class="authorInfo">
+         <span class="h-card"> 
+           <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
+         </span>
+        
+         <time class="dt-published" datetime="2019-09-07 5:16:00">2019-09-013 </time>
+      </span>
+        <figure class="card__thumbnail">
+            <img class="u-featured" src="/photos/oceancity2019/billowonbrigatine.jpg" alt="sign that Says McVerry on Brigatine established 2018" >
          
-        <img class="u-photo" src="https://quickthoughts.jgregorymcverry.com/file/2b291b69e9d162c306fb88dc43f7f610/thumb.jpg" alt="Flamethrower and Rocket">
-      </a>
-        <figcaption class="p-name">Flamethrower and Rocket</figcaption>
-       </figure>
- <figure class=" h-entry grid-item grid-item-sm">
-			<a class="u-url" href="https://quickthoughts.jgregorymcverry.com/2019/02/23/meet-flamethrower-and-rainbow-rocket">
+        </figure>
+        <div class="p-summary card__description">
+          <data class="p-category" value="oceancity">Ocean City</data> 2019 
+        </div>
+        from localssummer
+        </div>
+        <a  href="/edcampct" class="u-url button">View</a>
+
+    </article> 
+    <article class="h-entry card">
+        <header class="card__title">
+            <h3 class="p-name">Yankees 2019</h3>
+        </header>
+      <span class="authorInfo">
+         <span class="h-card"> 
+           <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
+         </span>
+        
+         <time class="dt-published" datetime="2019-09-29 5:16:00">2019-09-29 </time>
+      </span>
+        <figure class="card__thumbnail">
+            <img class="u-featured" src="/photos/yankees2019/housethatruthbuilt.jpg" alt="shot from road of Yankees Gate 4 entrance" >
          
-        <img class="u-photo" src="https://quickthoughts.jgregorymcverry.com/file/2b291b69e9d162c306fb88dc43f7f610/thumb.jpg" alt="Flamethrower and Rocket">
-      </a>
-        <figcaption class="p-name">Flamethrower and Rocket</figcaption>
-       </figure>
-     <figure class=" h-entry grid-item grid-item-sm">
-			<a class="u-url" href="https://quickthoughts.jgregorymcverry.com/2019/02/10/pinewood-derby-time">
-    
-        <img class="u-photo" src="https://quickthoughts.jgregorymcverry.com/file/b2d470f5ad2dcbfa04026a25690a8a58/thumb.jpg" alt="Painintg Pinewood Derby">
-      </a>
-        <figcaption class="p-name">Getting Ready for the Derby</figcaption>
-       </figure>
- <figure class=" h-entry grid-item grid-item-sm">
-			<a class="u-url" href="https://quickthoughts.jgregorymcverry.com/2019/02/10/pinewood-derby-time">
-         
-        <img class="u-photo" src="https://quickthoughts.jgregorymcverry.com/file/b2d470f5ad2dcbfa04026a25690a8a58/thumb.jpg" alt="Painintg Pinewood Derby">
-      </a>
-        <figcaption class="p-name">Getting Ready for the Derby</figcaption>
-       </figure>
-    <figure class=" h-entry grid-item grid-item-sm">
-			<a class="u-url" href="https://quickthoughts.jgregorymcverry.com/2019/02/10/pinewood-derby-time">
-        <img class="u-photo" src="https://quickthoughts.jgregorymcverry.com/file/b2d470f5ad2dcbfa04026a25690a8a58/thumb.jpg" alt="Painintg Pinewood Derby">
-      </a>
-        <figcaption class="p-name">Getting Ready for the Derby</figcaption>
-       </figure>
-	<figure class=" h-entry grid-item grid-item-sm">
-			<a class="u-url" href="https://quickthoughts.jgregorymcverry.com/2019/02/10/pinewood-derby-time">
-        <img class="u-photo" src="https://quickthoughts.jgregorymcverry.com/file/b2d470f5ad2dcbfa04026a25690a8a58/thumb.jpg" alt="Painintg Pinewood Derby">
-      </a>
-        <figcaption class="p-name">Getting Ready for the Derby</figcaption>
-       </figure>
-    <figure class=" h-entry grid-item grid-item-sm">
-			<a class="u-url" href="https://quickthoughts.jgregorymcverry.com/2019/02/10/pinewood-derby-time">
-        <img class="u-photo" src="https://quickthoughts.jgregorymcverry.com/file/b2d470f5ad2dcbfa04026a25690a8a58/thumb.jpg" alt="Painintg Pinewood Derby">
-      </a>
-        <figcaption class="p-name">Getting Ready for the Derby</figcaption>
-       </figure>
-     
-	<!--
-	<div class="grid">
+        </figure>
+        <div class="p-summary card__description">
+          Hitting Yankees Staidum
+        </div>
+        <a  href="/yankees2019" class="u-url button">View</a>
 
-		<div class="grid-item grid-item-db">
-			<a href="#">1/2 articles this month</a>
-			<time class="grid-date" datetime="YYYY-MM-DD">Month DD, YYYY</time>
-		</div>
-		<div class="grid-item grid-item-db">
-			<a href="#">2/2 articles this month</a>
-			<time class="grid-date" datetime="YYYY-MM-DD">Month DD, YYYY</time>
-		</div>
-
-	</div>
-
-	<h1>March</h1>
-
-	<div class="grid">
-
-		<div class="grid-item grid-item-db">
-			<a href="#">1/3 articles this month</a>
-			<time class="grid-date" datetime="YYYY-MM-DD">Month DD, YYYY</time>
-		</div>
-		<div class="grid-item grid-item-sm">
-			<a href="#">2/3 articles this month</a>
-			<time class="grid-date" datetime="YYYY-MM-DD">Month DD, YYYY</time>
-		</div>
-		<div class="grid-item grid-item-sm">
-			<a href="#">3/3 articles this month</a>
-			<time class="grid-date" datetime="YYYY-MM-DD">Month DD, YYYY</time>
-		</div>
-
-	</div>
-
-	<h1> April </h1>
-
-	<div class="grid">
-
-		<div class="grid-item grid-item-sm">
-			<a href="#">1/4 articles this month</a>
-			<time class="grid-date" datetime="YYYY-MM-DD">Month DD, YYYY</time>
-		</div>
-		<div class="grid-item grid-item-sm">
-			<a href="#">2/4 articles this month</a>
-			<time class="grid-date" datetime="YYYY-MM-DD">Month DD, YYYY</time>
-		</div>
-		<div class="grid-item grid-item-sm">
-			<a href="#">3/4 articles this month</a>
-			<time class="grid-date" datetime="YYYY-MM-DD">Month DD, YYYY</time>
-		</div>
-		<div class="grid-item grid-item-sm">
-			<a href="#">4/4 articles this month</a>
-			<time class="grid-date" datetime="YYYY-MM-DD">Month DD, YYYY</time>
-		</div>
-
-	</div>
-
-	<h1>May</h1>
-
-	<div class="grid">
-
-		<div class="grid-item grid-item-lg">
-			<a href="#">1/5 articles this month</a>
-			<time class="grid-date" datetime="YYYY-MM-DD">Month DD, YYYY</time>
-		</div>
-		<div class="grid-item grid-item-sm">
-			<a href="#">2/5 articles this month</a>
-			<time class="grid-date" datetime="YYYY-MM-DD">Month DD, YYYY</time>
-		</div>
-		<div class="grid-item grid-item-sm">
-			<a href="#">3/5 articles this month</a>
-			<time class="grid-date" datetime="YYYY-MM-DD">Month DD, YYYY</time>
-		</div>
-		<div class="grid-item grid-item-sm">
-			<a href="#">4/5 articles this month</a>
-			<time class="grid-date" datetime="YYYY-MM-DD">Month DD, YYYY</time>
-		</div>
-		<div class="grid-item grid-item-sm">
-			<a href="#">5/5 articles this month</a>
-			<time class="grid-date" datetime="YYYY-MM-DD">Month DD, YYYY</time>
-		</div>
-
-	</div>
--->
-
-<!-- <div class="grid">
-	<div class="grid-item grid-item-lg">
-		<span>1</span>
-		<h1>Aspect ratio grid</h1>
-		<p> Lorem ipsum </p>
-	</div>
-	<div class="grid-item grid-item-sm">2</div>
-	<div class="grid-item grid-item-sm">3</div>
-	<div class="grid-item grid-item-db">4</div>
-	<div class="grid-item grid-item-lg grid-item-right">5</div>
-	<div class="grid-item grid-item-sm">6</div>
-	<div class="grid-item grid-item-db">7</div>
-	<div class="grid-item grid-item-sm">8</div>
-	<div class="grid-item grid-item-fw">9</div>
-
-</div> -->
-
-    
- </main>
+    </article>
+</section>
+    </main>
+<?php include 'footer.php'; ?>
   </body>
-  <footer><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
-  </footer>
+  
 </html>
