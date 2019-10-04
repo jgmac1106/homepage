5132228003f08e5dca76c3b119dc9928476dd786
Greg McVerry <jgregmcverry@gmail.com>
2019-10-04 09:25:36 -0400

    adding header, head, and footer
    adding files

---
+++ header.php
@@ -0,0 +1,46 @@
+ <header>
+      <a href="index.html">
+        <div class="leading">
+          <p>
+            INTERTEXTrEVOLUTION
+          </p>
+          <p class="tagline">
+            Make.Hack.Play.Learn
+          </p>
+        </div>
+      </a>
+      <nav class="Navbar__Items">
+        <div class="Navbar__Link">
+          
+          <a href="/aboutme.html" >About</a>
+        </div>
+        <div class="Navbar__Link">
+          <a href="/articles.html" >Articles</a>
+        </div>
+        <div class="Navbar__Link">
+          <a href="https://quickthoughts.jgregorymcverry.com" >Blog</a>
+        </div>
+        
+        <div class="Navbar__Link">
+          <a href="/contact.html">Contact</a>
+        </div>
+        <div class="Navbar__Link">
+          <a href="/resume.html" class="navLinks">Vita</a>
+        </div>
+        <div class="Navbar__Link">
+          <a href="https://consulting.jgregorymcverry.com/" class="navLinks">Consulting</a>
+        </div>
+        <div class="Navbar__Link">
+          <a href="/2toPonder.html" class="navLinks">Podcasts</a>
+        </div>
+        <div class="Navbar__Link">
+          <a href="/following.html" class="navLinks">Following</a>
+        </div>
+        <div class="Navbar__Link">
+          <a href="/annotations.html" class="navLinks">Annotations</a>
+        </div>
+        <div class="Navbar__Link">
+          <a href="/imagecredits.html" class="navLinks">Img Credits</a>
+        </div>
+      </nav>
+    </header>
