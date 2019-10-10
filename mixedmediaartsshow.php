2ff7413fcf43d786634fcfd5515b97fd6b962c2e
Greg McVerry <jgregmcverry@gmail.com>
2019-10-10 08:13:12 -0400

    adding IndieWebCamp NYC Photos
    adding IndieWebCamp NYC Photos

---
+++ mixedmediaartsshow.php
@@ -0,0 +1,158 @@
+<?php include 'galleryhead.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+</header>
+<div class="child-page-listing h-entry">
+	<h2 class="p-name">Mixed Media Art Show</h2>
+	<h3>
+	Collection by <a class="u-author h-card" href="https://jgregorymcverry.com">Greg McVerry</a> and published <time class="dt-published" datetime="2019-10-08 4:20:00">2019-10-08 </time>
+	</h3>
+	<img class="featured u-featured" style="width=50%" src="/photos/mixedmediaartshow/digitalbeing.gif">
+	<div class="copy e-content">
+		<p>After the first night of IndieWebCamp NYC I stumbled into a mixed media art show. I share some of the pictures.
+		<div class="grid-container ">
+			<figure id="1" class="h-entry gallery">
+				<div class="galleryPhoto">
+					<a href="http://jgregorymcverry.com/photos/oceancity2019/thefamily.JPG">
+					<img class="u-photo" src="/photos/mixedmediaartshow/digitalbeing.jpg" alt="close up of artist instagram handle digitalbeing">      </a>
+				</div>
+				<figcaption class="galleryCaption" >
+				<span class="picTitle p-name">@DigitalBeing</span>
+				<time datetime="2019-10-05 21:41:00" class="dt-published">2019-09-05</time>
+				<p class="e-content">I didn't get the artist name beyond @digitalbeing who ahd my favorite exihibit</p>
+				</figcaption>
+			</figure>
+			<figure id="2" class="gallery h-entry">
+				<div class="galleryPhoto">
+					<a href="/photos/mixedmediaartshow/monitorroom.jpg">
+					<img class="u-photo" src="/photos/mixedmediaartshow/monitorroom.jpg" alt="purple trippy screens everywhere">    </a>
+				</div>
+				<figcaption class="galleryCaption" >
+				<span class="picTitle p-name">The purple monitor room</span>
+				<time datetime="2019-10-05 21:41:00" class="dt-published">2019-09-05</time>
+				<p class="e-content">One room had these cool monitors and blubs everywhere</p>
+				</figcaption>
+			</figure>
+			<figure id="8" class="gallery h-entry">
+						<figcaption class="galleryCaption" >
+						<span class="picTitle p-name">Digital Being</span><time datetime="2019-10-05 21:41:00" class="dt-published">2019-10-05</time>
+						<p class="e-content"> So many cool sounds and lights associated with this piece</p>
+						</figcaption>
+						<div class="galleryPhoto">
+							<a href="/photos/mixedmediaartshow/digitalbeing2.jpg">
+							<img class="u-photo" src="/photos/mixedmediaartshow/digitalbeing2.jpg" alt="shot of the piece from the side showing lights">      </a>
+						</div>
+					</figure>
+				<figure id="4" class="gallery h-entry">
+					<figcaption class="galleryCaption" >
+					<span class="picTitle p-name">Reclaimed Monitors</span><time datetime="2019-10-05 21:41:00" class="dt-published">2019-10-05</time>
+					<p class="e-content">Digitalbeing made an exhibit out of reclaimed monitors. Here is close up of single</p>
+					</figcaption>
+					<div class="galleryPhoto">
+						<a href="/photos/mixedmediaartshow/blackandwhitepaintings.jpg">
+						<img class="u-photo" src="/photos/mixedmediaartshow/blackandwhitepaintings.jpg" alt="painting with geometrical shapes">      </a>
+					</figure>
+					<figure id="5" class="gallery h-entry">
+						<figcaption class="galleryCaption" >
+						<span class="picTitle p-name">All three B&W Paintings</span><time datetime="2019-10-05 21:41:00" class="dt-published">2019-10-05</time>
+						<p class="e-content">Here are all three paintings in the collection </p>
+						</figcaption>
+						<div class="galleryPhoto">
+							<a href="/photos/mixedmediaartshow/allthreepaintings.jpg">
+							<img class="u-photo" src="/photos/mixedmediaartshow/allthreepaintings.jpg">      </a>
+						</div>
+						
+					</figure>
+					<figure id="6" class="gallery h-entry">
+						<figcaption class="galleryCaption" >
+						<span class="picTitle p-name">My favorite of the Three</span><time datetime="2019-10-05 21:41:00" class="dt-published">2019-10-05</time>
+						<p class="e-content"> Out of the three this pic spoke to me the most. I love the lines and the "kraken hiding </p>
+						</figcaption>
+						<div class="galleryPhoto">
+							<a href="/photos/mixedmediaartshow/myfav.jpg">
+							<img class="u-photo" src="/photos/mixedmediaartshow/myfav.jpg" alt="close up a of a painting in black in white with odd geometric shapes">      </a>
+						</div>
+					</figure>
+					
+					<figure id="12" class="gallery h-entry">
+						<figcaption class="galleryCaption" >
+						<span class="picTitle p-name">The colors danced with the vibrancy of sound</span><time datetime="2019-10-05 21:41:00" class="dt-published">2019-10-05</time>
+						<p class="e-content">Sounds fade to shades of meaning as collapsing colors surround</p>
+						</figcaption>
+						<div class="galleryPhoto">
+							<a href="/photos/mixedmediaartshow/purpleroom.gif">
+							<img class="u-photo" src="photos/mixedmediaartshow/purpleroom.gif" alt="explosions of purple light">      </a>
+						</div>
+					</figure>
+					<figure id="3" class="gallery h-entry">
+				<figcaption class="galleryCaption" >
+				<span class="picTitle p-name">Colorful Paintings</span> <time datetime="2019-10-05 21:41:00" class="dt-published">2019-10-05</time>
+				<p class="e-content">Much of the art had a colorful explosion feel and used a ton of gemoetric patterns and/or randomness</p>
+				</figcaption>
+				<div class="galleryPhoto">
+					<a href="/photos/mixedmediaartshow/colorfulpaintings.jpg">
+					<img class="u-photo" src="/photos/mixedmediaartshow/colorfulpaintings.jpg" alt="three painting hanging on the wall">  </a>
+				</figure>
+					
+					<figure id="9" class="gallery h-entry">
+						<figcaption class="galleryCaption" >
+						<span class="picTitle p-name">Close Up of Light</span><time datetime="2019-10-05 21:41:00" class="dt-published">2019-10-05</time>
+						<p class="e-content"> The organge haze cut through our digital maze as we gazed upon questions </p>
+						</figcaption>
+						<div class="galleryPhoto">
+							<a href="/photos/mixedmediaartshow/digitalbeinglight.jpg">
+							<img class="u-photo" src="/photos/mixedmediaartshow/digitalbeinglight.jpg" alt="close up of an orangespot light on the ground">      </a>
+						</div>
+					</figure>
+					<figure id="10" class="gallery h-entry">
+						<figcaption class="galleryCaption" >
+						<span class="picTitle p-name">Sophie Exhibit</span><time datetime="2019-10-05 21:41:00" class="dt-published">2019-10-05</time>
+						<p class="e-content"> Another cool exihibit was Sophie. </p>
+						</figcaption>
+						<div class="galleryPhoto">
+							<a href="/photos/mixedmediaartshow/sophie.jpg">
+							<img class="u-photo" src="/photos/mixedmediaartshow/sophie.jpg" alt="close up of words in front of a screen">      </a>
+						</div>
+					</figure>
+					<figure id="11" class="gallery h-entry">
+						<figcaption class="galleryCaption" >
+						<span class="picTitle p-name">Foreboding Words</span><time datetime="2019-10-05 21:41:00" class="dt-published">2019-10-05</time>
+						<p class="e-content"> Will we rule the bots or will they rule us </p>
+						</figcaption>
+						<div class="galleryPhoto">
+							<a href="/photos/mixedmediaartshow/sophiewords.jpg">
+							<img class="u-photo" src="/photos/mixedmediaartshow/sophiewords.jpg" alt="A human may not injure a robot.">      </a>
+						</div>
+					</figure>
+					<figure id="7" class="gallery h-entry">
+						<figcaption class="galleryCaption" >
+						<span class="picTitle p-name">Full Digital Being Exihibit</span><time datetime="2019-10-05 21:41:00" class="dt-published">2019-10-05</time>
+						<p class="e-content"> The full interactive monitor display </p>
+						</figcaption>
+						<div class="galleryPhoto">
+							<a href="/photos/mixedmediaartshow/hishesafe.jpg">
+							<img class="u-photo" src="/photos/mixedmediaartshow/digitalbeing1.jpg" alt="The full digital being show">      </a>
+						</div>
+					</figure>
+
+					
+						</div>
+				
+				<div class="grid-containerPano ">
+					<figure id="12" class="gallery h-entry">
+						<figcaption class="galleryCaption" >
+						<span class="picTitle p-name">Art You Can Play With</span><time datetime="2019-10-05 21:41:00" class="dt-published">2019-10-05</time>
+						<p class="e-content">Become art. Be Art. Enjoy Art</p>
+						</figcaption>
+						<div class="galleryPhoto">
+							<a href="/photos/mixedmediaartshow/panoramicpurpleroom.jpg">
+							<img class="u-photo" src="photos/mixedmediaartshow/panoramicpurpleroom.jpg" alt="explosions of purple light">      </a>
+						</div>
+					</figure>
+									</div>
+			</div>
+				<footer>
+				<?php include 'footer.php'; ?>
+			</body>
+			
+		</html>
