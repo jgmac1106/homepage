b3a98cfa435651f7f1ef142b411664d3b89ba9ab
Greg McVerry <jgregmcverry@gmail.com>
2019-10-16 19:38:01 -0400

    adding a new note
    lets see if this works

---
+++ notes/2019-10-15-4.php
@@ -0,0 +1,26 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="note3">
+
+        
+      
+        <time datetime="2019-10-15 19:34:20" class="dt-published">2019-09-15 19:34</time>
+        <p class="e-content">This is so awesome! Got GitHub actions working and this should be first note I push live instantly by saving in my text editor....bad news I wrote over my repo trying to get all my different local versions on point.  </p>
+        </figcaption>
+      </figure>
+ 
+  Also on  <a class="u-url url" href="https://jgregorymcverry.com/notes/2019-10-08-1" rel="canonical"><strong>My Notes</strong></a>
+  <a class="u-syndication" href="https://brid.gy/about#webmentions">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
