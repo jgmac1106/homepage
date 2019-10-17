1313457aff46e6082035fae8bb4afc330af7408c
Greg McVerry <jgregmcverry@gmail.com>
2019-10-16 21:47:32 -0400

    deleting unused stylesheets
    deleting unused stylesheets

--- articles.php
+++ articles.php
@@ -390,17 +390,4 @@
   
 </html>
 
-    © 2019 GitHub, Inc.
-    Terms
-    Privacy
-    Security
-    Status
-    Help
-
-    Contact GitHub
-    Pricing
-    API
-    Training
-    Blog
-    About
 
