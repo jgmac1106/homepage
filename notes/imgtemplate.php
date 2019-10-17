e6c548f6c38d1fb1de2575d4a42d91799d4b58ad
Greg McVerry <jgregmcverry@gmail.com>
2019-10-16 20:14:56 -0400

    adding notes and templates
    Adding notes and templates

---
+++ notes/imgtemplate.php
@@ -0,0 +1,28 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+
+          <img   class="u-photo" src="" alt=""> 
+          
+      
+  
+      
+        <time datetime="2019-10-10 9:24:20" class="dt-published">2019-09-13 9:24</time>
+        <p class="e-content"></p>
+      
+ 
+  Also on  <a class="u-url url" href="#" rel="canonical"><strong>My Notes</strong></a>
+  <a class="u-syndication" href="https://brid.gy/about#webmentions">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
---
+++ notes/replytemplate.php
@@ -0,0 +1,27 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="note2">
+
+        
+  
+      
+        <time datetime="2019-10-10 5:54:20" class="dt-published">2019-09-13 9:54</time>
+       <p>In reply to <a class="u-in-reply-to" href="#">note</a> by <a class="h-card p-name" href="#">Name</a></p>
+        <p class="e-content"></p>
+     
+ 
+  Also on  <a class="u-url url" href="#" rel="canonical"><strong>My Notes</strong></a>
+  <a class="u-syndication" href="<a class="u-syndication" href="https://brid.gy/about#webmentions">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
