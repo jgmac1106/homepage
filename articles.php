f3b0138474f4f7205b29c31262ecb264da133d1d
Greg McVerry <jgregmcverry@gmail.com>
2019-10-16 21:40:12 -0400

    reverting back articles.
    will need to fix the <p> spanning a <span>

--- articles.php
+++ articles.php
@@ -1,25 +1,25 @@
 
 <?php include 'head.php'; ?>
-<link rel="alternate" type="application/rss+xml" href="https://granary.io/url?input=html&output=rss&url=https%3A%2F%2Fjgregorymcverry.com%2Farticles">
 <?php include 'header.php'; ?>
   <main class="h-feed">
-  <h2 class="p-name">My Feed</h2>
+  
      <div class="h-card">
   <a class="p-name u-url" href="http://jgregorymcverry.com" rel="me">Greg McVerry</a><br>
     </div>
-<div class="card-container">
+<section class="card-container">
     <article class="h-entry card">
         <header class="card__title">
             <h3 class="p-name">IndieWebCamp NYC Day 2 Update</h3>
         </header>
-      <div class="authorInfo">
-         <div class="h-card"> 
+      <span class="authorInfo">
+         <span class="h-card"> 
            <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
-         </div>
+         </span>
+        
          <time class="dt-published" datetime="2019-10-06 10:59:00">6<sup>th</sup> October 2019</time>
-          </div>
+      </span>
         <figure class="card__thumbnail">
-            <img class="u-featured" src="/photos/iwcnyc2019/Day2PhotoNYC2019" alt="" >
+            <img class="u-featured" src="/videos/MVIMG_20190807_150253.jpg" alt="" >
          
         </figure>
         <div class="p-summary card__description">
@@ -28,18 +28,19 @@
           </p>
         </div>
         <a  href="/iwcupdatenyc2019.html" class="u-url button">View Now</a>
+
     </article> 
   <article class="h-entry card">
         <header class="card__title">
             <h3 class="p-name">Designs that Inspire</h3>
         </header>
-      <div class="authorInfo">
-         <div class="h-card"> 
+      <span class="authorInfo">
+         <span class="h-card"> 
            <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
-         </div>
+         </span>
         
          <time class="dt-published" datetime="2019-08-17 06:41:00">17<sup>th</sup> July 2019</time>
-      </div>
+      </span>
         <figure class="card__thumbnail">
             <img class="u-featured" src="/photos/assets/055438aa-8d6a-403c-a7ff-c97119f02627%2F10990743203_7a694363a8_z.jpg?v=1566139221847" alt="time lapse of ferris wheel at night with RV in front" >
          
@@ -57,13 +58,13 @@
         <header class="card__title">
             <h3 class="p-name">edCampCT19 Reflections</h3>
         </header>
-      <div class="authorInfo">
-         <div class="h-card"> 
+      <span class="authorInfo">
+         <span class="h-card"> 
            <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
-         </div>
+         </span>
         
          <time class="dt-published" datetime="2019-08-17 06:41:00">17<sup>th</sup> July 2019</time>
-      </div>
+      </span>
         <figure class="card__thumbnail">
             <img class="u-featured" src="https://edcampct.files.wordpress.com/2019/01/edcampct_header_2019.jpg" alt="edcampct logo" >
          
@@ -78,13 +79,13 @@
         <header class="card__title">
             <h3 class="p-name">Assessment in the Literacy Classroom</h3>
         </header>
-      <div class="authorInfo">
-         <div class="h-card"> 
+      <span class="authorInfo">
+         <span class="h-card"> 
            <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
-         </div>
+         </span>
         
          <time class="dt-published" datetime="2019-08-08 16:41:00">08<sup>th</sup> August 2019</time>
-      </div>
+      </span>
         <figure class="card__thumbnail">
             <img class="u-featured" src="https://cdn.glitch.com/055438aa-8d6a-403c-a7ff-c97119f02627%2F28542506368_d1978328e1_k.jpg?v=1565715843262" alt="three fames showing a face melting by a clock" >
          
@@ -100,13 +101,13 @@
         <header class="card__title">
             <h3 class="p-name">#BarefootForDai</h3>
         </header>
-      <div class="authorInfo">
-         <div class="h-card"> 
+      <span class="authorInfo">
+         <span class="h-card"> 
            <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
-         </div>
+         </span>
         
          <time class="dt-published" datetime="2019-08-07 15:38:00">07<sup>th</sup> August 2019</time>
-      </div>
+      </span>
         <figure class="card__thumbnail">
             <img class="u-featured" src="/videos/MVIMG_20190807_150253.jpg" alt="my dirty feet after a run" >
          
@@ -121,13 +122,13 @@
         <header class="card__title">
             <h3 class="p-name">IndieWeb Updates</h3>
         </header>
-      <div class="authorInfo">
-         <div class="h-card"> 
+      <span class="authorInfo">
+         <span class="h-card"> 
            <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
-         </div>
+         </span>
         
          <time class="dt-published" datetime="2019-07-01 10:33:00">1<sup>st</sup>July 2019</time>
-      </div>
+      </span>
         <figure class="card__thumbnail">
             <img class="u-featured" src="https://cdn.glitch.com/055438aa-8d6a-403c-a7ff-c97119f02627%2Fbumperposter.gif?1553175891571" alt="IndieWeb Camp" >
          
@@ -142,13 +143,13 @@
         <header class="card__title">
             <h3 class="p-name">Webmention Badges at #PressEdConf19</h3>
         </header>
-      <div class="authorInfo">
-         <div class="h-card"> 
+      <span class="authorInfo">
+         <span class="h-card"> 
            <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
-         </div>
+         </span>
         
          <time class="dt-published" datetime="2019-04-19 11:38:00">19<sup>th</sup> April 2019</time>
-      </div>
+      </span>
         <figure class="card__thumbnail">
             <img class="u-featured" src="https://quickthoughts.jgregorymcverry.com/service/web/imageproxy/aHR0cHM6Ly9wcmVzc2VkY29uZi5vcmcvd3AtY29udGVudC91cGxvYWRzL3NpdGVzLzIvMjAxOS8wMS9wcmVzc2VkLTIwMTkucG5n" alt="PressEdConf19 logo" >
           
@@ -163,13 +164,13 @@
         <header class="card__title">
             <h3 class="p-name">Growing the #IndieWeb at IndieWebCamp New Haven</h3>
         </header>
-      <div class="authorInfo">
-         <div class="h-card"> 
+      <span class="authorInfo">
+         <span class="h-card"> 
            <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
-         </div>
+         </span>
         
          <time class="dt-published" datetime="2019-04-05 08:38:00">5<sup>th</sup>April 2019</time>
-      </div>
+      </span>
         <figure class="card__thumbnail">
             <img class="u-featured" src="https://quickthoughts.jgregorymcverry.com/file/607fc915723937cd53befbaaa908a7c2/thumb.jpg" alt="five attendees at IndieWebCamp" >
           
@@ -184,13 +185,13 @@
         <header class="card__title">
             <h3 class="p-name">Video Guide to a Homebrew Website Club Page</h3>
         </header>
-      <div class="authorInfo">
-         <div class="h-card"> 
+      <span class="authorInfo">
+         <span class="h-card"> 
            <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
-         </div>
+         </span>
         
          <time class="dt-published" datetime="2019-03-03 10:33:00">3<sup>rd</sup>March 2019</time>
-      </div>
+      </span>
         <figure class="card__thumbnail">
             <img class="u-featured" src="https://cdn.glitch.com/055438aa-8d6a-403c-a7ff-c97119f02627%2Fbumperposter.gif?1553175891571" alt="" >
           <!-- Drew McWeeney shared under a Creative Commons (BY-NC-ND) license-->
@@ -205,13 +206,13 @@
         <header class="card__title">
             <h3 class="p-name">Innovation Systems and System Network Agent Theory and #IndieWeb</h3>
         </header>
-      <div class="authorInfo">
-         <div class="h-card"> 
+      <span class="authorInfo">
+         <span class="h-card"> 
            <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
-         </div>
+         </span>
         
          <time class="dt-published" datetime="2019-03-03 10:33:00">3<sup>rd</sup>March 2019</time>
-      </div>
+      </span>
         <figure class="card__thumbnail">
             <img class="u-featured" src="https://quickthoughts.jgregorymcverry.com/file/1000292aaf670e22ff3c3ad845f6ab1b" alt="Theoeritical Model of Social Network Agency Framework" >
           <!--"all under control again" flickr photo by bigbahookie https://flickr.com/photos/craigmarren/6086280669 shared under a Creative Commons (BY-NC-ND) license-->
@@ -226,13 +227,13 @@
         <header class="card__title">
             <h3 class="p-name">Badge from Your Own Domain</h3>
         </header>
-      <div class="authorInfo">
-         <div class="h-card"> 
+      <span class="authorInfo">
+         <span class="h-card"> 
            <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
-         </div>
+         </span>
         
          <time class="dt-published" datetime="2019-02-27 14:57:00">15<sup>th</sup>January 2019</time>
-      </div>
+      </span>
         <figure class="card__thumbnail">
             <img class="u-featured" src="https://cdn.glitch.com/055438aa-8d6a-403c-a7ff-c97119f02627%2F6086280669_31b75a6ff4_z.jpg?1551294560711">
           <!--"all under control again" flickr photo by bigbahookie https://flickr.com/photos/craigmarren/6086280669 shared under a Creative Commons (BY-NC-ND) license-->
@@ -247,13 +248,13 @@
         <header class="card__title">
             <h3 class="p-name">Collection: Open Source Blogging </h3>
         </header>
-      <div class="authorInfo">
-         <div class="h-card"> 
+      <span class="authorInfo">
+         <span class="h-card"> 
            <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
-         </div>
+         </span>
         
          <time class="dt-published" datetime="2019-01-15 06:38:00">15<sup>th</sup>January 2019</time>
-      </div>
+      </span>
         <figure class="card__thumbnail">
             <img class="u-featured" src="https://farm8.static.flickr.com/7360/13893986777_eed0e820b6_z.jpg">
           <!--"New build with Mimesis Monday" flickr photo by Elif Ayiter/Alpha Auer/..../ https://flickr.com/photos/alpha_auer/13893986777 shared under a Creative Commons (BY-NC-ND) license-->
@@ -268,14 +269,14 @@
         <header class="card__title">
             <h3 class="p-name">Saying Goodbye to Thimble</h3>
         </header>
-      <div class="authorInfo">
-         <div class="h-card"> 
+      <span class="authorInfo">
+         <span class="h-card"> 
            <p class="p-author h-card">jgmac1106</p> 
            <img class="u-photo" style="border-radius:50%" src="https://0.academia-photos.com/275850/62912/68932/s200_j._gregory.mcverry.jpg" alt="">
-         </div>
+         </span>
         
          <time class="dt-published" datetime="2018-12-19 09:33:00">19<sup>th</sup> December 2018</time>
-      </div>
+      </span>
         <figure class="card__thumbnail">
             <img class="u-featured" src="https://cdn.glitch.com/055438aa-8d6a-403c-a7ff-c97119f02627%2FDs-2rTzWkAE5Y3J.jpg?1545143411776">
         </figure>
@@ -289,13 +290,13 @@
         <header class="card__title">
             <h3 class="p-name">Creative Commons Global Summit App</h3>
         </header>
-      <div class="authorInfo">
-         <div class="h-card"> 
+      <span class="authorInfo">
+         <span class="h-card"> 
            <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> </p>
-         </div>
+         </span>
         
           <time class="dt-published" datetime="2018-12-8 08:38:00">8<sup>th</sup> December 2018</time>
-      </div>
+      </span>
         <figure class="card__thumbnail">
             <img class="u-featured" src="https://farm5.static.flickr.com/4846/45670455672_82edff836f_z.jpg">
           <!--IndieWebCamp Berlin 2018 | Day 2shared under a <a href="https://creativecommons.org/licenses/by-nc-sa/2.0/">Creative Commons (BY-NC-SA) license</a> -->
@@ -312,14 +313,14 @@
         <header class="card__title">
             <h3 class="p-name">UX of Parenting: Teething</h3>
         </header>
-      <div class="authorInfo">
-         <div class="h-card"> 
+      <span class="authorInfo">
+         <span class="h-card"> 
            <p class="p-author h-card">jgmac1106</p> 
            <img class="u-photo" style="border-radius:50%" src="https://0.academia-photos.com/275850/62912/68932/s200_j._gregory.mcverry.jpg" alt="">
-         </div>
+         </span>
         
          <time class="dt-published" datetime="2018-10-12 17:46:00">12<sup>th</sup> October 2018</time>
-      </div>
+      </span>
           
         <figure class="card__thumbnail">
             <img class="u-featured" src="https://archive.jgregorymcverry.com/wp-content/uploads/2018/10/Screen-Shot-2018-10-10-at-9.54.33-PM.png">
@@ -335,7 +336,7 @@
             <h3 class="p-name">Building a Podcast Feed</h3>  
         </header>
      
-      <p class="p-author h-card">jgmac1106<div class="authorInfo"><img class="u-photo" style="border-radius:50%" src="https://0.academia-photos.com/275850/62912/68932/s200_j._gregory.mcverry.jpg" alt=""></div></p>
+      <p class="p-author h-card">jgmac1106<span class="authorInfo"><img class="u-photo" style="border-radius:50%" src="https://0.academia-photos.com/275850/62912/68932/s200_j._gregory.mcverry.jpg" alt=""></span></p>
       <time class="dt-published" datetime="2018-11-20 10:16:00">13<sup>th</sup> Novemeber 20th, 2018</time>
       
         <figure class="card__thumbnail">
@@ -352,7 +353,7 @@
             <h3 class="p-name">#FemHack Grant</h3>  
         </header>
      
-      <p class="p-author h-card">jgmac1106<div class="authorInfo"><img class="u-photo" style="border-radius:50%" src="https://0.academia-photos.com/275850/62912/68932/s200_j._gregory.mcverry.jpg" alt="profile photo"></div></p>
+      <p class="p-author h-card">jgmac1106<span class="authorInfo"><img class="u-photo" style="border-radius:50%" src="https://0.academia-photos.com/275850/62912/68932/s200_j._gregory.mcverry.jpg" alt="profile photo"></span></p>
       <time class="dt-published" datetime="2018-11-13 11:46:00">13<sup>th</sup> Novemeber 2018</time>
       
         <figure class="card__thumbnail">
@@ -371,7 +372,7 @@
       <span>
       <p class="p-author h-card">jgmac1106</p>
       <time class="dt-published" datetime="2018-11-13 11:46:00">13<sup>th</sup> Novemeber 2018</time>
-      </div>
+      </span>
         <figure class="card__thumbnail">
             <img class="u-featured" src="https://archive.jgregorymcverry.com/wp-content/uploads/2018/10/8579134341_23e92cd81d_k-1200x800.jpg" alt="">
         </figure>
@@ -382,9 +383,24 @@
 
     </article>
     
-</div>
+</section>
     </main>
 <?php include 'footer.php'; ?>
   </body>
   
 </html>
+
+    © 2019 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Status
+    Help
+
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+
