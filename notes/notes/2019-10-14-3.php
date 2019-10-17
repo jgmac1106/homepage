8064cdf00e7237c0bc1e036efc8484f523f7cf98
Greg McVerry <jgregmcverry@gmail.com>
2019-10-16 21:04:20 -0400

    fixing stylesheet
    mispelled body, wasted a bit of time before I figured that out

---
+++ notes/notes/2019-10-14-3.php
@@ -0,0 +1,27 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="note2">
+
+        
+  
+      
+        <time datetime="2019-10-10 5:54:20" class="dt-published">2019-09-13 9:54</time>
+       <p>In reply to <a class="u-in-reply-to" href="https://jgregorymcverry.com/notes/2019-10-14-1">note</a> by @jgmac1106</p>
+        <p class="e-content">If you would like to learn more about the Wangunk here is a decent paper: https://www.iaismuseum.org/wp-content/uploads/2017/02/Pre-colonial_History_of_the_Wangunk-1.pdf Please take this day and reflect on the genocide that built our nation</p>
+     
+ 
+  Also on  <a class="u-url url" href="https://jgregorymcverry.com/notes/2019-10-14-3" rel="canonical"><strong>My Notes</strong></a>
+  <a class="u-syndication" href="<a class="u-syndication" href="https://brid.gy/about#webmentions">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
---
+++ notes/notes/notetemplate.php
@@ -0,0 +1,20 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+  	<div class="note h-entry" id="#">
+ 
+<time class="dt-published" datetime="2019-10-06T13:26:05+0000">October 6th 2019</time>
+ <p class="p-name e-content"></a>
+ </p>
+  Also on  <a class="u-url url" href="#" rel="canonical"><strong>My Notes</strong></a>
+  <a href="https://brid.gy/about#webmentions">Published with Bridgy</a>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
