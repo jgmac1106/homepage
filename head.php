d0484129ff161e65f95ca9903937cd30628a4ec4
Greg McVerry <jgregmcverry@gmail.com>
2019-10-20 07:13:57 -0400

    publishing new notes
    publishing notes

--- head.php
+++ head.php
@@ -13,4 +13,8 @@
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-9ralMzdK1QYsk4yBY680hmsb4/hJ98xK3w0TIaJ3ll4POWpWUYaA2bRjGGujGT8w" crossorigin="anonymous">
      <link href="https://fonts.googleapis.com/css?family=Heebo|Quicksand&display=swap" rel="stylesheet"> 
 <link rel="canonical" href="https://jgregorymcverry.com<?php echo $_SERVER['PHP_SELF']; ?>" />
- 
+ We project ourselves on to the world and then consume the ways the world projects identities back onto us. Somwhere in this refactoring light our selves dance amongst them selves until we our Me(s) emerge in a choreographed mess of ever unfolding rythms of time.
+
+In this journey we can choose to be the DJ or always let social media silos choose the next song.
+
+Thank you microblog for letting us take control of our soundtrack of symbols. 
---
+++ notes/2019-09-18-2.php
@@ -0,0 +1,36 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+          <video class="u-video" controls width="480">
+
+    
+
+    <source src="https://archive.org/download/edu407fall19kickoff/EDU407Fall19Module7WrapUp.mp4">
+      <track src="/videos/5writingtips/EDU407Fall19Module7WrapUp.sbv" label="English" kind="captions" srclang="en-us" default >
+
+    Sorry, your browser doesn't support embedded videos.
+</video>
+          
+      
+  
+      
+        <time datetime="2019-10-10 15:34:20" class="dt-published">2019-09-18 15:34</time>
+        <p class="e-content"> UPDATE: Fixed the vdieo link to the correct video. I made a video describing my 5 best tips for teaching #writing in #elemchat #nctevillage #literacies</p>
+      
+ 
+  Also on  <a class="u-url url" href="https://jgregorymcverry.com/notes/2019-09-18-1" rel="canonical"><strong>My Notes</strong></a>
+  <a class="u-syndication" href="https://brid.gy/about#webmentions">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
