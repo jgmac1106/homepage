6f28129be816e5e8012b1fe2c50d45b170eaaa1b
Greg McVerry <jgregmcverry@gmail.com>
2019-11-03 13:55:05 -0500

    Update footer.php
    trying to go to one fexbox iin footer, moving nav inside

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
-<div class="Nav_footer" >
-        <ul>
+
+        
+    
+    <div class="flex-item">
+    	<ul>
           <li class="Navbar__Link_footer" >
           <a href="/aboutme" >About</a>
         </li>
@@ -31,8 +34,6 @@
           <a href="/imagecredits" class="navLinks">Img Credits</a>
         </li>
     </ul>
-      </div>
-    <div class="flex-item"">
       <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>. by
       <p class="h-card"><a class="u-url-u-uid" href="jgregorymcverry.com"><span class="p-name">Greg McVerry</span></a>
       <a   href="https://microformats.org"><img class="https://indieweb.org/images/7/72/microformats-button.png"></a><br>
