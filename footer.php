361dab408b1e540df8594875f8b5513a9ab4b6b4
Greg McVerry <jgregmcverry@gmail.com>
2019-11-03 09:15:19 -0500

    removing inline styling
    trying to scrub off the the Halloween make-up

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 
-    <div class="flex-item"style="font-family: 'Creepster', cursive;">
-      <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>. by<br>
-      <a class="u-uid url" href="jgregorymcverry.com">Greg McVerry</a>
+    <div class="flex-item"">
+      <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>. by
+      <p class="h-card"><a class="u-url-u-uid" href="jgregorymcverry.com"><span class="p-name">Greg McVerry</span></a>
       <a   href="https://microformats.org"><img class="https://indieweb.org/images/7/72/microformats-button.png"></a><br>
       <a   href="https://indieweb.org"><img src="https://indieweb.org/images/4/4a/indiewebcamp-button.png"></a>
       <a  href="https://indieweb.org/webmention"><img src="https://indieweb.org/images/0/03/webmention-button.png"></a>
