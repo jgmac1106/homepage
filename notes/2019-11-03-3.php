43774b43e3c2b7803f201cac86de19c6cc988ad4
Greg McVerry <jgregmcverry@gmail.com>
2019-11-03 15:16:33 -0500

    Create 2019-11-03-3.php
    adding a note

---
+++ notes/2019-11-03-3.php
@@ -0,0 +1,21 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-10-06T15:14:20+0000">Nobember 3rd, 2019 15:14</time>
+ <p class="p-name e-content">Sweet! I think I finally have my navigation and footer figured out in a way I like, on small screens the nav jumps from the header to the footer, I should have used grid areas to do this but I am using display:none....shhhhh 
+ </p>
+  Also on  <a class="u-url url" href="https://jgregorymcverry.com/notes/<?php echo $_SERVER['PHP_SELF']; ?> "rel="canonical"><strong>My Notes</strong></a>
+  <a href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
