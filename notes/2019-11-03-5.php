610a8d1596c4c418ec2881dcbfbcc160f89456ea
Greg McVerry <jgregmcverry@gmail.com>
2019-11-03 19:50:24 -0500

    Create 2019-11-03-5.php
    following Billy Meinke

---
+++ notes/2019-11-03-5.php
@@ -0,0 +1,21 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-11-03T19:44:20+0000">November 11th, 2019</time>
+ <div class="p-name e-content"><p>I just added</p><ul>  <li class="h-card p-follow-of"><a class="p-name u-url" href="http://billymeinke.com/"><img class="u-photo" src="http://billymeinke.com/img/billy_armory_max.jpg">Billy Meinke</a><span class="p-note"> technologist that works on open learning environments</span></li></ul><p>to the <a href="https://jgregorymcverry.com/following#openpedagogy">#OpenPedagogy</a> section of my following page. #BeYourOwnSocialNetwork
+ </div>
+  Also on  <a class="u-url url" href="https://jgregorymcverry.com/<?php echo $_SERVER['PHP_SELF']; ?> "rel="canonical"><strong>My Notes</strong></a>
+  <a href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
