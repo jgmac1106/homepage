0e9a46a1acdc3ffcf0fe95c7037c8dfaf544aef3
Greg McVerry <jgregmcverry@gmail.com>
2019-11-04 10:40:40 -0500

    Following Laura
    Adding Laura's h-card to instructional design

---
+++ notes/2019-11-04-2.php
@@ -0,0 +1,22 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-11-03T10:34:20+0000">November 4th, 2019</time>
+ <div class="p-name e-content"><p>@LauraPasquini I just added</p><ul><li class="h-card"><a class="p-name u-url" href="https://techknowtools.com/" rel="colleague"><img class="u-photo" src="https://techknowtools.files.wordpress.com/2019/08/laura_headshot_19.jpg?w=650&h=650">Laura Pasquini </a><span class="p-note">explores how knowledge, technology, and community influence learning & professional development
+</span></li></ul><p>to the <a href="https://jgregorymcverry.com/following#instructionaldesign">#instructionaldesgin</a> section of my following page. #BeYourOwnSocialNetwork
+ </div>
+  Also on  <a class="u-url url" href="https://jgregorymcverry.com<?php echo $_SERVER['PHP_SELF']; ?> "rel="canonical"><strong>My Notes</strong></a>
+  <a href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
