28235bc82affe8e8bf49420784791978c02525d1
Greg McVerry <jgregmcverry@gmail.com>
2019-11-03 21:03:54 -0500

    Create 2019-11-03-12.php
    following April Hathcock

---
+++ notes/2019-11-03-12.php
@@ -0,0 +1,22 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-11-03T21:04:20+0000">November 3rd, 2019</time>
+ <div class="p-name e-content"><p>@AprilHatcock I just added</p><ul><li class="h-card"><a class="p-name u-url" href="https://aprilhathcock.wordpress.com/" rel="colleague"><img class="u-photo" src="https://aprilhathcock.files.wordpress.com/2016/02/img_2006.jpg">April Hathcock</a><span class="p-note"> At The Intersection: Blog about the intersection of libraries, law, feminisim, and diversity.
+</span></li></ul><p>to the <a href="https://jgregorymcverry.com/following#openpedagogy">#OpenPedagogy</a> section of my following page. #BeYourOwnSocialNetwork
+ </div>
+  Also on  <a class="u-url url" href="https://jgregorymcverry.com<?php echo $_SERVER['PHP_SELF']; ?> "rel="canonical"><strong>My Notes</strong></a>
+  <a href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
