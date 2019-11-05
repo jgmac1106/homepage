c917c4e6eaf86cc02c0b13ae0be4bd480e318642
Greg McVerry <jgregmcverry@gmail.com>
2019-11-05 07:01:27 -0500

    tweet to scsu
    adding notes

--- notes/2019-11-05-2.php
+++ notes/2019-11-05-2.php
@@ -7,13 +7,13 @@
   	<div class="note h-entry" id="#">
 <a class="u-author" href="/"></a>
 <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/SCSU/status/1191430086630760449">tweet</a>by @scsu</p>
-          <img   class="u-photo" src="http://giphygifs.s3.amazonaws.com/media/Ssv4QhVIYociQ/200w_d.gif" alt="scene from Alice and Wonderlnad of a teacher with anamorphic students writing on small chalkboard"> 
+          <img   class="u-photo" src="http://giphygifs.s3.amazonaws.com/media/Ssv4QhVIYociQ/giphy-downsized-large.gif" alt="scene from Alice and Wonderlnad of a teacher with anamorphic students writing on small chalkboard"> 
           
       
   
       
-        <time datetime="2019-11-03 5:24:20" class="dt-published">2019-11-03 5:24</time>
-        <p class="e-content">#MondayMoments</p>
+        <time datetime="2019-11-05 5:44:20" class="dt-published">2019-11-05 5:24</time>
+        <p class="e-content">#MondayMoments with a better resolution gif, please remember the alt text for our owl students who enjoy twettes by using a screen reader</p>
       
  
   Also on  <a class="u-url url" href="https://jgregorymcverry.com<?php echo $_SERVER['PHP_SELF']; ?> "rel="canonical"><strong>My Notes</strong></a>
