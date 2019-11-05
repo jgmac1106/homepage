3f63f80272ca18d8e76a377df141e1807347957d
Greg McVerry <jgregmcverry@gmail.com>
2019-11-05 05:26:37 -0500

    Create 2019-11-05-1.php
    adding a note

---
+++ notes/2019-11-05-1.php
@@ -0,0 +1,28 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<p>In reply to <a class="u-in-reply-to" href="https://twitter.com/SCSU/status/1191430086630760449">tweet</a>by @scsu</p>
+          <img   class="u-photo" src="https://media.giphy.com/media/Ssv4QhVIYociQ/source.gif" alt="scene from Alice and Wonderlnad of a teacher with anamorphic students writing on small chalkboard"> 
+          
+      
+  
+      
+        <time datetime="2019-11-03 5:24:20" class="dt-published">2019-11-03 5:24</time>
+        <p class="e-content">#MondayMoments</p>
+      
+ 
+  Also on  <a class="u-url url" href="https://jgregorymcverry.com<?php echo $_SERVER['PHP_SELF']; ?> "rel="canonical"><strong>My Notes</strong></a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
