606d6abc06d378eab3681ad603cdec2bced195d6
Greg McVerry <jgregmcverry@gmail.com>
2019-11-07 09:20:47 -0500

    adding note
    adding note

--- notes/2019-11-07-2.php
+++ notes/2019-11-07-2.php
@@ -7,7 +7,7 @@
   	<div class="note h-entry">
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="<?php echo date("Y/m/d")?>"><?php echo date("Y/m/d")?></time>
- <p class="p-name e-content">@goifoundation is having a quick planning meeting to talk about our #IndieWeb roll out of #OER Ghana network as part of our @Mozilla Open Leaders X program...All welcome to join <a href=""
+ <p class="p-name e-content">@goifoundation is having a quick planning meeting to talk about our #IndieWeb roll out of #OER Ghana network as part of our @Mozilla Open Leaders X program...All welcome to join <a href="https://zoom.us/j/931267235">Zoom Link</a>
  </p>
   Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a>
 </div>
