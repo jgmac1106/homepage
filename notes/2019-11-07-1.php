acd3d5c1189abd46a1eed5b8f0054742b5baab39
Greg McVerry <jgregmcverry@gmail.com>
2019-11-07 09:18:32 -0500

    adding photo h/t for logo
    u-photo note

---
+++ notes/2019-11-07-1.php
@@ -0,0 +1,28 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+          <img   class="u-photo" src="https://jgregorymcverry.com/photos/goifphotos/OPEN.jpg" alt="OER Ghana logo"> 
+          
+      
+  
+      
+        <time class="dt-published" datetime="<?php echo date("Y/m/d")?>"><?php echo date("Y/m/d")?></time>
+        <p class="e-content">Huge h/t to <a class="h-card" href="https://www.facebook.com/Creativemay/">Kretivmay Arts</a>For designing the #OER Ghana logo for our upcoming @Mozilla Leaders X Program. Contact @goifoundation to learnm more</p>
+      
+ 
+  <a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a>
+  <a class="u-syndication" href="https://brid.gy/about#webmentions">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
