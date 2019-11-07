cd66ce7c9ef15994c45cce67ddea08d8a08ff82f
Greg McVerry <jgregmcverry@gmail.com>
2019-11-07 09:15:32 -0500

    adding post about goif
    photo  and note

--- notes/imgtemplate.php
+++ notes/imgtemplate.php
@@ -6,13 +6,13 @@
     <div class="notewrapper">
   	<div class="note h-entry" id="#">
 <a class="u-author" href="/"></a>
-          <img   class="u-photo" src="https://jgregorymcverry.com/photos/art/frozenscarecrow.jpg" alt="headless scraecrow sitting in chair covered in frost"> 
+          <img   class="u-photo" src="https://jgregorymcverry.com/photos/goifphotos/OPEN.jpg" alt="OER Ghana logo"> 
           
       
   
       
         <time class="dt-published" datetime="<?php echo date("Y/m/d")?>"><?php echo date("Y/m/d")?></time>
-        <p class="e-content">#SilentSunday</p>
+        <p class="e-content">Huge h/t to <a class="h-card" href="https://www.facebook.com/Creativemay/">Kretivmay Arts</a>For designing the #OER Ghana logo for our upcoming @Mozilla Leaders X Program. Contact @goifoundation to learnm more</p>
       
  
   <a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a>
