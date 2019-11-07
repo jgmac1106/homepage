b8492f1fae78bff64dd7227f5e33492aa7add043
Greg McVerry <jgregmcverry@gmail.com>
2019-11-07 15:31:12 -0500

    adding a note
    adding a note

---
+++ notes/2019-11-07-3.php
@@ -0,0 +1,35 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+          <video controls width="80%">
+
+    <source src="https://jgregorymcverry.com/videos/howtowriteasciencepoem.mp4"
+            type="video/mp4">
+
+
+    Sorry, your browser doesn't support embedded videos.
+</video>
+
+          
+      
+  
+      
+        <time class="dt-published" datetime="<?php echo date("Y/m/d")?>"><?php echo date("Y/m/d")?></time>
+        <p class="e-content">Hey #edu307 #edu407Fall19 here is my example of how to take a science concept and create poetry that reinforces key vocabulary</p>      
+ 
+  <a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a>
+  <a class="u-syndication" href="https://brid.gy/about#webmentions">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
