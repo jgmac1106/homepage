2dfa5dba96182ecb264d8454e6ad1169a11816bd
Greg McVerry <jgregmcverry@gmail.com>
2019-11-08 17:15:54 -0500

    Create 2019-11-08-2.php
    adding note

---
+++ notes/2019-11-08-2.php
@@ -0,0 +1,28 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="note2">
+      <a class="u-author" href="/"></a>
+
+        
+  
+      <time class="dt-published" datetime="2019-11-08T16:54:20+0000">November 8th, 2019 16:54</time>
+       <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/johanbove/status/1192898871002832896">Tweet</a> by <a class="h-card p-name" href="https://jgregorymcverry.com">@jgmac1106</a></p>
+      
+
+<p>Oops, I messed up Aaron's url <a href="https://aaronparecki.com">https://aaronparecki.com</a> </p>
+ 
+<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a>
+  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
