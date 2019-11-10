049f21be1ebeee5ee11fc55821d38aba6790e280
Greg McVerry <jgregmcverry@gmail.com>
2019-11-10 07:12:05 -0500

    Create 2019-11-01-1.php
    silent sunday note

---
+++ notes/2019-11-01-1.php
@@ -0,0 +1,28 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-11-10T06:54:20+0000">November 10th 2019 6:54</time>
+          <img   class="u-photo" src="https://jgregorymcverry.com/photos/arts/westrockHamden.jpg" alt="foliage shot with tree framing picture on right"> 
+          
+      
+  
+      
+        <p class="e-content">#silentsunday #clmooc</p>
+      
+ 
+  <a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a>
+  <a class="u-syndication" href="https://brid.gy/about#webmentions">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
