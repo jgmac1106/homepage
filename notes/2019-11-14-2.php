a2b7a5689f09eab0a035e31d25f7099ae2409ecb
Greg McVerry <jgregmcverry@gmail.com>
2019-11-14 14:05:10 -0500

    Create 2019-11-14-2.php
    add a video note

---
+++ notes/2019-11-14-2.php
@@ -0,0 +1,29 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-11-11T16:04:20+0000">November 14th 2019 16:04</time>
+          <video  controls poster="/videos/sevenminutevocab.png "class="u-video" src="https://jgregorymcverry.com/videos/VocabGraphicOrganizers"> 
+          </video>
+          
+      
+  
+      
+        <p class="e-content">In this episode of Dr. Mac's 7 Minute teaching tips we cover using vocabulary graphic organizers with informational texts.</p>
+      
+ 
+  <a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a>
+  <a class="u-syndication" href="https://brid.gy/about#webmentions">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
