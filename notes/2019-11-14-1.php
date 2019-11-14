4b3c831f4a73d05a0aa202ea94de4096ae550420
Greg McVerry <jgregmcverry@gmail.com>
2019-11-14 09:45:08 -0500

    Create 2019-11-14-1.php
    adding note

---
+++ notes/2019-11-14-1.php
@@ -0,0 +1,29 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-11-11T09:14:20+0000">November 14th 2019 9:14</time>
+          <video  controls class="u-video" src="https://jgregorymcverry.com/videos/WeLoveData.mp4"> 
+          </video>
+          
+      
+  
+      
+        <p class="e-content">We love data.</p>
+      
+ 
+  <a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a>
+  <a class="u-syndication" href="https://brid.gy/about#webmentions">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
