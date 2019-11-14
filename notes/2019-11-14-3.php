b160d9bcbd8a7a8a42e96ebc1ec79b18828c1dc0
Greg McVerry <jgregmcverry@gmail.com>
2019-11-14 15:55:30 -0500

    Create 2019-11-14-3.php
    adding video note

---
+++ notes/2019-11-14-3.php
@@ -0,0 +1,29 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-11-11T15:54:20+0000">November 14th 2019 15:54</time>
+          <video  controls poster="https://jgregorymcverry.com/videos/respond2poetry.png "class="u-video" src="https://jgregorymcverry.com/videos/RespondToPoetryWithPictures.mp4"> 
+          </video>
+          
+      
+  
+      
+        <p class="e-content">In this episode of Dr. Mac's 7 Minute Teaching Tips we cover responding to poetry with images</p>
+      
+ 
+  <a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a>
+  
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
