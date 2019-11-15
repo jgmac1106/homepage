25b74bb4791b12a7a00e96145746418e9249e0b6
Greg McVerry <jgregmcverry@gmail.com>
2019-11-15 15:10:03 -0500

    Create 2019-11-15-2.php
    adding a video note

---
+++ notes/2019-11-15-2.php
@@ -0,0 +1,21 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-11-11T15:14:20+0000">November 14th 2019 15:14</time>
+          <video  controls class="u-video" poster="/videos/notetaking.png" src="https://jgregorymcverry.com/videos/notetaking101.mp4"> 
+          </video>
+        <p class="e-content">In this #7MinuteTeachingTip I cover the basic research, common misconceptions, and strategies to teach notetaking.</p>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
