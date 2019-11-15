1bea877734479c1bc33c3c5041c49d08bba794ff
Greg McVerry <jgregmcverry@gmail.com>
2019-11-15 11:33:35 -0500

    Update 2019-11-15-1.php
    fixing spacing issue

--- notes/2019-11-15-1.php
+++ notes/2019-11-15-1.php
@@ -7,7 +7,7 @@
   	<div class="note h-entry">
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2019-11-15T011:14:20+0000">November 15th 2019 11:14</time>
- <p class="p-name e-content">I just finished publishing my 2019 Annual Report for @SCSU. Been a great year of trying to model digitally engaged scholarship and encouraging #OpenPedagogy<a href="https://jgregorymcverry.com/annualreport2019">2019 Annual Report</a>
+ <p class="p-name e-content">I just finished publishing my 2019 Annual Report for @SCSU. Been a great year of trying to model digitally engaged scholarship and encouraging #OpenPedagogy <a href="https://jgregorymcverry.com/annualreport2019">2019 Annual Report</a>
  </p>
   Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a>
 </div>
