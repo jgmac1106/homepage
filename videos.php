1e4801d48b55d2fbdffb6474c20125c0ffb4ab33
Greg McVerry <jgregmcverry@gmail.com>
2019-11-16 12:40:56 -0500

    adding video page
    add a video page

---
+++ videos.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article>
+  <h1>Videos</h1>
+  <div class="pubinfo">
+  <span class="summary">
+    <p class="p-summary">My Video Feeds</p>
+    </span>
+ </div>
+    <div class="copy">
+  <div class="e-content">
+ <section class="h-feed" id="7minuteTips">
+  <h2 class="p-name">7 Minute Teaching Tips</h2>
+  <time class="dt-updated" datetime="2019-11-16T11:24:20+0000">November 16th 2019 11:24</time>
+  <div class="videoblock">
+   <video  controls class="u-video" poster="/videos/notetaking.png" src="https://jgregorymcverry.com/videos/notetaking101.mp4"> 
+          </video>
+           <video class="h-entry"  controls poster="https://jgregorymcverry.com/videos/respond2poetry.png "class="u-video" src="https://jgregorymcverry.com/videos/RespondToPoetryWithPictures.mp4"> 
+          </video>
+           <video  class="h-entry" controls poster="/videos/sevenminutevocab.png "class="u-video" src="https://jgregorymcverry.com/videos/VocabGraphicOrganizers.mp4"> 
+          </video>
+        </div>
+ </section>
+  <section id="edu307 h-feed">
+    <h2 class="p-name">EDU 307 Videos</h2>
+    <div class="videoblock">
+    <video class="h-entry" controls poster="/videos/vanhookchilLitisWonderful.jpg" class="u-video" src="https://jgregorymcverry.com/videos/EDU307MondayUpdate2019-11-11.mp4"> 
+          </video>
+  </div>
+   <section id="edu307 h-feed">
+    <h2 class="p-name">IndieWeb Videos</h2>
+    <div class="videoblock">
+    <video class="h-entry" controls poster="https://indieweb.org/images/thumb/6/67/indiewebcamp-logo-lockup-color%403x.png/1200px-indiewebcamp-logo-lockup-color%403x.png" class="u-video" src="https://archive.org/download/indiewebandoer.mp4"> 
+          </video>
+          <video class="h-entry" controls poster="https://indieweb.org/images/thumb/6/67/indiewebcamp-logo-lockup-color%403x.png/1200px-indiewebcamp-logo-lockup-color%403x.png" class="u-video" src=" https://archive.org/download/indiewebeducationmeetup.mp4"> 
+          </video>
+
+  </div>
+ </section>
+    </div>
+  </div>
+    </article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+
+</html>
