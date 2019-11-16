72fb93bbec6abd09b69f089e02d47880064d0a1f
Greg McVerry <jgregmcverry@gmail.com>
2019-11-16 10:16:37 -0500

    Create 2019-11-16-1.php
    adding video note

---
+++ notes/2019-11-16-1.php
@@ -0,0 +1,29 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-11-11T09:14:20+0000">November 14th 2019 9:14</time>
+          <video  controls class="u-video" poster="tantekwcus.png" src="https://jgregorymcverry.com/videos/tantekwcus.mp4"> 
+          </video>
+          
+      
+  
+      
+        <p class="e-content">A wonderful talk from <a class="h-card" href="https://tantek.com">Tantek</a> at WCUS19.</p>
+      
+ 
+  <a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a>
+  <a class="u-syndication" href="https://brid.gy/about#webmentions">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
