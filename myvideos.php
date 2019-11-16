bdb55f23eded914b747ab924c6ba0f04e3a7001b
Greg McVerry <jgregmcverry@gmail.com>
2019-11-16 17:08:38 -0500

    Update myvideos.php
    adding videos

--- myvideos.php
+++ myvideos.php
@@ -64,6 +64,15 @@
       <p class="p-summary">Getting Started on Poetry Module</p>
       <time class="dt-published" datetime="2019-11-1111:24:20+0000">2019-11-11</time>
     </div>
+    <div class="h-entry">
+        <p class="p-name">How to Write A Reading Response</p>
+        <a class="p-author" href="/"></a>
+    <video  controls poster="/videos/vanhookchilLitisWonderful.jpg" class="u-video" src="https://jgregorymcverry.com/videos/https://archive.org/embed/howtowriteareflectiontoreading/HowToWriteAReflectionToReading.mp4"></video>
+      
+      <p class="p-summary">Getting Started on Poetry Module</p>
+      <time class="dt-published" datetime="2019-09-14T01:24:20+0000">2019-09-14</time>
+    </div>
+    
   </div>
        </section>   
    <section id="indieweb" class="videofeed h-feed">
@@ -77,11 +86,11 @@
       <time class="dt-published" datetime="2019-11-07T11:24:20+0000">2019-11-07</time>
     </div>
    <div class="h-entry">
-        <p class="p-name">OER and Ghana</p>
+        <p class="p-name">IndieWeb Summit Update</p>
     <video controls poster="https://indieweb.org/images/thumb/6/67/indiewebcamp-logo-lockup-color%403x.png/1200px-indiewebcamp-logo-lockup-color%403x.png" class="u-video" src="https://archive.org/download/indiewebandoer/IndieWeb%20and%20OER.mp4"> 
           </video>
-          <p class="p-summary">Launch of the OER Ghana Video</p>
-      <time class="dt-published" datetime="2019-11-07T11:24:20+0000">2019-11-07</time>
+          <p class="p-summary">An update from the second day of 2019 Summit</p>
+      <time class="dt-published" datetime="2019-11-07T11:24:20+0000">2019-06-30</time>
     </div>
 </div>
  </section>
@@ -89,19 +98,81 @@
     <h2 class="p-name">Foundations of Reading Videos</h2>
     <div class="videoblock">
       <div class="h-entry">
-        <p class="p-name">OER and Ghana</p>
-    <video controls poster="https://indieweb.org/images/thumb/6/67/indiewebcamp-logo-lockup-color%403x.png/1200px-indiewebcamp-logo-lockup-color%403x.png" class="u-video" src="https://archive.org/download/indiewebandoer/IndieWeb%20and%20OER.mp4"> 
+        <p class="p-name">Phonemic Awareness and Phonics</p>
+    <video controls  class="u-video" src="https://jgregorymcverry.com/videos/FoundationsofReadingestPrep.mp4"> 
           </video>
-          <p class="p-summary">Launch of the OER Ghana Video</p>
-      <time class="dt-published" datetime="2019-11-07T11:24:20+0000">2019-11-07</time>
+          <p class="p-summary">Review of the First Two Sections of Practice Questions</p>
+      <time class="dt-published" datetime="2013-11-07011:24:20+0000">2013-11-00</time>
     </div>
     <div class="h-entry">
-        <p class="p-name">OER and Ghana</p>
-    <video controls poster="https://indieweb.org/images/thumb/6/67/indiewebcamp-logo-lockup-color%403x.png/1200px-indiewebcamp-logo-lockup-color%403x.png" class="u-video" src="https://archive.org/download/indiewebandoer/IndieWeb%20and%20OER.mp4"> 
+        <p class="p-name">Foundations Open Response Item</p>
+    <video controls p class="u-video" src="https://jgregorymcverry.com/videos/HowtoAnswerFoundationofReadingOpen0Answe20Question.mp4"> 
           </video>
-          <p class="p-summary">Launch of the OER Ghana Video</p>
-      <time class="dt-published" datetime="2019-11-07T11:24:20+0000">2019-11-07</time>
+          <p class="p-summary">Tutorial on Answering Essay Questions</p>
+      <time class="dt-published" datetime="2019-11-07T11:24:20+0000">2019-06-07</time>
     </div>
+</div>
+ </section>
+  <section id="indieweb" class="videofeed h-feed">
+    <h2 class="p-name">Literacies</h2>
+    <div class="videoblock">
+      <div class="h-entry">
+        <p class="p-name">Academic Writing Tutorial One</p>
+    <video controls  class="u-video" src="https://jgregorymcverry.com/videos/Academic%20Writing%20Tutorial%20Four-%20The%20Idea%20Pocket.mp4"> 
+          </video>
+          <p class="p-summary">Rule One: It's a Game</p>
+      <time class="dt-published" datetime="2014-03-17T11:24:20+0000">2014-03-17</time>
+    </div>
+    <div class="h-entry">
+        <p class="p-name">Academic Writing Tutorial Two</p>
+    <video controls  class="u-video" src="https://jgregorymcverry.com/videos/Academic%20Writing%20Tutorial%20Two%20Do%20Not%20Be%20Wishy%20Washy.mp4"> 
+          </video>
+          <p class="p-summary">Rule Two: Don't Be Wishy Washy</p>
+      <time class="dt-published" datetime="2014-03-17T11:24:20+0000">2014-03-17</time>
+    </div>
+   <div class="h-entry">
+        <p class="p-name">Academic Writing Tutorial Three</p>
+    <video controls  class="u-video" src="https://jgregorymcverry.com/videos/Academic%20Writing%20Tutorial%20Three%20Play%20with%20Words.mp4"> 
+          </video>
+          <p class="p-summary">Rule Three: Play with Words</p>
+      <time class="dt-published" datetime="2014-03-17T11:24:20+0000">2014-03-17</time>
+    </div>
+      <div class="h-entry">
+        <p class="p-name">Academic Writing Tutorial Four</p>
+    <video controls  class="u-video" src="https://jgregorymcverry.com/videos/Academic%20Writing%20Tutorial%20Four-%20The%20Idea%20Pocket.mp4"> 
+          </video>
+          <p class="p-summary">Rule Four: The Idea Pocket</p>
+      <time class="dt-published" datetime="2014-03-17T11:24:20+0000">2014-03-17</time>
+    </div>
+    <div class="h-entry">
+        <p class="p-name">Blending SyllablesPart One</p>
+    <video controls  class="u-video" src="https://jgregorymcverry.com/videos/Blending%20Syllables%20Lesson%20Part%20One.mp4"> 
+          </video>
+          <p class="p-summary">Learn how to Teach Syllable Blending</p>
+      <time class="dt-published" datetime="2014-03-17T11:24:20+0000">2014-03-17</time>
+    </div>
+    <div class="h-entry">
+        <p class="p-name">Blending Syllables Part Two</p>
+    <video controls  class="u-video" src="https://jgregorymcverry.com/videos/Blending%20Syllables%20Part%20Two.mp4"> 
+          </video>
+          <p class="p-summary">Learn how to Teach Syllable Blending</p>
+      <time class="dt-published" datetime="2014-03-17T11:24:20+0000">2014-03-17</time>
+    </div>
+    <div class="h-entry">
+        <p class="p-name">BlendingSyllables Part Three</p>
+    <video controls  class="u-video" src="https://jgregorymcverry.com/videos/Blending%20Syllables%20Part%203.mp4"> 
+          </video>
+          <p class="p-summary">Learn how to Teach Syllable Blending</p>
+      <time class="dt-published" datetime="2014-03-17T11:24:20+0000">2014-03-17</time>
+    </div>
+    <div class="h-entry">
+        <p class="p-name">Bananas and the History of Literacy</p>
+    <video controls  class="u-video" src="Bananas%20and%20the%20History%20of%20Literacy.mp4"> 
+          </video>
+          <p class="p-summary">Pecha Flickr Talk on History of Literacy</p>
+      <time class="dt-published" datetime="2014-03-17T11:24:20+0000">2014-03-17</time>
+    </div>
+    
 </div>
  </section>
     </div>
