2e3102a683dbb72f51e559aedd1c2ff1079549ff
Greg McVerry <jgregmcverry@gmail.com>
2019-11-17 15:45:43 -0500

    Create 2019-11-17-2.php
    adding a photo post

---
+++ notes/2019-11-17-2.php
@@ -0,0 +1,28 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-11-17T06:54:20+0000">November 107h 2019 15:44</time>
+          <img   class="u-photo" src="https://jgregorymcverry.com/photos/AccraGoif2019-11-17.png" alt="screenshot of meeting"> 
+          
+      
+  
+      
+        <p class="e-content">@goifoundation monthly meeting and #IndieWeb meet up. Come learn how we are using @wikipedia and personal websites to create #OER networks across Ghana</p>
+      
+ 
+  <a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a>
+  <a class="u-syndication" href="https://brid.gy/about#webmentions">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
