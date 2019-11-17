771a9dc7915d287181cfb36b8de367e531553449
Greg McVerry <jgregmcverry@gmail.com>
2019-11-17 10:13:10 -0500

    Update footer.php
    adding child disclaimer

--- footer.php
+++ footer.php
@@ -36,8 +36,9 @@
     </ul>
 </div>
 <div class="footer_info">
-      <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>. by
+      <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a> by
       <p class="h-card"><a class="u-url-u-uid" href="jgregorymcverry.com"><span class="p-name">Greg McVerry</span></a>
+        <p>Any video or image containing likeness of children is All Rights Reserved. Just ask me, and I will share.</p>
       <a   href="https://microformats.org"><img class="https://indieweb.org/images/7/72/microformats-button.png"></a><br>
       <a   href="https://indieweb.org"><img src="https://indieweb.org/images/4/4a/indiewebcamp-button.png"></a>
       <a  href="https://indieweb.org/webmention"><img src="https://indieweb.org/images/0/03/webmention-button.png"></a>
