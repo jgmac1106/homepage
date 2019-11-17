bcbd00367d2609a86de1210160f55146bf59ca46
Greg McVerry <jgregmcverry@gmail.com>
2019-11-17 09:17:30 -0500

    Create untitledpoem.php
    forgot file extension

---
+++ untitledpoem.php
@@ -0,0 +1,320 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">Untitled Poem, No Title Till its Done</h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregory McVerry</a>
+     on <time class="dt-published" datetime="2019-11-217 09:04:20">17<sup>th</sup>November 2019</time></p>
+    <p>
+      Updated <time class="dt-updated" datetime="2019-11-217 09:04:20">17<sup>th</sup> November  2019</time></p>
+   
+  <span class="summary">
+    <p class="p-summary">A daily collaborative poem between Greg McVerry and Kevin Hodgson started on 2019-11-09</p>
+    </span>
+ </div>
+    <div class="copy">
+
+  <div class="e-content">
+    <p class="Greg">
+      Skin plastered against tattered holes<br>
+of a torn blanket.<br>
+Rough threads of <br>
+unwoven time<br>
+scratching against frozen thighs<br>
+She sighs, glancing beyond<br>
+a shoulder tucked close to<br>
+heart<br>
+beyond the cold<br>
+frosted window she see nothing<br>
+nothing at all<br>
+</p>
+<p class="Kevi">
+ Fingers caress<br>
+these holes, torn,<br>
+of this blanket, worn;<br>
+She threads time<br>
+like a shoulder tucked<br>
+close to the heart,<br>
+the start of nothing<br>
+at all<br>
+</p>
+       <p class="Greg">
+      The moon collapsed over horizons split<br>
+shattered space in<br>
+illuminating mysterious truths<br>
+as time melts<br>
+answers hang above broken canopies<br>
+towering conceptions move as the rabbit<br>
+dances<br>
+feet wrapped in dew fed grass<br>
+energy from within<br>
+fleeting realities in a world<br>
+undefined<br>
+<br>
+</p>
+<p class="Kevin">
+I misread your<br>
+canopies as<br>
+cantaloupes -<br>
+misconceptions,<br>
+indeed — yet, if<br>
+one believes that<br>
+one reads with intent,<br>
+then the fruit <br>
+as moon may still <br>
+have been time,<br>
+well spent<br>
+:)<br>
+</p>
+<p class="Greg">
+ fingers lay bare<br>
+ripping flesh<br>
+of a once shared cantaloupe<br>
+now seeds spread<br>
+a gelatinous mess  <br>
+in only one hand<br>
+viscid vicissitudes<br>
+drip from the mouth<br>
+<br>
+half untouched<br>
+slices of <br>
+memories of time<br>
+unspent<br>
+he licks<br>
+fingers…<br>
+</p>
+<p class="Kevin">
+Spit the seeds<br>
+into soil, use your<br>
+toes to plant<br>
+the stars, <br>
+this softening<br>
+touch of sound,<br>
+from mouth to<br>
+ground, <br>
+for some of these<br>
+meteors fall <br>
+their way up<br>
+</p>
+<p class="Greg">
+  touch of sound<br>
+tossed abound into <br>
+vacuums of space<br>
+softening tenors<br>
+hiding a blazed fate<br>
+seeking to knock,<br>
+knock on Nebula's gate<br>
+Our eternal seed<br>
+unlocking the graveyard of the sun<br>
+or nurseries collapsing<br>
+in on life?<br>
+</p>
+<p class="Kevin">
+We found the Gate<br>
+too late — sadly,<br>
+our fate …<br>
+<br>
+#smallpoems (Expanse connections?)<br>
+</p>
+<p class="Greg">
+Doors and corners<br>
+of time unfold to<br>
+envelop realities<br>
+as two specks<br>
+drawn together<br>
+collapse inward<br>
+driving<br>
+their energy apart<br>
+so they stay<br>
+frosted windows, dew covered<br>
+toes<br>
+carried together, forever separated<br>
+on the winds of a fallen star<br>
+</p>
+<p class="Kevin">
+The ink door, folded<br>
+in the paper corner, folded<br>
+in the word floor, <br>
+- nothing<br>
+more is more <br>
+mystery than that <br>
+- this poem’s engine<br>
+still pushes for even more<br>
+</p>
+<p class="Greg">
+They drew<br>
+within<br>
+two meteors<br>
+escaping the well<br>
+reborn in clouds<br>
+to have passion<br>
+drive apart<br>
+<br>
+<br>
+The frost on the window<br>
+the dew in the grass<br>
+time marches<br>
+as space collapses<br>
+life folded<br>
+like a Liu Cixin novel<br>
+new engines<br>
+always<br>
+</p>
+<p class="Kevin">
+Long we spent<br>
+on exploration<br>
+for the remains<br>
+of the meteorite<br>
+<br>
+broken upon<br>
+entry and scattered<br>
+among the ruins<br>
+of this writing<br>
+<br>
+the forest hides<br>
+the trees, the dust<br>
+we may never see<br>
+<br>
+still, we wander<br>
+in search of<br>
+the stories<br>
+</p>
+<p class="Greg">
+in hysteria of historia<br>
+meteors fall up<br>
+bent realities in<br>
+might of flight<br>
+escape the well<br>
+no meteorite<br>
+no broken shell<br>
+matter finite<br>
+stories ensconced<br>
+in quarries<br>
+of vacuum<br>
+bound by nature's<br>
+response<br>
+an eternity<br>
+together<br>
+flying apart<br>
+</p>
+<p class="Kevin">
+Light bends, too,<br>
+like justice,<br>
+every color becoming<br>
+one before breaking<br>
+back apart - the heart<br>
+understands<br>
+</p>
+<p class="Greg">
+In this color of sound<br>
+incandescent notes<br>
+vibrate on <br>
+<br>
+the neck of<br>
+&#160;&#160;&#160;&#160;nothingness<br>
+<br>
+for music crawls out <br>
+from spaces between notes<br>
+harmony driven from source<br>
+collapsing on the event horizon<br>
+of their souls<br>
+</p>
+<p class="Kevin">
+The gaps and space <br>
+play music<br>
+you can only hear<br>
+when you listen<br>
+<br>
+its incandescent <br>
+notes glisten <br>
+with possibilities<br>
+<br>
+the poems you wrote<br>
+the stories you found<br>
+the songs you sing<br>
+<br>
+emerge as one as<br>
+harmonic convergence<br>
+points on the horizon<br>
+</p>
+<p class="Greg">
+waves lifted<br>
+through murky<br>
+haze<br>
+sounds sifted as<br>
+time shaves<br>
+notes sinking<br>
+afloat<br>
+simple misery of happiness<br>
+Metronome just gets louder<br>
+as the beat is lost<br>
+night never rising<br>
+over the horizon<br>
+sun<br>
+succumbs<br>
+lost in a second act<br>
+</p>
+<p class="Kevin">
+Steady,<br>
+<br>
+goes this<br>
+metronome -<br>
+the pulsing beat<br>
+of home<br>
+<br>
+we watch<br>
+the arm swing<br>
+back and forth<br>
+<br>
+and back again,<br>
+and back again<br>
+back<br>
+again<br>
+back<br>
+<br>
+these poems<br>
+of great<br>
+unknown<br>
+</p>
+<p class="Greg">
+the broken glances<br>
+and incessant sounds of<br>
+Maelzel’s  stolen curse<br>
+no longer provide shelter<br>
+Order lost<br>
+Selective tones seek escape<br>
+of rhythm <br>
+ &#160;and rhyme<br>
+an Object to Be Destroyed<br>
+reconfigured in time<br>
+back<br>
+and back again,<br>
+</p>
+<p class="Kevin">
+ A song like this,<br>
+deconstructed,<br>
+becomes little more<br>
+than spare parts<br>
+ — broken notes,<br>
+tangled keys, half<br>
+harmonies, misshapen <br>
+melodies - we gather<br>
+what’s left - we, <br>
+the mechanics<br>
+of muse --and whistle<br>
+the tune back into the wind<br>
+to let it sing again<br>
+</p>
+  </div>
+      </div>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
