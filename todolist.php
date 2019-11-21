158a44a148cc335fa106cd04e6130ffb02161b12
Greg McVerry <jgregmcverry@gmail.com>
2019-11-21 07:37:39 -0500

    Create todolist.php
    adding to-do list

---
+++ todolist.php
@@ -0,0 +1,41 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">To-Do List</h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregory McVerry</a>
+     on <time class="dt-published" datetime="2019-11-21 07:34:20">21<sup>st</sup> November 2019</time></p>
+    <p>
+      Updated <time class="dt-updated" datetime="2019-11-21 07:34:20">21<sup>st</sup> November 2019</time></p>
+   
+  <span class="summary">
+    <p class="p-summary">My To-Do List for Non-Teaching Days</p>
+    </span>
+ </div>
+    <div class="copy">
+  <div class="e-content">
+  <div class="h-entry" >
+    <a class="h-card p-author" href="/"></a>
+    <p class="p-name">2019-11-21</p>
+    <ul>
+      <li class="h-item" class="20191121-1">Annotate ACUE Effective Online Learning Modules 🍅🍅🍅🍅🍅</li>
+      <li class="h-item" class="20191121-2">Elm CityWebmaker emails 🍅</li>
+      <li class="h-item" class="20191121-3">OER Ghana 10 Week Curriculum Map 🍅🍅</li>
+      <li class="h-item" class="20191121-4">Post EDU 407 Teaching Novels Video 🍅</li>
+      <li class="h-item" class="20191121-5">Read EDU 307 Teaching Write Post 🍅🍅</li>
+      <li class="h-item" class="20191121-6">Video feedback to EDU 407 Lesson Plans 🍅🍅🍅🍅🍅</li>
+      <li class="h-item" class="20191121-7">Write a post about building a video page, annual report 🍅</li>
+    </ul>
+  </div>
+      </div>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
