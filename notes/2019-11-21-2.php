ed19f7e0783438307146d63cadbf3b32bcc1bdc2
Greg McVerry <jgregmcverry@gmail.com>
2019-11-21 11:00:41 -0500

    sadik
    adding photo and note

---
+++ notes/2019-11-21-2.php
@@ -0,0 +1,28 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-11-21T10:54:20+0000">November 21st 2019 10:54</time>
+          <img   class="u-photo" src="https://jgregorymcverry.com/photos/SadikIndieWeb.png" alt="Sreenshot with Sadik"> 
+          
+      
+  
+      
+        <p class="e-content">Learning how to add events to th #IndieWeb wiki with Sadik. Getting excited for the launch of the goifnetwork Open Leaders X training sponsored by @scsu, @goifoundation, and @mozilla</p>
+      
+ 
+  <a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a>
+  <a class="u-syndication" href="https://brid.gy/about#webmentions">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
