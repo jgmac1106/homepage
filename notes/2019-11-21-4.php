f3ad1efc9d37dfda87679f15c9427c82a68011aa
Greg McVerry <jgregmcverry@gmail.com>
2019-11-21 11:47:02 -0500

    Create 2019-11-21-4.php
    adding note

---
+++ notes/2019-11-21-4.php
@@ -0,0 +1,21 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-11-21 08:14:20">21<sup>st</sup> November 2019</time></p>
+<p>In reply to <a class="u-in-reply-to" href="https://jgregorymcverry.com/todolist#20191121-2">Todo list</a>
+ <p class="p-name e-content">Spent one 🍅🍅🍅 on Goifnetwork.org planning. Little bit more than allocated but we got events added to #IndieWeb wiki, created a grants page, and talked about the overwhelming interest there is to get going in project
+ </p> 
+  Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
