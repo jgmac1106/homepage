bba4635d35a0e4a3f73351613cab4f1a40498d0a
Greg McVerry <jgregmcverry@gmail.com>
2019-11-22 10:07:14 -0500

    Create misreadingscienceofreading.php
    adding a post

---
+++ misreadingscienceofreading.php
@@ -0,0 +1,144 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">Response to Misreading the Science of Reading</h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregory McVerry</a>
+     on <time class="dt-published" datetime="2019-11-22 10:04:20">22<sup>nd</sup>November 2019</time></p>
+    <p>
+      Updated <time class="dt-updated" datetime="2019-11-22 10:04:20">22<sup>nd</sup>November 2019</time></p>
+   
+  <span class="summary">
+    <p class="p-summary">Thread of notes published during #NCTE19 about the dangerous myth on "science of reading."</p>
+    </span>
+ </div>
+    <div class="copy">
+  <div class="e-content">
+   <div class="notewrapper">
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_1" data-screenname="jgmac1106"  class="h-entry note" dir="auto">
+<a class="entity-mention entity-mention-first" href="https://twitter.com/ehanford">@ehanford</a> Maybe very little new research in reading research has come out since 80s that has fundementally shifted our understanding. Marketing documents from Amplify are not research. When you dig into citations, recycled, often just NRP. <a class="entity-hashtag" href="/hashtag/ncte19">#ncte19</a> (<a class="entity-url" data-preview="true" rel="canonical" href="https://quickthoughts.jgregorymcverry.com/s/2gKDNO">quickthoughts.jgregorymcverry.com/s/2gKDNO</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i>Also on <a class="u-syndication" href="https://twitter.com/jgmac1106/status/1197867027286908929">Twitter</a></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_2" data-screenname="jgmac1106" class="h-entry note" dir="auto">
+Look how you frame the narraritive "teachers don't know this science" "<span class="entity-embed"><span class="twitter-player"><blockquote class="twitter-tweet" data-conversation="none" data-align="center" data-dnt="true"><a class="u-syndication" href="https://twitter.com/ehanford/status/1197721612759556098">https://twitter.com/ehanford/status/1197721612759556098</a></blockquote></span></span>" "teacher prep is bad" <span class="entity-embed"><span class="twitter-player"><blockquote class="twitter-tweet" data-conversation="none" data-align="center" data-dnt="true"><a class="u-syndication" href="https://twitter.com/ehanford/status/1197721653289111554">https://twitter.com/ehanford/status/1197721653289111554</a></blockquote></span></span> this is an agenda and not research and also not based in reality. Frankly I find it insulting <a class="entity-hashtag" href="/hashtag/ncte19">#ncte19</a> (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/WkE1H">quickthoughts.jgregorymcverry.com/s/WkE1H</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i>Also on <a class="u-syndication" href="https://twitter.com/jgmac1106/status/1197868235967913985">Twitter</a></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_3" data-screenname="jgmac1106" Also on  class="h-entry note" dir="auto">
+<a class="entity-mention entity-mention-first" href="https://twitter.com/ehanford">@ehanford</a> Folks love quantitative theoretical models. Looking at variance explained by different variables helps us understand reading. But to suggest Gough &amp; Tunmer's model is superior leaves many question. So many better models with &lt; error <a class="entity-hashtag" href="/hashtag/ncte19">#ncte19</a> (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/A4S6D">quickthoughts.jgregorymcverry.com/s/A4S6D</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i><a class="u-syndication" href="https://twitter.com/jgmac1106/status/1197869634998931457" >Twitter</a></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_4" data-screenname="jgmac1106" Also on  class="h-entry note" dir="auto">
+also think concept of "fight" &amp; "wars" itself toxic. Reflects and reinforces a long history of exclusion, mysoginy, and racism in literacy research. Plus real war is real bad. Let's not equate people yelling at the Goodmans during Town Hall as war… <a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/2019/11/22/also-think-concept-of-fight-wars">quickthoughts.jgregorymcverry.com/2019/11/22/als…</a>
+<sup class="tw-permalink"><i class="fas fa-link"></i><a class="u-syndication" href="https://twitter.com/jgmac1106/status/1197870613500702721" >Twitter</a></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_5" data-screenname="jgmac1106" Also on  class="h-entry note" dir="auto">
+Thx for Kilpatrick recommendation, I respect his work. I think you should also read folks like <a class="entity-mention" href="https://twitter.com/davidekirkland">@davidekirkland</a> and "Search Past Silence, you frame this as a "fight" between phonics and phonology...it isnt It's about people, other narratives <a class="entity-hashtag" href="/hashtag/ncte18">#ncte18</a> (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/tQnUA">quickthoughts.jgregorymcverry.com/s/tQnUA</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i><a class="u-syndication" href="https://twitter.com/jgmac1106/status/1197872134187552770">Twitter</a></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_6" data-screenname="jgmac1106" Also on  class="h-entry note" dir="auto">
+Big issue is NRP, on which "science of reading" has as 1 leg of 2 leg stool (other brain scan research) specifically and explicitly omitted research we know also imporves reading. Motivation, writing, and much more. <a class="entity-hashtag" href="/hashtag/ncte19">#ncte19</a> <a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/2019/11/22/thx-for-kilpatrick-recommendation-i-respect-his">quickthoughts.jgregorymcverry.com/2019/11/22/thx…</a> (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/TocRz">quickthoughts.jgregorymcverry.com/s/TocRz</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i><a class="u-syndication" href="https://twitter.com/jgmac1106/status/1197872945089204225" >Twitter</a></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_7" data-screenname="jgmac1106"  class="h-entry note" dir="auto">
+And most teachers who won't fold under political pressure of "science of reading" aren't against explicit phonics instruction. Heck <a class="entity-hashtag" href="/hashtag/CCSS">#CCSS</a> has a foundational skills sections and many teacher prep programs take Foundations of Reading <a class="entity-hashtag" href="/hashtag/ncte19">#ncte19</a> (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/1M06Qx">quickthoughts.jgregorymcverry.com/s/1M06Qx</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i>Also on <a class="u-syndication" href="https://twitter.com/jgmac1106/status/1197873705449349127">Twitter</a></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_8" data-screenname="jgmac1106"  class="h-entry note" dir="auto">
+implore anyone interested to go track down ttechnical reports of the NRP and read the foot notes. Decide for yourself if the inclusion and exclusion criteria need revisiting. But most importantly STOP treating it like an efficacy study <a class="entity-hashtag" href="/hashtag/ncte19">#ncte19</a> Bad… <a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/2019/11/22/implore-anyone-interested-to-go-track-down">quickthoughts.jgregorymcverry.com/2019/11/22/imp…</a>
+<sup class="tw-permalink"><i class="fas fa-link u-syndication"></i>Also on <a class="u-syndication" href="https://twitter.com/jgmac1106/status/1197874489691914240">Twitter</a></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_9" data-screenname="jgmac1106" class="h-entry note" dir="auto">
+The NRP is not some random design study that put explicit phonics instruction up against miscue analysis, or compared different diagnostic assessments of fake word recognition up against MSV...franky I care for neither. but had nothign to do with NRP… <a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/2019/11/22/the-nrp-is-not-some-random-design">quickthoughts.jgregorymcverry.com/2019/11/22/the…</a>
+<sup class="tw-permalink"><i class="fas fa-link"></i>Also on <a class="u-syndication" href="https://twitter.com/jgmac1106/status/1197875768145125380">Twitter</a></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_10" data-screenname="jgmac1106"  class="h-entry note" dir="auto">
+I teach running records, listening to kids read great way to identify phonics then spelling help (though I am faster at recongizing phonics level by reading their writing)...the MSV stuff...might as well throw darts at wall. I skip it. <a class="entity-hashtag" href="/hashtag/ncte19">#ncte19</a> (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/1brXvQ">quickthoughts.jgregorymcverry.com/s/1brXvQ</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i>Also on <a class="u-syndication" href="https://twitter.com/jgmac1106/status/1197877213380648960">Twitter</a></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_11" data-screenname="jgmac1106" class="h-entry note" dir="auto">
+but I hold that true for most diagnostic assessments, by time they get to classroom level ecological factors have stripped almost all validity and reliability. Like when DRA2 scores work their way in SLOs and fall scoring stricter than spring <a class="entity-hashtag" href="/hashtag/ncte19">#ncte19</a> (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/sikto">quickthoughts.jgregorymcverry.com/s/sikto</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i>Also on <a class="u-syndication" href="https://twitter.com/jgmac1106/status/1197878121191661568">Twitter></a></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_12" data-screenname="jgmac1106" class="h-entry note" dir="auto">
+<a class="entity-mention entity-mention-first" href="https://twitter.com/ehanford">@ehanford</a> In the end I don't think it will matter. Foundational skills don't take much time. 7 minutes a day for phonological awareness and some explicit phonics intruction. One place technology will be better at doing than humans IMO. <a class="entity-hashtag" href="/hashtag/ncte19">#ncte19</a> (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/nxLee">quickthoughts.jgregorymcverry.com/s/nxLee</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i>Also on <a class="u-syndication" href="https://twitter.com/jgmac1106/status/1197879212746694658">Twitter</a></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_13" data-screenname="jgmac1106" class="h-entry note" dir="auto">
+<a class="entity-mention entity-mention-first" href="https://twitter.com/ehanford">@ehanford</a> <a class="entity-mention entity-mention-first" href="https://twitter.com/karenvaites">@karenvaites</a> <a class="entity-mention entity-mention-first" href="https://twitter.com/LaurenTTutor718">@LaurenTTutor718</a> I will leave with this. If SVR is such simple model how is it dialogical reading is one of the most replicable studies in <a class="entity-hashtag" href="/hashtag/ece">#ece</a> <a class="entity-hashtag" href="/hashtag/literacies">#literacies</a>? All that talk about pictures and connections....<a class="entity-hashtag" href="/hashtag/ncte19">#ncte19</a> (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/XoF0K">quickthoughts.jgregorymcverry.com/s/XoF0K</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i>Also on <a class="u-syndication" href="https://twitter.com/jgmac1106/status/1197881639302500352"> Twitter</a></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_14" data-screenname="jgmac1106"  class="h-entry note" dir="auto">
+<a class="entity-mention entity-mention-first" href="https://twitter.com/ehanford">@ehanford</a> <a class="entity-mention entity-mention-first" href="https://twitter.com/karenvaites">@karenvaites</a> <a class="entity-mention entity-mention-first" href="https://twitter.com/LaurenTTutor718">@LaurenTTutor718</a> If variable like motivation and family do not matter. Why is ist we find setting up text messaging in early childhood classrooms may improve literacy rates? Could it be two people can be right at the same through… <a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/2019/11/22/ehanford-karenvaites-laurenttutor718-if-variable-like">quickthoughts.jgregorymcverry.com/2019/11/22/eha…</a>
+<sup class="tw-permalink"><i class="fas fa-link"></i>Also on <a class="u-syndication" href="https://twitter.com/jgmac1106/status/1197882380360523776">Twitter</a></sup>
+</div>
+</div>
+</div>
+</div>
+</article>
+  </div>
+      </div>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
