5ba8916e9170e164e33b271562d90c6679f92321
Greg McVerry <jgregmcverry@gmail.com>
2019-11-25 09:30:24 -0500

    adding a note
    adding a note

---
+++ notes/2019-11-25.php
@@ -0,0 +1,30 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="note2">
+      <a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-11-22 10:04:20">22<sup>nd</sup>November 2019</time>
+       <p>Week note Reflection of  <a class="u-in-reply-to" href="https://jgregorymcverry.com/todolist#2019-11-22">My To Do list</a>       
+
+  <p class="p-name e-content">Thrusday's to do list ended up taking me through most of the wrrkend</p>
+  <p>I got the OER curriculum map work done. You can check it out on our <a href="http://wiki.goifnetwork.org/OER_Ghana:_Mozilla_Leaders_X_Training_Curriculum">wiki</a>.</p>
+  <p>I also did one round of video feedback for #edu407 lessons and posted all the videos for #edu307 and #edu407.</p>
+  <p>I took on some client work this week. I haven't had as many contracts this year so this is a much needed suprise. One was planned. I am developing a series of modules o best practices for online teaching.</p>
+  <p>The other project was a rush job for a client. I was given a ton of survey data, with only three quantifiable question and asked to do a data analysis and make recommendations. I think it went well but it ate up my weekend. 
+  <p>I did get to sneak in a game of Risk, well three turrns with the kids on Sunday, and John's lego robotics team made it to states.</p>
+     
+ 
+<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a>
+  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
