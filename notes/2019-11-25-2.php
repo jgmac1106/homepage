2ba8f0ce98e4a2edd84457751b750f46faf5a154
Greg McVerry <jgregmcverry@gmail.com>
2019-11-25 10:02:39 -0500

    Update 2019-11-25-2.php
    fixing spacing

--- notes/2019-11-25-2.php
+++ notes/2019-11-25-2.php
@@ -7,7 +7,7 @@
   	<div class="note h-entry">
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2019-11-25 09:44:20">25<sup>th</sup>November 2019</time>
- <p class="p-name e-content">I published a <a href="https://jgregorymcverry.com/notes/2019-11-25-1">weeknote</a>on what I accomplished last week
+ <p class="p-name e-content">I published a <a href="https://jgregorymcverry.com/notes/2019-11-25-1">week note</a> on what I accomplished last week
  </p>
   Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a>
    <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a><a href="https://jgregorymcverry.com/notes/2019-11-25-1" rel="prev">← note</a>
