10a95d1b992667aab640ad7ab260db38c88d79fe
Greg McVerry <jgregmcverry@gmail.com>
2019-11-26 12:46:01 -0500

    syndication links
    switching syndication

--- notes/2019-11-26-3.php
+++ notes/2019-11-26-3.php
@@ -7,10 +7,10 @@
   	<div class="note h-entry">
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2019-11-26 10:54:20">26<sup>th</sup>November 2019</time>
- <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/jgmac1106/status/1199368757388427264">My To Do List</a></p>
- <p class="p-name e-content">Spent another 🍅🍅 on #edu307 videos. Gonna break for lunch and get back at it in an hour.<a href="https://jgregorymcverry.com/todolist#20191126-1">https://jgregorymcverry.com/todolist</a>
+ <p>In reply to <a class="u-in-reply-to" href="https://jgregorymcverry.com/todolist#20191126-1">My To Do List</a></p>
+ <p class="p-name e-content">Spent another 🍅🍅 on #edu307 videos. Gonna break for lunch and get back at it in an hour. <a href="https://jgregorymcverry.com/todolist#20191126-1">https://jgregorymcverry.com/todolist</a>
  </p>
-  Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2019-11-26-4" rel="next">Next Note  <a href="https://jgregorymcverry.com/notes/2019-11-26-3" rel="prev">Previous Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  Also on <a class="u-syndication" href="https://twitter.com/jgmac1106/status/1199382908437442560"><strong>Twitter</strong></a> <a href="https://jgregorymcverry.com/notes/2019-11-26-4" rel="next">Next Note  <a href="https://jgregorymcverry.com/notes/2019-11-26-3" rel="prev">Previous Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
   	</main>
   		<footer>
