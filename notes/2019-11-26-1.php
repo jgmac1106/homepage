c1d31c8a3e494716bd094e2c28f529426ae21efc
Greg McVerry <jgregmcverry@gmail.com>
2019-11-26 11:44:13 -0500

    fixing year
    fixing year

--- notes/2019-11-26-1.php
+++ notes/2019-11-26-1.php
@@ -9,7 +9,7 @@
 <time class="dt-published" datetime="2019-11-26 10:54:20">26<sup>th</sup>November 2019</time>
  <p class="p-name e-content">Hey @scsu #edu307Fall19 and #edu407Fall19 working on my #edu307 and #edu407 feedback in today's to-do list <a href="https://jgregorymcverry.com/todolist#20191126">https://jgregorymcverry.com/todolist</a>
  </p>
-  Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a><a href="https://jgregorymcverry.com/notes/2016-11-26-2" rel="next">Next Note <a href="https://jgregorymcverry.com/notes/2016-11-25-2" rel="prev">Previous Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a><a href="https://jgregorymcverry.com/notes/2019-11-26-2" rel="next">Next Note <a href="https://jgregorymcverry.com/notes/2019-11-25-2" rel="prev">Previous Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
   	</main>
   		<footer>
