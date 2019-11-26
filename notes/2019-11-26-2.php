c44ec370c0fc8fc9e9f74bba416d28c74f2db02f
Greg McVerry <jgregmcverry@gmail.com>
2019-11-26 11:39:06 -0500

    Update 2019-11-26-2.php
    adding reply to tweet

--- notes/2019-11-26-2.php
+++ notes/2019-11-26-2.php
@@ -7,7 +7,7 @@
   	<div class="note h-entry">
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2019-11-26 10:54:20">26<sup>th</sup>November 2019</time>
- <p>In reply to <a class="u-in-reply-to" href="https://jgregorymcverry.com/todolist#2019-11-26-1">My To Do List</a></p>
+ <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/jgmac1106/status/1199366445072822272">My To Do List</a></p>
  <p class="p-name e-content">Spent 🍅🍅 on #edu307 videos. Need to make a tutorial on the difference between a short response for a test and a reading analysis post,<a href="https://jgregorymcverry.com/todolist#20191126">https://jgregorymcverry.com/todolist</a>
  </p>
   Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a><a href="https://jgregorymcverry.com/notes/2016-11-26-3" rel="next">Next Note <a href="https://jgregorymcverry.com/notes/2016-11-25-1" rel="prev">Previous Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
