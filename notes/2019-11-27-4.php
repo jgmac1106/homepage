94ac5c19c278b08e5ee941211da68f6654fd8984
Greg McVerry <jgregmcverry@gmail.com>
2019-11-27 12:48:40 -0500

    Update 2019-11-27-4.php
    removing video

--- notes/2019-11-27-4.php
+++ notes/2019-11-27-4.php
@@ -8,7 +8,7 @@
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2019-11-27 12:24:20">27<sup>th</sup>November 2019</time>
  <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/jgmac1106/status/1199726737053114368">My To Do List</a></p>
- <p class="p-name e-content">Spent 🍅🍅 making a video for @scsu #edu106 on the basics of surveys as we study how to use Google forms and Sheets to calculate desrciptive statistics with formulas <a class="u-video" href="https://jgregorymcverry.com/videos/BasicsOfWritingSurveyQuestions.mp4">https://jgregorymcverry.com/videos/BasicsOfWritingSurveyQuestions.mp4</a> Captions coming. #literacies #digped
+ <p class="p-name e-content">Spent 🍅🍅 making a video for @scsu #edu106 on the basics of surveys as we study how to use Google forms and Sheets to calculate desrciptive statistics with formulas. Find link to video on link to website below. Captions coming. #literacies #digped
  </p>
   Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a><a href="https://jgregorymcverry.com/notes/2016-11-27-5" rel="next">Next Note <a href="https://jgregorymcverry.com/notes/2016-11-27-3" rel="prev">Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
