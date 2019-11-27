543e154aac31dde360d09ab08daab66c1849eb54
Greg McVerry <jgregmcverry@gmail.com>
2019-11-27 10:10:02 -0500

    adding files
    adding files

---
+++ festivaloflearningproposal.php
@@ -0,0 +1,59 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">Draft of Possible Festival of Learning session</h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregory McVerry</a>
+     on <time class="dt-published" datetime="2019-11-27 10:10:20">25<sup>th</sup> Novemner 2019</time></p>
+    <p>
+      Updated <time class="dt-updated" datetime="2019-11-27 10:04:20">25<sup>th</sup> July 2019</time></p>
+   
+  <span class="summary">
+    <p class="p-summary">How Can We Reduce the Carbon Impact of Academic Conferencres?</p>
+    </span>
+ </div>
+    <div class="copy">
+  <div class="e-content">
+    <p>Please note this draft reperesents the ideas of only one of the presenters. Other authors have not had chance to offer ideas or shape proposal. This will change before submission.</p>
+   <h2>Striving for Remote Parity: Reducing the Carbon Crater Left By Academic Conferences</h2>
+<p>As our world burns and cities of the indigenous sink many scholars around the world seek to lower their impact on the climate crisis. Our biggest carbon sink fills as we seek out conferences. 1,000s of academics with the privilege and power to travel descend on cities through air, rail, and car. Sales reps pitch consumption in vendor halls awash in swag.. All to present a paper for 20 minutes. Is there a better way forward?</p>
+<p>This session examines three case studies of alternative conference models that can drastically reduce the carbon impact of conferences while also embracing open pedagogies; increasing the knowledge growth of participants; and providing access to learning to contingent faculty, marginalized peoples, and other less privileged learners.</p>
+<p>We will begin with a brief introduction of the impact academic conferences have on our carbon footprint. Then we will meet in focus groups with both local and remote participants and discuss the history, benefits, and problems of large scale conferences. Using the following discussion prompts:</p>
+<ul>
+<li>What is the history behind academic conferences?</li>
+<li>What are the benefits of large scale gatherings?</li>
+<li>What are the problems with conferences?</li>
+<li>Where does power lie and how does it get reinforced on conference circuit?</li>
+</ul>
+<p>Next the presenters will meet with a group and present their case study. They will discuss (these may change between now and submission):</p>
+<ul>
+<li>PressEdConf an online conference conducted on Twitter</li>
+<li>Virtually Connecting: A community bringing online discussions to conferences</li>
+<li>IndieWebCamp: Global movement of local events to encourage people to build websites</li>
+</ul>
+<p>After each facilitator presents, the groups will dissolve and reform using jigsaw methods. The new groups will then be given a matrix to find common patterns across the different case studies.</p>
+<p>This will include but is not limited to:</p>
+<ul>
+<li>Keynote Speakers</li>
+<li>Vendors</li>
+<li>Food</li>
+<li>Travel</li>
+<li>Networked Technologies</li>
+<li>Diversity and Inclusion</li>
+<li>Open Practices</li>
+<li>Impact on Learning</li>
+</ul>
+<p>We will then reconvene as a whole group and brainstorm a set of tips we could offer our colleagues who help to organize academic conferences.</p>
+  </div>
+      </div>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
