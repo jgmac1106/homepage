67cdafc9cf2febbc302d9b0dd7cdbfdea8e1e4c5
Greg McVerry <jgregmcverry@gmail.com>
2019-11-27 10:42:48 -0500

    Update 2019-11-27-1.php

--- notes/2019-11-27-1.php
+++ notes/2019-11-27-1.php
@@ -7,7 +7,7 @@
   	<div class="note h-entry">
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2019-11-27 10:24:20">27<sup>th</sup>November 2019</time>
- <p class="p-name e-content">Here is today's @scsu to do list. <a href="https://jgregorymcverry.com/todolist#20191127">https://jgregorymcverry.com/todolist#20191127</a> Underestimated number of 🍅 required yesterday. three videos in one section and then the other section.</p>
+ <p class="p-name e-content">Here's today's @scsu to do list. <a href="https://jgregorymcverry.com/todolist#20191127">https://jgregorymcverry.com/todolist#20191127</a> Underestimated number of 🍅 required yesterday. three videos in one section and then the other section.</p>
   Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a><a href="https://jgregorymcverry.com/notes/2016-11-27-2" rel="next">Next Note <a href="https://jgregorymcverry.com/notes/2016-11-26-4" rel="prev">Previous Note</a> 
 </div>
   	</main>
