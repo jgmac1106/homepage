943185ee325e2f963caaf16209bdb8e704155c62
Greg McVerry <jgregmcverry@gmail.com>
2019-11-27 15:08:50 -0500

    fix mistake
    fix spelling mistake

--- notes/2019-11-27-5.php
+++ notes/2019-11-27-5.php
@@ -8,7 +8,7 @@
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2019-11-27 10:24:20">27<sup>th</sup>November 2019</time>
  <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/jgmac1106/status/1199747832388968448">My To Do List</a></p>
- <p class="p-name e-content">Spent 🍅🍅🍅🍅 on drafting an @SCSU #socialjustice grant proposal for <a href="https://eachchildlearns.org/">https://eachchildlearns.org/</a> and <a href="https://goifnetwork.org">https://goifnetwork.org</a> Check it out: <a href="https://jgregorymcverry.com/Y3huns3m">https://jgregorymcverry.com/Y3huns3m</a>. Wasn't ontodo list today bucousins invaded house making recording of video fedback impossible. Back to #edu307 and #407 as soon as cousins leave
+ <p class="p-name e-content">Spent 🍅🍅🍅🍅 on drafting an @SCSU #socialjustice grant proposal for <a href="https://eachchildlearns.org/">https://eachchildlearns.org/</a> and <a href="https://goifnetwork.org">https://goifnetwork.org</a> Check it out: <a href="https://jgregorymcverry.com/Y3huns3m">https://jgregorymcverry.com/Y3huns3m</a>. Wasn't on todo list today but cousins invaded house making recording of video fedback impossible. Back to #edu307 and #407 as soon as cousins leave
  </p>
   Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a><a href="https://jgregorymcverry.com/notes/2019-11-27-6" rel="next">Next Note <a href="https://jgregorymcverry.com/notes/2019-11-27-4" rel="prev">Previous Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
