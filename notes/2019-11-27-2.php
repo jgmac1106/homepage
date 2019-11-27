5cb4ac0e43099423d25bd794f677f0334083b043
Greg McVerry <jgregmcverry@gmail.com>
2019-11-27 11:27:59 -0500

    Update 2019-11-27-2.php
    switch out reply link

--- notes/2019-11-27-2.php
+++ notes/2019-11-27-2.php
@@ -7,10 +7,10 @@
   	<div class="note h-entry">
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2019-11-27 10:24:20">27<sup>th</sup>November 2019</time>
- <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/jgmac1106/status/1199715354139865091">My To Do List</a></p>
+ <p>In reply to <a class="u-in-reply-to" href="https://jgregorymcvery.com/todolist#20191127-2">My To Do List</a></p>
  <p class="p-name e-content">Spent 🍅🍅🍅 on drafting a session proposal for the Festival of Learning #FoL2020 ,<a href="https://festival.bccampus.ca/">https://festival.bccampus.ca/</a>
  </p>
-  Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a><a href="https://jgregorymcverry.com/notes/2016-11-27-3" rel="next">Next Note <a href="https://jgregorymcverry.com/notes/2016-11-27-1" rel="prev">Previous Note</a> 
+  Also on<a class="u-syndication" href="https://twitter.com/jgmac1106/status/1199715354139865091"><strong>Twitter</strong></a> <a href="https://jgregorymcverry.com/notes/2016-11-27-3" rel="next">Next Note <a href="https://jgregorymcverry.com/notes/2016-11-27-1" rel="prev">Previous Note</a> 
 </div>
   	</main>
   		<footer>
