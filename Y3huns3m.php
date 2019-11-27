f25c1aa9ea6e5c6619681af778a857c7275ea626
Greg McVerry <jgregmcverry@gmail.com>
2019-11-27 14:48:11 -0500

    Create Y3huns3m.php
    adding article

---
+++ Y3huns3m.php
@@ -0,0 +1,92 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">Y3 hun s3m: Global Learning at Local Scale </h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregory McVerry</a>
+     on <time class="dt-published" datetime="2019-11-27 14:44:20">27<sup>th</sup> Novemner 2019</time></p>
+    
+   
+  <span class="summary">
+    <p class="p-summary"> Creating OER Networks in New Haven and Ghana</p>
+    </span>
+ </div>
+    <div class="copy">
+  <div class="e-content">
+   <h2>Justification</h2>
+   <p>Yε hun sεm or our story in English, drives all our #socialjustice efforts at the Global Open Initiative Foundation (Accra, Ghana)  and Conecpts of Adaptive of Learning (New Haven, CT). We believe in order to recognize the narratives of others you must first own your own. This Social Justice grant propsal bewteen Dr. J Gregory McVerry, GOF, and CFAL, look to co-sponsor two events, one in Ghana and one in New Haven that will network togther into a large global event. This #socialjustice grant of $2,500 will cover event costs in both cities and provide travel to Dr. McVerry to facilitate the event in Ghana.</p> 
+   <p>On Novemner 18th a Southern student asked Dr Bettina Love how we could reduce tensions between the Black community and immigrants and students from Ghana. Dr. Love, in her history of Hip Hop and Creativity approach noted that it was a matter of unknown history. Students from Ghana often come from a level of priviledge and may not understand how African-Americans do not succeeed in the "land of opporunity" We hope by sharing Y3 hun s3m (our story) we can build new global connections and understandings.</p>
+   <p>The Center for Adaptive Learning strives to improve literacy in New Haven with a focus on how we read, write, and participate in a digital era. They have recently begun to host the <a href="elmcitywebmakers.com">Elm City Webmakers</a> where participants learn to own their data, control their digital identities, and make better connections by building their own websites. This program, facilitated by Dr. McVerry has learners of all ages understand that by shaping your story you help to ensure our story gets told.</p>
+<p>Increasing digital literacy is an esssential element of #socialjustice and the best route to acheive this increased literacy is through perosnal websites. We know barriers to access and opportuntities are resulting in a digital divide. We know that popular social media platfroms amplify hate, sexism, and racism. This project seeks a better way forward by rebuilding the web one personal site at a time.</p>
+<h2>Project Description</h2>
+<p>This $2,500 dollar grant will fund the culminating event in our <a href="https://goifnetwork.org/">OER (open educational resources) Ghana project</a>. This will be a two day unconference event in both New Haven, hosted at cFAL or SCSU and in Accra, Ghana hosted by GOIF Foundation. In 2018 Mozilla, the makers of Firefox, recognized Sadik Shadu and Dr. McVerry as fellows in their Open Leaders program. Sadik and Greg are training open leaders in fundrasising, organizing, and teaching skills in order to launch local networks like the Elm City Webmakers and the Global Open Initative. This <a href="http://wiki.goifnetwork.org/OER_Ghana:_Mozilla_Leaders_X_Training_Curriculum">ten week class</a> begins with participants telling their story by building a website, learning and reflecting on something openly, then curating or creating OER, to teach something. Finally participants must go back to their communities and "Do something." This pedagogical model was developed at SCSU in Dr. McVerry's EDU106 classroo;.</p>
+<p>The two day event, in both New Haven and Ghana, will cover the use of wikipedia and building personal websites. In terms of editing wikipedia articles we will have participants in Ghana translating articles about the civil rights movement into Twi, the local dialect spoken. Participants in New Haven will research and learn about artist in Ghana and improve these pages. For the articles translated in Twi we will focus on Emmit Till, Trayvon Martin, the African Diaspora, and civil rights movement. In New Haven we will work closely with GOIF to identify artist to research and write about.</p>
+<p>The second half of the first day will include unconference sessions, where participants develop and facilitate the sessions. These topics will focus on the whys and hows of building personal websites.</p>
+<p>Then on day two particpipants will work on a hackday project and build out or develop their websites. The day will end by doing live deoms of the project. Given the differences in time zones the Ghana team will present remotely to New Haven and the New Haven demos will be recorded for participants to watch in Ghana that night or the next day.</p>
+<p>Wew will utilize all open source tools for this project. Any participant in New Haven and Ghana who attend the event will be given a personal website using the Open Source platform <a href="https://withknown.com">Known</a>. Every local network also gets a private chatroom and a wiki.</p>
+<h3>Stakeholder Responsibilities</h3>
+<p>Dr. J Gregory McVerry will act as the principal investigator of the project. He will be developing the twn week curriculum and planning and facilitating the meet up in Ghana.</p>
+<p>Sadik Shadu will host the bi-weekly online meetings leading up to the event. This will include time for event planning, leadership training for participants, and time to work on your websites. Sadik will handle alllogistic planning for the two day event.</p>
+<p>Jennifer Ricker, Executive Director of CfAL, will coordinate the local event in New haven. She, and the CfAL team, in coordination with Dr. McVerry, will host bimonthly IndieWe meet ups to build personal websites. She will also plan and handle all logistics for the New Haven event.</p>
+<h3>Timeline</h3>
+<ul>
+  <li>December</li>l
+  <ul>
+    <li>
+    Host montly GOIF meeting-Sadik
+</li>
+<li>Plan Elm City Webmaker meetups-Greg and Jennifer</li>
+<li>Host Elm City Webmaker meetups-Greg and Jennifer</li>
+<li>Apply for other microgrants for sustainability-Greg</li>
+<li>Develop Open Leaders curriculum-Greg</li>
+<li>Set up infrastructure for CfAL server-Greg</li>
+</ul>
+<li>January</li>
+<ul>
+  <li>Begin 10 week Open Leaders Program-Sadik and Greg</li>
+   <li>
+    Host montly GOIF meeting-Sadik
+</li>
+<li>Host Elm City Webmaker meetups-Greg and Jennifer</li>
+  </ul>
+  <li>February</li>
+  <ul>
+    <li>Weeks 5-9 of Open Leaders Training-Sadik</li>
+    <li>Plan cummulative Weekend meet ups in Ghana and New Haven</li>
+    <li>
+    Host montly GOIF meeting-Sadik
+</li>
+<li>Host Elm City Webmaker meetups-Greg and Jennifer</li>
+  </ul>
+  <li>March</li>
+  <ul>
+    <li>Host IndieWebCamp,  New Haven-Jennifer</li>
+    <li>Host IndieWebCamp, Accra, Greg and Sadik-Jennifer
+  </ul>
+</ul>
+<h3>Budget $2,500</h3>
+<ul>
+  <li>$800 Food and Space Rental in Ghans</li>
+  <li>$300 for Food in New Haven</li>
+  <li>$160 space rental for technology room in Accra for meet ups. GOIF must cover the cost of security of $20 per event and two events per month</li>
+  <li>$1,000 travel for Dr. McVerry to facilitate event in Accra, Ghana</li>
+  <li>$320 Cost of hosting and server management for CfAL</li>
+</ul>
+<h3>Deliverables</h3>
+<p>We will measure success of the program by the number of attendees, the number of edits to wikipedia, the number of personal websites laucnhed, and number of OER resources added to the wiki. We will check these variables at three different time points. Immediately following the weekend. Then at the end of May and finally at the end of June. We will also track participantion numbers in the local bimontly meet ups in Ghana and  New Haven. The use of open source tools and the wiki to track our materials will also provide a long history of contributions that can be analyzed.</p>
+    
+  
+
+  </div>
+      </div>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
