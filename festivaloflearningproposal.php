631ade5092122b224e9e51750f4de4c436dbcee0
Greg McVerry <jgregmcverry@gmail.com>
2019-11-30 06:59:32 -0500

    Update festivaloflearningproposal.php
    adding final draft

--- festivaloflearningproposal.php
+++ festivaloflearningproposal.php
@@ -4,48 +4,31 @@
     </header>
   <main class="longreads">   
   <article class="h-entry">
-  <h1 class="p-name">Draft of Possible Festival of Learning session</h1>
+  <h1 class="p-name">Striving for Remote Parity: Reducing the Carbon Crater Left By Academic Conferences</h1>
   <div class="pubinfo">
-  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregory McVerry</a>
-     on <time class="dt-published" datetime="2019-11-27 10:10:20">25<sup>th</sup> Novemner 2019</time></p>
-    
+  <p>Published by <a class="p-author h-card" href="https://chendricks.org/">Christina Hendricks</a>, <a class="p-author h-card" href="https://autummcaines.com/">Autumm Caines</a>, and  <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregory McVerry</a>
+     on <time class="dt-published" datetime="2019-11-30 06:54:20">30<sup>th</sup> Novemember 2019</time></p>
+    <small>Featured Image: Remix of <a title="Carbon footprint" href="https://flickr.com/photos/wheatfields/3102519174">Carbon footprint</a> flickr photo by <a href="https://flickr.com/people/wheatfields">allispossible.org.uk</a> shared under a <a href="https://creativecommons.org/licenses/by/2.0/">Creative Commons (BY) license</a> </small>
    
   <span class="summary">
-    <p class="p-summary">How Can We Reduce the Carbon Impact of Academic Conferencres?</p>
+    <p class="p-summary">How Can We Reduce the Carbon Impact of Academic Conferences?</p>
     </span>
  </div>
+ <img class="featured u-featured" src="/photos/conferenceclimate.png">
     <div class="copy">
   <div class="e-content">
-    <p>Please note this draft represents the ideas of only one of the presenters. Other authors have not had chance to offer ideas or shape proposal. This will change before submission.</p>
-   <h2>Striving for Remote Parity: Reducing the Carbon Crater Left By Academic Conferences</h2>
-<p>As our world burns and cities of the indigenous sink many scholars around the world seek to lower their impact on the climate crisis. Our biggest carbon sink fills as we seek out conferences. 1,000s of academics with the privilege and power to travel descend on cities through air, rail, and car. Sales reps pitch consumption in vendor halls awash in swag.. All to present a paper for 20 minutes. Is there a better way forward?</p>
-<p>This session examines three case studies of alternative conference models that can drastically reduce the carbon impact of conferences while also embracing open pedagogies; increasing the knowledge growth of participants; and providing access to learning to contingent faculty, marginalized peoples, and other less privileged learners.</p>
-<p>We will begin with a brief introduction of the impact academic conferences have on our carbon footprint. Then we will meet in focus groups with both local and remote participants and discuss the history, benefits, and problems of large scale conferences. Using the following discussion prompts:</p>
+    
+<p>As our world burns, many scholars around the world seek to lower their impact on the climate crisis. One of our biggest carbon sinks fills as we seek out conferences. Thousands of academics with the privilege and power to travel descend on cities through air, rail, and car, with each conference trip &ldquo;amounting to 7% of an average individuals annual C02 emissions&rdquo; according to one study (Spinellis and Louridas, 2013). Sales reps pitch consumption in vendor halls awash in wasteful swag. Is there a better way forward?</p>
+<p>This session examines case studies of alternative conference models that can drastically reduce the carbon impact of conferences while also embracing open pedagogies and providing greater access to those for whom long-distance travel to conferences is difficult or impossible. The session will itself model a hybrid approach to conference sessions, with some facilitators and participants attending onsite and some online.</p>
+<p>We will begin with a brief introduction of the impact academic conferences have on our carbon footprint. Then the facilitators will present case studies of alternative conference models, such as <a href="https://www.google.com/url?q=https://pressedconf.org/&amp;sa=D&amp;ust=1575117230490000">PressEd conference </a>(takes place entirely on Twitter), <a href="https://www.google.com/url?q=https://indieweb.org/&amp;sa=D&amp;ust=1575117230490000">IndieWebCamp</a>&nbsp;(a global movement of local events), and <a href="https://www.google.com/url?q=http://virtuallyconnecting.org/&amp;sa=D&amp;ust=1575117230490000">Virtually Connecting</a>&nbsp;(a community that connects people at conferences with those not onsite).</p>
+<p>After each facilitator presents, small groups onsite and online (through breakout room in a videoconference) will discuss the benefits and drawbacks of traditional and alternative conference models around topics such as:</p>
 <ul>
-<li>What is the history behind academic conferences?</li>
-<li>What are the benefits of large scale gatherings?</li>
-<li>What are the problems with conferences?</li>
-<li>Where does power lie and how does it get reinforced on conference circuit?</li>
+<li>Equity, diversity, inclusion, accessibility</li>
+<li>Learning from people outside one&rsquo;s local network</li>
+<li>Open practices</li>
+<li>Costs (in terms of money and time/effort; both to host and participate)</li>
 </ul>
-<p>Next the presenters will meet with a group and present their case study. They will discuss (these may change between now and submission):</p>
-<ul>
-<li>PressEdConf an online conference conducted on Twitter</li>
-<li>Virtually Connecting: A community bringing online discussions to conferences</li>
-<li>IndieWebCamp: Global movement of local events to encourage people to build websites</li>
-</ul>
-<p>After each facilitator presents, the groups will dissolve and reform using jigsaw methods. The new groups will then be given a matrix to find common patterns across the different case studies.</p>
-<p>This will include but is not limited to:</p>
-<ul>
-<li>Keynote Speakers</li>
-<li>Vendors</li>
-<li>Food</li>
-<li>Travel</li>
-<li>Networked Technologies</li>
-<li>Diversity and Inclusion</li>
-<li>Open Practices</li>
-<li>Impact on Learning</li>
-</ul>
-<p>We will then reconvene as a whole group and brainstorm a set of tips we could offer our colleagues who help to organize academic conferences.</p>
+<p>We will then ask participants to reflect on and discuss in their small groups concrete suggestions they can give to people who organize specific conferences they are involved in (so participants will come away with a specific action item). At the end of the session we will reconvene as a large group and ask a few people to share insights and suggestions from their small group discussions.</p>
   </div>
       </div>
 </article>
