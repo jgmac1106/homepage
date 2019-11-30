967ccc6a172f308b1a9c99ad8f61001f03691221
Greg McVerry <jgregmcverry@gmail.com>
2019-11-30 10:21:31 -0500

    Update pagenotfound.php
    addig directions

--- pagenotfound.php
+++ pagenotfound.php
@@ -5,7 +5,7 @@
   <main class="longreads">  
   <div class="img404"> 
  <img  src="/photos/assets/404htcashortrade.jpg" alt=""></div>
- <h2>You may want to check my <a href="https://archive.jgregorymcverry,com">Archive</a></h2> 
+ <h2>You may want to check my <a href="https://archive.jgregorymcverry.com">Archive</a> or just add archive in front of the url you were looking for like https://archive.jgregorymcverry.com/</h2> 
  <small>Image from <a href="https://cashortrade.org">CashorTrade.org</a></small>   </main>
     <footer>
 <?php include 'footer.php'; ?>
