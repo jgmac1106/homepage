d9f31cd70fe9def029bb93a9b3728c531cbff5ed
Greg McVerry <jgregmcverry@gmail.com>
2019-11-30 09:14:44 -0500

    adding 404 page
    adding 404 page

---
+++ pagenotfound.php
@@ -0,0 +1,15 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">  
+  <div class="img404"> 
+ <img  src="/photos/assets/404htcashortrade.jpg" alt=""></div>
+ <h2>You may want to check my <a href="https://archive.jgregorymcverry,com">Archive</a></h2> 
+ <small>Image from <a href="https://cashortrade.org">CashorTrade.org</a></small>   </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
