4442c25ddaca3cb7d7372c4fded0255922820b06
Greg McVerry <jgregmcverry@gmail.com>
2019-12-03 10:27:46 -0500

    adding note
    adding note

--- notes/2019-12-03-3.php
+++ notes/2019-12-03-3.php
@@ -7,7 +7,7 @@
   	<div class="note h-entry">
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2019-11-27 10:24:20">27<sup>th</sup>November 2019</time>
- <p>In reply to <a class="u-in-reply-to" href="https://jgregorymcverry.com/notes/2019-12-03-1">My To Do List</a></p>
+ <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/jgmac1106/status/1201878197358473216">My To Do List</a></p>
  <p class="p-name e-content">Hey #edu307 #edu307Fall19 #edu407Fall19 hoping in 🚗 now on way to @scsu will be late for office hours. Will open a Zoom room as soon as I get in for any virtual meetings. Starting day recording video feedback for #EDU407Fall19</p>
   Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a><a href="https://jgregorymcverry.com/notes/2019-12-03-2" rel="prev">Prev Note</a>a> <a href="https://jgregorymcverry.com/notes/2019-12-03-4" rel="next">Next Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
