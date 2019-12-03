b5c48d4e91333c5c9a82e778bf7f04d836e55e3f
Greg McVerry <jgregmcverry@gmail.com>
2019-12-03 09:51:57 -0500

    Create 2019-12-03-2.php
    adding note

---
+++ notes/2019-12-03-2.php
@@ -0,0 +1,21 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-11-27 10:24:20">27<sup>th</sup>November 2019</time>
+ <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/jgmac1106/status/1201825287849304064">My To Do List</a></p>
+ <p class="p-name e-content">Spent 🍅🍅🍅🍅 shoveling snow. Bit mroe than expected. Now to head off to @scsu. Kids home from school. Gonna make a mess of today's to do list.
+ </p>
+  Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a><a href="https://jgregorymcverry.com/notes/2019-12-03-1" rel="prev">Prev Note</a>a> <a href="https://jgregorymcverry.com/notes/2019-12-03-3" rel="next">Next Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
