de65cdae60aca39533601d82a78894cf83e9e784
Greg McVerry <jgregmcverry@gmail.com>
2019-12-04 09:15:43 -0500

    Update 2019-12-04-1.php
    fixing missing closng tag

--- notes/2019-12-04-1.php
+++ notes/2019-12-04-1.php
@@ -8,7 +8,7 @@
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2019-12-04 09:14:20">4<sup>th</sup>December 2019</time>
  <p class="p-name e-content">Publishing today's @scsu to-do list. Hope to make up some time lost to the weather yesterday. <a href="https://jgregorymcverry.com/tod0olist#20191203">https://jgregorymcverry.com/tod0olist#20191203</a></p>
-  Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2019-12-03-3" rel="prev">Previous Note</a> <a href="https://jgregorymcverry.com/notes/2019-12-04-2" rel="next">Next Note  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2019-12-03-3" rel="prev">Previous Note</a> <a href="https://jgregorymcverry.com/notes/2019-12-04-2" rel="next">Next Note </a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
   	</main>
   		<footer>
