01bb78b1552a0265cb2f1a343af224461ab7b46a
Greg McVerry <jgregmcverry@gmail.com>
2019-12-04 09:20:51 -0500

    Update 2019-12-04-1.php
    adding note

--- notes/2019-12-04-1.php
+++ notes/2019-12-04-1.php
@@ -6,9 +6,18 @@
   	<div class="notewrapper">
   	<div class="note h-entry">
 <a class="u-author" href="/"></a>
-<time class="dt-published" datetime="2019-12-04 09:14:20">4<sup>th</sup>December 2019</time>
- <p class="p-name e-content">Publishing today's @scsu to-do list. Hope to make up some time lost to the weather yesterday. <a href="https://jgregorymcverry.com/tod0olist#20191203">https://jgregorymcverry.com/tod0olist#20191203</a></p>
-  Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2019-12-03-3" rel="prev">Previous Note</a> <a href="https://jgregorymcverry.com/notes/2019-12-04-2" rel="next">Next Note </a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+<time class="dt-published" datetime="2019-12-04 09:24:20">4<sup>th</sup>December 2019</time>
+ <div class="p-name e-content"> <p>Here are the times for #edu307 book club meert ups this week #EDU307Fall19</p>
+ 	<ul>
+ 		<li>Wednesday 12:00-13:00 f2f or onlien Davis 118</li>
+ 		<li>Thursday 6:00-6:40(ish). online</li>
+ 		<li> Thurdsay 12:00-13:00</li>
+ 		<li>Thursday 20:30-21:30</li>
+ 		<li>Friday 12:00-13:00</li>
+ 		<li>Saturday 7:00-8:00</li>
+ 		<li>Sunday 20:30-21:30</li>
+ 	</ul></div>
+  Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2019-12-04-1" rel="prev">Previous Note</a> <a href="https://jgregorymcverry.com/notes/2019-12-04-3" rel="next">Next Note </a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
   	</main>
   		<footer>
