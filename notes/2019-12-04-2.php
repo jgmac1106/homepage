07125ec8fd0f6aebe3a80a16ec5a1bc0d51d4c59
Greg McVerry <jgregmcverry@gmail.com>
2019-12-04 09:52:19 -0500

    Create 2019-12-04-2.php
    add note

---
+++ notes/2019-12-04-2.php
@@ -0,0 +1,19 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-12-04 09:44:20">3<sup>rd</sup>December 2019</time>
+ <p class="p-name e-content">Publishing today's @scsu to-do list. Hoping to make up time for the weather yesterday.  <a href="https://jgregorymcverry.com/tod0olist#20191204">https://jgregorymcverry.com/tod0olist#20191204</a></p>
+  Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2019-12-04-1" rel="prev">Previous Note</a> <a href="https://jgregorymcverry.com/notes/2019-12-04-3" rel="next">Next Note  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
