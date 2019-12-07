8365071967c5ec3c89c23153547bb3e94343abdc
Greg McVerry <jgregmcverry@gmail.com>
2019-12-07 14:40:51 -0500

    Create 2019-12-07-1.php
    adding video note

---
+++ notes/2019-12-07-1.php
@@ -0,0 +1,29 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-12-07T16:34:20+0000">December 7th  2019 16:34</time>
+          <video  controls class="u-video" src="https://jgregorymcverry.com/videos/mindsweepers.mp4"> 
+          </video>
+          
+      
+  
+      
+        <p class="e-content">Congrats to the Mindsweepers who had an amazing showing at the First Lego League Connecticut State Championship</p>
+      
+ 
+  <a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a><br><a href="https://jgregorymcverry.com/notes/2019-12-05-1" rel="prev">Previous Note</a> <a href="#" rel="next">Next Note </a>
+  <a class="u-syndication" href="https://brid.gy/about#webmentions">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
