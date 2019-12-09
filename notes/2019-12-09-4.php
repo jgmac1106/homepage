6b20c57fc621581acc81ce2df64d7b0f40c29495
Greg McVerry <jgregmcverry@gmail.com>
2019-12-09 11:28:20 -0500

    adding notes
    adding notes

---
+++ notes/2019-12-09-4.php
@@ -0,0 +1,29 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-12-09T11:24:20+0000">December 9th  2019 11:24</time>
+          <video  controls class="u-video" src="https://jgregorymcverry.com/videos/7MinuteTeachingTipAnalyzingGoogleForms.mp4"> 
+          </video>
+          
+      
+  
+      
+        <p class="e-content">Hey #edu106 here is a quick video I made on how to solve for descriptive statistics using Google Forms</p>
+      
+ 
+  <p>Also On<a class="u-url url" href="https://jgregorymcverry.com/notes/2019-12-09-4" rel="canonical"><strong>My Notes</strong></a> <br><a href="https://jgregorymcverry.com/notes/2019-12-09-3" rel="prev">Previous Note</a> <a href="https://jgregorymcverry.com/notes/2019-12-09-5" rel="next">Next Note </a>
+  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
