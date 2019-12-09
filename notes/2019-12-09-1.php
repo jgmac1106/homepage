50d3431deb76796ea5d81cf3a68633f814e08eae
Greg McVerry <jgregmcverry@gmail.com>
2019-12-09 11:06:49 -0500

    adding notes
    adding notes

---
+++ notes/2019-12-09-1.php
@@ -0,0 +1,18 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-12-09 10:44:20">8<sup>th</sup>December 2019</time>
+ <p class="p-name e-content">Published today's @scsu to do list. Week different on campus M, W, RF. Avaiable remote T and R. <a href="https://jgregorymcverry.com/todolist#20191209">https://jgregorymcverry.com/todolist#20191209<a></a><p> Also on <a href="https://jgregorymcverry.com/notes/2019-12-08-1"rel="canonical"><strong>My Notes</strong></a><br><a href="https://jgregorymcverry.com/notes/2019-12-07-2" rel="prev">Previous Note</a> <a href="#" rel="next">Next Note </a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
