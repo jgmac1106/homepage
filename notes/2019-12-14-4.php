b456b14a87cbcc989d47a9a4d9757c1ff5c662db
Greg McVerry <jgregmcverry@gmail.com>
2019-12-14 08:00:04 -0500

    Create 2019-12-14-4.php
    add note

---
+++ notes/2019-12-14-4.php
@@ -0,0 +1,34 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="note2">
+      <a class="u-author" href="/"></a>
+
+        
+  <p><time class="dt-published" datetime="2019-12-14 08:04:20">14<sup>th</sup>December 2019</time></p>
+      
+      
+       <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/jgmac1106/status/1205834016420745217">@jgmac1106</a> </p>
+        <img   class="u-photo" src="https://media.giphy.com/media/b55x0VFpFKm7S/giphy.gif" alt="writing is hard"> 
+        
+          
+           
+          
+
+<p class="p-name e-content">Then as the next semester peaks around the corner and we need to pivot to syllabi we simply conclude...</p>
+     
+ 
+<a class="u-url url" href="https://jgregorymcverry.com/notes/2019-12-14-4" rel="canonical"><strong>My Notes</strong></a><br><a href="https://jgregorymcverry.com/notes/2019-12-14-1" rel="prev">Previous Note</a> <a href="https://jgregorymcverry.com/notes/2019-12-14-5" rel="next">Next Note </a>
+  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
