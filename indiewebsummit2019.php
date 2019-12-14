72cfd9ec21c91faa56f68916ffc6ac2f0f905018
Greg McVerry <jgregmcverry@gmail.com>
2019-12-14 07:17:28 -0500

    add notes
    add notes

---
+++ indiewebsummit2019.php
@@ -0,0 +1,30 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">My IndieWeb Summit Trip</h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregory McVerry</a>
+     on <time class="dt-published" datetime="2019-11-25 18:14:20">25<sup>th</sup> Novemner 2019</time></p>
+    <p>
+      Updated <time class="dt-updated" datetime="2019-11-25 18:14:20">25<sup>th</sup> July 2019</time></p>
+   
+  <span class="summary">
+    <p class="p-summary">Messing About with GPX</p>
+    </span>
+ </div>
+    <div class="copy">
+  <div class="e-content">
+    <iframe style="90%" src="https://jgregorymcverry.com/music/index.php/apps/gpxpod/publicFile?token=MRk02LafhzziEqo&path=%2F&filename=2019-06-27-17-04-00.gpx&autopopup=y&autozoom=y&showchart=y&tableutd=y&layer=OpenStreetMap&overlay=&displaymarkers=y&showpics=y&transp=n&lineborders=y&simplehover=n&rteaswpt=n&arrow=n&sidebar=1&lineweight=5&color=none&picstyle=p&tooltipstyle=h&draw=tw&waystyle=Pin%2C%20Blue&unit=metric"></iframe>
+  </div>
+      </div>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
