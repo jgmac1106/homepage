228771c10922a3ba74c505402c9b66b6a59d90c6
Greg McVerry <jgregmcverry@gmail.com>
2019-12-14 12:27:36 -0500

    Update 2019-12-14-3.php
    fix url

--- notes/2019-12-14-3.php
+++ notes/2019-12-14-3.php
@@ -21,7 +21,7 @@
 <p class="p-name e-content">By the third week..we try to breath and think....</p>
      
  
-<a class="u-url url" href="https://jgregorymcverry.com/notes/2019-12-14-2" rel="canonical"><strong>My Notes</strong></a><br><a href="https://jgregorymcverry.com/notes/2019-12-14-3" rel="prev">Previous Note</a> <a href="https://jgregorymcverry.com/notes/2019-12-14-4" rel="next">Next Note </a>
+<a class="u-url url" href="https://jgregorymcverry.com/notes/2019-12-14-2" rel="canonical"><strong>My Notes</strong></a><br><a href="https://jgregorymcverry.com/notes/2019-12-14-2" rel="prev">Previous Note</a> <a href="https://jgregorymcverry.com/notes/2019-12-14-4" rel="next">Next Note </a>
   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
 </div>
