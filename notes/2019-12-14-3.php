e843e974c4e4d3df62a57d78a677073f4024d8eb
Greg McVerry <jgregmcverry@gmail.com>
2019-12-14 07:53:39 -0500

    Create 2019-12-14-3.php
    add note

---
+++ notes/2019-12-14-3.php
@@ -0,0 +1,34 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="note2">
+      <a class="u-author" href="/"></a>
+
+        
+  <p><time class="dt-published" datetime="2019-12-14 07:54:20">14<sup>th</sup>December 2019</time></p>
+      
+      
+       <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/jgmac1106/status/1205830427338035201">@jgmac1106</a> </p>
+      <img   class="u-photo" src="https://media.giphy.com/media/SvKmv2uvUtEOc/giphy.gif" alt="get better at writing"> 
+        
+          
+           
+          
+
+<p class="p-name e-content">By the third week..we try to breath and think....</p>
+     
+ 
+<a class="u-url url" href="https://jgregorymcverry.com/notes/2019-12-14-2" rel="canonical"><strong>My Notes</strong></a><br><a href="https://jgregorymcverry.com/notes/2019-12-14-1" rel="prev">Previous Note</a> <a href="https://jgregorymcverry.com/notes/2019-12-14-4" rel="next">Next Note </a>
+  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
