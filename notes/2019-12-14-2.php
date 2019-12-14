ef0b217436a1d9374ac3c22113b38c007a8d7093
Greg McVerry <jgregmcverry@gmail.com>
2019-12-14 07:49:13 -0500

    add note
    add note

---
+++ notes/2019-12-14-2.php
@@ -0,0 +1,34 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="note2">
+      <a class="u-author" href="/"></a>
+
+        
+  <p><time class="dt-published" datetime="2019-12-14 07:44:20">14<sup>th</sup>December 2019</time></p>
+      
+      
+       <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/jgmac1106/status/1205830427338035201">@jgmac1106</a> </p>
+       <img   class="u-photo" src="https://media.giphy.com/media/lRwiAPgpdNYmk/giphy.gif" alt="angry writing"> 
+        
+          
+           
+          
+
+<p class="p-name e-content">Then we get all like...</p>
+     
+ 
+<a class="u-url url" href="https://jgregorymcverry.com/notes/2019-12-14-1" rel="canonical"><strong>My Notes</strong></a><br><a href="https://jgregorymcverry.com/notes/2019-12-14-1" rel="prev">Previous Note</a> <a href="https://jgregorymcverry.com/notes/2019-12-14-3" rel="next">Next Note </a>
+  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
