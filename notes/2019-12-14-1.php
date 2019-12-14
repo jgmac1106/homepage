d3c81f56d384580582633d4b5988add55b9182ca
Greg McVerry <jgregmcverry@gmail.com>
2019-12-14 07:39:59 -0500

    Update 2019-12-14-1.php
    fix notes

--- notes/2019-12-14-1.php
+++ notes/2019-12-14-1.php
@@ -21,7 +21,7 @@
 <p class="p-name e-content">How your professors spend their break</p>
      
  
-<a class="u-url url" href="https://jgregorymcverry.com/notes/2019-12-14-1"> "rel="canonical"><strong>My Notes</strong></a><br><a href="https://jgregorymcverry.com/notes/2019-12-09-9" rel="prev">Previous Note</a> <a href="https://jgregorymcverry.com/notes/2019-12-14-2" rel="next">Next Note </a>
+<a class="u-url url" href="https://jgregorymcverry.com/notes/2019-12-14-1" rel="canonical"><strong>My Notes</strong></a><br><a href="https://jgregorymcverry.com/notes/2019-12-09-9" rel="prev">Previous Note</a> <a href="https://jgregorymcverry.com/notes/2019-12-14-2" rel="next">Next Note </a>
   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
 </div>
