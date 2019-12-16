4da694c6061cd1bd88954eae24449582647d62be
Greg McVerry <jgregmcverry@gmail.com>
2019-12-16 13:48:57 -0500

    Create 2019-12-16-1.php
    adding note

---
+++ notes/2019-12-16-1.php
@@ -0,0 +1,23 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-11-21 08:14:20">21<sup>st</sup> November 2019</time></p>
+
+ <p class="p-name e-content">Spent one 🍅🍅🍅🍅 on the ACUE modules for effective online learning and then 🍅🍅  on the @goiffoundation's @MozOpenLeaders curriculum: <a href="https://wiki.goifnetwork.org/OER_Ghana:_Mozilla_Leaders_X_Training_Curriculum">https://wiki.goifnetwork.org/OER_Ghana:_Mozilla_Leaders_X_Training_Curriculum</a> Gonna break for lunch and its back to end of semster feedback.
+ </p> 
+<a class="u-url url" href="https://jgregorymcverry.com/notes/2019-12-14-4" rel="canonical"><strong>My Notes</strong></a><br><a href="https://jgregorymcverry.com/notes/2019-12-14-4" rel="prev">Previous Note</a> <a href="https://jgregorymcverry.com/notes/2019-12-6-2" rel="next">Next Note </a>
+  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
