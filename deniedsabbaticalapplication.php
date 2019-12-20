2c182b7afc7e9567bbdaa6f0ae06a1be2c1fe315
Greg McVerry <jgregmcverry@gmail.com>
2019-12-20 08:51:02 -0500

    Create deniedsabbaticalapplication.php
    adding my denied sabbatical application

---
+++ deniedsabbaticalapplication.php
@@ -0,0 +1,166 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">Elm City Webmakers Sabbatical Application</h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregory McVerry</a>
+     on <time class="dt-published" datetime="2019-12-20 08:04:20">20<sup>th</sup> December 2019</time></p>
+  
+  <span class="summary">
+    <p class="p-summary">Increasing Digital Literacies Across the Greater New Haven Area</p>
+    </span>
+ </div>
+    <div class="copy">
+  <div class="e-content">
+    <div>
+<p><strong>Summary</strong></p>
+<p>The Elm City Webmakers program seeks to improve readiness for online learning in communities that serve marginalized peoples. Specifically, this project will improve the efficacy and agency of participants by connecting their passions to a personal website. We will use a formative design experiment to explore if providing 7-12 students with a website and blog to reflect on their learning prepares them for higher education in a networked society.</p>
+<p><strong>Problem</strong></p>
+<p>Every year students will enroll into higher education for the first time and never step on campus. The increase in online education has been astronomical. Based on recent surveys 33% of all college students will take an online class next year (Allan &amp; Seaman, 2015). Yet the drop out and retention rates are also high (Boston &amp; Ice 2011) especially among students of color, first generation students, and community colleges (Hachey, Wladis &amp; Conway 2013, 23). Students must adjust to the distance, flexibility, interactions and technology (Arbuagh, 2004). Most importantly students need the opportunity to learn how to learn in online environments.</p>
+<p>This sabbatical leave will provide me the opportunity to develop the Elm City Webmaker program whichseeks to provide students with the digital literacies skills they need to thrive as online learners. By reducing the time loss student face when entering the online classroom for the first time we hope to improve persistence and skills required in the modern workforce.</p>
+<p><strong>Overview of Study</strong></p>
+<p>Using a theory of change (see Appendix A) based on the perspective of Intentional Equitable Hospitality (Bali, Caines, A., Hogue, DeWaard, &amp; Friedrich, 2018) Elm City Webmakers provides students an opportunity to increase their flexibility, quality of interactions, and remove the distancing in online education. To accomplish this we focus on building digital literacies and technology self-efficacy skills by teaching students tolearn when learning online as they become bloggers in a networked of connected learners.</p>
+<p>This sabbatical will look to seed digital learning and networking experiences across New Haven schools and cultural organizations that work with school aged children. The research will be co-sponsored by the Concepts of Adaptive Learning Foundation (CfAL). CfAL is a non profit in New Haven set to improve the education of public school children attending under-performing school districts in Connecticut through the use of technology.</p>
+<p>Over the course of six months participants will undergo four curricular modules (See Appendix A for Theory of Change):</p>
+<p>&lt;![if !supportLists]&gt;&middot; &lt;![endif]&gt;Module One, &ldquo;Tell Your Truth&rdquo;-Every participating group and member willdevelop a personal website, blog, and identity</p>
+<p>&lt;![if !supportLists]&gt;&middot; &lt;![endif]&gt;Module Two, &ldquo;Learn Something&rdquo; Every participating group and member will document how they learn knowledge or skill within their chosen passion.</p>
+<p>&lt;![if !supportLists]&gt;&middot; &lt;![endif]&gt;Module Three, &ldquo;Teach Something&rdquo; Every participating group and member will teach knowledge or skill within their chosen passion.</p>
+<p>&lt;![if !supportLists]&gt;&middot; &lt;![endif]&gt;Module Four, &ldquo;Do Something&rdquo; Every participating group and member will engage in an inquiry project to better their selves or the world.</p>
+<p>Through iterative design-based methodologies we will identify pedagogical moves that best support readiness for online learning.</p>
+<p><strong>Innovative Technology</strong></p>
+<p>Our Elm City Webmaker plan will raise success in online learning through the use of innovative technology. Specifically, all participants will be given a blog on the Known platform. This platform uses webmentions to create space for dialogical utterances (Bakhtin, 1970).</p>
+<p>A webmention is a W3, the board that approves web technologies, standard that allows two websites to communicate. If a student writes a post another student can reply to the post from their own website. They do this by publishing a post type called a reply. The webmention is then sent and this new post shows up on the original post as comment. This creates a sense of agency and community from each student&rsquo;s website and is made possible because of microformats.</p>
+<p>Microformats are a type of data that any HTML element can carry. This metadata is used in a variety of tools and existing building blocks to create different post types. Students can post bookmarks, photos, replies, read, listen, and blog posts.</p>
+<p><strong>Population</strong></p>
+<p>The Elm City Webmakers will serve k12 students in the New Haven Area. In New Haven 33% of students graduate on grade level in English and 31% in Math. We need to recenter our educational opportunities on marginalized people. Overall this grant seeks to recruit 30-60 students across to be in the program.</p>
+<p>Recruitment for this program will be done through local clubs and community arts organization by CfAL and Rain of Hope. Driving this program through a student passion rather than through a lens of intervention or &ldquo;overcoming the gap&rdquo; is an essential element of this program. Using snowball methods we will first identify potential club and organization facilitators. We will then choose a third organization for the program with the goal of 5-10students involved with each group (see Appendix A for a Timeline).</p>
+<p><strong>Methods</strong></p>
+<p>This grant will utilize formative experiments (Cobb, Confrey, diSessa, Lehrer, &amp; Schauble, 2003; Reinking &amp; Bradley, 2008) to ask what could be from a mixed-methods perspective. This project will use four phases to meet our goals: 1:) Collect Baseline Data, 2:) Develop Intervention. 3:) Implement Intervention, and 4:) Retrospective Analysis.</p>
+<p>We begin our study with the pedagogical goals of improving the self-efficacy and agency around the use of web technologies found in online learning.</p>
+<p>Specifically we will ask:</p>
+<p>&lt;![if !supportLists]&gt;&middot; &lt;![endif]&gt;How can we can encourage students to openly blog about their passions in order to ensure resiliency in future online endeavors?</p>
+<p>&lt;![if !supportLists]&gt;&middot; &lt;![endif]&gt;Does a community space for networked learning lead to greater self-efficacy with web technologies?</p>
+<p>&lt;![if !supportLists]&gt;&middot; &lt;![endif]&gt;Does a community space for networked learning lead to greater expression of agency and activism?</p>
+<p>A variety of data sources will be used to answer these questions through the four phases of analysis. Quantitative data will include a previously validated study of frequency of internet use and the change in frequency of HTML elements. Participant HTML ability will be used as a proxy measure for overall digital literacies skills. While this does not capture all the complexities of digital literacies it is hypothesized that use of HTML elements correlates strongly and can explain much of the variance in digital literacies models.Inter-class-correlations will be calculated and all assumptions for a growth curve analysis tested. If the number of participants allows for adequate power, a growth curve analysis using repeated measures, after controlling for previous internet use, will be conducted to estimate knowledge growth .</p>
+<p>Blog post and/or website pages will be collected at set intervals. HTML parsers, which can separate pieces of websites based on specific tags, will be used to quantify the use of HMTLand to track changes to websites over time. In a growth curve analysis, the measures are dependent on each other in regards to time, meaning we want improvement based on time. We hope, using a repeated measures ANOVA, controlling for previous internet usethat there is an effect size of .3, when examining the difference in mean scores of the frequencies of HTML elements in our growth curve models.</p>
+<p>We will also develop a reflection based badging system to track digital literacies skills to compensate for using a proxy measure. A series of badges (see Appendix C for example) will be developed from the Liberal Education Program Technology Fluency standards. Participants will submit reflections explaining how different media they create meets the criteria for competency.</p>
+<p>To track network effect, meaning connections between participants, a variety of tools will be used. First a feed of all user websites and comments will be collected. Second, local hashtags will be monitored for frequency of growth. Finally, webmentions will be used to capture whenever a website receives a mention across the web. A webmention is a link between two websites where website A lets website B know, "I linked to your site," and then website A can display that mention. To analyze this data dimensions of tie strength, or how well people and community are connected, will be measured in on three scales trust, enjoying socializing, or discussing important matters (Hampton &amp; Ling, 2013) across a media.</p>
+<p>Qualitative data will include (a) field notes recorded and analyzed(b) teacher logs with observations and reflections about the Elm City Webmakers(c) weekly interviews/debriefings (15-30 minutes); and (d) periodic focus-group interviews with students.This data will be transcribed and By employing &ldquo;a variant of Glaser and Strauss&rsquo;s (1967) &ldquo;constant comparative method&rdquo; (p. 38) we will identify key moments that illustrate Bakhtin&rsquo;s dialogical utterances (Leiman, 2004) that lead to knowledge growth on the technological skills required in Southern Connecticut State University Liberal Education Program Technology Fluency Skills (See Appendix C).</p>
+<p>The qualitative and quantitative data will then be triangulated using thematic network analysis (Attride-Sterling, 2001) in order toengage in what Tashakkori and Teddlie (1998) call &ldquo;theoretical triangulation&rdquo; (p. 41), which will align with our intervention and add to the theory of Intentional Equitable Hospitality. This analysis in turn will help us identify elements of our intervention thateither enhanced or inhibited the pedagogical goal based on our theory of change (see Appendix B).</p>
+<p><strong>Budget And Resources</strong></p>
+<p>The unique partnership between Southern Connecticut State University, CfAL, and Rain of Hope provides the personnel, budget, and supervision to ensure a successful program that will lead to the hypothesized outcomes as outlined in the theory of change (See Appendix A). The web hosting, instructional staff, space, and data analysis required for this project are already secured.</p>
+<p>Each organization will have their own secured shared host that they maintain. This allows each group to protect the privacy and data of their youth members. The cost of the shared hosting will be $200 for two years. This will allow the program to continue a year after the research project gets completed. This cost of $200, provided by the organizations will allow every member to have their own website on a subdomain.Each organization will choose a domain for their network.</p>
+<p>CfAL will provide the space for the interventions and meetings. There will be bi-weekly design studios where students will come and work with me and have instruction based on their needs. The organization have the option to run th program with their own facilitators on the off week.</p>
+<p>I will handle all the data analysis and handle training all the facilitators on the process of data collection. I will also handle training the local CfAL on maintaining and updating the technical shared hosting server.</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p><strong>References</strong></p>
+<p>Allen, I. E., &amp; Seaman, J. (2007). <em>Online nation: Five years of growth in online learning</em>. Sloan Consortium. PO Box 1238, Newburyport, MA 01950.</p>
+<p>Arbaugh, J. B. (2004). Learning to learn online: A study of perceptual changes between multiple online course experiences. <em>The internet and higher education</em>, <em>7</em>(3), 169-182.</p>
+<p>Attride-Stirling, J. (2001). Thematic networks: an analytic tool for qualitative research. <em>Qualitative research</em>, <em>1</em>(3), 385-405.</p>
+<p>Boston, W. E., &amp; Ice, P. (2011). Assessing retention in online learning: An administrative perspective. <em>Online Journal of Distance Learning Administration</em>, <em>14</em>(2).</p>
+<p>Glaser, B., &amp; Strauss, A. (1967). Grounded theory: The discovery of grounded theory. <em>Sociology the journal of the British sociological association</em>, <em>12</em>(1), 27-49.</p>
+<p>Hachey, A., Conway, K., &amp; Wladis, C. (2013). Community colleges and underappreciated assets: Using institutional data to promote success in online learning. <em>Online Journal of Distance Learning Administration</em>, <em>16</em>(1), 1-18.</p>
+<p>Hampton, K. N., &amp; Ling, R. (2013). Explaining communication displacement and large-scale social change in core networks: A cross-national comparison of why bigger is not better and less can mean more. <em>Information, Communication &amp; Society</em>, <em>16</em>(4), 561-589.</p>
+<p>Leiman, M. (2004). Dialogical sequence analysis. <em>The dialogical self in psychotherapy</em>, 255-269.</p>
+<p>Tashakkori, A., Teddlie, C., &amp; Teddlie, C. B. (1998). <em>Mixed methodology: Combining qualitative and quantitative approaches</em> (Vol. 46). Sage.</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p><strong>Appendix A Theory of Change</strong></p>
+
+<img src="/photos/theoryofchange.png" alt="theory of change">
+<p><strong>Appendix B Timeline</strong></p>
+<table>
+<tbody>
+<tr>
+<td>
+<p><strong>December</strong></p>
+<p>Finalize Curriculum</p>
+<p>Sign Memorandum of Understanding with participants</p>
+<p>Meeting between CfAL and Researcher</p>
+<p>&nbsp;</p>
+</td>
+<td>
+<p><strong>January</strong></p>
+<p>Content validation of four modules In the curriculum</p>
+<p>&nbsp;</p>
+</td>
+<td>
+<p><strong>February </strong></p>
+<p>Remote weekly development meetings</p>
+<p>Finalize Social reader service infrastructure</p>
+<p>Recruit participants</p>
+<p>Parent meetings</p>
+<p>Participant outreach</p>
+<p>&nbsp;</p>
+</td>
+</tr>
+<tr>
+<td>
+<p><strong>March</strong></p>
+<p>Create individual sites for users</p>
+<p>Remote weekly development meetings</p>
+<p>Collect Baseline Data</p>
+<p>&nbsp;</p>
+</td>
+<td>
+<p><strong>April</strong></p>
+<p>Begin module one</p>
+<p>User support</p>
+<p>Weekly user site maintenance</p>
+<p>Data analysis</p>
+<p>&nbsp;</p>
+</td>
+<td>
+<p><strong>May</strong></p>
+<p>Begin moduletwo</p>
+<p>User support</p>
+<p>Weekly user site maintenance</p>
+<p>&nbsp;</p>
+</td>
+</tr>
+<tr>
+<td>
+<p><strong>June</strong></p>
+<p>Completemodule two</p>
+<p>User support</p>
+<p>Weekly user site maintenance</p>
+<p>Data analysis</p>
+<p>Face to Face Meeting</p>
+<p>&nbsp;</p>
+</td>
+<td>
+<p><strong>July</strong></p>
+<p>Beginmodule three</p>
+<p>User support</p>
+<p>Weekly user site maintenance</p>
+<p>Data analysis</p>
+<p>&nbsp;</p>
+</td>
+<td>
+<p><strong>August</strong></p>
+<p>Complete module four</p>
+<p>Final data analysis</p>
+<p>&nbsp;</p>
+</td>
+</tr>
+</tbody>
+</table>
+<p><strong>Appendix C</strong></p>
+<p><strong>Example of a Tech Fluency Badge</strong></p>
+<p>&lt;![if !vml]&gt;<img src="/photos/badgeexample.png" alt="graphics wiz badge"/></p>
+</div>
+  </div>
+      </div>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
