ebf32dd900cee4dc3375e4844dbfb020f06704c4
Greg McVerry <jgregmcverry@gmail.com>
2019-12-29 09:43:21 -0500

    Create 2019-12-29-1.php
    add photos

---
+++ notes/2019-12-29-1.php
@@ -0,0 +1,29 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2019-11-10T06:54:20+0000">November 10th 2019 6:54</time>
+          <img   class="u-photo" src="https://jgregorymcverry.com/photos/arts/2019-12-29sheepskinhollow1.jpg" alt="lake through trees"> 
+          <img   class="u-photo" src="https://jgregorymcverry.com/photos/arts/2019-12-29sheepskinhollow2.jpg" alt="closeup of trail marker"> 
+          <img   class="u-photo" src="https://jgregorymcverry.com/photos/arts/2019-12-29sheepskinhollow3.jpg" alt="waterfall over rocks framed by tree"> 
+          <img   class="u-photo" src="https://jgregorymcverry.com/photos/arts/2019-12-29sheepskinhollow4.jpg" alt="clsoe up of moss on rock"> 
+          
+      
+  
+      
+        <p class="e-content">#silentsunday #clmooc</p>
+      
+ Also on<a href="https://jgregorymcverry.com/notes/2019-12-29-1" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2019-12-23-1" rel="prev">Previous Note</a>  <a href="#" rel="next"> Next Note  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
