8f43a1d2b7739f652be3fde2b6f3b62c8d14b1d2
Greg McVerry <jgregmcverry@gmail.com>
2020-01-02 10:43:49 -0500

    Update 2020-01-02indiewebupdate.php
    fixing format

--- 2020-01-02indiewebupdate.php
+++ 2020-01-02indiewebupdate.php
@@ -1,18 +1,25 @@
-<?php include '../head.php'; ?>
+<?php include 'head.php'; ?>
 </head>
- <?php include '../header.php'; ?>
-</header>  
-  <main class="longreads"> 
-  	<div class="h-entry">
-      <a class="u-author" href="/"></a>
-
-        
-  <time class="dt-published" datetime="2020-01-02 10:24:20">2<sup>nd</sup>January 2020</time></p>
-       <p>In reply to <a class="u-in-reply-to" href="https://thoughtshrapnel.com/2020/01/01/microcast-81/">Doug's Episode 81</a>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">Politics of Plumbing: IndieWeb and Federation</h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregory McVerry</a>
+     on <time class="dt-published" datetime="2019-12-19 12:34:20">19<sup>th</sup> December 2019</time></p>
+  <p>In reply to <a class="u-in-reply-to" href="https://thoughtshrapnel.com/2020/01/01/microcast-81/">Doug's Episode 81</a></p>
+  <span class="summary">
+    <p class="p-summary">Exploring Equality and Anarchy in the the IndieWeb Movement</p>
+    </span>
+ </div>
+    <div class="copy">
+ 
+       
       <audio controls src="https://jgregorymcverry.com/podcasts/iwc/2020-01-2IndieWebUpdate.mp3"></audio>
 
- <div class="e-content"><p class="p-name">Politics of Plumbing: IndieWeb and Federation
- </p>
+ <div class="e-content">
+
  <p>Show Notes</p>
 <ul>
   <li><a href="https://thoughtshrapnel.com/2020/01/01/microcast-81/">Doug's microcast</a></li>
@@ -27,7 +34,7 @@
  
 
 </div>
-</div>
+</article>
   	</main>
   		<footer>
      <?php include '../footer.php'; ?>
