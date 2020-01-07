4346fab028ace0edced0cd97a9bcb850cf5e0f76
Greg McVerry <jgregmcverry@gmail.com>
2020-01-07 09:12:21 -0500

    Update 2020-01-07-1.php
    fixing image url

--- notes/2020-01-07-1.php
+++ notes/2020-01-07-1.php
@@ -7,7 +7,7 @@
   	<div class="note h-entry">
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2020-01-07 08:54:20">7<sup>th</sup>January 2019</time>
-<img class="featured u-featured" src="photos/assets/iwc2020header.jpg">
+<img class="u-featured" src="https://jgregorymcverry.com/photos/assets/iwc2020header.jpg">
  <p class="p-name e-content">Hey #literacies, #digped, #DoOO, #femedtech, #oer, and #openpedagogy folks we are organizing @IndieWebCamp Online Feb 8-9th, <a href="https://events.indieweb.org/2020/02/indiewebcamp-online-gUpSVUxbUKhv">https://events.indieweb.org/2020/02/indiewebcamp-online-gUpSVUxbUKhv</a> join us and lets learn together on how to build a better social web. 
  </p>
   Also on<a href="https://jgregorymcverry.com/notes/2020-01-07-1" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-01-02-1" rel="prev">Previous Note</a> <a href="https://jgregorymcverry.com/notes/2020-01-07-2" rel="next">Next Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
