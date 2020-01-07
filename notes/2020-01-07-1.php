0cff13f254e358e95914728f2bccecbae58d1f4a
Greg McVerry <jgregmcverry@gmail.com>
2020-01-07 09:02:37 -0500

    adding a note
    adding a note

---
+++ notes/2020-01-07-1.php
@@ -0,0 +1,21 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-01-07 08:54:20">7<sup>th</sup>January 2019</time>
+<img class="featured u-featured" src="photos/assets/iwc2020header.jpg">
+ <p class="p-name e-content">Hey #literacies, #digped, #DoOO, #femedtech, #oer, and #openpedagogy folks we are organizing @IndieWebCamp Online Feb 8-9th, <a href="https://events.indieweb.org/2020/02/indiewebcamp-online-gUpSVUxbUKhv">https://events.indieweb.org/2020/02/indiewebcamp-online-gUpSVUxbUKhv</a> join us and lets learn together on how to build a better social web. 
+ </p>
+  Also on<a href="https://jgregorymcverry.com/notes/2020-01-07-1" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-01-02-1" rel="prev">Previous Note</a> <a href="https://jgregorymcverry.com/notes/2020-01-07-2" rel="next">Next Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
