7ccea6c98ad88f3d5107526c84c5e25e20e113c5
Greg McVerry <jgregmcverry@gmail.com>
2020-01-07 09:45:40 -0500

    Create 2020-01-07-2.php
    adding note

---
+++ notes/2020-01-07-2.php
@@ -0,0 +1,20 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-01-07 09:94:20">7<sup>th</sup>January 2019</time>
+
+ <p class="p-name e-content">Time to shut down the social media, close all the news sites, and silence notification. Firing up the tomato-timer and tackling the <a href="https://jgregorymcverry.com/todolist#2020-1-05">https://jgregorymcverry.com/todolist#2020-1-05</a>
+  Also on<a href="https://jgregorymcverry.com/notes/2020-01-07-2" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-01-07-1" rel="prev">Previous Note</a> <a href="https://jgregorymcverry.com/notes/2020-01-07-3" rel="next">Next Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
