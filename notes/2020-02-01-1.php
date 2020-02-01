6a773652092fbe7c93e2c514545a3ff739649308
Greg McVerry <jgregmcverry@gmail.com>
2020-02-01 07:59:09 -0500

    Update 2020-02-01-1.php
    remove p-follow-of

--- notes/2020-02-01-1.php
+++ notes/2020-02-01-1.php
@@ -7,7 +7,7 @@
   	<div class="note h-entry">
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2020-02-01 07:44:20">1<sup>st </sup>February</time>
- <div class="p-name e-content"><p> @wkrakower #satchat I just added</p><ul><li class="p-follow-of"><a class="p-name  " href="http://www.billykrakower.com/blog" title=""><img class="u-photo" src="http://www.billykrakower.com/uploads/2/5/9/2/25924437/img-0057_orig.jpgg"  alt=""  title="quest to transform schools into bastions of creativity and wonder" /> Billy Krakower</a> <span class="p-note"> a nationally recognized educator and authore</span></li></ul><p>to the <a href="https://jgregorymcverry.com/following#openpedagogy">#edreform</a> section of my following page. #BeYourOwnSocialNetwork
+ <div class="p-name e-content"><p> @wkrakower #satchat I just added</p><ul><li><a class="p-name" href="http://www.billykrakower.com/blog" title=""><img class="u-photo" src="http://www.billykrakower.com/uploads/2/5/9/2/25924437/img-0057_orig.jpgg"  alt=""  title="quest to transform schools into bastions of creativity and wonder" /> Billy Krakower</a> <span class="p-note"> a nationally recognized educator and authore</span></li></ul><p>to the <a href="https://jgregorymcverry.com/following#openpedagogy">#edreform</a> section of my following page. #BeYourOwnSocialNetwork
  </div>
  Also on<a class="u-url" href="https://jgregorymcverry.com/notes/2020-01-20-1" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-01-30-1" rel="prev">Previous Note</a> <a href="#" rel="next">Next Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
   
