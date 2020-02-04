738b226d37cc161b5b85ec65639091b4855837ab
Greg McVerry <jgregmcverry@gmail.com>
2020-02-04 10:15:18 -0500

    Update 2020-02-04-1.php
    adding link

--- notes/2020-02-04-1.php
+++ notes/2020-02-04-1.php
@@ -10,7 +10,7 @@
 <img class="u-photo" src="https://quickthoughts.jgregorymcverry.com/file/40357041a82647b0a8c37b415a9906f1/thumb.jpg" alt="three attendees standing at event">
 <img class="u-photo" src="https://quickthoughts.jgregorymcverry.com/file/1df70880a79cc6d1a5d882207f948fbc/MVIMG_20200203_154536.jpg" alt="participant working on flyer">
 <img class="u-photo" src="https://quickthoughts.jgregorymcverry.com/file/e54bbdcc567f86ab9be85cf5dc99a2f1/thumb.jpg" alt="participant setting goals for personal website">
- <p class="p-name e-content">The first #IndieWeb Meet Up of the Elm City Webmakers. Participants set goals, worked on graphic design and marketing skills, and played with video editing</p>
+ <p class="p-name e-content">The first #IndieWeb <a href="https://events.indieweb.org/2020/02/elm-city-webmakers-YNEud9CnNn6l">Meet Up</a> of the Elm City Webmakers. Participants set goals, worked on graphic design and marketing skills, and played with video editing</p>
  Also on<a href="https://jgregorymcverry.com/notes/2020-02-04-1" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-02-02-1" rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
   	</main>
