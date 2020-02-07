9e646c14773f07d63a92aa6a4d0a1254a6b6ab50
Greg McVerry <jgregmcverry@gmail.com>
2020-02-07 15:19:38 -0500

    Update installingknownonreclaimhosting.php
    fixing video

--- installingknownonreclaimhosting.php
+++ installingknownonreclaimhosting.php
@@ -15,7 +15,7 @@
     <p class="p-summary">Step By Step Guide to Installing Known</p>
     </span>
  </div>
-   <video class="featured u-video" controls  src="https//jgregorymcverry.com/videos/InstallingKnownOnReclaimHosting.mp4">
+   <video class="featured u-video" controls  src="https://jgregorymcverry.com/videos/InstallingKnownOnReclaimHosting.mp4">
    </video>
     <div class="copy">
 
@@ -24,14 +24,14 @@
       <li>Make a domain or subdomain in CPanel</li>
       <li>Issue a Let's Encrypt Certifcate</li>
       <li>Go back to CPanel and click on All Applications</li>
-      <li>Choose Known and then select your new domain or subdomain. make sure to instal in https</li>li>
+      <li>Choose Known and then select your new domain or subdomain. make sure to install in https</li>li>
       <li>After installation go back to CPanel and chose Terminal</li>
       <li>Change directories direectories to your new domain or subdomain by typing: cd directoryname</li>
-      <li>Copy this line into Terminal. It updates to the latest version: git clone <code>https://github.com/idno/known.git</code></li>
+      <li>Copy this line into Terminal. It updates to the latest version: git clone <pre>https://github.com/idno/known.git</pre></li>
       <li>Go to <a href="https://getcomposer.org/download/"> Get Composer</a></li>
       <li>Copy the code at the top and paste it into Terminal. I can not add the download code here as it changes with new versions of Terminal</li>
-      <li>Then paste this line into Terminal <code>cd known; composer install</code></li>
-      <li>When that is done paste the following line into Terminal to remove a file permissions error: <code>find . -type f | xargs -i{} chmod 644 {}</code></li>
+      <li>Then paste this line into Terminal: <pre>cd known; composer install</pre></li>
+      <li>When that is done paste the following line into Terminal to remove a file permissions error: <pre>find . -type f | xargs -i{} chmod 644 {}</pre></li>
       <li>You are done and your Known site is now up to date</li>
 
      </ul> 
