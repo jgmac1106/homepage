33b44ddebab2b5e4bb40781faa1c5ed9495982e2
Greg McVerry <jgregmcverry@gmail.com>
2020-02-09 08:22:45 -0500

    Create 2020-02-09-1.php
    adding video

---
+++ notes/2020-02-09-1.php
@@ -0,0 +1,31 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-02-09T08:24:20+0000">February 09, 2020</time>
+          <video  controls class="u-video" poster="https://indieweb.org/images/thumb/6/67/indiewebcamp-logo-lockup-color%403x.png/1200px-indiewebcamp-logo-lockup-color%403x.png">
+            <source src="https://jgregorymcverry.com/videos/IWCOnlineDay2Update.mp4" type="video/mp4">
+            
+          </video>
+          
+      
+  
+      
+        <p class="e-content">Hey #IndieWeb here is a quick update from Day 2 of IndieWebCamp Online </p>
+      
+ 
+  <p>Also On<a class="u-url" href="https://jgregorymcverry.com/notes/2019-12-09-2" rel="canonical"><strong>My Notes</strong></a> <br><a href="https://jgregorymcverry.com/notes/2020-02-04-1" rel="prev">Previous Note</a> 
+  
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
