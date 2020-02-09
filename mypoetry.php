8dc3240909a0f315bcdcdffc3edfe9c2d8aa5ca0
Greg McVerry <jgregmcverry@gmail.com>
2020-02-09 18:07:37 -0500

    first attempt at a poetry page
    tried to do initial layout

---
+++ mypoetry.php
@@ -0,0 +1,165 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="poetry">   
+  <div class="grid-container">
+    <div class="h-feed">
+      <div>
+   <h2 class="p-name">Poembox</h2>
+   <p class="p-summary">A boombox of bad poems</p>
+ </div>
+ <div class="h-entry audiopoem">
+  <p class="p-name">(un)Celebration</p>
+  <a class="p-author" href="/"></a>
+  <time class="dt-published" datetime="2020-02-01 13:34:20">01<sup>st</sup> February 2020</time>
+ <audio class="u-audio" src="https://jgregorymcverry.com/poetry/(un)Celebration.m4a" controls="controls">
+</audio>
+</div>
+<div class="h-entry audiopoem">
+  <p class="p-name">(re)Celebration</p>
+  <a class="p-author" href="/"></a>
+  <time class="dt-published" datetime="2020-02-01 13:34:20">01<sup>st</sup> February 2020</time>
+ <audio class="u-audio" src="https://jgregorymcverry.com/poetry/(re)Celebration.m4a" controls="controls">
+</audio>
+</div>
+<div class="h-entry audiopoem">
+  <p class="p-name">Color of Boogie</p>
+  <a class="p-author" href="/"></a>
+  <time class="dt-published" datetime="2020-02-02 12:54:20">2<sup>nd</sup> February 2020</time>
+ <audio class="u-audio" src="https://jgregorymcverry.com/poetry/ColorOfBoogie.m4a" controls="controls">
+</audio>
+</div>
+<div class="h-entry audiopoem">
+  <p class="p-name">Teaching in a Time of Crisis</p>
+  <a class="p-author" href="/"></a>
+  <time class="dt-published" datetime="2020-02-05 10:14:20">5<sup>th</sup> February 2020</time>
+ <audio class="u-audio" src="https://jgregorymcverry.com/poetry/TeachingInTimeofCrisis.m4a" controls="controls">
+</audio>
+</div>
+<div class="h-entry audiopoem">
+  <p class="p-name">Home in Our Heliosphere</p>
+  <a class="p-author" href="/"></a>
+  <time class="dt-published" datetime="2020-02-06 12:54:20">6<sup>th</sup> February 2020</time>
+ <audio class="u-audio" src="https://jgregorymcverry.com/poetry/ColorOfBoogie.m4a" controls="controls">
+</audio>
+</div>
+<div class="h-entry audiopoem">
+  <p class="p-name">Color of Boogie</p>
+  <a class="p-author" href="/"></a>
+  <time class="dt-published" datetime="2020-02-07 09:04:20">7<sup>th</sup> February 2020</time>
+ <audio class="u-audio" src="https://jgregorymcverry.com/poetry/HomeInOurHeliosphere.m4a" controls="controls">
+</audio>
+</div>
+<div class="h-entry audiopoem">
+  <p class="p-name">Ridgeline</p>
+  <a class="p-author" href="/"></a>
+  <time class="dt-published" datetime="2020-02-09 06:14:20">9<sup>th</sup> February 2020</time>
+ <audio class="u-audio" src="https://jgregorymcverry.com/poetry/PoetryAsPlay.m4a" controls="controls">
+</audio>
+</div>
+<div class="h-entry audiopoem">
+  <p class="p-name">Poetry as Play</p>
+  <a class="p-author" href="/"></a>
+  <time class="dt-published" datetime="2020-02-09 06:44:20">9<sup>th</sup> February 2020</time>
+ <audio class="u-audio" src="https://jgregorymcverry.com/poetry/PoetryAsPlay.m4a" controls="controls">
+</audio>
+</div>
+</div>
+<div class="h-feed poetry featured">
+  <div>
+  <p class="p-name">Featured Poems</p>
+  <p class="p-summary">Check out the Highlights</p>
+  <a class="p-author" href="/"></a>
+  <time class="dt-updated" datetime="2020-02-09 06:44:20">9<sup>th</sup> February 2020</time>
+</div>
+<div class="h-entry">
+  <p class="p-name">Untitled Poem</p>
+  <p class="p-summary">A collaborative poem written with Kevin Hogdson every day for 30 Days</p>
+  <div class="e-content">
+    <p class="Greg">
+      Skin plastered against tattered holes<br>
+of a torn blanket.<br>
+Rough threads of <br>
+unwoven time<br>
+scratching against frozen thighs<br>
+She sighs, glancing beyond<br>
+a shoulder tucked close to<br>
+heart<br>
+beyond the cold<br>
+frosted window she see nothing<br>
+nothing at all<br>
+</p>
+<p class="Kevi">
+ Fingers caress<br>
+these holes, torn,<br>
+of this blanket, worn;<br>
+She threads time<br>
+like a shoulder tucked<br>
+close to the heart,<br>
+the start of nothing<br>
+at all<br>
+</p>
+       <p class="Greg">
+      The moon collapsed over horizons split<br>
+shattered space in<br>
+illuminating mysterious truths<br>
+as time melts<br>
+answers hang above broken canopies<br>
+towering conceptions move as the rabbit<br>
+dances<br>
+feet wrapped in dew fed grass<br>
+energy from within<br>
+fleeting realities in a world<br>
+undefined<br>
+<br>
+<a class="u-url" href="/untitledpoem">Read More</a>
+</p>
+  </div>
+  </div>
+</div>
+<div class="smallpoems">
+  <div>
+<p>something</p>
+  </div>
+  </div>
+  <div class="h-feed backstage">
+    <div>
+    <p class="p-name">How it is done</p>
+  <p class="p-summary">Read how poems came to be </p>
+  <a class="p-author" href="/"></a>
+  <time class="dt-updated" datetime="2020-02-09 06:44:20">9<sup>th</sup> February 2020</time>
+</div>
+  <ul>
+    <li class="h-entry"><span class="p-name">Poem name</span> <span class="dt-published"><time class="dt-updated" datetime="2020-02-09 06:44:20">9<sup>th</sup> February 2020</time></span> <span class="p-summary">A description</span> <a class="u-url" href="#">Poem</a></li>
+    <li class="h-entry"><span class="p-name">Poem name</span> <span class="dt-published"><time class="dt-updated" datetime="2020-02-09 06:44:20">9<sup>th</sup> February 2020</time></span> <span class="p-summary">A description</span> <a class="u-url" href="#">Poem</a></li>
+  </ul>
+  </div>
+    <div class="h-feed poetrycircle">
+    <div>
+    <p class="p-name">Poets I rely on</p>
+  <p class="p-summary">Find your inspiration </p>
+  <a class="p-author" href="/"></a>
+  <time class="dt-updated" datetime="2020-02-09 06:44:20">9<sup>th</sup> February 2020</time>
+</div>
+ <?php include 'poetry.php'; ?>
+  </div>
+   <div class="h-feed poemreads">
+    <div>
+    <p class="p-name">Poets I am reading</p>
+  <p class="p-summary">Find your mentor text </p>
+  <a class="p-author" href="/"></a>
+  <time class="dt-updated" datetime="2020-02-09 06:44:20">9<sup>th</sup> February 2020</time>
+</div>
+  <ul>
+    <li class="h-entry"><span class="p-name">Poem name</span> <span class="dt-published"><time class="dt-updated" datetime="2020-02-09 06:44:20">9<sup>th</sup> February 2020</time></span> <span class="p-summary">A description</span> <a class="u-url" href="#">Poem</a></li>
+    <li class="h-entry"><span class="p-name">Poem name</span> <span class="dt-published"><time class="dt-updated" datetime="2020-02-09 06:44:20">9<sup>th</sup> February 2020</time></span> <span class="p-summary">A description</span> <a class="u-url" href="#">Poem</a></li>
+  </ul>
+  </div>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
