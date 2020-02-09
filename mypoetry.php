1a3dd28bbeec9c5f4a9ffb395ab5896b3d93b5bd
Greg McVerry <jgregmcverry@gmail.com>
2020-02-09 18:20:17 -0500

    Update mypoetry.php
    flexbox included in the php files was messing things up

--- mypoetry.php
+++ mypoetry.php
@@ -142,7 +142,15 @@ undefined<br>
   <a class="p-author" href="/"></a>
   <time class="dt-updated" datetime="2020-02-09 06:44:20">9<sup>th</sup> February 2020</time>
 </div>
- <?php include 'poetry.php'; ?>
+<ul><li class="h-card p-follow-of"><a class="p-name  u-url" href="http://dogtrax.edublogs.org/">Kevin Hogdson</a><span clas="p-note">Poet, friend, mentor</span></li>
+<li class="h-card p-follow-of"><a class="p-name  u-url" href="https://soconsider.wordpress.com/" title="CLMOOC Friend">Sheri Edwards</a> <span class="p-note"> CLMOOC Friend</span></li>
+<li class="h-card p-follow-of"><a class="p-name  u-url" href="http://taniasheko.com/" title="Teacher Librarian in Melbourne, Australia">Tania Sheko</a> <span class="p-note"> Teacher Librarian in Melbourne, Australia</span></li>
+<li class="h-card p-follow-of"><a class="p-name  u-url" href="http://tellio.blogspot.com/" title="Philpsopher, poet, farmer">Terry Elliot</a> <span class="p-note"> Philpsopher, poet, farmer</span></li>
+<li class="h-card p-follow-of"><a class="p-name  u-url" href="https://etalesandstories.tumblr.com/" title="Learning Technologist in Alice Springs, Australia">Wendy Taleo</a> <span class="p-note"> Learning Technologist in Alice Springs, Australia</span></li>
+<li class="h-card p-follow-of"><a class="p-name  u-url" href="https://developingwriters.org/" title="blogging about composition in the digital age">Anna Smith</a> <span class="p-note"> blogging about composition in the digital age</span></li>
+<li class="h-card p-follow-of"><a class="p-name  u-url" href="http://www.rhsin.com/" title="the goal is to continue to create things that resonate with the soul.">R.H. Sin</a> <span class="p-note"> the goal is to continue to create things that resonate with the soul.</span></li>
+<li class="h-card p-follow-of"><a class="p-name  u-url" href="https://mastodon.social/tags/smallpoems" title="the small poem collections shared on Mastodon">Mastodon Small Poems</a> <span class="p-note"> the small poem collections shared on Mastodon</span></li>
+</ul>
   </div>
    <div class="h-feed poemreads">
     <div>
