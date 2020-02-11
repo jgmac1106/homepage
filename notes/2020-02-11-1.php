31344b3a8bf68b4e58d1eb06894c50136e9e8dd0
Greg McVerry <jgregmcverry@gmail.com>
2020-02-11 14:35:28 -0500

    Create 2020-02-11-1.php
    adding my iwc online demo

---
+++ notes/2020-02-11-1.php
@@ -0,0 +1,31 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-02-011T16:34:20+0000">February 11, 2020</time>
+          <video  controls class="u-video" poster="https://indieweb.org/images/thumb/6/67/indiewebcamp-logo-lockup-color%403x.png/1200px-indiewebcamp-logo-lockup-color%403x.png" preload="metadata">
+            <source class="u-video" src="https://jgregorymcverry.com/videos/IndieWebCampOnlineDemo.mp4" type="video/mp4">
+               <track label="English" kind="subtitles" srclang="en" src="iwconlinedemo.sbv" default>
+          </video>
+          
+      
+  
+      
+        <p class="e-content">Hey #IndieWeb here is my demo for @IndieWebCamp Online 2020. I made a page for my poetry</p>
+      
+ 
+  <p>Also On<a class="u-url url" href="https://jgregorymcverry.com/notes/2020-02-11-1" rel="canonical"><strong>My Notes</strong></a> <br><a href="https://jgregorymcverry.com/notes/2020-02-09-1" rel="prev">Previous Note</a> 
+  
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
