d5aaa2e28e2466b27b75badb74fd72d0cd9d48a8
Greg McVerry <jgregmcverry@gmail.com>
2020-02-11 20:46:43 -0500

    Create 2020-02-11-2.php
    add note

---
+++ notes/2020-02-11-2.php
@@ -0,0 +1,19 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-02-11 20:44:20">2<sup>nd</sup>February  2019</time>
+ <p class="p-name e-content">Excited to demo my <a href="jgregorymcverry.com/mypoetry">Poem Page</a>at IndieWebCamp 2020</p>
+  Also on<a class="u-url url" href="https://jgregorymcverry.com/notes/2020-02-11-2"rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-02-11-1" rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
