ee212ef74fada27ed21f2a33e9d942a017cfcd70
Greg McVerry <jgregmcverry@gmail.com>
2020-02-13 16:21:08 -0500

    add a note
    adding note

---
+++ notes/2020-02-13-3.php
@@ -0,0 +1,29 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-02-13T16:24:20+0000">February 13, 2020</time>
+       
+
+          
+      
+  
+      
+        <p class="e-content"><p> Hey #edu506 I share some recent #2toPonder episodes where I dicuss the differences between skills, strategies, and content. <a href="https://jgregorymcverry.com/notes/2020-02-13-1">https://jgregorymcverry.com/notes/2020-02-13-1</a></p>
+      
+ 
+  <p>Also On<a class="u-url url" href="https://jgregorymcverry.com/notes/2020-02-13-3" rel="canonical"><strong>My Notes</strong></a> <br><a href="https://jgregorymcverry.com/notes/2020-02-13-2" rel="prev"> Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
