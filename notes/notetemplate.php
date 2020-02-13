dc136930dfcd0f621c0445794d8101b65da1f19a
Greg McVerry <jgregmcverry@gmail.com>
2020-02-13 11:21:02 -0500

    add article
    inertia of art

--- notes/notetemplate.php
+++ notes/notetemplate.php
@@ -7,8 +7,8 @@
   	<div class="note h-entry">
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2019-11-30 08:44:20">30<sup>th</sup>November 2019</time>
- <p class="p-name e-content">Proud to be submitting this proposal on conferences and climate change with @autumm and @chendricksUBC on how we can lower carbon footprint of conferences while striving for better equity  <a href="https://jgmac1106.me/climatecrisis">https://jgmac1106.me/climatecrisis</a> #ncte19 #lra19</p>
-  Also on<a class="u-url url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a><a href="https://jgregorymcverry.com/notes/2019-11-30-2" rel="next">Next Note <a href="https://jgregorymcverry.com/notes/2019-11-27-5" rel="prev">Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+ <p class="p-name e-content"></p>
+  Also on<a class="u-url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a><a href="https://jgregorymcverry.com/notes/2019-11-30-2" rel="next">Next Note <a href="https://jgregorymcverry.com/notes/2019-11-27-5" rel="prev">Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
   	</main>
   		<footer>
