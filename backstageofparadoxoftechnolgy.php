d2b5c69f847d49f8eab8186dccf1f611d8ebf098
Greg McVerry <jgregmcverry@gmail.com>
2020-02-13 12:36:02 -0500

    Update backstageofparadoxoftechnolgy.php
    adding a post

--- backstageofparadoxoftechnolgy.php
+++ backstageofparadoxoftechnolgy.php
@@ -12,7 +12,7 @@
 
     
     <span class="summary">
-    <p class="p-summary">Backstage post of how a poem came to be </p>
+    <p class="p-summary">Backstage post of how the poem Paradox of Technology came to be </p>
     </span>
   </div>
 <img class="featured u-featured" src="https://i1.wp.com/clmooc.com/wp-content/uploads/2020/01/PoetRY_Port_LogoYes-lowres-e1580420665271.png?fit=800%2C800&ssl=1" alt="screenshot of my notes">
@@ -20,11 +20,14 @@
 
   <div class="e-content">
     <img class="u" src="https://jgregorymcverry.com/photos/screenshofCreatePoem.jpg" alt="screenshot of my notes">
-     <p>The first thing I did was write about shoes. A pair of docs as a paradox. What better way then to play with definition then switch meaning on sounds. Not so much a 10 cent word but a paradigm</p>
-     <p>All the while I was thinking hard on wanting to talk about the paradox of design in technology. For decades we have wanted to lower the friction of apps but this in turn has lead to lack of access to technology. Jack Jamieson turned me on to Urulsa Frnklin as we worked on our never to be finsihed Open Pedagogy book. I wanted to play with Franklin's ideas of holitistic and prespr
-     <p>Here is the poem <a href="https://quickthoughts.jgregorymcverry.com/2020/02/11/inertia-of-art-create-poem-for-the-clmooc-poetryport">Inertia of Art</a>
+     <p>The first thing I did was write <a href="https://quickthoughts.jgregorymcverry.com/2020/02/10/poetryport-smallpoems-clmoocfor-first-time-since-8th">about shoes</a>. A pair of docs as a paradox. What better way then to play with definition then switch meaning on sounds. Not so much a 10 cent word but a paradigm</p>
+     <p>All the while I was thinking hard on wanting to talk about the paradox of design in technology. For decades we have wanted to lower the friction of apps but this in turn has lead to lack of access to technology. Jack Jamieson turned me on to Urulsa Frnklin as we worked on our never to be finsihed Open Pedagogy book. I wanted to play with Franklin's ideas of holitistic and prescriptive technology. Ever since I started researching how we teach web credibility I ran into a paradox. Designers constantly want to make sites appear more credibile and teachers want students to be more critical.</p>
+     <p>I believe the best way to deal with this is have students answer, "My url is." Ursula Franklin provides a perfect frame for the difference between a personal website and social media.</p> 
+     <p>I began by first re-reading Franklin. I grabbed words that spoke to me. Then I went on a rhyming word rodeo.</p>
+     <p>I searched for words and then grouped them by family. Next I went through and marked them as a +. - , or nuetral connotation. As I was researching words a few phrases and verses popped in my head. I jotted thos down. Not all made the poem. I didn't play with the metaphor of equinox at all. After I had the connotations and word lists I fired up my Known blog and drafted the poem. I then recorded it. I didn't have my good mic with me. I need to order a mic for the office. </p>
+     <p>Here is the poem <a href="https://quickthoughts.jgregorymcverry.com/2020/02/11/inertia-of-art-create-poem-for-the-clmooc-poetryport">Paradox of Technology</a>
       </div>
-<p><small>Also on <a class="u-syndication" href="https://indieweb.xyz/en/poetry">IndieWeb Poetry</a></small> <small>Also on <a class="u-syndication" href="https://clmoocring.jgregorymcverry.com/clmooc-planet/">CLMOOC Planet</a></small></p>
+<p><small>Also on <a class="u-syndication" href="https://indieweb.xyz/en/poetry">IndieWeb Poetry</a></small> <small>Also on <a class="u-syndication" href="https://clmoocring.jgregorymcverry.com/clmooc-planet/">CLMOOC Planet</a></small><br /><a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a></p>
 </article>
     </main>
     <footer>
