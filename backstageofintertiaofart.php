f819bc8ec46b034e7660754f90af0e1fa248acae
Greg McVerry <jgregmcverry@gmail.com>
2020-02-13 11:37:06 -0500

    Update backstageofintertiaofart.php
    fixing img

--- backstageofintertiaofart.php
+++ backstageofintertiaofart.php
@@ -4,7 +4,7 @@
     </header>
   <main class="longreads">   
   <article class="h-entry">
-  <h1 class="p-name">How I wrote Paradox of Technology: A #Clmooc #poetryport poem</h1>
+  <h1 class="p-name">How I wrote the Inertia of Art: A #Clmooc #poetryport poem</h1>
   <div class="pubinfo">
   <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
      on <time class="dt-published" datetime="2020-005-8 10:54:20">13<sup>th</sup> February 2020</time></p>
@@ -15,10 +15,11 @@
     <p class="p-summary">Backstage post of how a poem came to be </p>
     </span>
   </div>
-<img class="featured u-featured" src="https://jgregorymcverry.com/photos/screenshofCreatePoem.jpg" alt="screenshot of my notes">
+<img class="featured u-featured" src="https://i1.wp.com/clmooc.com/wp-content/uploads/2020/01/PoetRY_Port_LogoYes-lowres-e1580420665271.png?fit=800%2C800&ssl=1" alt="screenshot of my notes">
     <div class="copy">
 
   <div class="e-content">
+    <img class="u" src="https://jgregorymcverry.com/photos/screenshofCreatePoem.jpg" alt="screenshot of my notes">
      <p>Every day the #clmooc crew releases a new word for our <a href="https://clmooc.com/come-sail-into-the-poetry-port/">Poetry Port</a>. On 2020-02-11 the word was create. I really didn't know where to begin. Creation is such a violent process for me. I kick scream and curse. I throw stuff, it's bad, but that's art. I was stuck</p>
      <p>Then on the drive to work I was listening to "Redemption Ark" by Alistair Reynolds. In chapter 15 right arond the seveenteen mark a character explains how intertia works when describing a new intertia supressing drive</p>
      <p>The way Reynolds described a force of matter we all take for granted sounded poetic. Especially this line of "Matter is lazy."</p>
