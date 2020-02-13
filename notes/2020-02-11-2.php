b249472e68da8d3168814a39b69fe770dbaff1d9
Greg McVerry <jgregmcverry@gmail.com>
2020-02-13 14:33:31 -0500

    adding notes
    add rel next to last note, add new note

--- notes/2020-02-11-2.php
+++ notes/2020-02-11-2.php
@@ -8,7 +8,7 @@
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2020-02-11 20:44:20">2<sup>nd</sup>February  2020</time>
  <p class="p-name e-content">Excited to demo my <a href="jgregorymcverry.com/mypoetry">Poem Page</a>at IndieWebCamp 2020</p>
-  Also on<a class="u-url url" href="https://jgregorymcverry.com/notes/2020-02-11-2"rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-02-11-1" rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  Also on<a class="u-url url" href="https://jgregorymcverry.com/notes/2020-02-11-2"rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-02-13-2" rel="next">Next Note</a> <a href="https://jgregorymcverry.com/notes/2020-02-11-1" rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
   	</main>
   		<footer>
