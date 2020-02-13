0b53d9b553c663700e6f2d4444567f66448009ef
Greg McVerry <jgregmcverry@gmail.com>
2020-02-13 11:47:46 -0500

    adding titles
    adding title to summary, start new post

---
+++ backstageofparadoxoftechnolgy.php
@@ -0,0 +1,35 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">How I wrote the Paradox of Technology: A #Clmooc #poetryport poem</h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
+     on <time class="dt-published" datetime="2020-005-8 11:34:20">13<sup>th</sup> February 2020</time></p>
+    <small>Image: Screenshot by me shared under a <a href="https://creativecommons.org/licenses/by-sa/2.0/">Creative Commons (BY-SA) license</a> </small>
+
+    
+    <span class="summary">
+    <p class="p-summary">Backstage post of how a poem came to be </p>
+    </span>
+  </div>
+<img class="featured u-featured" src="https://i1.wp.com/clmooc.com/wp-content/uploads/2020/01/PoetRY_Port_LogoYes-lowres-e1580420665271.png?fit=800%2C800&ssl=1" alt="screenshot of my notes">
+    <div class="copy">
+
+  <div class="e-content">
+    <img class="u" src="https://jgregorymcverry.com/photos/screenshofCreatePoem.jpg" alt="screenshot of my notes">
+     <p>The first thing I did was write about shoes. A pair of docs as a paradox. What better way then to play with definition then switch meaning on sounds. Not so much a 10 cent word but a paradigm</p>
+     <p>All the while I was thinking hard on wanting to talk about the paradox of design in technology. For decades we have wanted to lower the friction of apps but this in turn has lead to lack of access to technology. Jack Jamieson turned me on to Urulsa Frnklin as we worked on our never to be finsihed Open Pedagogy book. I wanted to play with Franklin's ideas of holitistic and prespr
+     <p>Here is the poem <a href="https://quickthoughts.jgregorymcverry.com/2020/02/11/inertia-of-art-create-poem-for-the-clmooc-poetryport">Inertia of Art</a>
+      </div>
+<p><small>Also on <a class="u-syndication" href="https://indieweb.xyz/en/poetry">IndieWeb Poetry</a></small> <small>Also on <a class="u-syndication" href="https://clmoocring.jgregorymcverry.com/clmooc-planet/">CLMOOC Planet</a></small></p>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
