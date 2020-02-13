7ca83e6887e47d3261d0bea655141983aecd325b
Greg McVerry <jgregmcverry@gmail.com>
2020-02-13 13:14:20 -0500

    Create backstageofpeaceforphd.php
    adding backstage post

---
+++ backstageofpeaceforphd.php
@@ -0,0 +1,44 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">How I wrote the Peace for the part time PhD: A #Clmooc #poetryport poem</h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
+     on <time class="dt-published" datetime="2020-005-8 13:14:20">13<sup>th</sup> February 2020</time></p>
+    <small>Image: Screenshot by me shared under a <a href="https://creativecommons.org/licenses/by-sa/2.0/">Creative Commons (BY-SA) license</a> </small>
+
+    
+    <span class="summary">
+    <p class="p-summary">Backstage post of how the poem "Peace for the Part-time PhD" came to be #poetryport </p>
+    </span>
+  </div>
+<img class="featured u-featured" src="https://i1.wp.com/clmooc.com/wp-content/uploads/2020/01/PoetRY_Port_LogoYes-lowres-e1580420665271.png?fit=800%2C800&ssl=1" alt="screenshot of my notes">
+    <div class="copy">
+
+  <div class="e-content">
+   
+     <p>This was gonna be tough. I picked up a gift poem. It was randomnly assigned to Kevin but given the paramters of the poem, writing about finishing a PhD, we both agreed I would make a better match (given I took me from 2006-2013 to finish my PhD).</p>
+     <p>I was asked to write about how to "how to keep going in a part time PhD ..." while using the words="Methodology / epistemology / ontology / Theoretical  Framework / Gap / originality / too many words / not enough words / imposter syndrome / why ? / so what ? ...oh and procrastination" I kept meaning to get back to it but I forgot the word procrastination...Oh BTW I was asked to make the poem to rhyme as much as possible. This was a fun challenge</p>
+     <p> I first began by building my rhyming word bank. This is a new tool in my poet's tool kit (Just heard a truck pull up I think my ryming dictionary may have arrived. Use a paper one...the online ones...leave much to be desired ...got a new thesaurus too...) and I am going to stick with it. The rhyming rodeo sparks many an idea. I</p>
+       <img class="u" src="https://jgregorymcverry.com/photos/peacewordbank.jpg" alt="screenshot of my notes">
+       <p>I then jotted a few first verses. I also decided I was going to try and write the poem using the text structure of a PhD. Have a verse for research question, theoeretical perspectives all the way to the conclusuion. </p>
+       <p>I kept up with my personal learning goal of working on internal near rhymes with assonance. I really want to play with the sounds of the words in my poems to influence the tone and meaning. Encoded some cadence. Next I left for work...Except I couldn't get the poem out of my head. I missed a bunch of my auideobook and I had to relisten on the way home. I like audiobooks, the force me to concentratre, which in turn makes a better driver, a lot of thought can occur between two blips of a blinker.</p>
+       <p>When i got to campus I raced into our department office and jotted down what was stuck in my head. Nothing is worse than forgetting the ppreferct verse. And the once you remember arre never as tender and powerful. You see that where the pen takes off on the page. I then went to my office and sat and worked on the poem. I feel bad for the student sitting next me. Had to be asking, "Why is crazy guy talking to himself and having weird fits?"</p>
+       <img class="u" src="https://jgregorymcverry.com/photos/notesfrompeacetophd.jpg" alt="screenshot of my notes">
+       <p>After class I put on the final touches. I wanted it out early since the person I was gifting the poem to lived in the UK. So as soon as students left I put on the final touches and recorded the poem. Again I hadd to use a crappy internal mic. I think I just need to carry a mic with me everywhere I go during poetry month. I have never really recorded or perfomed poems before #poetryport. I wanted my students to see me taking new risks as a poet. Plus NCTE added a speaking and listening strand and what better tool than poetry?</p>
+
+       
+     <p>Here is the poem <a href="https://quickthoughts.jgregorymcverry.com/2020/02/12/peace-for-the-part-time-phd-a-clmooc-poetry-port-gift">Peace for The part Time PhD</a>
+      </div>
+<p><small>Also on <a class="u-syndication" href="https://indieweb.xyz/en/poetry">IndieWeb Poetry</a></small> <small>Also on <a class="u-syndication" href="https://clmoocring.jgregorymcverry.com/clmooc-planet/">CLMOOC Planet</a></small><br /><a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a></p>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
