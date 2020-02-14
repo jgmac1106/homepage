3b9fbb739b47a0906793ad2584deb32461595aab
Greg McVerry <jgregmcverry@gmail.com>
2020-02-14 12:39:12 -0500

    Create chickeneggorduckofdigital.php
    add a poem

---
+++ chickeneggorduckofdigital.php
@@ -0,0 +1,158 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">Chicked and Egg (Err Duck) of the Digital Dichotomy </h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
+     on <time class="dt-published" datetime="2020-02-14 12:24:20">13<sup>th</sup> February 2020</time></p>
+    <small>Image: Screenshot by me shared under a <a href="https://creativecommons.org/licenses/by-sa/2.0/">Creative Commons (BY-SA) license</a> </small>
+
+    
+    <span class="summary">
+    <p class="p-summary">A #Clmooc #poetryport gift for @miazamoraPhD @cogdog and the #netnarr crew </p>
+    </span>
+  </div>
+<img class="featured u-featured" src="https://i1.wp.com/clmooc.com/wp-content/uploads/2020/01/PoetRY_Port_LogoYes-lowres-e1580420665271.png?fit=800%2C800&ssl=1" alt="screenshot of my notes">
+    <div class="copy">
+
+  <div class="e-content">
+    <video class="u-video" controls src="https://jgregorymcverry.com/videos/ChickenEggDigitalDichotomy.mp4">
+<p>Noone displaces<br/>
+humanity<br/>
+in online<br/>
+spaces<br/>
+For some<br/>
+IRL is<br/>
+My Scared Hell<br/>
+The Digital? A whimsical<br/>
+and critical as<br/>
+pivotal to<br/>
+anything<br/>
+physical.<br/>
+<br/>
+No analog,<br/>
+No offline.<br/>
+Not in the pedandtic nerd<br/>
+sense of that's "not what analog means"<br/>
+but means of making<br/>
+meaning arent't about bleeding<br/>
+into some "better place"<br/>
+Might as well believe in<br/>
+Ducks from Space.<br/>
+<br/>
+Campbell notes,<br/>
+myth a metaphor<br/>
+so is the false dichotomy<br/>
+in front of me<br/>
+that Alice should only choose<br/>
+real doors.<br/>
+<br/>
+Magic in chasing rabbits<br/>
+along paths well worned<br/>
+Fear of Missing Out not new<br/>
+used to sit alone<br/>
+by a phone alone<br/>
+before swiped right<br/>
+all night<br/>
+What is Plato's Cave<br/>
+but Fear Of Moving Out<br/>
+<br/>
+Not a matter of connectivism<br/>
+but self control<br/>
+over capitalism<br/>
+<br/>
+Order the VSCO pack<br/>
+Sit Back<br/>
+Throw in a scrunchie<br/>
+then grab some munchies with the cool<br/>
+crew?<br/>
+That script ain't new.<br/>
+Besides that's not you.<br/>
+You can do better<br/>
+Go Watch Heathers.<br/>
+<br/>
+It is work<br/>
+but it boils down to<br/>
+don't be a jerk.<br/>
+And own your story<br/>
+A Priori<br/>
+Don't gather data on an<br/>
+algorithmic platter<br/>
+Finish the Sentence , "My url is"<br/>
+Save the world<br/>
+<br/>
+Notifications play on the needy<br/>
+in us<br/>
+while feeding the greedy<br/>
+amongst us<br/>
+leaves web filled with racist graffiti<br/>
+and comments seedy<br/>
+we get drawn to the wreck<br/>
+Now it's facebook's paycheck<br/>
+<br/>
+People don't hang hate from their door<br/>
+Less rage on a home page<br/>
+How can you tell truth online until you<br/>
+shape your own? And if not you, then who?<br/>
+<br/>
+Step one<br/>
+Open the phone<br/>
+turn the brain drain beeps to none<br/>
+Be here now<br/>
+keep a purpose<br/>
+converse with those<br/>
+in your time and space<br/>
+in voice or text<br/>
+make the choice<br/>
+of presence<br/>
+<br/>
+Step two<br/>
+Visit silo-quits<br/>
+It's a sick list<br/>
+the worlds<br/>
+tinkers and thinkers<br/>
+who reject the retch<br/>
+of silos, and say Hell no.<br/>
+If you use facebook you pay for hate<br/>
+Instagram funds your misery as you scroll<br/>
+through<br/>
+too perfect, great light, what a night, good fight<br/>
+All attack vectors against are democracy<br/>
+You okay with this? For what?<br/>
+Baby and cat pictures?<br/>
+<br/>
+Seek richer<br/>
+connections from your own<br/>
+Domain. Reject false<br/>
+markers of fame.<br/>
+<br/>
+Step three<br/>
+Find your community<br/>
+Have a passiong, got a blog<br/>
+so do others, gather<br/>
+lather in the seeds of knowledge<br/>
+on the edge of your feed<br/>
+reader<br/>
+be a seeker on mission<br/>
+to network with those who share<br/>
+your vision<br/>
+<br/>
+Step Four<br/>
+know when to reflect<br/>
+to disconnect<br/>
+not from the digital<br/>
+nor the analog<br/>
+but from it all<br/></p>
+      </div>
+<p><small>Also on <a class="u-syndication" href="https://indieweb.xyz/en/poetry">IndieWeb Poetry</a></small> <small>Also on <a class="u-syndication" href="https://clmoocring.jgregorymcverry.com/clmooc-planet/">CLMOOC Planet</a></small><br /><a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a></p>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
