f9a306faa940b3b0c5e5f2b8d481436aed919ad9
Greg McVerry <jgregmcverry@gmail.com>
2020-02-15 14:13:17 -0500

    adding new poem
    adding poem

---
+++ poetryisastretch.php
@@ -0,0 +1,50 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">Poetry is a Stretch</h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
+     on <time class="dt-published" datetime="2020-02-15 14:14:20">13<sup>th</sup> February 2020</time></p>
+     <small>Featured Image: Remix of <a title="tasty world" href="https://flickr.com/photos/67194724@N03/13285083934">tasty world</a> flickr photo by <a href="https://flickr.com/people/67194724@N03">new 1lluminati</a> shared under a <a href="https://creativecommons.org/licenses/by/2.0/">Creative Commons (BY) license</a> </small>
+
+    
+    <span class="summary">
+    <p class="p-summary">Poetry is a Strech: A #clmooc #poetryport poem</p>
+    </span>
+<img class="featured u-featured" src="https://jgregorymcverry.com/photos/screenshofCreatePoem.jpg" alt="screenshot of my notes">
+    <div class="copy">
+
+  <div class="e-content">
+         <div id="crossfade">
+  
+  <img src="/photos/stretchpoem/PoetryisaStretch" alt="Poetry is a stretch">
+    <img src="/photos/stretchpoem/oftruthandtalent" alt="of truth and talent">
+    <img src="/photos/stretchpoem/MindandMetaphor" alt="mind and metaphor">
+    <img src="/photos/stretchpoem/ofloveandpain" alt="love and pain">
+    <img src="/photos/stretchpoem/theTolerantandTimid" alt="the tolerant and timid">
+     <img src="/photos/stretchpoem/vividimagespulledover" alt="vivid images pulled over"> 
+     <img src="/photos/stretchpoem/newvclothsof" alt="new cloths of">
+       <img src="/photos/stretchpoem/truth" alt="truth"> 
+         <img src="/photos/stretchpoem/PoetryisaStretch" alt="Poetry is a stretch">
+              <img src="/photos/stretchpoem/ofmeaning" alt="of meaning">
+                <img src="/photos/stretchpoem/pulledgleaming" alt="pulled gleaming">
+                   <img src="/photos/stretchpoem/fromseasof" alt="from seas of">
+                   <img src="/photos/stretchpoem/starspushed" alt="stars pushed">
+                     <img src="/photos/stretchpoem/oninfinitewaves" alt="on infinite waves">
+                     <img src="/photos/stretchpoem/receddingintostone" alt="recedding into stones">
+                     <img src="/photos/stretchpoem/receddingintostone" alt="of time">
+
+  
+      </div>
+<p><small>Also on <a class="u-syndication" href="https://indieweb.xyz/en/poetry">IndieWeb Poetry</a></small> <small>Also on <a class="u-syndication" href="https://clmoocring.jgregorymcverry.com/clmooc-planet/">CLMOOC Planet</a></small></p>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
