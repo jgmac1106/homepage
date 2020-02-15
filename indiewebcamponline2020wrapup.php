9771657f8e405961f9c5fa8f2f52181e00114eed
Greg McVerry <jgregmcverry@gmail.com>
2020-02-15 11:16:10 -0500

    Create indiewebcamponline2020wrapup.php
    add post

---
+++ indiewebcamponline2020wrapup.php
@@ -0,0 +1,59 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">Announcing IndieWebCamp Online 2020</h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
+     on <time class="dt-published" datetime="2020-005-8 09:44:20">5<sup>th</sup> January 2020</time></p>
+    <small>Featured Image: Remix of <a title="tasty world" href="https://flickr.com/photos/67194724@N03/13285083934">tasty world</a> flickr photo by <a href="https://flickr.com/people/67194724@N03">new 1lluminati</a> shared under a <a href="https://creativecommons.org/licenses/by/2.0/">Creative Commons (BY) license</a> </small>
+
+    
+    <span class="summary">
+    <p class="p-summary">On Feb 8-9th bloggers, hackers, developers, educators, and others will gather for a two day online IndieWebCamp.</p>
+    </span>
+ </div>
+   <img class="featured u-featured" src="photos/assets/iwc2020header.jpg">
+    <div class="copy">
+
+  <div class="e-content">
+    <p>Last week David Shanske and I organized IndieWebCamp Online 2020. We had over fifteen people join us for the camp and hard four wonderful sessions. There were a few hiccups and lessons learned as we try to smoopth out global online conferences that span multiple timezones.</p>
+    <p>On a personal note the start of the conference was hectic for me. Changes on the home front meant my partner needed me to handle haircuts for the boys and basketball practice. I joined the intros from a parking lot of the Salon and sat through the sessions on the floor of a school. That is the flexibility of online conferences. They fit the parenting lifestyle.</p>
+    <h2>Keynotes</h2>
+      <iframe src="https://archive.org/embed/keynotesandintrosiwconline2020" width="640" height="480" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>
+      <p>David Started us off sharing his IndieWeb Journey. His talk was an honest reflection describing how he went from never being a blogger to being a key community organizer in the WordPress community. WordPress is the most popular #IndieWeb platform and David does much of the work himself.</p>
+      <iframe src="https://archive.org/embed/keynotesandintrosiwconline2020?start=1097" width="640" height="480" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>
+      <p>Up next Sadik went over our program in Ghana to use IndieWeb building to help support an OER network. In fact Sadik just spent 14 hours on a bus to organize and facilitate the first IndieWebCamp in Ghana. He and others are gathered for <a href="https://indieweb.org/2020/Navrongo">Art and Feminism + IndieWeb OER Launch 2020 </a>. He shared how we are hoping to train a cohort of people over ten weeks.</p>
+      <iframe src="https://archive.org/embed/keynotesandintrosiwconline2020?start=2700" width="640" height="480" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>
+      <p>Then Vika went to show the CMS she has been creating called kitty box. As Vika put it, It is a place to put all your cat pictures, thus "Kitty Box."
+        <p>This IndiewebCamp we had the unique opportunity to hear from three people in the community who have never given a keynote address. They each got to share a unique expertise.</p>
+        <h2>Sessions</h2>
+
+        <p>We had four sessions this conference. Last year we went with a 24 hour block of time and anyone could schedule a session at anytime. Tis year after feedback we tried to go with a traditional synchronous block. This left out some people in the EU who had evening plans. Not sure the perfect for an online conference that spanned from Vancouver to Russia, through Sofie, Bulgaria and Ghana.</p>
+        <iframe src="https://archive.org/embed/introductiontoindiewebwhybuildingblocksiwconline2020" width="640" height="480" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>
+        <p>In the first session we covered different building blocks. We then pulled back fromt he jargon a little bit and had everyone begin with their "why" for IndieWeb. Then we talked about making the personal choice of balancing code and content.</p>
+        <iframe src="https://archive.org/embed/feedthefeedsiwconline2020" width="640" height="480" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>
+        <p>Next we went over Feeds and feed discovery. I and Chris Aldrich demonstrated how we add a mutliple feeds to different places on our homepage. Most of our audience uses an RSS reader and the expected behaivor is to drop a url in a reader and then choose what feeds to subscribe. Chris won the cake with 32. I only have a paltry 8-9. Though too much choice can be no chice at all. So maybe what we are doing isn't best.</p>
+        <p>We then went over Granary and how many of us use that to generate the XML for a RSS by just using the h-feed on our page. Granary is awesome.</p>
+        <iframe src="https://archive.org/embed/collectionsiwconline2020" width="640" height="480" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>
+        <p>Up next came collections. We discussed how you could think about feeds in something that is not chronological ordser. This matters to me for poetry and others for serialized comics that might be read oldest first.</p>
+        <iframe src="https://archive.org/embed/secretsofwordpressexpertsiwconline2020" width="640" height="480" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>
+        <p>There was a WordPress session but I did not make it because I had to head off to the next TWO basketball games</p>
+        <p>We also forgot to take any pictures all day. That was silly. Note to next year. Take more screenshots
+        <h2>Hack Day</h2>
+        <p>For Hack day I wanted to work on a page for my poetry. I ended up here <a href="https://jgregorymcverry.com/mypoetry">My Poetry</a>. My Grid is still all wrong. It is amazing how fast you fall out of practice. I spent much of last year learning CSS Grid and I knew the basics well. Then I moved to learning a bit of PHP and thenwent content heavy as my semester began. I am making some small errors driving me crazy. I also learned that Granary doesn't yet support fragement links. I filed an issue Anywhoo here was my <a href="https://jgregorymcverry.com/notes/2020-02-11-1">demo</a>
+          <h2>Final Reflections</h2>
+          <p>I want to keep honing the online conference format. Searching for the elusive UTC time is impossible. I prefer goign back to the 12 or 24 session window and just letting people fire up their own sessions if one other person wants to join them. I would keep the keynotes and intros as synchronus as possible. Maybe do some in different time zones if the geographic ticketing data sugegsts people really spread out.</p>
+          <p>Hack day participation fell. Need to think of ways of keeping people engaged. We tried to live demo times but it seemed too late for EU to early for the wWest Coast and then nobody was ready to demo during the second slot. I think we need to come to enjoy demos in process rather than waiting for something shippable. </p>
+  </div>
+      </div>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
