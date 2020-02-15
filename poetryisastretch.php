7ba9efeb14021f2b0bb112c13fffee48edaac4e1
Greg McVerry <jgregmcverry@gmail.com>
2020-02-15 14:26:22 -0500

    Update poetryisastretch.php
    fix spelling mistake

--- poetryisastretch.php
+++ poetryisastretch.php
@@ -1,50 +1,45 @@
 <?php include 'head.php'; ?>
 </head>
 <?php include 'header.php'; ?>
-    </header>
-  <main class="longreads">   
-  <article class="h-entry">
-  <h1 class="p-name">Poetry is a Stretch</h1>
-  <div class="pubinfo">
-  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
-     on <time class="dt-published" datetime="2020-02-15 14:14:20">13<sup>th</sup> February 2020</time></p>
-    
+</header>
+<main class="longreads">
+<article class="h-entry">
+<h1 class="p-name">Poetry is a Stretch</h1>
+<div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a>
+on <time class="dt-published" datetime="2020-02-15 14:14:20">13<sup>th</sup> February 2020</time></p>
 
-    
-    <span class="summary">
-    <p class="p-summary">Poetry is a Strech: A #clmooc #poetryport poem</p>
-    </span>
 
-    <div class="copy">
-
+<span class="summary">
+  <p class="p-summary">Poetry is a Stretch: A #clmooc #poetryport poem</p>
+</span>
+<div class="copy">
   <div class="e-content">
-         <div id="crossfade">
-  
-  <img src="/photos/stretchpoem/PoetryisaStretch" alt="Poetry is a stretch">
-    <img src="/photos/stretchpoem/oftruthandtalent" alt="of truth and talent">
-    <img src="/photos/stretchpoem/MindandMetaphor" alt="mind and metaphor">
-    <img src="/photos/stretchpoem/ofloveandpain" alt="love and pain">
-    <img src="/photos/stretchpoem/theTolerantandTimid" alt="the tolerant and timid">
-     <img src="/photos/stretchpoem/vividimagespulledover" alt="vivid images pulled over"> 
-     <img src="/photos/stretchpoem/newvclothsof" alt="new cloths of">
-       <img src="/photos/stretchpoem/truth" alt="truth"> 
-         <img src="/photos/stretchpoem/PoetryisaStretch" alt="Poetry is a stretch">
-              <img src="/photos/stretchpoem/ofmeaning" alt="of meaning">
-                <img src="/photos/stretchpoem/pulledgleaming" alt="pulled gleaming">
-                   <img src="/photos/stretchpoem/fromseasof" alt="from seas of">
-                   <img src="/photos/stretchpoem/starspushed" alt="stars pushed">
-                     <img src="/photos/stretchpoem/oninfinitewaves" alt="on infinite waves">
-                     <img src="/photos/stretchpoem/receddingintostone" alt="recedding into stones">
-                     <img src="/photos/stretchpoem/receddingintostone" alt="of time">
+    <div id="crossfade">
+      
+      <img src="/photos/stretchpoem/PoetryisaStretch" alt="Poetry is a stretch">
+      <img src="/photos/stretchpoem/oftruthandtalent" alt="of truth and talent">
+      <img src="/photos/stretchpoem/MindandMetaphor" alt="mind and metaphor">
+      <img src="/photos/stretchpoem/ofloveandpain" alt="love and pain">
+      <img src="/photos/stretchpoem/theTolerantandTimid" alt="the tolerant and timid">
+      <img src="/photos/stretchpoem/vividimagespulledover" alt="vivid images pulled over">
+      <img src="/photos/stretchpoem/newvclothsof" alt="new cloths of">
+      <img src="/photos/stretchpoem/truth" alt="truth">
+      <img src="/photos/stretchpoem/PoetryisaStretch" alt="Poetry is a stretch">
+      <img src="/photos/stretchpoem/ofmeaning" alt="of meaning">
+      <img src="/photos/stretchpoem/pulledgleaming" alt="pulled gleaming">
+      <img src="/photos/stretchpoem/fromseasof" alt="from seas of">
+      <img src="/photos/stretchpoem/starspushed" alt="stars pushed">
+      <img src="/photos/stretchpoem/oninfinitewaves" alt="on infinite waves">
+      <img src="/photos/stretchpoem/receddingintostone" alt="recedding into stones">
+      <img src="/photos/stretchpoem/receddingintostone" alt="of time">
+      
+    </div>
+    <p><small>Also on <a class="u-syndication" href="https://indieweb.xyz/en/poetry">IndieWeb Poetry</a></small> <small>Also on <a class="u-syndication" href="https://clmoocring.jgregorymcverry.com/clmooc-planet/">CLMOOC Planet</a></small></p>
+  </article>
+</main>
+<footer>
+  <?php include 'footer.php'; ?>
+</body>
 
-  
-      </div>
-<p><small>Also on <a class="u-syndication" href="https://indieweb.xyz/en/poetry">IndieWeb Poetry</a></small> <small>Also on <a class="u-syndication" href="https://clmoocring.jgregorymcverry.com/clmooc-planet/">CLMOOC Planet</a></small></p>
-</article>
-    </main>
-    <footer>
-<?php include 'footer.php'; ?>
-
-  </body>
- 
 </html>
