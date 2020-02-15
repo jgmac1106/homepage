6322f7649d70c1a8ec8bd19e35d73b3bb9d09721
Greg McVerry <jgregmcverry@gmail.com>
2020-02-14 20:47:41 -0500

    Create IWCNavrongoKeynote.php
    adding keynote post

---
+++ IWCNavrongoKeynote.php
@@ -0,0 +1,211 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name"></h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
+     on <time class="dt-published" datetime="2020-014-8 20:44:20">14<sup>th</sup> February 2020</time></p>
+    
+
+    
+    <span class="summary">
+    <p class="p-summary">Keynote for the @MozOpenLeaders #IndieWeb and #OER #Ghana program</p>
+    </span>
+
+    <div class="copy">
+
+  <div class="e-content">
+    <p>This keynote is a collection of posts from a conversation I had with a friend on why IndieWeb matters to education</p>
+     <div class="note-wrapper">
+      <div class="t-main">
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_1" data-screenname="jgmac1106" data-tweet="1226325762032197632" class="h-entry note" dir="auto">
+<a class="entity-mention entity-mention-first" href="https://twitter.com/NomadWarMachine">@NomadWarMachine</a> More <a class="entity-hashtag" href="/hashtag/ethos">#ethos</a> rather than <a class="entity-hashtag" href="/hashtag/stuff">#stuff</a> or step by step journey. Begins with principled community: <a class="entity-url" data-preview="true" href="https://indieweb.org/principles">indieweb.org/principles</a> guided by norms: <a class="entity-url" data-preview="true" href="https://indieweb.org/code-of-conduct">indieweb.org/code-of-conduct</a> towards shared goal of owning data, better connections and controlling your… <a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/2020/02/09/more-ethos-rather-than-stuff-or-step">quickthoughts.jgregorymcverry.com/2020/02/09/mor…</a>
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_2" data-screenname="jgmac1106" data-tweet="1226326559960846336" class="h-entry note" dir="auto">
+<a class="entity-hashtag" href="/hashtag/IndieWeb">#IndieWeb</a> philosophy works for education because believe every learner deserves right to tell their story. It isn't just <a class="entity-hashtag" href="/hashtag/cs4all">#cs4all</a>, the way we read, write and participated as shifted at a rate exponentially greater than any time in history. (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/rWgQd">quickthoughts.jgregorymcverry.com/s/rWgQd</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_3" data-screenname="jgmac1106" data-tweet="1226327440114569216" class="h-entry note" dir="auto">
+Matter of equity and protecting democracy. Can never know how truth is shaped online until you begin to shape your own. Believe in hyper local web where we root infrastructure in libraries and local press, way we have always done it ...since ever (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/5oCEV">quickthoughts.jgregorymcverry.com/s/5oCEV</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_4" data-screenname="jgmac1106" data-tweet="1226328311816695808" class="h-entry note" dir="auto">
+For edu <a class="entity-hashtag" href="/hashtag/IndieWeb">#IndieWeb</a> approach works best IMO for three reasons: it is the most resilient, sustainable, and accessible way to build your Commonoplace book <a class="entity-url" data-preview="true" href="https://indieweb.org/commonplace_book">indieweb.org/commonplace_bo…</a>. What is <a class="entity-mention" href="https://twitter.com/creativecommons">@creativecommons</a> for then to store and share the story of us. (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/IjBpn">quickthoughts.jgregorymcverry.com/s/IjBpn</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_5" data-screenname="jgmac1106" data-tweet="1226328965876334592" class="h-entry note" dir="auto">
+Resilient in the sense that the first web pages are still readable today. <a class="entity-hashtag" href="/hashtag/IndieWeb">#IndieWeb</a> tools built on a value of open standards like HTML. This ensures a longevity of data portability, especially when you own it from your domain. (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/2hnsvB">quickthoughts.jgregorymcverry.com/s/2hnsvB</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_6" data-screenname="jgmac1106" data-tweet="1226329652446273544" class="h-entry note" dir="auto">
+Like with <a class="entity-hashtag" href="/hashtag/poetryport">#poetryport</a> right now I love all audio poems being shared but also saddened because I know someday they will all be gone. Teachers hit with more site deaths than anyone: <a class="entity-url" data-preview="true" href="https://indieweb.org/site-deaths">indieweb.org/site-deaths</a> We can upload poems to our sites. resilient (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/IXVxS">quickthoughts.jgregorymcverry.com/s/IXVxS</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_7" data-screenname="jgmac1106" data-tweet="1226330503755767813" class="h-entry note" dir="auto">
+So just owning data, preferably from own domain, is <a class="entity-hashtag" href="/hashtag/IndieWeb">#IndieWeb</a> enough, but then you can start doing cool stuff like having webrings and RSS Planets like we do with <a class="entity-hashtag" href="/hashtag/clmooc">#clmooc</a>, but a lot of <a class="entity-hashtag" href="/hashtag/IndieWeb">#IndieWeb</a> tools use metadata called microformats to connect (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/1Szd4p">quickthoughts.jgregorymcverry.com/s/1Szd4p</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_8" data-screenname="jgmac1106" data-tweet="1226331530630438912" class="h-entry note" dir="auto">
+This is a tiny bit of data that is carried by any HTML element. <a class="entity-url" data-preview="true" href="https://developer.mozilla.org/en-US/docs/Web/HTML/microformats">developer.mozilla.org/en-US/docs/Web…</a> This means all your social and feed data is both human readable and machine readable in one place. Resilient. Stone tablet or new tablet you can read it. (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/8wjeL">quickthoughts.jgregorymcverry.com/s/8wjeL</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_9" data-screenname="jgmac1106" data-tweet="1226332399665065984" class="h-entry note" dir="auto">
+Not having to repeat same thing in two different places also makes <a class="entity-hashtag" href="/hashtag/IndieWeb">#IndieWeb</a> approach more sustainable. So does lack of boards, and bosses, and dependence on soft money. Done a lot of Open Source, I am done getting heavily involved with orgs (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/gjCbr">quickthoughts.jgregorymcverry.com/s/gjCbr</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_10" data-screenname="jgmac1106" data-tweet="1226333137313181697" class="h-entry note" dir="auto">
+Want to be an organizer? Organize two events. That's it, we welcome more organizers. Plus the focus on local events and local communities rather than massive conferences is more sustainable on a carbon level. (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/1sJD3s">quickthoughts.jgregorymcverry.com/s/1sJD3s</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_11" data-screenname="jgmac1106" data-tweet="1226333822008090625" class="h-entry note" dir="auto">
+Think about all the energy that must go into tracking you across social media silos, spitting out algorithms based on your only move. Why not just have a tiny little website. Sure the energy difference is small, but should that matter? It is a crisis. (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/1qEkBS">quickthoughts.jgregorymcverry.com/s/1qEkBS</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_12" data-screenname="jgmac1106" data-tweet="1226334625909346305" class="h-entry note" dir="auto">
+The <a class="entity-hashtag" href="/hashtag/Indieweb">#Indieweb</a> philosophy of do over say is also a more sustainable approach. So much <a class="entity-hashtag" href="/hashtag/edtech">#edtech</a> innovation is vaporware promises of tools of tomorrow, semantics that never settle. All the tools used in <a class="entity-hashtag" href="/hashtag/IndieWeb">#IndieWeb</a> community exist today, not tomorrow (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/XpOVE">quickthoughts.jgregorymcverry.com/s/XpOVE</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_13" data-screenname="jgmac1106" data-tweet="1226335493106937856" class="h-entry note" dir="auto">
+This makes <a class="entity-hashtag" href="/hashtag/indieweb">#indieweb</a> stuff accessible to all, but not as much as a commitment to HTML. You want the most accessible approach to <a class="entity-hashtag" href="/hashtag/OER">#OER</a> that reaches all learners, works with screen readers, is easier for localisation you use HTML. Full stop. (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/VfKcS">quickthoughts.jgregorymcverry.com/s/VfKcS</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_14" data-screenname="jgmac1106" data-tweet="1226336515988566016" class="h-entry note" dir="auto">
+Focus on self and just building for you was successful in ensuring sustainability butthink it hurt accessibility a bit as folks who may thrive in communities by building communities may not have landed, but conjecture, it is amazing learning space (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/1BzoGr">quickthoughts.jgregorymcverry.com/s/1BzoGr</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_15" data-screenname="jgmac1106" data-tweet="1226337605702356992" class="h-entry note" dir="auto">
+I am examining it from a lens of Innovation Systems, seeing how agentive change, organizes and fitlers upwards to trickle down again, in terms of getting started it is a matter of deciding the balance between content and code? How to spend you time (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/L7PXD">quickthoughts.jgregorymcverry.com/s/L7PXD</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_16" data-screenname="jgmac1106" data-tweet="1226338844691304448" class="h-entry note" dir="auto">
+Can go with turnkey service <a class="entity-mention" href="https://twitter.com/microdotblog">@microdotblog</a> which just works out of box and lets you connect to tumblr, twitter, linkedin, mastodon, have a fediverse instance of your site, homepages and web pages,..phew,,,plus an intentionally hospitable community (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/1dGneB">quickthoughts.jgregorymcverry.com/s/1dGneB</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_17" data-screenname="jgmac1106" data-tweet="1226339990440939528" class="h-entry note" dir="auto">
+At the next level there is WordPress. Works best with one of the themes developed by the community and a few well maintained plugins. Install them and you are off to the races. WordPress is hard to do if you want to keep your current theme. (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/VyB88">quickthoughts.jgregorymcverry.com/s/VyB88</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_18" data-screenname="jgmac1106" data-tweet="1226340947493031937" class="h-entry note" dir="auto">
+Then there is where I am. i install a CMS on a shared host. There a number of CMSs with <a class="entity-hashtag" href="/hashtag/IndieWeb">#IndieWeb</a> building blocks. Drupal, Kirby, Perch, alot of people use static site generators and some tools to to include <a class="entity-hashtag" href="/hashtag/IndieWeb">#IndieWeb</a> stuff (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/1D10kr">quickthoughts.jgregorymcverry.com/s/1D10kr</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_19" data-screenname="jgmac1106" data-tweet="1226341675477409796" class="h-entry note" dir="auto">
+After that people build their own CMSs, and connect with all the tools. That is their hobby. They want to play with code, others content. Point is get a blog, find a passion, share and build a better web, (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/29rTpU">quickthoughts.jgregorymcverry.com/s/29rTpU</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_20" data-screenname="jgmac1106" data-tweet="1226342619363540992" class="h-entry note" dir="auto">
+See all these notes in this thread originated on my website and I use a tool called webmentions to POSSE, publish on your own site syndicate elsewhere (every in my case, except facebook, evil company). Webmentions are awesome, (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/owdaM">quickthoughts.jgregorymcverry.com/s/owdaM</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_21" data-screenname="jgmac1106" data-tweet="1226343708225830914" class="h-entry note" dir="auto">
+Then there other open standards like micropub and microsub that allow me to read and write directly from a social reader this stuff is cool: <span class="entity-embed"><iframe class="youtube-player keep-ratio-4-3" src="https://www.youtube.com/embed/gwECN4uVuHg" frameborder="0" allowFullScreen></iframe></span> so the technical barriers are lower and soon we can do this from mobile (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/2gcji9">quickthoughts.jgregorymcverry.com/s/2gcji9</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+<div class="row t-tweet">
+<div class="col-12">
+<div id="tweet_22" data-screenname="jgmac1106" data-tweet="1226344545073364992" class="h-entry note" dir="auto">
+So <a class="entity-mention" href="https://twitter.com/NomadWarMachine">@nomadwarmachine</a> do I know of an Intro guide to <a class="entity-hashtag" href="/hashtag/IndieWeb">#IndieWeb</a>..not one I liked..so I wrote one, from my own domain, where I own the data and chose how I wanted to share it. That's the <a class="entity-hashtag" href="/hashtag/IndieWeb">#IndieWeb</a> style and why it works for education. (<a class="entity-url" data-preview="true" href="https://quickthoughts.jgregorymcverry.com/s/xUXlH">quickthoughts.jgregorymcverry.com/s/xUXlH</a>)
+<sup class="tw-permalink"><i class="fas fa-link"></i></sup>
+</div>
+</div>
+</div>
+</div>
+     </div>
+      </div>
+<p><small>Also on <a class="u-syndication" href="https://indieweb.xyz/en/poetry">IndieWeb Poetry</a></small> <small>Also on <a class="u-syndication" href="https://clmoocring.jgregorymcverry.com/clmooc-planet/">CLMOOC Planet</a></small></p>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
