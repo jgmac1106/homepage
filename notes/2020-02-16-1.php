42d2b602cd070beb491e57ae8ffcaac4fdba50bb
Greg McVerry <jgregmcverry@gmail.com>
2020-02-16 11:06:06 -0500

    add a note
    adding note

---
+++ notes/2020-02-16-1.php
@@ -0,0 +1,29 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-02-16T11:04:20+0000">February 13, 2020</time>
+       
+
+          
+      
+  
+      
+        <p class="e-content"><p> I just added pagination to my article feed on my wesbite <a href="https://jgregorymcverry.com/2020/02">https://jgregorymcverry.com/2020/02</a>. Decided to go with a yyyy/mm structure. Still need to fix some granary links, and then it is on to pagination of notes. #IndieWeb</p>
+      
+ 
+  <p>Also On<a class="u-url url" href="https://jgregorymcverry.com/notes/2020-02-16-1" rel="canonical"><strong>My Notes</strong></a> <br><a href="https://jgregorymcverry.com/notes/2020-02-13-3" rel="prev"> Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
--- notes/notetemplate.php
+++ notes/notetemplate.php
@@ -7,8 +7,17 @@
   	<div class="note h-entry">
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2019-11-30 08:44:20">30<sup>th</sup>November 2019</time>
- <p class="p-name e-content"></p>
-  Also on<a class="u-url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?> "rel="canonical"><strong>My Notes</strong></a><a href="https://jgregorymcverry.com/notes/2019-11-30-2" rel="next">Next Note <a href="https://jgregorymcverry.com/notes/2019-11-27-5" rel="prev">Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+ <p class="p-name e-content">
+ 	Just get it done<br />
+ 	the one<br/>
+ 	metric that<br/>
+ 	matters<br/>
+ 	The aesthetic patterns just<br/>
+   scattered chatter<br/>
+   
+ </p>
+ <a class="u-url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?>
+  Also on "rel="canonical"><strong>My Notes</strong></a><a href="https://jgregorymcverry.com/notes/2019-11-30-2" rel="next">Next Note <a href="https://jgregorymcverry.com/notes/2019-11-27-5" rel="prev">Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
   	</main>
   		<footer>
