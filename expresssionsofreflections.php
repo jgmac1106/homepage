a472e4c8c732a81e77ad0b7970a09ffca1690cda
Greg McVerry <jgregmcverry@gmail.com>
2020-02-16 13:03:07 -0500

    Update expresssionsofreflections.php
    fix file

--- expresssionsofreflections.php
+++ expresssionsofreflections.php
@@ -18,7 +18,7 @@
 <img class="featured u-featured" src="https://i1.wp.com/clmooc.com/wp-content/uploads/2020/01/PoetRY_Port_LogoYes-lowres-e1580420665271.png?fit=800%2C800&ssl=1" alt="screenshot of my notes">
     <div class="copy">
   <div class="e-content">
-    <video controls src="https://jgregorymcverry.com/videos/ExpressionsofReflections.mp4"><br/>
+    <video controls src="https://jgregorymcverry.com/videos/ExpressionsOfReflections.mp4"><br/>
 When Piper glances<br/>
 Bard<br/>
 reflections of<br/>
