c9ebb3db9e1eb249d12f85ea80ba5e2117a15d17
Greg McVerry <jgregmcverry@gmail.com>
2020-02-16 07:42:27 -0500

    Update articles.php
    update articles

--- articles.php
+++ articles.php
@@ -9,6 +9,144 @@
   <a class="p-name u-url" href="http://jgregorymcverry.com" rel="me">Greg McVerry</a><br>
     </div>
 <section class="card-container">
+  <article class="h-entry card">
+        <header class="card__title">
+            <h3 class="p-name">Poetry Is A Stretch</h3>
+        </header>
+      <div class="authorInfo">
+         <div class="h-card"> 
+           <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
+         </div>
+        
+       <time class="dt-published" datetime="2020-02-14 12:24:20">13<sup>th</sup> February 2020</time>
+      </div>
+        <figure class="card__thumbnail">
+                <img class="featured u-featured" src="/photos/stretchpoem/PoetryisaStretch" alt="Poetry is a Stretch">
+         
+        </figure>
+        <div class="p-summary card__description">
+          <p>
+            A #Clmooc #poetryport poem made of pixelated gifs
+          </p>
+        </div>
+        <a  href="/poetryisastretch" class="u-url button">Read More</a>
+
+    </article> 
+  <article class="h-entry card">
+        <header class="card__title">
+            <h3 class="p-name">Chicken and Egg (Err Duck) of the Digital Dichotomy</h3>
+        </header>
+      <div class="authorInfo">
+         <div class="h-card"> 
+           <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
+         </div>
+        
+       <time class="dt-published" datetime="2020-02-14 12:24:20">13<sup>th</sup> February 2020</time>
+      </div>
+        <figure class="card__thumbnail">
+                <img class="featured u-featured" src="https://i1.wp.com/clmooc.com/wp-content/uploads/2020/01/PoetRY_Port_LogoYes-lowres-e1580420665271.png?fit=800%2C800&ssl=1" alt="screenshot of my notes">
+         
+        </figure>
+        <div class="p-summary card__description">
+          <p>
+            A #Clmooc #poetryport gift for @miazamoraPhD @cogdog and the #netnarr crew 
+          </p>
+        </div>
+        <a  href="/chickeneggorduckofdigital" class="u-url button">Read More</a>
+
+    </article> 
+  <article class="h-entry card">
+        <header class="card__title">
+            <h3 class="p-name">Art & Feminism and IndieWeb Navrongo Keynote</h3>
+        </header>
+      <div class="authorInfo">
+         <div class="h-card"> 
+           <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
+         </div>
+        
+        <time class="dt-published" datetime="2020-02-14 20:44:20">14<sup>th</sup> February 2020</time>
+      </div>
+        <figure class="card__thumbnail">
+                <img class="featured u-featured small"  src="https://goifnetwork.org/images/OPENNESSS66.png" alt="OER Ghana">
+         
+        </figure>
+        <div class="p-summary card__description">
+          <p>
+            Keynote for the @MozOpenLeaders #IndieWeb and #OER #Ghana program
+          </p>
+        </div>
+        <a  href="/IWCNavrongoKeynote" class="u-url button">Read More</a>
+
+    </article> 
+  <article class="h-entry card">
+        <header class="card__title">
+            <h3 class="p-name">How I wrote the Paradox of Technology: A #Clmooc #poetryport poem</h3>
+        </header>
+      <div class="authorInfo">
+         <div class="h-card"> 
+           <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
+         </div>
+        
+        <time class="dt-published" datetime="2020-02-13 11:34:20">13<sup>th</sup> February 2020</time>
+      </div>
+        <figure class="card__thumbnail">
+                <img class="featured u-featured small"  src="https://i1.wp.com/clmooc.com/wp-content/uploads/2020/01/PoetRY_Port_LogoYes-lowres-e1580420665271.png?fit=800%2C800&ssl=1" alt="screenshot of my notes">
+         
+        </figure>
+        <div class="p-summary card__description">
+          <p>
+            Backstage post of how the poem "Paradox of Technology" came to be
+          </p>
+        </div>
+        <a  href="/backstageofparadoxoftechnology" class="u-url button">Read More</a>
+
+    </article> 
+   <article class="h-entry card">
+        <header class="card__title">
+            <h3 class="p-name">How I wrote the Peace for the part time PhD: A #Clmooc #poetryport poem</h3>
+        </header>
+      <div class="authorInfo">
+         <div class="h-card"> 
+           <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
+         </div>
+        
+        <time class="dt-published" datetime="2020-005-8 10:54:20">13<sup>th</sup> February 2020</time>
+      </div>
+        <figure class="card__thumbnail">
+                <img class="featured u-featured small"  src="https://i1.wp.com/clmooc.com/wp-content/uploads/2020/01/PoetRY_Port_LogoYes-lowres-e1580420665271.png?fit=800%2C800&ssl=1" alt="screenshot of my notes">
+         
+        </figure>
+        <div class="p-summary card__description">
+          <p>
+            Backstage post of how the poem "Peace for the part time PhD" came to be
+          </p>
+        </div>
+        <a  href="/backstageofpeaceforphd" class="u-url button">Read More</a>
+
+    </article> 
+  <article class="h-entry card">
+        <header class="card__title">
+            <h3 class="p-name">Installing Known on Reclaim Hosting</h3>
+        </header>
+      <div class="authorInfo">
+         <div class="h-card"> 
+           <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
+         </div>
+        
+        <time class="dt-published" datetime="2020-02-07T15:14:20">7<sup>th</sup> February 2020</time>
+      </div>
+        <figure class="card__thumbnail">
+                <img class="featured u-featured" src="https://withknown.com/img/full_logo_blk.png">
+         
+        </figure>
+        <div class="p-summary card__description">
+          <p>
+            Step by step video guide for installing Known using Reclaim Hosting CPanel
+          </p>
+        </div>
+        <a  href="/installingknownonreclaimhosting" class="u-url button">Read More</a>
+
+    </article> 
      <article class="h-entry card">
         <header class="card__title">
             <h3 class="p-name">Announcing IndieWebCamp Online 2020</h3>
@@ -18,7 +156,7 @@
            <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
          </div>
         
-         <time class="dt-published" datetime="2020-005-8 09:44:20">5<sup>th</sup> January 2020</time></p>
+         <time class="dt-published" datetime="2020-005-8 09:44:20">5<sup>th</sup> January 2020</time>
       </div>
         <figure class="card__thumbnail">
                 <img class="featured u-featured" src="photos/assets/iwc2020header.jpg">
