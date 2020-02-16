0a1cee2d247f94364050d8b7a56cbabcd070701b
Greg McVerry <jgregmcverry@gmail.com>
2020-02-16 12:58:35 -0500

    Create expresssionsofreflections.php
    adding a poem

---
+++ expresssionsofreflections.php
@@ -0,0 +1,47 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">Expressions of Reflections </h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
+     on <time class="dt-published" datetime="2020-02-16 12:54:20">16<sup>th</sup> February 2020</time></p>
+    <small>Image: Screenshot by me shared under a <a href="https://creativecommons.org/licenses/by-sa/2.0/">Creative Commons (BY-SA) license</a> </small>
+
+    
+    <span class="summary">
+    <p class="p-summary">A #Clmooc #poetryport gift for @karenatsharon using  the word expression. </p>
+    </span>
+  </div>
+<img class="featured u-featured" src="https://i1.wp.com/clmooc.com/wp-content/uploads/2020/01/PoetRY_Port_LogoYes-lowres-e1580420665271.png?fit=800%2C800&ssl=1" alt="screenshot of my notes">
+    <div class="copy">
+  <div class="e-content">
+    <video controls src="https://jgregorymcverry.com/videos/ExpressionsofReflections.mp4"><br/>
+When Piper glances<br/>
+Bard<br/>
+reflections of<br/>
+expressions in<br/>
+contrary states<br/>
+bounce back<br/>
+from Blake's<br/>
+plates.<br/>
+<br/>
+Constraints<br/>
+on creativity<br/>
+in a marriage of fates.<br/>
+If it is God that is<br/>
+within us<br/>
+then it must be Satan who<br/>
+dims us<br/>
+      </div>
+<p><small>Also on <a class="u-syndication" href="https://indieweb.xyz/en/poetry">IndieWeb Poetry</a></small> <small>Also on <a class="u-syndication" href="https://clmoocring.jgregorymcverry.com/clmooc-planet/">CLMOOC Planet</a></small><br /><a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a></p>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
