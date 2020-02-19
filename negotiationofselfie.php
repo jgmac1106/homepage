d2fb92d6d50449cbd5f4247e3bf0ab6d7a3707de
Greg McVerry <jgregmcverry@gmail.com>
2020-02-19 06:13:20 -0500

    Create negotiationofselfie.php
    add post

---
+++ negotiationofselfie.php
@@ -0,0 +1,116 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">Negotiation of teh Selfie </h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
+     on <time class="dt-published" datetime="2020-02-19 06:14:20">19<sup>th</sup> February 2020</time></p>
+    <small>Image: Screenshot by me shared under a <a href="https://creativecommons.org/licenses/by-sa/2.0/">Creative Commons (BY-SA) license</a> </small>
+
+    
+    <span class="summary">
+    <p class="p-summary">A #Clmooc #poetryport gift for @digitalalverman looking at how we project identities on the world and the world projects identies back #writingcommunity</p>
+    </span>
+  </div>
+<img class="featured u-featured" src="https://i1.wp.com/clmooc.com/wp-content/uploads/2020/01/PoetRY_Port_LogoYes-lowres-e1580420665271.png?fit=800%2C800&ssl=1" alt="poetry poetry port logo">
+    <div class="copy">
+
+  <div class="e-content">
+    <video controls src="https://jgregorymcverry.com/videos/NegotiationOfTheSelfie.mp44"></video><br/>
+<p>Two giants<br/>
+bending realities in<br/>
+Unified dimensions<br/>
+of altering axis<br/>
+0f self, A<br/>
+negotiation shifted<br/>
+in identities defined for us<br/>
+as we cast I , me, you we,<br/>
+into the heavens<br/>
+gravitational waves of reality<br/>
+as we can't see the physics within<br/>
+the same<br/>
+identities,<br/>
+projections which<br/>
+reflect<br/>
+Me(s)<br/>
+Within we(s)<br/>
+If literacies, multiple<br/>
+identities must follow<br/>
+<br/>
+You, me, I, we<br/>
+project quantum<br/>
+fluctuations on thee<br/>
+<br/>
+society,<br/>
+a projection of<br/>
+relativity<br/>
+Like Einsten's<br/>
+papers, two realities<br/>
+negotiate an<br/>
+existence where<br/>
+neither can truly be<br/>
+<br/>
+Accelerating truths<br/>
+bring on new<br/>
+weights of gravity<br/>
+time dilations of frozen crisis,<br/>
+a maligned star<br/>
+burning with the fusion of a<br/>
+broken heart<br/>
+<br/>
+and expeditious explosion<br/>
+of joy as diamond<br/>
+caviar<br/>
+cascades from neutrino<br/>
+stars.<br/>
+<br/>
+An Agent of action<br/>
+sending ripples<br/>
+through time and space<br/>
+While<br/>
+Quantum mechanics<br/>
+on a cosmic scale<br/>
+dance in minutiae of self<br/>
+through<br/>
+four dimensions<br/>
+of space spinning in<br/>
+gyrations of souls<br/>
+unified systems in unpredictable<br/>
+energies<br/>
+<br/>
+heavenly bodies, us each,<br/>
+big objects, planets,<br/>
+colliding galaxies<br/>
+our Me(s) warped around us by<br/>
+Newton's laws<br/>
+Identity, like<br/>
+Gravity birthed int he<br/>
+Enlightment<br/>
+Yet that same<br/>
+magnetism of will,<br/>
+draws force from the well<br/>
+that<br/>
+compels love<br/>
+both strong and weak<br/>
+is it a fourth power we seek?<br/>
+for<br/>
+“Only the hand that erases<br/>
+can write the true thing.”<br/>
+Each quarky twitch<br/>
+'a switch behind two<br/>
+indescribable forces<br/>
+writing your yous<br/>
+onto the world<br/></p>
+      </div>
+<p><small>Also on <a class="u-syndication" href="https://indieweb.xyz/en/poetry">IndieWeb Poetry</a></small> <small>Also on <a class="u-syndication" href="https://clmoocring.jgregorymcverry.com/clmooc-planet/">CLMOOC Planet</a></small><br /><a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a></p>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
