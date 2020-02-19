b39d9b75bf09820cf706aa11bde210a1af27fb4b
Greg McVerry <jgregmcverry@gmail.com>
2020-02-19 11:41:26 -0500

    Create seeksimplicty.php
    add a post

---
+++ seeksimplicty.php
@@ -0,0 +1,53 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">Seek Simplicity </h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
+     on <time class="dt-published" datetime="2020-02-19 11:34:20">19<sup>th</sup> February 2020</time></p>
+    <small>Image <a href="https://creativecommons.org/licenses/by-sa/2.0/">Creative Commons (BY-SA) license</a> </small>
+
+    
+    <span class="summary">
+    <p class="p-summary">Seek Simplicity a #poetryport gift for @dajbelshaw who philosophies on systems, society, and schools make the world a bit better every day. </p>
+    </span>
+  </div>
+<img class="featured u-featured" src="https://i1.wp.com/clmooc.com/wp-content/uploads/2020/01/PoetRY_Port_LogoYes-lowres-e1580420665271.png?fit=800%2C800&ssl=1" alt="screenshot of my notes">
+    <div class="copy">
+
+  <div class="e-content">
+    <audio controls src="https://jgregorymcverry.com/poetry/SeekSimplicity.mp4"></video><br/>
+<p>Seek simplicity<br />
+of seasons in all you do.<br />
+A child's smile in<br />
+the mundane. As Monday melts<br />
+to Tuesday, a sublimation of<br />
+time<br />
+Art found in patterns<br />
+of your sponge<br />
+gliding over dishes<br />
+wishes undone by<br />
+clear direction of a risen<br />
+sun<br />
+not tomorrow, not yesterday<br />
+Just Now<br />
+A solstice of small<br />
+moments stolen<br />
+from an equinox<br />
+of complexity that<br />
+recedes in slow puffs of the<br />
+Soul<br />
+#smallopoems</p>
+      </div>
+<p><small>Also on <a class="u-syndication" href="https://indieweb.xyz/en/poetry">IndieWeb Poetry</a></small> <small>Also on <a class="u-syndication" href="https://clmoocring.jgregorymcverry.com/clmooc-planet/">CLMOOC Planet</a></small><br /><a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a></p>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
