d28313385346be7c433f660fe8cd6d77282cb669
Greg McVerry <jgregmcverry@gmail.com>
2020-02-20 16:28:20 -0500

    Create riksybird.php
    adding poem

---
+++ riksybird.php
@@ -0,0 +1,53 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+</header>
+<main class="longreads">
+<article class="h-entry">
+<h1 class="p-name">Poetry is a Stretch</h1>
+<div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a>
+on <time class="dt-published" datetime="2020-02-20 16:24:20">20<sup>th</sup> February 2020</time></p>
+
+
+<span class="summary">
+  <p class="p-summary">Poetry is a Stretch: A #clmooc #poetryport poem</p>
+</span>
+<div class="copy">
+  <div class="e-content">
+    <audio  autoplay>
+      <source src="poetry/poetryisastretch.mp4">
+      </audio>
+    <div id="crossfade">
+      
+     <div class="riskybird">
+       <p>Take the risk</p>
+     </div>
+       <div class="riskybird">
+       <p>of first</p>
+     </div>
+     <div class="riskybird">
+       <p>flight</p>
+     </div>
+     <div class="riskybird">
+       <p>But choose</p>
+     </div>
+      <div class="riskybird">
+       <p>a branch</p>
+     </div>
+      <div class="riskybird">
+       <p>a close to </p>
+     </div>
+      <div class="riskybird">
+       <p>the ground</p>
+     </div>
+      
+    </div>
+    <p><small>Also on <a class="u-syndication" href="https://indieweb.xyz/en/poetry">IndieWeb Poetry</a></small> <small>Also on <a class="u-syndication" href="https://clmoocring.jgregorymcverry.com/clmooc-planet/">CLMOOC Planet</a></small></p>
+  </article>
+</main>
+<footer>
+  <?php include 'footer.php'; ?>
+</body>
+
+</html>
