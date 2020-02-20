4d6ec95ea2da683c8fbc61b1f51eae67d2d549c0
Greg McVerry <jgregmcverry@gmail.com>
2020-02-19 22:23:00 -0500

    Create 2020-02-19-1.php
    add follow post

---
+++ notes/2020-02-19-1.php
@@ -0,0 +1,21 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-02-19 10:24:20">19<sup>th</sup> February  2020</time>
+ <div class="p-name e-content"><p>I just added</p><ul><li class="p-follow-of"><a class="p-name  u-url" href="http://www.constanceiloh.com/" title="a scholar rising"><img class="u-photo" src="https://images.squarespace-cdn.com/content/v1/53178f5fe4b02cc79a13099b/1566013667874-GMHDVAXD2SXZCB04HIEP/ke17ZwdGBToddI8pDm48kBvHIGXDPYFVN9nQckzozA1Zw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PIYifAC8ds3SnTtkKtLynnVgt4uzUXFGnur3gOh9jfzLQKMshLAGzx4R3EDFOm1kBS/iloh_picture_%2B%25284%2529.jpg?format=1500w"  alt=""  title="a scholar rising" />Constance Iloh</a> <span class="p-note"> A scholar rising</span></li></ul><p>to the <a href="https://jgregorymcverry.com/following#Education">#Education</a> section of my following page. #BeYourOwnSocialNetwork
+ </div>
+ Also on<a href="https://jgregorymcverry.com/notes/2020-02-19-1" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-02-17-1" rel="prev">Previous Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
