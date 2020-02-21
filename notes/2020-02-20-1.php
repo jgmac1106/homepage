dfee4dd7f74a32afadda40dbc75444242f826935
Greg McVerry <jgregmcverry@gmail.com>
2020-02-21 06:40:16 -0500

    Update 2020-02-20-1.php
    add license info

--- notes/2020-02-20-1.php
+++ notes/2020-02-20-1.php
@@ -7,8 +7,10 @@
   	<div class="note h-entry" id="#">
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2020-02-17T08:44:20+0000">February 17, 2020</time>
+     
+     <a class="u-in-reply-to" href="https://twitter.com/dogtrax/status/1230255342308790275">In Reply toT weet</a><a class="u-repost-of" href="https://twitter.com/dogtrax/status/1230255342308790275">
      <img class="u-photo" src="/photos/art/poemfromkevin.jpg" alt="A solstice of small moments, stolen">
-     <a class="u-in-reply-to" href="https://twitter.com/dogtrax/status/1230255342308790275">In Reply toTweet</a> 
+   </a>
 
           
       
@@ -16,7 +18,7 @@
       
         <p class="p-name e-content">#poetryport #netnarr<br />
         	A solstice of small moments, stolen
-        <small><a title="https://pbs.twimg.com/media/ERK-kN-XkAAD7ne?format=jpg&name=large">Comma Makes a Poem</a> flickr photo by <a href="https://dogtrax.edublogs.com">dogtrax</a> shared under a <a href="https://creativecommons.org/licenses/by/2.0/">Creative Commons (BY) license</a> </small></p>
+        <small class="h-cite"><a title="https://pbs.twimg.com/media/ERK-kN-XkAAD7ne?format=jpg&name=large">Comma Makes a Poem</a> flickr photo by <a href="https://dogtrax.edublogs.com">dogtrax</a> shared under a <a href="https://creativecommons.org/licenses/by/2.0/" rel="license">Creative Commons (BY) license</a> </small></p>
       
  
   <p>Also On<a class="u-url url" href="https://jgregorymcverry.com/notes/2020-02-20-1" rel="canonical"><strong>My Notes</strong></a> <br/><a href="https://jgregorymcverry.com/notes/2020-02-20-2" rel=next>Next Note</a>  <a href="https://jgregorymcverry.com/notes/2020-02-19-1" rel="prev"> Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
