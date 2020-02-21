8da2a78e26e0728ef69a9ecee2217d97e000f124
Greg McVerry <jgregmcverry@gmail.com>
2020-02-21 13:03:40 -0500

    Create IndieWebCampNavrongo.php
    add a post

---
+++ IndieWebCampNavrongo.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>INTERTEXTrEVOLUTION</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="/reset.css">
+		
+		<link rel="stylesheet" href="styles.css">
+		<link rel="stylesheet" href="css/gallery2.css">
+		<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-9ralMzdK1QYsk4yBY680hmsb4/hJ98xK3w0TIaJ3ll4POWpWUYaA2bRjGGujGT8w" crossorigin="anonymous">
+		<script src="/script.js" defer></script>
+		<link rel="webmention" href="https://webmention.io/jgmac1106homepage.glitch.me/webmention" />
+		<link rel="pingback" href="https://webmention.io/jgmac1106homepage.glitch.me/xmlrpc" />
+		<link rel="authorization_endpoint" href="https://indieauth.com/auth">
+		<link rel="token_endpoint" href="https://tokens.indieauth.com/token">
+		<link rel="webmention" href="https://webmention.io/jgregorymcverry.com/webmention" />
+		<link rel="pingback" href="https://webmention.io/jgregorymcverry.com/xmlrpc" />
+	</head>
+	<body>
+		<header>
+			<a href="index.html">
+				<div class="leading">
+					<h1 class="leading-bigtext">
+					INTERTEXTrEVOLUTION
+					</h1>
+					<h2 class="bigtext">
+					Make.Hack.Play.Learn
+					</h2>
+				</div>
+			</a>
+			
+		</header>
+		
+			<div class="child-page-listing">
+				<h2 class="h-entry">IndieWebCamp Navrongo : OLX Ghana</h2>
+				<h3>
+				Collection by <a class="u-author h-card" href="https://jgregorymcverry.com">Greg McVerry</a> and published <time class="dt-published" datetime="2020-02-21 12:54:20">2020-02-21 </time>
+				</h3>
+				<div class="grid-container">
+					<figure id="3685" class="h-entry gallery">
+						<div class="galleryPhoto">
+							<a href="#">
+							<img class="u-photo" src="http://jgregorymcverry.com/photos/goifphotos/groupphoto.jpg" alt="screenshot of video chat GOIF group on left jgmac1106 on right">      </a>
+						</div>
+						<figcaption class="galleryCaption" >
+						Launching the Camp
+						<time datetime="2020-02-15 10:41:00" class="dt-published">2020-02-15</time>
+						</figcaption>
+					</figure>
+					<figure id="2" class="h-entry gallery">
+						
+						<div class="galleryPhoto">
+							<a href="#">
+							<img class="u-photo" src="https://jgregorymcverry.com/photos/goifphotos/sadikgoalsetting" alt="Sadike reviewing Code of Conduct">    </a>
+						</div>
+						<figcaption class="galleryCaption" >
+						Sadik Reviewing IWC CoC
+						<time datetime="2020-02-15 10:41:00">2020-02-15</time>
+						</figcaption>
+					</figure>
+					<figure id="3" class="h-entry gallery">
+						
+						<div class="galleryPhoto">
+							<a href="#">
+							<img class="u-photo" src="https://jgregorymcverry.com/photos/goifphotos/uruslasage.jpg" alt="Ursula as remote">    </a>
+							
+						</div>
+						<figcaption class="galleryCaption">
+						Ursula Joins from Accra  <time datetime="2020-02-15 10:41:00" >2020-02-15</time>
+						
+						</figcaption>
+					</figure>
+					<figure id="4" class="h-entry gallery">
+						<figcaption class="galleryCaption" >
+						Setting Our Golas <time datetime="2020-02-15 10:41:00">2020-02-15</time>
+						</figcaption>
+						<div class="galleryPhoto">
+							<a href="https://jgregorymcverry.com/goifphotos/goalsetting2.jpg">
+							<img class="u-photo" src="https://jgregorymcverry.com/goifphotos/goalsetting2.jpg" alt="Partiicpant in front of camera sharing goaals">  </a>
+						</div>
+					</figure>
+						<figure id="4" class="h-entry gallery">
+						<figcaption class="galleryCaption" >
+						Setting Our Golas <time datetime="2020-02-15 10:41:00">2020-02-15</time>
+						</figcaption>
+						<div class="galleryPhoto">
+							<a href="https://jgregorymcverry.com/goifphotos/goalsetting3.jpg">
+							<img class="u-photo" src="https://jgregorymcverry.com/goifphotos/goalsetting3.jpg" alt="Partiicpant in front of camera sharing goaals">  </a>
+						</div>
+					</figure>
+					<figure id="4" class="h-entry gallery">
+						<figcaption class="galleryCaption" >
+						Setting Our Golas <time datetime="2020-02-15 10:41:00">2020-02-15</time>
+						</figcaption>
+						<div class="galleryPhoto">
+							<a href="https://jgregorymcverry.com/goifphotos/goalsetting4.jpg">
+							<img class="u-photo" src="https://jgregorymcverry.com/goifphotos/goalsetting4.jpg" alt="Partiicpant in front of camera sharing goaals">  </a>
+						</div>
+					</figure>
+					<figure id="4" class="h-entry gallery">
+						<figcaption class="galleryCaption" >
+						Setting Our Golas <time datetime="2020-02-15 10:41:00">2020-02-15</time>
+						</figcaption>
+						<div class="galleryPhoto">
+							<a href="https://jgregorymcverry.com/goifphotos/goalsetting5.jpg">
+							<img class="u-photo" src="https://jgregorymcverry.com/goifphotos/goalsetting5.jpg" alt="Partiicpant in front of camera sharing goaals">  </a>
+						</div>
+					</figure>
+					
+					
+					
+				</div>
+				
+				
+		
+		</body>
+		<footer><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+	</footer>
+</html>
