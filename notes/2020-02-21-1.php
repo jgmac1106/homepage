848fa0d3cdb30b7b525f9b02daa7d5ad2212a339
Greg McVerry <jgregmcverry@gmail.com>
2020-02-21 13:44:08 -0500

    Create 2020-02-21-1.php
    add photo

---
+++ notes/2020-02-21-1.php
@@ -0,0 +1,31 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note">
+    <div class="grid-container">
+          <figure id="1" class="h-entry gallery"><link class="u-in-reply-to" href="https://events.indieweb.org/2020/02/indiewebcamp-navrongo-olx-ghana-CmCiz3daOPWB"></link>
+            <div class="galleryPhoto">
+              <a href="#">
+              <img class="u-photo" src="http://jgregorymcverry.com/photos/goifphotos/groupphoto.png" alt="screenshot of video chat GOIF group on left jgmac1106 on right">      </a>
+            </div>
+            <figcaption class="galleryCaption" ><span class="p-name">
+            Launching the Camp
+            </span><time datetime="2020-02-15 10:41:00" class="dt-published">2020-02-15</time>
+            </figcaption>
+          </figure>
+      </div>
+ 
+  <p>Also On<a class="u-url url" href="https://jgregorymcverry.com/notes/2020-02-21-1" rel="canonical"><strong>My Notes</strong></a> <br/> ><a href="https://jgregorymcverry.com/notes/2020-02-20-2" rel="prev"> Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
