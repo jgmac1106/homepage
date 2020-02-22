edcee87ff9bc93b42b1d170d369f8ea10eae15a8
Greg McVerry <jgregmcverry@gmail.com>
2020-02-22 07:24:18 -0500

    add remix poem
    adding a poem

---
+++ remixpoem.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>All Learning A Remix</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/css/circlepoem.css">
+  </head>  
+  <body>
+    <div class="main h-entry" role="main">
+      <h1 class="p-name">
+        A Remix Poem for #Poetryport
+      </h1>
+       <a class="u-author h-card" href="https://jgregorymcverry.com">
+      <img class="u-photo" href="https://jgregorymcverry.com/photos/assets/thumb.jpg" alt="">jgmac1106</a>
+      
+      <div class="e-content">
+      <p>
+        All learning begins with remix. Of culture, self, tools, and society. Just a matter if you want to live a life of fresh beats or dance to an old tune.
+      </p>
+      <p>
+       Ain't a right answer, but if you want to learn more check out thinkers like Julia Kristeva, Bettina Smith, Amy Burvall, Ebony Thomas, Mikhail Bakhtin, Anna the Cow, David E. Kirkland. Ignore the Dadas, except for everything they said and did. Especially the poetry, burn all of it, and recite a new poem from the scarps of ashes that escape the flames.
+      </p>
+      <button id="animateButton">
+        Just Start the Poem
+      </button>
+      <div class="container">
+        <div id="circle" class="circle">
+         Remix
+        </div>
+      </div>
+      <p id="endMessage"></p>
+         </div>
+    </div>
+    <script src="/poemscript.js" defer></script>
+    <div class="glitchButton" style="position:fixed;top:20px;right:20px;"></div>
+    <script src="https://button.glitch.me/button.js"></script>
+  </body>
+</html>
