8aee9efce242cf92d160c1acecdfd449e7b17cdd
Greg McVerry <jgregmcverry@gmail.com>
2020-02-22 18:39:32 -0500

    adding a poem
    adding a poem

---
+++ heavenlyremixes.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Risky Bird</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- import the webpage's stylesheet -->
+    <link rel="stylesheet" href="/css/crossfade.css">
+    
+    <!-- import the webpage's javascript file -->
+    <script src="/script.js" defer></script>
+  </head>  
+  <body>
+    <div class="main h-entry" role="main">
+      <h1 class="p-name">
+        Heavenly Remixes
+      </h1>
+       <a class="u-author h-card" href="https://jgregorymcverry.com">
+      <img class="u-photo" src="https://jgregorymcverry.com/photos/assets/thumb.jpg" alt=""><br />jgmac1106</a><br />
+      <p>Published on <time class="dt-published" datetime="2020-02-22 18:34:20">22<sup>nd</sup> February 2020</time></p>
+      <p class="p-summary">A #poetryport poem as a gift to the songs that go unsung. Even the lonliest stars sings</p>
+      <div class="e-content">
+    <div id="DarkStar">
+      <!-- Goal is to have each div fade in and out on the 3 second counter-->
+      
+     <div  class="crashes">
+       <p class="loveyou">All stars sing<br />
+        remixing<br />
+            the first<br />
+        beat<br /></p>
+     </div>
+       <div class="pouring">
+       <p class="loveyou">outer layers <br />
+           belting tunes<br />
+      in heaven's lonely embrace</p>
+     </div>
+     <div class="light">
+       <p class="loveyou">Rhythms arise<br />
+        spun on quantum tectronics<br />
+      needle oscilliating<br />
+    from the core</p>
+     </div>
+     <div class="intoAshes">
+       <p>As the galaxies<br />
+        dance to their<br /></p>
+     </div>
+      <div class="ReasonTatters">
+       <p class="love you">own rhytm
+      </p>
+     </div>
+      <div class="ForcesTear">
+       <p class="love you">as we whistle </p>
+     </div>
+      <div class="LooseFromAxis">
+       <p class="loveyou">to the same tune</p>
+     </div>
+      
+    </div>
+</div>
+</
+    <!-- include the Glitch button to show what the webpage is about and
+          to make it easier for folks to view source and remix -->
+ 
+  </body>
+</html>
---
+++ pumpkin.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Pumpkin</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- import the webpage's stylesheet -->
+    <link rel="stylesheet" href="/style.css">
+    
+    <!-- import the webpage's javascript file -->
+    <script src="/script.js" defer></script>
+  </head>  
+    <div id="pumpkin">
+      <!-- Goal is to have each div fade in and out on the 3 second counter-->
+      
+     <div  class="dog">
+       <p class="loveyou">I look at you<br />
+sleeping on the bed,<br />
+Old, stiff, still my lovely puppy but<br />
+Sometimes dazed and confused.</p>
+     </div>
+       <div class="dog1">
+       <p class="loveyou">I know,<br />
+My time with you is finite, </p>
+     </div>
+     <div class="dog2">
+       <p class="loveyou">You are coming to the end<br />
+And all I will be left with is memories</p>
+     </div>
+     <div class="dog3">
+       <p class="loveyou">Of our runs on the beach and the<br />wind in our ears.</p>
+     </div>
+      
+      
+    </div>
+  </body>
+</html>
