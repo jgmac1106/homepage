c0f726aae192f2a2f34c445177d84a41ab1f5aa0
Greg McVerry <jgregmcverry@gmail.com>
2020-02-22 07:46:42 -0500

    add image
    fix image

--- remixpoem.php
+++ remixpoem.php
@@ -13,14 +13,15 @@
         Side B Learning
       </h1>
        <a class="u-author h-card" href="https://jgregorymcverry.com">
-      <img class="u-photo" href="https://jgregorymcverry.com/photos/assets/thumb.jpg" alt="">jgmac1106</a>
+      <img class="u-photo" src="https://jgregorymcverry.com/photos/assets/thumb.jpg" alt="">jgmac1106</a><br />
+      <p>Published on <time class="dt-published" datetime="2020-02-22 07:44:20">22<sup>nd</sup> February 2020</time>
       <p class="p-summary">A #poertyport gift poem for @amyburvall who knows why the whimsy must win.</p>
       <div class="e-content">
       <p>
         All learning begins with remix. Of culture, self, tools, and society. Just a matter if you want to live a life of fresh beats or dance to an old tune.
       </p>
       <p>
-       Ain't a right answer, but if you want to learn more check out thinkers like Julia Kristeva, Bettina Smith, Amy Burvall, Ebony Thomas, Mikhail Bakhtin, Anna the Cow, David E. Kirkland. Ignore the Dadas, except for everything they said and did. Especially the poetry, burn all of it, and recite a new poem from the scarps of ashes that escape the flames.
+       Ain't a right answer, but if you want to learn more check out thinkers like Julia Kristeva, Bettina Smith, Amy Burvall, Ebony Thomas, Mikhail Bakhtin, Anna the Cow, David E. Kirkland. Ignore the Dadas, except for everything they said and did. Especially the poetry, burn all of it, and recite a new poem from the scraps of ashes that escape the flames.
       </p>
       <button id="animateButton">
         Just Start the Poem
