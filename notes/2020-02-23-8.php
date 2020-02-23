48a557b114ebdce813a000ecd335ef7ee58f5c0e
Greg McVerry <jgregmcverry@gmail.com>
2020-02-23 15:21:30 -0500

    add a poem
    add a poem

---
+++ notes/2020-02-23-8.php
@@ -0,0 +1,35 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-02-29T15:14:20+0000">February 23, 2020</time>
+ 
+          
+      
+  
+      
+        <p class="p-name e-content">Think white people<br />
+        	write about nature<br />
+        	because their lives<br />
+        	so silent?<br/>
+        Missing the sounds<br />
+        of city and<br />
+        struggle, eyes<br />
+        cast to skies<br />
+    of kingdoms?</p>
+ 
+  <p>Also On<a class="u-url url" href="https://jgregorymcverry.com/notes/2020-02-23-8" rel="canonical"><strong>My Notes</strong></a> <br/> <a href="https://jgregorymcverry.com/notes/2020-02-23-7" rel="prev"> Previous Note</a><a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
