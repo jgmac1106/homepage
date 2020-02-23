3a39ef5b0b6c7fbb55049f6d610851eae924ee4c
Greg McVerry <jgregmcverry@gmail.com>
2020-02-23 15:22:46 -0500

    Update poetryportpoems.php
    add the rest of the month, fix date alignment add poems for 22 and 23

--- poetryportpoems.php
+++ poetryportpoems.php
@@ -5,7 +5,7 @@
 </header>
    <article class="h-feed">
    <h1 class="p-name">Poetry Port Poems</h1>
-   <time class="dt-updated" datetime="2020-02-21 11:04:20"></time>
+   <time class="dt-updated" datetime="2020-02-23 15:34:20"></time>
 <p class="p-summary">A collection of the poems I have published as part of the #clmooc poetry project</p>
     <a href="/" class="u-author h-card">
     <img class="u-photo avatar" src="https://quickthoughts.jgregorymcverry.com/file/2d6c9cfed7ac8e849f492b5bc7e6a630/thumb.jpg" class="u-photo"> 
@@ -23,6 +23,12 @@
   <div class="week-day">Saturday</div>  
 </div>
 <div class="week">
+  <div class="day"></div>
+  <div class="day"></div>
+  <div class="day"></div>
+  <div class="day"></div>
+  <div class="day"></div>
+  <div class="day"></div>
   <div class="day h-entry">
     <time class="dt-published day-label" datetime="2020-02-01 12:00:00">1</time>
     
@@ -32,6 +38,8 @@
     </div>
     
   </div>
+</div>
+<div class="week">
   <div class="day h-entry">
     <time class="dt-published day-label" datetime="2020-02-02 12:00:00">2</time>
     <div><span class="event event-end p-name">Warmth</span><br /><p class="e-content">
@@ -83,6 +91,8 @@
 
     </p></div>
   </div>
+</div>
+<div class="week">
   <div class="day h-entry">
     <time class="dt-published day-label" datetime="2020-02-09 12:00:00">9</time>
     <div><span class="event event-end p-name">Virtually</span><br /><p class="e-content">
@@ -132,6 +142,8 @@
       <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/16/steel-sparksoff-tuskcut-inpillars-of-rainbow-steelmuzzle">Mosaic of Magic</a><br />
     </p></div>
   </div>
+</div>
+<div class="week">
   <div class="day h-entry">
     <time class="dt-published day-label" datetime="2020-02-16 12:00:00">16</time>
     <div><span class="event event-end p-name">Free Expression</span><br /><p class="e-content">
@@ -173,11 +185,60 @@
       <a class="h-item" href="https://jgregorymcverry.com/riskybird">Risky Bird</a></p>
       </div>
   </div>
-  <div class="day ">
+  <div class="day h-entry">
     <time class="dt-published day-label h-entry" datetime="2020-02-21 12:00:00">21</time>
-    <div><span class="event event-end">Memories</span><br /><p class="e-content">
+    <div><span class="event event-end p-name">Memories</span><br /><p class="e-content">
     <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/21/three-body-problem-a-poetryport-poem-for-the-theme-memory">Three Body Problem</a></div></p>
   </div>
+   <div class="day h-entry">
+    <time class="dt-published day-label h-entry" datetime="2020-02-21 12:00:00">22</time>
+    <div><span class="event event-end p-name">Remix</span><br /><p class="e-content">
+    <a class="h-item" href="https://jgregorymcverry.com/notes/2020-02-23-3">Heavenly Remix</a><br />
+    <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/23/when-people-ask-me-what-i-doi">Am I a Poet?</a><br />
+    <a class="h-item" href="https://jgregorymcverry.com/remixcsspoem">Remix with CSS</a><br />
+    <a class="h-item" href="https://jgregorymcverry.com/oldblogcode">Old Blog Code</a><br />
+    <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/s/VPMU4">Potty with Van Gogh</a><br />
+    <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/22/four-haikus-remixed-from-the-gltich-haiku-generator-haiku-generator-by">Four Haikus</a><br />
+    <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/22/a-poetryport-balckout-poem-remixing-my-work-dogtrax-and-karenatsharon">Blackout Poem</a><br />
+    <a class="h-item" href="https://jgregorymcverry.com/remixpoem">Remix</a><br />
+  </div></p>
+  </div>
+</div>
+ <div class="week">
+ 
+  <div class="day h-entry">
+    <time class=" day-label" datetime="2020-02-23 12:00:00">23</time>
+   <div><span class="event event-end p-name">Silent</span><br />
+    <a class="h-item" href="https://jgregorymcverry.com/notes/2020-02-23-5">Silencing Poets</a><br />
+    <a class="h-tem" href="https://jgregorymcverry.com/notes/2020-02-23-1">Meaning's Whispers</a><br />
+    <a class="h-item" href="https://jgregorymcverry.com/notes/2020-02-23-7">Winter is Silent</a>a><br />
+     <a class="h-item" href="https://jgregorymcverry.com/notes/2020-02-23-8">Nature's Poem</a>a><br />
+  </div>
+  </div>
+  <div class="day">
+    <time class="day-label" datetime="2020-02-24 12:00:00">24</time>
+    <div><span class="event event-end">Explore</span></div>
+  </div>
+  <div class="day">
+    <time class=" day-label" datetime="2020-02-25 12:00:00">25</time>
+    <div><span class="event event-end">Joy</span></div>
+  </div>
+  <div class="day">
+    <time class=" day-label" datetime="2020-02-26 12:00:00">26</time>
+    <div><span class="event event-end">Confidence</span></div>
+  </div>
+  <div class="day">
+    <time class=" day-label" datetime="2020-02-27 12:00:00">27</time>
+    <div><span class="event event-end">Diversity</span></div>
+  </div>
+  <div class="day ">
+    <time class="day-label" datetime="2020-02-28 12:00:00">28</time>
+    <div><span class="event event-end">Introspection</span></div>
+</div>
+ <div class="day ">
+    <time class="day-label" datetime="2020-02-29 12:00:00">29</time>
+    <div><span class="event event-end">Welcome</span></div>
+</div>
 </div>
     </article>
     <footer>
