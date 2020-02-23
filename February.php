4b5740917ee63dc3f1d4f311ae1725911083cc4e
Greg McVerry <jgregmcverry@gmail.com>
2020-02-23 09:20:37 -0500

    Create February.php
    add page

---
+++ February.php
@@ -0,0 +1,121 @@
+<?php include('/home/jgregory/public_html/head.php'); ?>
+</head>
+<?php include('/home/jgregory/public_html/header.php'); ?>
+</header>
+<?php include 'header.php'; ?>
+</header>
+   <article class="h-feed">
+   <h1 class="p-name">February Notes</h1>
+   <time class="dt-updated" datetime="2020-02-21 11:04:20"></time>
+<p class="p-summary">My quick and random thoughts this month.</p>
+    <a href="/" class="u-author h-card">
+    <img class="u-photo avatar" src="https://quickthoughts.jgregorymcverry.com/file/2d6c9cfed7ac8e849f492b5bc7e6a630/thumb.jpg" class="u-photo"> 
+    <span class="p-name">jgmac1106</span>
+  </a>
+
+
+<div class="week">
+  <div class="week-day">Sunday</div>
+  <div class="week-day">Monday</div>
+  <div class="week-day">Tuesday</div>
+  <div class="week-day">Wednesday</div>
+  <div class="week-day">Thursday</div>
+  <div class="week-day">Friday</div>
+  <div class="week-day">Saturday</div>  
+</div>
+<div class="week">
+  <div class="day">
+    <time class="day-label" datetime="2020-02-01 12:00:00">1</time>
+    
+    <div><a class="event event-end h-entry u-url" href="/notes/2020-02-01-1"><time class="dt-published p-name" datetime="2020-02-01 07:44:20">7:44</time></a>
+    
+  </div>
+  <div class="day">
+    <time class="day-label" datetime="2020-02-02 12:00:00">2</time>
+    <div> <div class="h-entry"><a class="event event-end u-url" href="/notes/2020-02-02-1"><time class="dt-published p-name" datetime="2020-02-02T18:54:20+0000">8:54</time></a></div>
+  </div>
+  <div class="day">
+    <time class=" day-label" datetime="2020-02-03 12:00:00">3</time>
+    
+  </div>
+  <div class="day">
+    <time class="day-label" datetime="2020-02-04 12:00:00">4</time>
+    </div>
+  </div>
+  <div class="day">
+    </div>
+  </div>
+  <div class="day">
+    <time class="day-label" datetime="2020-02-06 12:00:00">6</time>
+    
+  </div>
+  <div class="day">
+    <time class="day-label" datetime="2020-02-07 12:00:00">7</time>
+    
+  </div>
+</div>
+    <div class="week">
+  <div class="day">
+    <time class=" day-label" datetime="2020-02-08 12:00:00">8</time>
+   
+    
+  <div class="day ">
+    <time class="day-label" datetime="2020-02-09 12:00:00">9</time>
+    
+  </div>
+  <div class="day">
+    <time class=" day-label" datetime="2020-02-10 12:00:00">10</time>
+    
+  </div>
+  <div class="day">
+    <time class=" day-label" datetime="2020-02-11 12:00:00">11</time>
+    
+  <div class="day">
+    <time class="day-label" datetime="2020-02-12 12:00:00">12</time>
+    <
+  </div>
+  <div class="day">
+    <time class="day-label" datetime="2020-02-13 12:00:00">13</time>
+    
+  </div>
+  <div class="day">
+    <time class=" day-label" datetime="2020-02-14 12:00:00">14</time>
+    
+</div>
+     <div class="week">
+  <div class="day">
+    <time class=" day-label" datetime="2020-02-15 12:00:00">15</time>
+  
+    
+  </div>
+  <div class="day">
+    <time class=" day-label" datetime="2020-02-16 12:00:00">16</time>
+   
+  </div>
+  <div class="day">
+    <time class="day-label" datetime="2020-02-17 12:00:00">17</time>
+    
+  </div>
+  <div class="day">
+    <time class=" day-label" datetime="2020-02-18 12:00:00">18</time>
+    
+  </div>
+  <div class="day">
+    <time class=" day-label" datetime="2020-02-19 12:00:00">19</time>
+    
+  </div>
+  <div class="day">
+    <time class=" day-label" datetime="2020-02-20 12:00:00">20</time>
+    
+  </div>
+  <div class="day ">
+    <time class="day-label" datetime="2020-02-21 12:00:00">21</time>
+    
+</div>
+    </article>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
