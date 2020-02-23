ad3fccf234f74d0bb42c115488311bfb85396737
Greg McVerry <jgregmcverry@gmail.com>
2020-02-23 08:33:47 -0500

    Create 2020:February.php
    add note feed

---
+++ 2020:February.php
@@ -0,0 +1,181 @@
+<?php include 'head.php'; ?>
+<link rel="alternate" type="application/rss+xml" href="https://granary.io/url?input=html&output=rss&url=https%3A%2F%2Fjgregorymcverry.com%2Fpoetryportpoems" title="Poetry Port Poems">
+</head>
+<?php include 'header.php'; ?>
+</header>
+   <article class="h-feed">
+   <h1 class="p-name">Poetry Port Poems</h1>
+   <time class="dt-updated" datetime="2020-02-21 11:04:20"></time>
+<p class="p-summary">A collection of the poems I have published as part of the #clmooc poetry project</p>
+    <a href="/" class="u-author h-card">
+    <img class="u-photo avatar" src="https://quickthoughts.jgregorymcverry.com/file/2d6c9cfed7ac8e849f492b5bc7e6a630/thumb.jpg" class="u-photo"> 
+    <span class="p-name">jgmac1106</span>
+  </a>
+
+
+<div class="week">
+  <div class="week-day">Sunday</div>
+  <div class="week-day">Monday</div>
+  <div class="week-day">Tuesday</div>
+  <div class="week-day">Wednesday</div>
+  <div class="week-day">Thursday</div>
+  <div class="week-day">Friday</div>
+  <div class="week-day">Saturday</div>  
+</div>
+<div class="week">
+  <div class="day">
+    <time class="day-label" datetime="2020-02-01 12:00:00">1</time>
+    
+    <div><a class="event event-end h-entry u-url" href="/notes/2020-02-01-1"><time class="dt-published p-name" datetime="2020-02-01 07:44:20">7:44</time></a>
+    
+  </div>
+  <div class="day">
+    <time class="day-label" datetime="2020-02-02 12:00:00">2</time>
+    <div> <div class="h-entry"><a class="event event-end u-url" href="/notes/2020-02-02-1"><time class="dt-published p-name" datetime="2020-02-02T18:54:20+0000">8:54</time></a></div>
+  </div>
+  <div class="day">
+    <time class="dt-published day-label" datetime="2020-02-03 12:00:00">3</time>
+    <div><span class="event event-end p-name">Collaboration</span><br /><p class="e-content">
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/03/poetryportsmallpoemsto-gonzos-of-the-plowput-forth-seeds">Gonzo Farmer</a><br />
+    </p></div>
+  </div>
+  <div class="day h-entry">
+    <time class="dt-published day-label" datetime="2020-02-04 12:00:00">4</time>
+    <div><span class="event event-end p-name">Gratitude</span><br /><p class="e-content">
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/04/poetryportclmoocsmallpoemsthanks-i-feelas-we-peel-away-onions">Thanks</a>
+    </p></div>
+  </div>
+  <div class="day h-entry">
+    <time class="dt-published day-label" datetime="2020-02-05 12:00:00">5</time>
+    <div><span class="event event-end p-name">Growth</span><br /><p class="e-content">
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/05/teaching-in-a-time-of-crisis-a-poetryport-poem-for">Teaching in Time of Crisis</a><br />
+    </p></div>
+  </div>
+  <div class="day h-entry">
+    <time class="dt-published day-label" datetime="2020-02-06 12:00:00">6</time>
+    <div><span class="event event-end p-name">Home</span><br /><p class="e-content">
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/06/home-in-our-heliosphere-to-the-spinners-of-the-world">Home in our Heliosphere</a><br />
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/06/i-lost-my-first-response-was-a">Digital Home</a><br />
+    </p></div>
+  </div>
+  <div class="day h-entry">
+    <time class="dt-published day-label" datetime="2020-02-07 12:00:00">7</time>
+    <div><span class="event event-end p-name">Color</span><br /><p class="e-content">
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/07/color-of-boogie-clmooc-poetryport-smallpoems">Color of Boogie</a><br />
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/08/nomadwarmachine-eyes-of-your-doodles-always-noodle">Oodles of Doodles</a><br />
+    </p></div>
+  </div>
+</div>
+    <div class="week">
+  <div class="day h-entry">
+    <time class="dt-published day-label" datetime="2020-02-08 12:00:00">8</time>
+   
+    <div><span class="event event-end p-name">Play</span><br /><p class="e-content">
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/08/poetry-as-play-poetyport-clmooc-smallpoems">Poetry as Play</a><br />
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/09/ridgeline-poetryport-clmooc">Ridgeline</a><br />
+
+    </p></div>
+  </div>
+  <div class="day h-entry">
+    <time class="dt-published day-label" datetime="2020-02-09 12:00:00">9</time>
+    <div><span class="event event-end p-name">Virtually</span><br /><p class="e-content">
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/media/edit/afe1292995c44dbc5807c13f4f1878e2">Feces with Faces</a><br />
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/09/poetryportclmoocso-in-tracing-the-etymology-of-virtually">False Potency</a><br />
+    </p></div>
+  </div>
+  <div class="day h-entry">
+    <time class="dt-published day-label" datetime="2020-02-10 12:00:00">10</time>
+    <div><span class="event event-end p-name">Paradox</span><br /><p class="e-content">
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/10/poetryport-smallpoems-clmoocfor-first-time-since-8th">Paradox</a><br />
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/11/paradox-of-technology-1">Paradox of Technology</a><br />
+    </p></div>
+  </div>
+  <div class="day h-entry">
+    <time class="dt-published day-label" datetime="2020-02-11 12:00:00">11</time>
+    <div><span class="event event-end p-name">Create</span><br /><p class="e-content">
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/11/inertia-of-art-create-poem-for-the-clmooc-poetryport">Inertia of Arts</a><br /></p></div>
+  </div>
+  <div class="day h-entry">
+    <time class="dt-published day-label" datetime="2020-02-12 12:00:00">12</time>
+    <div><span class="event event-end p-name">Peace</span><br /><p class="e-content">
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/12/peace-for-the-part-time-phd-a-clmooc-poetry-port-gift">Peace for the Part Time PhD</a><br />
+<a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/13/i-write-poetryto-discover-and-wonderat-glimpses">Why Poetry</a><br />
+    </p></div>
+  </div>
+  <div class="day h-entry">
+    <time class="dt-published day-label" datetime="2020-02-13 12:00:00">13</time>
+    <div><span class="event event-end p-name">Friendship</span><br /><p class="e-content">
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/13/missing-bowie-a-poetryport-gift-for-mrkubasek-and-hope-you">Missing Bowie</a><br />
+    </p></div>
+  </div>
+  <div class="day h-entry">
+    <time class="dt-published day-label" datetime="2020-02-14 12:00:00">14</time>
+    <div><span class="event event-end p-name">Love</span><br /><p class="e-content">
+      <a class="h-item" href="https://jgregorymcverry.com/chickeneggorduckofdigital">Chicken and Egg (Err Duck) of the Digital Dichotomy </a><br /></p></div>
+  </div>
+</div>
+     <div class="week">
+  <div class="day h-entry">
+    <time class="dt-published day-label" datetime="2020-02-15 12:00:00">15</time>
+  
+    <div><span class="event event-end p-name">Stretch</span><br /><p class="e-content">
+      <a class="h-item" href="https://jgregorymcverry.com/poetryisastretch">Poetry is a Stretch</a><br />
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/16/broken-splashesof-sunlight-spillacross-moonlit-bowsbouncing-on">Spilt Sunlight</a><br />
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/16/melt-awayin-foldsthat-hold-swayin-vacuumsof-thoughtwhile">Broken Reality</a><br />
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/16/steel-sparksoff-tuskcut-inpillars-of-rainbow-steelmuzzle">Mosaic of Magic</a><br />
+    </p></div>
+  </div>
+  <div class="day h-entry">
+    <time class="dt-published day-label" datetime="2020-02-16 12:00:00">16</time>
+    <div><span class="event event-end p-name">Free Expression</span><br /><p class="e-content">
+      <a class="h-item" href="https://jgregorymcverry.com/expresssionsofreflections">Expressions of Reflections</a><br />
+    </p></div>
+  </div>
+  <div class="day h-entry ">
+    <time class="dt-published day-label" datetime="2020-02-17 12:00:00">17</time>
+    <div><span class="event event-end p-name">Kindness</span><br /><p class="e-content">
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/17/poetry-is-kindness-clmooc-poetryport">Poetry is Kindness</a><br />
+
+      <a class="h-item" href="https://jgregorymcverry.com/notes/2020-02-17-1">Kindness of Connections</a><br />
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/18/truth-wrapped-in-blankets-of-kindness-a-poetryport-gift-for">Kindness Blanket</a></p>
+    </div>
+  </div>
+  <div class="day h-entry">
+    <time class="dt-published day-label" datetime="2020-02-18 12:00:00">18</time>
+    <div><span class="event event-end p-name">Negotiate</span><br /><p class="e-content">
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/19/poetry-is-negotiationsymbols-and-soundsin-a-semioticdance">Poetry is Negotiation</a>.<br />
+      <a class="h-item" href="https://jgregorymcverry.com/negotiationofselfie">Negotiation of Selfie</a><br />
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/19/a-lost-negotiation-of-selfis-like-coming">Lost Negotiation</a></p>
+    </div>
+  </div>
+  <div class="day h-entry">
+    <time class="dt-published day-label" datetime="2020-02-19 12:00:00">19</time>
+    <div><span class="event event-end p-name">Simplicity</span><br /><p class="e-content">
+<a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/19/poetry-is-simplicitya-cadence-of-our%CB%88k%C3%A4ntsh%C9%99sn%C9%99sbeating-to--1">Poetry is Simplicity</a><br />
+<a class="h-item" href="https://jgregorymcverry.com/seeksimplicity">Seek Simplicity</a><br />
+<a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/20/poetryportseek-simplicityin-consumptionthat-which-enters-a">Seek Simplicity: How?</a></p>
+    </div>
+  </div>
+  <div class="day h-entry">
+    <time class="dt-published day-label" datetime="2020-02-20 12:00:00">20</time>
+    <div><span class="event event-end p-name">Risk</span><br /><p class="e-content">
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/20/poetry-is-the-risk-of-freedom-a-poetryport-poem-writingcommunity">Poetry is Risk</a><br />
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/20/risky-beds-a-clmooc-poetryport-poem">Risky Beds</a><br />
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/21/risk-isrooted-in-privilege-speaking-out-easywhen">Priviledge</a><br />
+      <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/20/risk-adverse-a-poetryport-poem">Risk Adverse</a><br />
+      <a class="h-item" href="https://jgregorymcverry.com/riskybird">Risky Bird</a></p>
+      </div>
+  </div>
+  <div class="day ">
+    <time class="dt-published day-label h-entry" datetime="2020-02-21 12:00:00">21</time>
+    <div><span class="event event-end">Memories</span><br /><p class="e-content">
+    <a class="h-item" href="https://quickthoughts.jgregorymcverry.com/2020/02/21/three-body-problem-a-poetryport-poem-for-the-theme-memory">Three Body Problem</a></div></p>
+  </div>
+</div>
+    </article>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
