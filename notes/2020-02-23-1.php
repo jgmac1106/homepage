fafd42b73df050cd651749f1fb9ec5556f864eab
Greg McVerry <jgregmcverry@gmail.com>
2020-02-23 10:04:33 -0500

    add note
    add note

--- notes/2020-02-23-1.php
+++ notes/2020-02-23-1.php
@@ -7,7 +7,7 @@
   	<div class="note h-entry" id="#">
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2020-02-29T09:34:20+0000">February 23, 2020</time>
-   
+ <a class="u-in-reply-to" href="https://twitter.com/jgmac1106/status/1231594625573498881">In reply to tweet  
           
       
   
@@ -15,7 +15,7 @@
         <p class="p-name e-content">Meaning slips<br/>&nbsp;&nbsp;&nbsp;whispers to the soul<br/>&nbsp;&nbsp;in slience <br />singing &nbsp;&nbsp;between&nbsp;&nbsp;&nbsp;&nbsp;stanzas<br />infernces sought&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;in spaces</br />crawling &nbsp;&nbsp;&nbsp;&nbsp;through&nbsp;&nbsp;&nbsp;&nbsp;words<br/> large enough for all out truths </p>
       
  
-  <p>Also On<a class="u-url url" href="https://jgregorymcverry.com/notes/2020-02-23-1" rel="canonical"><strong>My Notes</strong></a> <br/> ><a href="https://jgregorymcverry.com/notes/2020-02-21-14" rel="prev"> Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  <p>Also On<a class="u-url url" href="https://jgregorymcverry.com/notes/2020-02-23-1" rel="canonical"><strong>My Notes</strong></a> <br/> > <a href="https://jgregorymcverry.com/notes/2020-02-23-2" rel="next"> Next Note</a><a href="https://jgregorymcverry.com/notes/2020-02-21-14" rel="prev"> Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
   
 </div>
 </div>
---
+++ notes/2020-02-23-2.php
@@ -0,0 +1,28 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-02-29T10:04:20+0000">February 23, 2020</time>
+ <a class="u-in-reply-to" href="https://twitter.com/jgmac1106/status/1231594625573498881">In reply to tweet  
+          
+      
+  
+      
+        <p class="p-name e-content">Ooops forgot th #poetryport hashtag <a class="-u-repost-of" href="https://twitter.com/jgmac1106/status/1231594625573498881">https://twitter.com/jgmac1106/status/1231594625573498881"</p>
+      
+ 
+  <p>Also On<a class="u-url url" href="https://jgregorymcverry.com/notes/2020-02-23-2" rel="canonical"><strong>My Notes</strong></a> <br/> ><a href="https://jgregorymcverry.com/notes/2020-02-23-1" rel="prev"> Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
