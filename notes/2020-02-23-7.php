abb5cd9830138fdec7e927fb234bc03fa50d7943
Greg McVerry <jgregmcverry@gmail.com>
2020-02-23 15:03:50 -0500

    fix spacing
    fix spacing

--- notes/2020-02-23-7.php
+++ notes/2020-02-23-7.php
@@ -19,11 +19,11 @@
         	cadence of&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;swoosh of<br />
         	soft snow&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;speedy sleds<br/><br>
         	Winter sits&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Winter sits<br />
-        	silent&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;silent<br />
+        	silent&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;silent<br />
         	no&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Yet I<br />
         	humming&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;enjoy this<br />
       rythm&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;spring<br />
-  to seasons&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Febraury Air</p>p>
+  to seasons&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Febraury Air</p>
  
   <p>Also On<a class="u-url url" href="https://jgregorymcverry.com/notes/2020-02-23-7" rel="canonical"><strong>My Notes</strong></a> <br/> <a href="https://jgregorymcverry.com/notes/2020-02-23-6" rel="prev"> Previous Note</a><a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
   
