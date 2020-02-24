9bf14b75d29a87d96affda6d32a0850084087c8c
Greg McVerry <jgregmcverry@gmail.com>
2020-02-23 19:24:36 -0500

    pushing poembox
    pushing poembox

--- mypoetry.php
+++ mypoetry.php
@@ -3,8 +3,9 @@
 </head>
 <?php include 'header.php'; ?>
     </header>
-  <main class="poetry">   
-  <div class="grid-container">
+  <main>   
+  <div class="poemwrapper">
+  <div class="poembox a"> 
     <div class="h-feed" id="audiopoem">
       <div>
    <h2 class="p-name">Poembox</h2>
@@ -67,7 +68,8 @@
 </audio>
 </div>
 </div>
-<div class="h-feed poetry featured" id="featuredpoems">
+    </div>
+  <div class="poembox b"><div class="h-feed poetry featured" id="featuredpoems">
   <div>
   <h2 class="p-name">Featured Poems</h2>
   <p class="p-summary">Check out the Highlights</p>
@@ -144,13 +146,28 @@ north for that final curtain.<br/></p>
 <p><a href="https://archive.jgregorymcverry.com/poetry-from-walkmyworld/" class="u-url">Read More</a></p>
 </div>
 </div>
+</div></div>
+  <div class="poembox c">
+    <div class="h-feed poetcircle" id="poetcircle">
+    <div>
+    <h2 class="p-name">Poets I rely on</h2>
+  <p class="p-summary">Find your inspiration </p>
+  <a class="p-author" href="/"></a>
+  <time class="dt-updated" datetime="2020-02-09 06:44:20">9<sup>th</sup> February 2020</time>
 </div>
-<div class="smallpoems">
- 
-<h2>Poems Feed</h2>
-<iframe src="https://www.inoreader.com/stream/user/1005993829/tag/mypoems/view/html?cs=m" frameborder="2" tabindex="-1"></iframe>
+<ul class="poetlist">
+  <li class="h-card p-follow-of"><a class="p-name  u-url" href="http://dogtrax.edublogs.org/">Kevin Hogdson</a><span clas="p-note">Poet, friend, mentor</span></li>
+<li class="h-card p-follow-of"><a class="p-name  u-url" href="https://soconsider.wordpress.com/" title="CLMOOC Friend">Sheri Edwards</a> <span class="p-note"> CLMOOC Friend</span></li>
+<li class="h-card p-follow-of"><a class="p-name  u-url" href="http://taniasheko.com/" title="Teacher Librarian in Melbourne, Australia">Tania Sheko</a> <span class="p-note"> Teacher Librarian in Melbourne, Australia</span></li>
+<li class="h-card p-follow-of"><a class="p-name  u-url" href="http://tellio.blogspot.com/" title="Philpsopher, poet, farmer">Terry Elliot</a> <span class="p-note"> Philpsopher, poet, farmer</span></li>
+<li class="h-card p-follow-of"><a class="p-name  u-url" href="https://etalesandstories.tumblr.com/" title="Learning Technologist in Alice Springs, Australia">Wendy Taleo</a> <span class="p-note"> Learning Technologist in Alice Springs, Australia</span></li>
+<li class="h-card p-follow-of"><a class="p-name  u-url" href="https://developingwriters.org/" title="blogging about composition in the digital age">Anna Smith</a> <span class="p-note"> blogging about composition in the digital age</span></li>
+</ul>
   </div>
-  <div class="h-feed backstage" id="backstage">
+  </div>
+  <div class="poembox d"><h2>Poems Feed</h2>
+<iframe src="https://www.inoreader.com/stream/user/1005993829/tag/mypoems/view/html?cs=m" frameborder="2" tabindex="-1"></iframe></div>
+  <div class="poembox e"><div class="h-feed backstage" id="backstage">
     <div>
     <h2 class="p-name" id="backstage">How it is done</h2>
   <p class="p-summary">Read how poems came to be </p>
@@ -167,24 +184,8 @@ north for that final curtain.<br/></p>
           <li class="h-entry"><span class="p-name">How to Use Images to Teach Poetry III</span> <span class="dt-published"><time class="dt-updated" datetime="2015-03-12 13:54:20">12<sup>st</sup>March 2015 </time></span> <span class="p-summary">Responding With Images</span> <a class="u-url" href="https://archive.jgregorymcverry.com/how-to-teach-poetry-with-images-part-iii/">Read More</a></li>
      <li class="h-entry"><span class="p-name">#BackStage Blog: Making a Pixelated Gif Poem</span> <time class="dt-updated" datetime="2018-02-21 13:54:20">21<sup>st</sup>Feburary 2018 </time> </span> <span class="p-summary">A how to guide on making a pixel poem using gifs</span> <a class="u-url" href="https://archive.jgregorymcverry.com/backstage-blog-making-a-pixelated-gif-poem/">Read More</a></li>
   </ul>
-  </div>
-    <div class="h-feed poetcircle" id="poetcircle">
-    <div>
-    <h2 class="p-name">Poets I rely on</h2>
-  <p class="p-summary">Find your inspiration </p>
-  <a class="p-author" href="/"></a>
-  <time class="dt-updated" datetime="2020-02-09 06:44:20">9<sup>th</sup> February 2020</time>
-</div>
-<ul class="follow">
-  <li class="h-card p-follow-of"><a class="p-name  u-url" href="http://dogtrax.edublogs.org/">Kevin Hogdson</a><span clas="p-note">Poet, friend, mentor</span></li>
-<li class="h-card p-follow-of"><a class="p-name  u-url" href="https://soconsider.wordpress.com/" title="CLMOOC Friend">Sheri Edwards</a> <span class="p-note"> CLMOOC Friend</span></li>
-<li class="h-card p-follow-of"><a class="p-name  u-url" href="http://taniasheko.com/" title="Teacher Librarian in Melbourne, Australia">Tania Sheko</a> <span class="p-note"> Teacher Librarian in Melbourne, Australia</span></li>
-<li class="h-card p-follow-of"><a class="p-name  u-url" href="http://tellio.blogspot.com/" title="Philpsopher, poet, farmer">Terry Elliot</a> <span class="p-note"> Philpsopher, poet, farmer</span></li>
-<li class="h-card p-follow-of"><a class="p-name  u-url" href="https://etalesandstories.tumblr.com/" title="Learning Technologist in Alice Springs, Australia">Wendy Taleo</a> <span class="p-note"> Learning Technologist in Alice Springs, Australia</span></li>
-<li class="h-card p-follow-of"><a class="p-name  u-url" href="https://developingwriters.org/" title="blogging about composition in the digital age">Anna Smith</a> <span class="p-note"> blogging about composition in the digital age</span></li>
-</ul>
-  </div>
-   <div class="h-feed poemreads" id="poemreads">
+  </div></div>
+  <div class="poembox f"><div class="h-feed poemreads" id="poemreads">
     <div>
     <h2 class="p-name" >Poets I am reading</h2>
   <p class="p-summary">Find your mentor text </p>
@@ -196,7 +197,8 @@ north for that final curtain.<br/></p>
     <li class="h-entry"><span class="p-name">Robert Hunter</span> <span class="dt-published"><time class="dt-published" datetime="2020-02-09 06:44:20">9<sup>th</sup> February 2020</time></span> <span class="p-summary">A collection of poems from my favorite lyricist</span> <a class="u-url" href="http://www.hunterarchive.com/files/Poetry/SentinelGateway.html">Sentinel</a><img src="https://img1.od-cdn.com/ImageType-400/1523-1/822/0C7/CA/%7B8220C7CA-0367-4D96-8621-3EB481089808%7DImg400.jpg"></li>
     <li class="h-entry"><span class="p-name">Bill Moyers</span> <span class="dt-published"><time class="dt-published" datetime="2020-02-09 06:44:20">9<sup>th</sup> February 2020</time></span> <span class="p-summary">Interviews with Poets</span> <a class="u-read-of" href="https://billmoyers.com/series/fooling-with-words-1999/">Fooling with Poets</a><img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1348765356l/615561.jpg"></li>
   </ul>
-  </div>
+  </div></div>
+</div>
     </main>
     <footer>
 <?php include 'footer.php'; ?>
