bbf8acaf5cc1802cda24073c93098ac499fb0865
Greg McVerry <jgregmcverry@gmail.com>
2020-02-25 15:18:20 -0500

    Update mypoetry.php
    fix wrong header level

--- mypoetry.php
+++ mypoetry.php
@@ -184,7 +184,7 @@
   </div>
 </div>
 <div class="h-cite">
-  <h1 class="p-name">Negotiation of the Selfie </h1>
+  <h3 class="p-name">Negotiation of the Selfie </h3>
   <div class="pubinfo">
   <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
      on <time class="dt-published" datetime="2020-02-19 06:14:20">19<sup>th</sup> February 2020</time></p>
