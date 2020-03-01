b4fede412f27c419fbd29d1403fbd1e53a2731ba
Greg McVerry <jgregmcverry@gmail.com>
2020-03-01 14:24:57 -0500

    Create 2020-03-01-1.php
    add a note

---
+++ notes/2020-03-01-1.php
@@ -0,0 +1,28 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<a class="u-reply-to" href="https://twitter.com/pennyjw/status/1231030229528805376">In reply to twwet
+<time class="dt-published" datetime="2020-02-29T15:14:20+0000">February 23, 2020</time>
+ 
+          
+      
+  <img class="u-photo" href="https://jgregorymcverry.com/poetry/concretetest.gif" alt="">
+      
+        <p class="p-name e-content">Here is your belated #poetryport gift. I need to make my puppet bigger. Was my first attempt at stop motion concrete poems</p>
+ 
+  <p>Also On<a class="u-url url" href="https://jgregorymcverry.com/notes/2020-03-02-1" rel="canonical"><strong>My Notes</strong></a> <br/>  <a href="https://jgregorymcverry.com/notes/2020-02-23-8" rel="prev"> Previous Note</a><a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
