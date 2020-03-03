130a899a0542dfab8aee7c9e201b391bb4308598
Greg McVerry <jgregmcverry@gmail.com>
2020-03-03 10:36:01 -0500

    add a page of data sources
    adding reposts of data sources

--- footer.php
+++ footer.php
@@ -36,6 +36,10 @@
     </ul>
 </div>
 <div class="footer_info">
+    <a href="/" class="h-card">
+    <img src="photos/assets/thumb.jpg" class="u-photo"> 
+    <span class="p-name">Greg McVerry</span>
+  </a>
       <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a> 
         <p>Any video or image containing likeness of children is All Rights Reserved.</p>
       <a   href="https://microformats.org"><img class="https://indieweb.org/images/7/72/microformats-button.png"></a><br>
