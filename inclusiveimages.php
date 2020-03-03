d78e4c5769993a30386dbc291881728f87eea9fc
Greg McVerry <jgregmcverry@gmail.com>
2020-03-03 06:47:26 -0500

    Update inclusiveimages.php
    add border

--- inclusiveimages.php
+++ inclusiveimages.php
@@ -8,104 +8,104 @@
 <blockquote>
 <table class="e-content" style="border: 1px solid black">
 <tbody>
-<tr>
-<td colspan="1" rowspan="1">
+<tr style="border: 1px solid black" > 
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>Collection</p>
 </td>
-<td colspan="1" rowspan="1">
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>Description</p>
 </td>
-<td colspan="1" rowspan="1">
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>License</p>
 </td>
 </tr>
-<tr>
-<td colspan="1" rowspan="1">
+<tr style="border: 1px solid black" > 
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>The Gender Spectrum Collection</p>
 <p><a href="https://www.google.com/url?q=https://genderphotos.vice.com/&amp;sa=D&amp;ust=1583236708341000">https://genderphotos.vice.com/</a></p>
 </td>
-<td colspan="1" rowspan="1">
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>&ldquo;The Gender Spectrum Collection is a stock photo library featuring images of trans and non-binary models that go beyond the clich&eacute;s.&rdquo;</p>
 </td>
-<td colspan="1" rowspan="1">
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>Creative Commons</p>
 <p>Attribution</p>
 <p>Non-Commercial</p>
 <p>No Derivatives</p>
 </td>
 </tr>
-<tr>
-<td colspan="1" rowspan="1">
+<tr style="border: 1px solid black" > 
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>Nappy</p>
 <p><a href="https://www.google.com/url?q=https://www.nappy.co/&amp;sa=D&amp;ust=1583236708343000">https://www.nappy.co/</a></p>
 </td>
-<td colspan="1" rowspan="1">
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>&ldquo;Beautiful, high-res photos of black and brown people. <a href="https://www.google.com/url?q=https://www.nappy.co/license&amp;sa=D&amp;ust=1583236708343000">For free.</a>&rdquo;</p>
 </td>
-<td colspan="1" rowspan="1">
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>Creative Commons&nbsp;Zero (Public Domain)</p>
 </td>
 </tr>
-<tr>
-<td colspan="1" rowspan="1">
+<tr style="border: 1px solid black" > 
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>PICNOI</p>
 <p><a href="https://www.google.com/url?q=https://picnoi.com/&amp;sa=D&amp;ust=1583236708344000">https://picnoi.com/</a></p>
 </td>
-<td colspan="1" rowspan="1">
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>&ldquo;Free Stock Photo for a Colorful World&rdquo;</p>
 </td>
-<td colspan="1" rowspan="1">
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>Traditional Copyright, but site grants permission for free use without giving credit to source</p>
 </td>
 </tr>
-<tr>
-<td colspan="1" rowspan="1">
+<tr style="border: 1px solid black" > 
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>CreateHerStock</p>
 <p><a href="https://www.google.com/url?q=https://createherstock.com/&amp;sa=D&amp;ust=1583236708345000">https://createherstock.com</a></p>
 </td>
-<td colspan="1" rowspan="1">
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>&ldquo;Your destination for images featuring melanated women&rdquo;</p>
 </td>
-<td colspan="1" rowspan="1">
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>Traditional Copyright, but site grants permission for free use without giving credit to source</p>
 </td>
 </tr>
-<tr>
-<td colspan="1" rowspan="1">
+<tr style="border: 1px solid black" > 
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>WOCinTech</p>
 <p><a href="https://www.google.com/url?q=https://www.flickr.com/photos/wocintechchat/&amp;sa=D&amp;ust=1583236708347000">https://www.flickr.com/photos/wocintechchat/</a></p>
 </td>
-<td colspan="1" rowspan="1">
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>Flickr curated photostream to reflect women of color in technology settings</p>
 </td>
-<td colspan="1" rowspan="1">
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>Creative Commons</p>
 <p>Attribution</p>
 </td>
 </tr>
-<tr>
-<td colspan="1" rowspan="1">
+<tr style="border: 1px solid black" > 
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>Redefining Women</p>
 <p><a href="https://www.google.com/url?q=https://thenounproject.com/Iconathon1/collection/redefining-women/&amp;sa=D&amp;ust=1583236708348000">https://thenounproject.com/Iconathon1/collection/redefining-women/</a></p>
 </td>
-<td colspan="1" rowspan="1">
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>Curated collection inside of The Noun Project</p>
 </td>
-<td colspan="1" rowspan="1">
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>Creative Commons</p>
 <p>Attribution</p>
 </td>
 </tr>
-<tr>
-<td colspan="1" rowspan="1">
+<tr style="border: 1px solid black" > 
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>Fresh-Folk</p>
 <p><a href="https://www.google.com/url?q=https://fresh-folk.com/&amp;sa=D&amp;ust=1583236708349000">https://fresh-folk.com/</a></p>
 </td>
-<td colspan="1" rowspan="1">
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>&ldquo;an illustration library of</p>
 <p>people and objects...Mix and Match. Each character comes with a range of poses, outfits and skin tones&rdquo;</p>
 </td>
-<td colspan="1" rowspan="1">
+<td colspan="1" rowspan="1" style="border: 1px solid black" > 
 <p>Creative Commons</p>
 <p>Attribution</p>
 </td>
