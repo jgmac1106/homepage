e8004cd48249ae9bfaa849318861f02ee36bf24c
Greg McVerry <jgregmcverry@gmail.com>
2020-03-04 21:22:58 -0500

    Create supertuesdayreflection.php
    supertuedsay reflection

---
+++ supertuesdayreflection.php
@@ -0,0 +1,56 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">Reflections on Super Tuesday Results</h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
+     on <time class="dt-published" datetime="2020-03-04 8:04:20">4<sup>th</sup>March 2020</time></p>
+    
+
+    
+    <span class="summary">
+    <p class="p-summary">My incorrect and incoherent analysis of Super Tuesday election. </p>
+    </span>
+  </div>
+    <div class="copy">
+
+  <div class="e-content">
+    <p>My thoughts on Super Tuesday. I am not that invested. My primary vote rarely matters. I live in Connecticut...weeks after we begin. I have no idea who is left by the time it gets to me. Instead I am forced to watch some game show of political survivor. Then a candidate is chosen for President, and no matter how I vote Connecticut will go Blue. So I guess this is like a review of how the game show contestants did. </p>
+     <h2>Bloomberg</h2>
+     <p>We witnessed the power of Black Lives Matter movement. Connections to the Reagan War on Drugs and the 90's "get tough on crime" response to the urban decay caused by redlining and blamed on hip-hop was a campaign ender (Unless you are Biden). This problem helped to take down both Amy Klobuchar and Kamala Harris. Problem for Bloomberg he was doing this just a fear years ago. I think I saw him throw some kid against the wall last week. </p>
+     <p>Then Elizabeth Warren came around the corner. knocked Bloomberg to the debate floor and wiped up the blood with his checkbook.</p>
+     <p>Hey, here's to American Samoa. So sad I live in a country with colonial property</p>
+     <h2>Warren</h2>
+     <p>Tough. Smart. Policy driven. Consistently last place. Why? I think it had as much to do with the word "free" as it did misogyny. Which is the same thing. Warren had the most progressive polices of any candidate, but the word free will not fly in America. It is a trigger word. Like Common Core math. The immediate response in the land of not poor but "failed billionaires" to the word "free" is, "Ain't nothing free. Whose gonna pay for it. No way you taking my paycheck and giving it away for some kid to go to college. What's college good for anyways. Get a real job."</p>
+     <p>If you believe in the progressive policies change up your messaging...and never...ever.. ever...like ever...ever...say free. Frame student loan forgiveness ("ahh poor idiot borrowed more should have gone somewhere cheaper") as a middle class stimulus package to fight Coronavirus. Make it a tax deduction that can go negative.</p>
+     <p>Take cue from health care. Folks stop saying free health care. They started saying "Medicare for All" and then watered down from there. Do not say "free college"  call it Opportunity Grants. Maybe make a national service requirement to qualify. Make free more palpable...Oops I said free.</p>
+     <p>Mysoginy clearly played a role in Bernie Sanders taking second place as the progressive candidate of choice. Warren had better policies and explained how they would be paid for, even if no one believed her math. You know being a girl and all.</p>
+     <p>There are five pillars to the success of the Republican Party over the last 10 years. Some spanning much further back. Limbaugh, Lutz, Norquist, Misogyny, and Racism. The first three kept the Republican Party on message. To a point where it is almost admirable. Lutz bent the language, Limbaugh spread it, and Norquist demanded fealty. Taught us fiscal conservative meant Republican not in power. </p>
+     <p>Misogyny and Racism, evils that flow through both parties, were weaponized by the Republican Party. Hillary Clinton was labeled the femi-nazi for fighting for Health Care. Then humiliated when her husband, the President, got impeached for lying about an affair with an intern. <p>Racism, most explicitly showed itself in redistricting and voter suppression issues. Tactics still being used today.</p>
+     <p>A bias against Massachusetts also played a role. Unless your name is Kennedy, and probaly because of the Kennedy's, and anyone who went to Harvard, people hate people from Massachusetts. I was biased by this bias myself as I saw no electoral college advantage from someone from Massachusetts. Especially a "liberal." It is an invitation to lose the general</p>
+     <h2>Sanders</h2>
+     <p>Seems popular in the Birkenstock Beltway. Most Americans are now sunbernt and think Sanders should retire. Maybe make a good living with the on kids on campus. They can eat Ben and Jerry's together and complain about Professors who listen to Phish music. Heck, invite Katy Tur.</p>
+     <p> Is that ageist, yes, well not the Tur part, that's more easter Egg, but age matters (unless you are Biden). Sanders hit not much a ceiling, but had the roof pushed in by an American public that feared nominating a socialist for the Democratic nomination.</p>
+     <p>Guns. A lot of moms hate Bernie's history on guns. This wasn't spoken about much in the debates, but as someone from a state where many know someone touched by Gun Violence. Moms, rich, poor, black, white, or latinx did not trust Bernie on gun violence.</p>
+     <p>People want to win. Turn out was massive for Biden. He ran a tiny campaign compared to the Sanders juggernaut. The Establishment and the public coalesced by a central drive to beat Trump. They want the primary over. It will be after Michigan.</p>
+     <p>I heard the line, "You only get to vote socialist once," from so many people who do not want to vote for Trump but may have if Bernie won the nomination. If you want a socialist movement in this country you can not start it at the Presidential level. You can't have a candidate praise despots (even though we have history of propping them up...and having it backfire...Osama Bin Laden ). Go for the state houses down ballot in the next election.</p>
+     <h2>Biden</h2>
+     <p>American Pragmatism. People want to win. Trump fears Biden so much he is willing to risk his entire Presidency extorting a foreign government. Therefore, Biden has best chance of winning. Therefore vote Biden. and hope he doesn't talk much. I am so excited by the turnout in the primaries. People primed to defeat Trump. This mattered more than any candidate. Any policy. Any debate. </p>
+     <p>The Obama Halo effect matters. When Clyburn said, "He knows us," he meant it. This helped turned the primary. The minority vote is often cast as monolithic, yet we saw the Latinx vote go for Sanders and the Black vote to Bernie. This will be interesting to study and understand in the coming weeks and months. The Black South either trust Biden, or at the least trust Biden to win.</p>
+     <p>Many people talk Medicare for All. So much discussion on health care. People like doing more than talking. Obama and Biden finished the work Hillary started (by adopting the Republican Plan) and facing the Tea Party Backlash (first three pillars of Republican success) which gave rise to Trumpism as the Donald perfected media manipulation by exploiting racism and misogyny (last two pillars of Republican success). People feel safe with Obamacare and fear Trump will take it away. The man who helped get it done is therefore the best person to protect it.</p>
+     <p>Biden has a compelling story. The last working class member of the Senate who took the train home everyday. A man who lost his first wife and children in a horrible accident and got back up. A man who lost another child, a rising star himself, and who then openly grieved with the nation. A man who has his last surviving son used in a Republican cover up and Trump plot to ruin his chances of being President. Someone who will stand up and call anyone attacking his family a liar. Even challenge them to push-sps...If we are picking based on Presidential Fitness Awards, my money is on Biden. Many Americans stood up and said, "Joe, we got your back."</p>
+     <p>Built in electoral advantage. We do not have a democratic election in our Republic. Founders feared the logic of the masses (not mentioning women couldn't vote and Blacks were only 3/5 a person). We elect people to an Electoral College. Hillary crushed Trump, but Trump won because of this archaic sexist and racist political history. Biden is Pennsylvania's second son. You can move it back to blue. This matters to people</p>
+     <p>So that's it folks. Parks closed. Moose out front should have told you. See you in the general after Michigan.</p>
+
+      </div>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
