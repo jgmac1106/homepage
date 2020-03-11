853e3d6560c47bc7f2fbae2aef6eafac2b78ee3c
Greg McVerry <jgregmcverry@gmail.com>
2020-03-11 12:23:35 -0400

    Update mypoetry.php
    add a poem

--- mypoetry.php
+++ mypoetry.php
@@ -16,7 +16,7 @@
    <p class="p-summary">A boombox of bad poems</p>
    <data class="p-category" value="arts"></data>
 
-    <time class="dt-updated" datetime="2020-03-07 08:34:20">7<sup>th</sup>March 2020</time>
+    <time class="dt-updated" datetime="2020-03-11 12:24:20">11<sup>th</sup></time>
    <img class="u-photo" src="https://jgregorymcverry.com/photos/poemboxpodcastart.jpg" style="width:200px">
    <a class="u-author h-card" href="https://jgregorymcverry.com/mypoetry#audiopoems">jgmac1106</a>
  </div>
@@ -260,6 +260,15 @@
 </audio>
 <data class="p-size" value="611356"></data><data class="p-duration" value="PT00H00M12S"></data>
 <p class="e-content"><a class="u-url" href="https://quickthoughts.jgregorymcverry.com/2019/01/13/constraints-oncreativityseen-throughamyburvallseyesrhizomatic-learning-breeds-that">Show Link</a></p>
+</div>
+ <div class="h-entry audiopoem">
+  <p class="p-name">Cry For My Children</p>
+  <a class="p-author" href="/"></a>
+  <time class="dt-published" datetime="2020-03-11 12:24:20">11<sup>th</sup>March 2020</time>
+ <audio class="u-audio" src="https://jgregorymcverry.com/poetry/cryformychildren.m4a" controls="controls">
+</audio>
+<data class="p-size" value="611356"></data><data class="p-duration" value="PT00H00M12S"></data>
+<p class="e-content"><a class="u-url" href="https://quickthoughts.jgregorymcverry.com/2020/02/27/cry-for-my-children-a-poetrypoem-of-diversity">Show Link</a></p>
 </div>
 </div>
 <div class="audiopoem">
