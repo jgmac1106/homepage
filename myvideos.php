0375e0ca5e508ca2d779bf54cb23858d49d685f1
Greg McVerry <jgregmcverry@gmail.com>
2020-03-12 11:13:08 -0400

    Update myvideos.php
    fix links

--- myvideos.php
+++ myvideos.php
@@ -55,7 +55,7 @@
       <p class="p-name">Making students feel safe</p>
 
       <a class="p-author" href="/"></a>
-   <video  controls class="u-video" poster="/videos/covidonline3.png" src="https://jgregorymcverry.com/videos/COVID-19OnlineTeachingIII.mp4"> 
+   <video  controls class="u-video" poster="/videos/covid19online3.png" src="https://jgregorymcverry.com/videos/COVID-19OnlineTeachingIII.mp4"> 
           </video>
           
       <p class="p-summary">Instructional Design Tips to Reduce Student Anxiety</p>
@@ -65,7 +65,7 @@
       <p class="p-name">Live Vlog of Instructional Design </p>
 
       <a class="p-author" href="/"></a>
-   <video  controls class="u-video" poster="/videos/covidonline4.png" src="https://jgregorymcverry.com/videos/COVID19InstructionalDesignIVLiveVlog.mp4"> 
+   <video  controls class="u-video" poster="/videos/covid19nline4.png" src="https://jgregorymcverry.com/videos/COVID19InstructionalDesignIVLiveVlog.mp4"> 
           </video>
           
       <p class="p-summary">Watch Me Go Step By Step Through My Instructional Design Process</p>
