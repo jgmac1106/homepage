f88a8392479543bdf4d3ea909ffa54490dce132e
Greg McVerry <jgregmcverry@gmail.com>
2020-03-14 13:00:38 -0400

    Create myrunningtrail.php
    adding a post

---
+++ myrunningtrail.php
@@ -0,0 +1,32 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">My IndieWeb Summit Trip</h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregory McVerry</a>
+     on <time class="dt-published" datetime="2020-03-12 12:44:20">12<sup>th</sup>March 2019</time></p>
+    <p>
+      
+   
+  <span class="summary">
+    <p class="p-summary">Tracking My Month of Getting Healthy</p>
+    </span>
+ </div>
+    <div class="copy">
+  <div class="e-content">
+    <iframe style="width: 90%; height: 90vh;" src="https://jgregorymcverry.com/music/index.php/apps/gpxmotion/view?path=%2Fgpx%2F2020-03-09-17-15-00.gpx"></iframe>
+    <p>It looks like my third lap got cut off. It took me just over 24 minutes to run/jog/walk the mile. I was stopping and clearing sticks from the trail, still I hope to improve the time. Might do some interval running today to help improve my stamina. </p>
+    <p>In terms of stating and eating healthy. I have stuck to nothing but three meals a day and only water (in forms of selzer and coffee. I did do two cheat days. Last Saturday I went to the Unviersity of Hartford Basketball and last night, just because. </p>
+  </div>
+      </div>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
