352e39e8c68909806e4848445147f475c72dfee4
Greg McVerry <jgregmcverry@gmail.com>
2020-03-14 17:24:50 -0400

    fix css
    add an h-card fix the layout on a post

---
+++ funh-card.php
@@ -0,0 +1,26 @@
+<?php include 'head.php'; ?>
+   <link rel="stylesheet" href="/css/funhcard.css">
+<?php include 'header.php'; ?>
+    </header>
+    <div class="typography-art h-card">
+    <div class="hello-world p-name"><p>J. Gregory McVery</p></div>
+    <div class="typography-sq p-summary">
+      <div class="full"><p>Make</p></div>
+      <div class="stack"><p>Hack</p></div>
+      <div class="developer"><a class="u-url" href="https://jgregorymcverry.com">Webmaker</a></div>
+      <div class="front-end"><p>Professor</p></div>
+      <div class="program"><p>Teacher</p></div>
+      <div class="design"><p>Poet</p></div>
+      <div class="code"><p>coder</p></div>
+      <div class="software"><p>play</p></div>
+      <div class="creative"><p>learn</p></div>
+      <div class="engineer"><p>Tr<span class="letter-g">ou</span>blemaker</p></div>
+      <div class="image"><img src="https://jgregorymcverry.com/photos/assets/thumb.jpg" class="u-photo" /></div>
+    </div>
+  </div>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
