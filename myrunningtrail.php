140dd67bd79832e8111f11b1da6629ea66cf6b72
Greg McVerry <jgregmcverry@gmail.com>
2020-03-14 13:34:45 -0400

    Update myrunningtrail.php
    add stats

--- myrunningtrail.php
+++ myrunningtrail.php
@@ -20,6 +20,15 @@
     <iframe style="width: 90%; height: 90vh;" src="https://jgregorymcverry.com/music/index.php/apps/gpxmotion/publicview?token=59DiocaSd2qhDbQ&layer=OpenStreetMap&autoplay=1&autozoom=1&loop=1"></iframe>
     <p>It looks like my third lap got cut off. It took me just over 24 minutes to run/jog/walk the mile. I was stopping and clearing sticks from the trail, still I hope to improve the time. Might do some interval running today to help improve my stamina. </p>
     <p>In terms of stating and eating healthy. I have stuck to nothing but three meals a day and only water (in forms of selzer and coffee. I did do two cheat days. Last Saturday I went to the Unviersity of Hartford Basketball and last night, just because. </p>
+    <ul>
+    <li>Total distance 4,975 ft</li>
+    <li>Steps 3070</li>l
+    <li>Time 00:24:00</li>
+    <li>+Elevation 277 ft</li>
+    <li>-Elevation 325 feet</li>
+    <li>Highest point 496 ft</li>
+    <li>lowest poinst 383 ft</li>
+  </ul>
   </div>
       </div>
 </article>
