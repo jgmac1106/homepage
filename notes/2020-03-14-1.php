7c2a7a6177f8f9fe1bf353ca17947048eeb7c52c
Greg McVerry <jgregmcverry@gmail.com>
2020-03-14 09:19:32 -0400

    Create 2020-03-14-1.php
    adding post

---
+++ notes/2020-03-14-1.php
@@ -0,0 +1,28 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry" id="#">
+<a class="u-author" href="/"></a>
+<a class="u-reply-to" href="https://twitter.com/pennyjw/status/1231030229528805376">In reply to twwet
+<time class="dt-published" datetime="2020-03-14T09:14:20+0000">March 14 2020</time>
+ 
+          
+      
+  <img class="u-photo" src="https://jgregorymcverry.com/poetry/concretetest.gif" alt="">
+      
+        <p class="p-name e-content">Watching @kevinmarks <a href="https://kevinmarks.com">https://kevinmarks.com</a> deliver a session on Building Blocks</p>
+ 
+  <p>Also On<a class="u-url url" href="https://jgregorymcverry.com/notes/2020-03-14-1" rel="canonical"><strong>My Notes</strong></a> <br/>  <a href="https://jgregorymcverry.com/notes/2020-02-23-8" rel="prev"> Previous Note</a><a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  
+</div>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
