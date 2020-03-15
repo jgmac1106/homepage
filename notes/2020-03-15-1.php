83b0e97fb9d551f72c59214e501f5e4aee644690
Greg McVerry <jgregmcverry@gmail.com>
2020-03-15 13:08:23 -0400

    Create 2020-03-15-1.php
    add a photo

---
+++ notes/2020-03-15-1.php
@@ -0,0 +1,26 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-03-15 13:04:20">15<sup>th</sup>March 2020</time>
+<img class="u-photo" src="/photos/iwclondon/Day2pic.png" alt="screenshot of participants">
+ <p class="p-name e-content">
+What an amazing demo day!! Yeah we didn't have the mad dash and energy of the live demos, but this felt mroe relaxed with an open question and answer session...No worries of getting kicked out of the venue. One of my favorites yet!
+   
+ </p>
+ <p>  Also on
+ <a class="u-url" href="https://jgregorymcverry.com<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "utf-8"); ?>
+ " rel="canonical"><strong>My Notes</strong></a><a href="https://jgregorymcverry.com/notes/2020-03-14-1" rel="prev">Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a></p>
+</div>
+  	</main>
+  	<div id="webmentions"></div>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
