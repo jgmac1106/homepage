2a1c7c1fb233a35b24c89c78bd736e2fc943d3ef
Greg McVerry <jgregmcverry@gmail.com>
2020-03-16 12:22:58 -0400

    add note
    add note

--- notes/2020-03-16-2.php
+++ notes/2020-03-16-2.php
@@ -9,11 +9,11 @@
 <time class="dt-published" datetime="2020-03-16 11:04:20">16<sup>th</sup>March 2020</time>
 
  <p class="p-name e-content">
-To everyone about to start their journey into recording microlectutes I highly recommened @screencasto. Best 15 dollars I spend on pedagogy. Not just recording, the dead simple but powerful editing interface.
+To everyone about to start their journey into recording microlectures I highly recommened @screencasto. Best 15 dollars I spend on pedagogy. Not just recording, the dead simple but powerful editing interface.
    
  </p>
  <p>  Also on
- <a class="u-url" href="https://jgregorymcverry.com/notes/2020-03-16-2" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-03-16-1" rel="prev">Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a></p>
+ <a class="u-url" href="https://jgregorymcverry.com/notes/2020-03-16-2" rel="canonical"><strong>My Notes</strong></a> <a class="u-url" href="https://jgregorymcverry.com/notes/2020-03-16-3" rel="next">Next </a> <a href="https://jgregorymcverry.com/notes/2020-03-16-1" rel="prev">Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a></p>
 </div>
   	</main>
   	<div id="webmentions"></div>
---
+++ notes/2020-03-16-3.php
@@ -0,0 +1,25 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-03-16 12:24:20">16<sup>th</sup>March 2020</time>
+
+ <p class="p-name e-content">
+I just published the 5th video in my series on Teaching Online In a Time of crisis. <a href="https://jgregorymcverry.com/myvideos#covid19instructionaldesign">https://jgregorymcverry.com/myvideos#covid19instructionaldesign</a>. I dicuss ten active learning techniques in ten minutes.
+   
+ </p>
+ <p>  Also on
+ <a class="u-url" href="https://jgregorymcverry.com/notes/2020-03-16-3" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-03-16-2" rel="prev">Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a></p>
+</div>
+  	</main>
+  	<div id="webmentions"></div>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
