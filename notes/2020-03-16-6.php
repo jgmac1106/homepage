aef95bafdf7262eac5c4460386d815102a48c270
Greg McVerry <jgregmcverry@gmail.com>
2020-03-16 16:44:12 -0400

    fix notes
    fix notes

--- notes/2020-03-16-6.php
+++ notes/2020-03-16-6.php
@@ -6,11 +6,12 @@
   	<div class="notewrapper">
   	<div class="note h-entry">
 <a class="u-author" href="/"></a>
-
-<time class="dt-published" datetime="2020-03-16 13:44:20">16<sup>th</sup>March 2020</time>
+<a class="u-in-reply-to" href="https://twitter.com/Karis_M_Jones/status/1239616626359709698">tweet</a>
+<time class="dt-published" datetime="2020-03-16 16:344:20">16<sup>th</sup>March 2020</time>
 
  <p class="p-name e-content">
- I just published the 4th video in my College Students Guide to Surviving Online Learning. <a href="https://jgregorymcverry.com/myvideos#OnlineSurvivalGuide">https://jgregorymcverry.com/myvideos#OnlineSurvivalGuide</a>/ I go over five time management strategies. If you have a torrent client please seed and help me spread out the bandwidth. 
+@karisa-m-jones this is such a great list! I might add being a participant observer is best. Choose a fandom space where you are interested and can then be onboarded as someone new to space. Nobody like to feel they are being stared at in a fish bow;
+   
  </p>
  <p>  Also on
  <a class="u-url" href="https://jgregorymcverry.com/notes/2020-03-16-5" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-03-16-4" rel="prev">Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a></p>
