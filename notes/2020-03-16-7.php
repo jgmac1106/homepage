2cb4dfda3a18a61f1bbdfe07e57aece609f205b8
Greg McVerry <jgregmcverry@gmail.com>
2020-03-16 19:16:29 -0400

    Update 2020-03-16-7.php
    remove u-in-reply-to

--- notes/2020-03-16-7.php
+++ notes/2020-03-16-7.php
@@ -6,11 +6,10 @@
   	<div class="notewrapper">
   	<div class="note h-entry">
 <a class="u-author" href="/"></a>
-<a class="u-in-reply-to" href="https://twitter.com/Karis_M_Jones/status/1239616626359709698">tweet</a>
 <time class="dt-published" datetime="2020-03-16 16:344:20">16<sup>th</sup>March 2020</time>
 
  <p class="p-name e-content">
-Second day of Coronacation...erm Sprin Break...loking to be busy...gonna facilitate drop in design studio for @scsu at 9:00. Help run a session on facilitating group work as 10:00, microlectures and Kalutra at 11:00, and then drop in design studio again at 16:00. We got your back, owls
+Day 2Coronacation,erm Sprin Break, be busy,facilitate drop in design studio for @scsu at 9:00. Help run session on facilitating group work  10:00, microlectures/Kalutra 11:00, then drop in design studio again 16:00. We got your back owls!!
    
  </p>
  <p>  Also on
