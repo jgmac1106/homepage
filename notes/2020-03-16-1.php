54dfcea40582f04b4c0749b35e48aa0cdc92030c
Greg McVerry <jgregmcverry@gmail.com>
2020-03-16 10:09:52 -0400

    add notes
    adding note

---
+++ notes/2020-03-16-1.php
@@ -0,0 +1,25 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-03-16 10:04:20">16<sup>th</sup>March 2020</time>
+<a class="u-in-reply-to" href="https://twitter.com/Jessifer/status/1239175861414375424">Tweet</a>
+ <p class="p-name e-content">
+@jessifer yet the research with online learning and grades is pretty replicable. Frequent grading and quizzes lead to higher student efficacy, satisfaction, course grades, and retention. We have to acknowlede in ungrading classes that grades make students feel safe. Hypothesis: can do the same with feedback and pass/fail.
+   
+ </p>
+ <p>  Also on
+ <a class="u-url" href="https://jgregorymcverry.com/notes/2020-03-16-1" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-03-15-1" rel="prev">Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a></p>
+</div>
+  	</main>
+  	<div id="webmentions"></div>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
