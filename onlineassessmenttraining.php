7bd7ccb6a062fc9169e361de1dca0e1ac59458a4
Greg McVerry <jgregmcverry@gmail.com>
2020-03-16 18:09:19 -0400

    Create onlineassessmenttraining.php
    add a post

---
+++ onlineassessmenttraining.php
@@ -0,0 +1,161 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">Improving Online Learning Through High Quality Feedback</h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
+     on <time class="dt-published" datetime="2020-03-16 18:04:20">16<sup>th</sup>March 2020</time></p>
+   
+
+    
+    <span class="summary">
+    <p class="p-summary">Lesson Plan on Tomorrow's Session On Feedback and Online Learning.</p>
+    </span>
+    <div class="copy">
+
+  <div class="e-content">
+
+<p>Goal: Participants in this session will develop strategies to adapt their assessments to online learning. We will discuss how feedback and not a measurement tool drives learning and develop ideas for continuous checks for understanding.</p>
+<p>Session Length: 45 min synchronous, 1 hour asynchronous</p>
+<p>Objectives:</p>
+<ul>
+<li>Brainstorm online assessment strategies aligned to course outcomes</li>
+</ul>
+<ul>
+<li>Create a rubric in Blackboard</li>
+<li>Create a quiz in Blackboard</li>
+<li>Write feedback</li>
+<li>Use polling tools for informal assessment</li>
+</ul>
+<p>Schedule:</p>
+<ul>
+<li>5 minute entrance slip</li>
+<li>10</li>
+<li>15 minute Begin at the End</li>
+<li>15 Minute Frequent Checks for Understanding</li>
+<li>10 minutes Deliver effective feedback</li>
+</ul>
+<p>Learning Activities:</p>
+<p>Begin by discussing their overall course outcomes. You can use the 5 year experiment (Wiggins). If students met fiuve years from now and needed to use your course content what would the student who took your class know and be able to do?</p>
+<p>You will not have time to complete the table but model how to take one course objective and identify the expected gains in knowledge and skills.</p>
+<p>Then as a group brainstorm ideas on how evidence of this growth can be elicited in online environments. Many attendees will have no training in online teaching or basic pedagogy and assessment techniques. Have a list of assessment ideas ready.</p>
+<p>Provide a document with the following table:</p>
+<p>&nbsp;</p>
+<table>
+<tbody>
+<tr>
+<td colspan="1" rowspan="1">
+<p>Course Outcome/Objective</p>
+</td>
+<td colspan="1" rowspan="1">
+<p>What must students know</p>
+</td>
+<td colspan="1" rowspan="1">
+<p>What must students do</p>
+</td>
+<td colspan="1" rowspan="1">
+<p>How can I elicit evidence of growth online?</p>
+</td>
+</tr>
+<tr>
+<td colspan="1" rowspan="1">&nbsp;</td>
+<td colspan="1" rowspan="1">&nbsp;</td>
+<td colspan="1" rowspan="1">&nbsp;</td>
+<td colspan="1" rowspan="1">&nbsp;</td>
+</tr>
+<tr>
+<td colspan="1" rowspan="1">&nbsp;</td>
+<td colspan="1" rowspan="1">&nbsp;</td>
+<td colspan="1" rowspan="1">&nbsp;</td>
+<td colspan="1" rowspan="1">&nbsp;</td>
+</tr>
+<tr>
+<td colspan="1" rowspan="1">&nbsp;</td>
+<td colspan="1" rowspan="1">&nbsp;</td>
+<td colspan="1" rowspan="1">&nbsp;</td>
+<td colspan="1" rowspan="1">&nbsp;</td>
+</tr>
+</tbody>
+</table>
+<p>After modeling how to use the graphic organizer demo how to make a rubric in Blackboard. Share a video tutorial with participants as well.</p>
+<p>After talking about final and major assessments shift to shorter more frequent checks for understanding. Explain the research that this helps to support the cognitive presence of students while also allowing you to assess and activate prior knowledge.</p>
+<p>Demo how to create a quiz in blackboard. Encourage participants to check to see if their text comes with ready made question banks.</p>
+<p>Then demo how polling tools can be used for quick assessments, by asking to rate their efficacy on assessing students online.</p>
+<p>Finally shift the conversation to delivering feedback which improves rather than simply measures learning. Provide a rubric for a discussion board. Pass out example posts. Demonstrate how you rate students using the language of the rubric, connect to explicit evidence of their progress, and provide actionable feedback.</p>
+<p>A criteria based rubric consist of the following parts:</p>
+<ol start="1">
+<li>Scale descriptors, scale values, and criteria</li>
+</ol>
+<ol start="2">
+<li>Criteria, final score, feedback statements</li>
+</ol>
+<ol start="3">
+<li>Scale values, validity, and reliability</li>
+</ol>
+<ol start="4">
+<li>Single scale, multiple criteria, feedback statement</li>
+</ol>
+<p>Ms Vazquez is utilizing a rubric where she gives one score across three different criteria. They are using:</p>
+<ol start="1">
+<li>Criterion-based rubric</li>
+</ol>
+<ol start="2">
+<li>Norm referenced rubric</li>
+</ol>
+<ol start="3">
+<li>Holistic rubric</li>
+</ol>
+<ol start="4">
+<li>Claven based referenced rubric</li>
+</ol>
+<p>Frequent checks for understanding support online learning by:</p>
+<ol start="1">
+<li>Improving cognitive presence</li>
+</ol>
+<ol start="2">
+<li>Activating prior knowledge</li>
+</ol>
+<ol start="3">
+<li>Addressing misconceptions</li>
+</ol>
+<ol start="4">
+<li>All of the above</li>
+</ol>
+<p>A multiple choice quiz question consists of</p>
+<ol start="1">
+<li>Reliable and valid answers, four choices, open ended questions</li>
+</ol>
+<ol start="2">
+<li>Question stems, unreasonable responses, correct responses</li>
+</ol>
+<ol start="3">
+<li>Question stem, distractors, forced response</li>
+</ol>
+<ol start="4">
+<li>Reliable and valid answers, jargon free questions</li>
+</ol>
+<p>Ms Polanske is examining her test results and sees an item more than 60% of students got incorrect. She decides not to include the item based on:</p>
+<ol start="1">
+<li>Her item difficulty analysis</li>
+</ol>
+<ol start="2">
+<li>The Internal consistency</li>
+</ol>
+<ol start="3">
+<li>Cronbach&rsquo;s Alpha</li>
+</ol>
+<ol start="4">
+<li>Random error rate</li>
+</ol>
+<p>The <a href="https://html-cleaner.com/css/">free online CSS minimizer</a> will compress the style files for your websites in seconds.</p>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
