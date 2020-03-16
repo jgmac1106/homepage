c030c9255b218c2f0aa3c7c16cdd994b4da680b2
Greg McVerry <jgregmcverry@gmail.com>
2020-03-16 16:34:06 -0400

    add note
    add note

--- notes/2020-03-16-4.php
+++ notes/2020-03-16-4.php
@@ -13,7 +13,7 @@ I just published the 4th video in my College Students Guide to Surviving Online 
    
  </p>
  <p>  Also on
- <a class="u-url" href="https://jgregorymcverry.com/notes/2020-03-16-4" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-03-16-3" rel="prev">Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a></p>
+ <a class="u-url" href="https://jgregorymcverry.com/notes/2020-03-16-4" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-03-16-5" rel="next">Next Note</a> <a href="https://jgregorymcverry.com/notes/2020-03-16-3" rel="prev">Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a></p>
 </div>
   	</main>
   	<div id="webmentions"></div>
---
+++ notes/2020-03-16-5.php
@@ -0,0 +1,26 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<a class="u-in-reply-to" href="https://twitter.com/Karis_M_Jones/status/1239616626359709698">tweet</a>
+<time class="dt-published" datetime="2020-03-16 16:344:20">16<sup>th</sup>March 2020</time>
+
+ <p class="p-name e-content">
+@karisa-m-jones this is such a great list! I might add being a participant observer is best. Choose a fandom space where you are interested and can then be onboarded as someone new to space. Nobody like to feel they are being stared at in a fish bow;
+   
+ </p>
+ <p>  Also on
+ <a class="u-url" href="https://jgregorymcverry.com/notes/2020-03-16-5" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-03-16-4" rel="prev">Previous Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a></p>
+</div>
+  	</main>
+  	<div id="webmentions"></div>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
