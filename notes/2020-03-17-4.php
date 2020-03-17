22ebd341ba0ad2cfef8aab7fa49f5a0257f82b6b
Greg McVerry <jgregmcverry@gmail.com>
2020-03-17 09:44:17 -0400

    Create 2020-03-17-4.php
    adding a note

---
+++ notes/2020-03-17-4.php
@@ -0,0 +1,25 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+
+<time class="dt-published" datetime="2020-03-17 09:34:20">17<sup>th</sup>March 2020</time>
+
+ <p class="p-name e-content">
+Wow what a turn out for our first open design studio to assist @scsu faculty as they get ready to move 100% online next week.
+ </p>
+ <p>  Also on
+ <a class="u-url" href="https://jgregorymcverry.com/notes/2020-03-17-4" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-03-17-3" rel="prev">Prev Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a></p>
+</div>
+  	</main>
+  	<div id="webmentions"></div>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
