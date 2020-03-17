650890f5af5869fada00c374322069327c4cd86b
Greg McVerry <jgregmcverry@gmail.com>
2020-03-17 07:11:44 -0400

    Create microlecturesandkaltura.php
    add a post

---
+++ microlecturesandkaltura.php
@@ -0,0 +1,108 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">Microlecture and Kaltura Training</h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
+     on <time class="dt-published" datetime="2020-03-16 18:04:20">16<sup>th</sup>March 2020</time></p>
+   
+
+    
+    <span class="summary">
+    <p class="p-summary">Lesson Plan For Today's @SCSU Session On Microlectures and Kaltura.</p>
+    </span>
+    <div class="copy">
+
+  <div class="e-content">
+
+
+<p>Goal: Create and upload a microlecture using the screencapture software Kaltura.</p>
+<p>Objectives:</p>
+<ul>
+<li>Identify best practices in microlectures</li>
+<li>Storyboard a video</li>
+<li>Create and record a video with a screenshare</li>
+<li>Publish and embed video in a Blackboard course</li>
+</ul>
+<p>Schedule:</p>
+<ul>
+<li>5 minute entrance slip</li>
+<li>10 minute analyze a video</li>
+<li>5 minutes effective practices</li>
+<li>10 miunte Kaltura tour</li>
+<li>20 minutes Plan a video</li>
+<li>10 minutes record and upload</li>
+</ul>
+<p>What does the research say?</p>
+<p>Shorter videos matter. In studies of online videos students will stop watching after about ten minutes, with seven minutes being the sweet spot for engagement (Guo, 2014). We also know from multimedia learning theory that people learn better with pictures and words alone (Mayer, 2008). Even with shorter videos you need to hold students accountable for watching videos (Darby &amp; Lang, 2019). Finally videos where proffesors actively preplanned are more effective learning tools (Guo, 2014).</p>
+<p>Learning Activities</p>
+<p>Entrance Slip</p>
+<p>Entrance slip: Answer the question would you rather be quarantined in the mountains or the beach?</p>
+<p>Then provide links to the exemplar videos</p>
+<ul>
+<li><a href="https://www.google.com/url?q=https://media.southernct.edu/media/Active%2BReading%2BTutorial%2B2A%2BUsing%2BHeadings/0_6xv5m48f&amp;sa=D&amp;ust=1584446751031000">Active Reading in College</a></li>
+<li><a href="https://www.google.com/url?q=https://media.southernct.edu/media/Add%2BSurvey%2BData/1_yg3dd864&amp;sa=D&amp;ust=1584446751031000">Add Survey Data</a></li>
+<li><a href="https://www.google.com/url?q=https://media.southernct.edu/media/EDU307%2BHow%2BTo%2BMake%2BLiterary%2BSlide%2BDeck/1_vm7x5bdi&amp;sa=D&amp;ust=1584446751032000">Making a Literary Element Slide Deck</a></li>
+<li><a href="https://www.google.com/url?q=https://media.southernct.edu/media/4NoteTakingStrategies/0_npomqfp3&amp;sa=D&amp;ust=1584446751032000">Note Taking Strategies</a></li>
+</ul>
+<p>Analyze Exemplar Text</p>
+<p>Have participants choose one of the four videos identify common elements. You want the audience to see that you activate prior knowledge, state the objectives, explicitly define key terms, model what you want students to know and do, keep them short.</p>
+<p>Explain that a good microlecture is much like a lesson plan</p>
+<p>Kaltura Tour</p>
+<p>Share your screen, and have a video or screehsnote ready on using Kaltura. You will not be able to use WebEx and Kalutra at the same time due to the microphone and camera being used.</p>
+<p>You need to demo:</p>
+<ul>
+<li>How to install</li>
+<li>How to choose screen, webcam, and both</li>
+<li>How to publish</li>
+<li>How to make unlisted</li>
+<li>How to set the video to download</li>
+<li>How to request captions</li>
+</ul>
+<p>Have each person then shut down WebEx and record a &ldquo;Hello World&rdquo; video</p>
+<p>Plan and Record a Video</p>
+<p>Next have participants storyboard a video. Review different ways you can storyboard. You can set up a three column system, print out ppt notes, or just use sticky notes</p>
+<p><img title="" src="/photos/scriptnotes.jpg" alt="" /></p>
+<p>&nbsp;</p>
+<table>
+<tbody>
+<tr>
+<td colspan="1" rowspan="1">
+<p>My script</p>
+</td>
+<td colspan="1" rowspan="1">
+<p>Assets to include (img, video)</p>
+</td>
+<td colspan="1" rowspan="1">
+<p>Keywords to highlight</p>
+</td>
+</tr>
+<tr>
+<td colspan="1" rowspan="1">&nbsp;</td>
+<td colspan="1" rowspan="1">&nbsp;</td>
+<td colspan="1" rowspan="1">&nbsp;</td>
+</tr>
+<tr>
+<td colspan="1" rowspan="1">&nbsp;</td>
+<td colspan="1" rowspan="1">&nbsp;</td>
+<td colspan="1" rowspan="1">&nbsp;</td>
+</tr>
+<tr>
+<td colspan="1" rowspan="1">&nbsp;</td>
+<td colspan="1" rowspan="1">&nbsp;</td>
+<td colspan="1" rowspan="1">&nbsp;</td>
+</tr>
+</tbody>
+</table>
+<p>Then have participants shut down WebEx and make their videos. Share the videos at the end.</p>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
