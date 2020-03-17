5e0c028f7a956441ed0435517c660fbf343ecd63
Greg McVerry <jgregmcverry@gmail.com>
2020-03-17 08:24:22 -0400

    Update 2020-03-17-3.php
    fix spelling mistakes

--- notes/2020-03-17-3.php
+++ notes/2020-03-17-3.php
@@ -10,8 +10,7 @@
 <time class="dt-published" datetime="2020-03-17 07:54:20">17<sup>th</sup>March 2020</time>
 
  <p class="p-name e-content">
-Hey @scsu Iwill be in tdesign studio from 9:00-10:00 to answer questions about blackboard, online teaching, help with tools, or just brainstorm ideas for your class. Bring your syllabi and questions. Just hang out and work with other faculty. Human contact important
-   
+Hey @scsu I will be in the design studio from 9:00-10:00 to answer questions about blackboard, online teaching, help with tools, or just brainstorm ideas for your class. Bring your syllabi and questions. Just hang out and work with other faculty. Human contact important
  </p>
  <p>  Also on
  <a class="u-url" href="https://jgregorymcverry.com/notes/2020-03-17-3" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-03-17-2" rel="prev">Prev Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a></p>
