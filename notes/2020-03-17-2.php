187edca96c5e49d4bd1fa5394365b6b3e36cd1f3
Greg McVerry <jgregmcverry@gmail.com>
2020-03-17 07:58:18 -0400

    Create 2020-03-17-2.php
    add note

---
+++ notes/2020-03-17-2.php
@@ -0,0 +1,26 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+
+<time class="dt-published" datetime="2020-03-17 07:54:20">17<sup>th</sup>March 2020</time>
+
+ <p class="p-name e-content">
+My kids must be living the life of Reily, another year, and another failed Leprachaun trap....You would think by now they could have improved the design and earn their keep with a bucket of gold. 
+   
+ </p>
+ <p>  Also on
+ <a class="u-url" href="https://jgregorymcverry.com/notes/2020-03-17-2" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-03-17-1" rel="prev">Prev Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a></p>
+</div>
+  	</main>
+  	<div id="webmentions"></div>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
