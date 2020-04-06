9bd506251d811bdfe7399b41156ef5e52959110f
Greg McVerry <jgregmcverry@gmail.com>
2020-04-06 08:40:24 -0400

    Create 2020-04-06-1.php
    add note

---
+++ notes/2020-04-06-1.php
@@ -0,0 +1,21 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-04-06 08:34:20">6<sup>th</sup> March 2020</time>
+ <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/scott_gruber/status/1247133129191469056">@scottgruber</a></p>
+ <p class="p-name e-content">Please share the link widely! Especially with yoru work in Cameroon I would love endorsements from educational leaders interested in the project. If you like the idea can you send a tweet as an endorsement. Just reply to my first tweet. 
+ </p>
+  Also on<a href="https://jgregorymcverry.com/notes/2020-04-06-1" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-03-31-1" rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
