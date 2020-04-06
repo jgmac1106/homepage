f81b8e767a8e068906392cd16867e6d341bd1419
Greg McVerry <jgregmcverry@gmail.com>
2020-04-06 11:18:32 -0400

    add note
    add note

--- notes/2020-04-06-5.php
+++ notes/2020-04-06-5.php
@@ -10,7 +10,7 @@
  <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/greeneterry/status/1247169384461803522">@greenterry</a></p>
  <p class="p-name e-content">@greenterry @francesbell I am submitting this <a href="https://jgregorymcverry.com/https://jgregorymcverry.com/lofilearningmozillagrant">https://jgregorymcverry.com/lofilearningmozillagrant</a> to @mozilla MOSS covid-19 challenges. Reqs calls for expert endorsement. Would like a critical eye as well from the #oer20 crowd, especially with a lens toward feminist pedagogy. Shout out on show may help get word out 
  </p>
-  Also on <a href="https://jgregorymcverry.com/notes/2020-04-06-5" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-04-06-4" rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  Also on <a href="https://jgregorymcverry.com/notes/2020-04-06-5" rel="canonical"><strong>My Notes</strong> <a href="https://jgregorymcverry.com/notes/2020-04-06-6" rel="next">Next Notes</a> <a href="https://jgregorymcverry.com/notes/2020-04-06-4" rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
   	</main>
   	 	<div id="webmentions"></div>
---
+++ notes/2020-04-06-6.php
@@ -0,0 +1,22 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-04-06 11:14:20">6<sup>th</sup> March 2020</time>
+ <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/francesbell/status/1247180265203683328">@francesbell</a></p>
+ <p class="p-name e-content">Thanks proposal grew out from @goifoundation team when we tried to do a remote @wikidata and @wikipedia edit-a-thon. <a href="https://quickthoughts.jgregorymcverry.com/2020/03/28/goifoundation-needing-to-go-synchronous-lo-fi-during-our-wikipedia-and">https://quickthoughts.jgregorymcverry.com/2020/03/28/goifoundation-needing-to-go-synchronous-lo-fi-during-our-wikipedia-and</a> We are looking to build up and scale an offline LMS just for the power and bandwidth issues.
+ </p>
+  Also on<a href="https://jgregorymcverry.com/notes/2020-04-06-6" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-04-06-5" rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  	 	<div id="webmentions"></div>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
