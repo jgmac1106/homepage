ee466f9a103fd1700079da008ba063a0080481b7
Greg McVerry <jgregmcverry@gmail.com>
2020-04-06 10:50:25 -0400

    Update 2020-04-06-5.php
    move link

--- notes/2020-04-06-5.php
+++ notes/2020-04-06-5.php
@@ -8,7 +8,7 @@
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2020-04-06 10:34:20">6<sup>th</sup> March 2020</time>
  <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/greeneterry/status/1247169384461803522">@greenterry</a></p>
- <p class="p-name e-content">@greenterry @francesbell I am submitting this to the @mozilla MOSS covid-19 challenges. Requirements calls for expert endorsement. I would like a critical eye as well from the #oer20 crowd, especially with a lens toward feminist pedagogy. A shout out on show may help get word out <a href="https://jgregorymcverry.com/https://jgregorymcverry.com/lofilearningmozillagrant">https://jgregorymcverry.com/lofilearningmozillagrant</a>
+ <p class="p-name e-content">@greenterry @francesbell I am submitting this <a href="https://jgregorymcverry.com/https://jgregorymcverry.com/lofilearningmozillagrant">https://jgregorymcverry.com/lofilearningmozillagrant</a> to @mozilla MOSS covid-19 challenges. Reqs calls for expert endorsement. Would like a critical eye as well from the #oer20 crowd, especially with a lens toward feminist pedagogy. Shout out on show may help get word out 
  </p>
   Also on <a href="https://jgregorymcverry.com/notes/2020-04-06-5" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-04-06-4" rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
