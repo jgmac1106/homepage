e7da2e5f9718fcc52227ebfc7d692dd208eff8a6
Greg McVerry <jgregmcverry@gmail.com>
2020-04-06 10:39:36 -0400

    add note
    adding note

--- lofilearningmozillagrant.php
+++ lofilearningmozillagrant.php
@@ -10,7 +10,7 @@
 <div class="pubinfo">
   <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregory McVerry</a>
 on <time class="dt-published" datetime="2020-04-06 06:54:20">6th<sup>th</sup> March 2020</time></p>
-<small>Image: IndieWebCamp Navrongo<a href="https://creativecommons.org/licenses/by-sa-nc/2.0/">Creative Commons (BY-SA-NC) license</a> by <a class=" h-card" href="http://sadik.goifnetwork.org/">Sadik Shahadu</a> </small>
+<small>Image: IndieWebCamp Navrongo<a href="https://creativecommons.org/licenses/by-sa-nc/2.0/">Creative Commons (BY-SA-NC) license</a> by <a href="http://sadik.goifnetwork.org/">Sadik Shahadu</a> </small>
 
 <span class="summary">
   <p class="p-summary">Help combat the growing global inequities in online learning during this global lock down. Access to education, on a healthy and accessible Internet, is a human right denied to many during the COVID-19 crisis. Please endorse this proposal.</p>
