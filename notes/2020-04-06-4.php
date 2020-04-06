c837622ab15dcc08bec043dbde660f8f3b3ef607
Greg McVerry <jgregmcverry@gmail.com>
2020-04-06 10:03:43 -0400

    add note
    add note

---
+++ notes/2020-04-06-4.php
@@ -0,0 +1,21 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-04-06 10:04:20">6<sup>th</sup> March 2020</time>
+ <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/nathan_stevens/status/1247152044458336256">@nathan_stevens</a></p>
+ <p class="p-name e-content">Weird, I am going to #edtechchat tonight but the topic was "Planning Ahead; Realizing We Can't Go back to Just Face to Face Teaching"
+ </p>
+  Also on <a href="https://jgregorymcverry.com/notes/2020-04-06-4" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-04-06-3" rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
