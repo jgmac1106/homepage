c2dc1a4ed52e290fb9a3f2ff0f122f8b38c50f09
Greg McVerry <jgregmcverry@gmail.com>
2020-04-06 08:51:46 -0400

    add a note
    add a note

--- notes/2020-04-06-1.php
+++ notes/2020-04-06-1.php
@@ -10,7 +10,7 @@
  <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/scott_gruber/status/1247133129191469056">@scottgruber</a></p>
  <p class="p-name e-content">Please share the link widely! Especially with your work in Cameroon. I would love endorsements from educational leaders interested in the project. If you like the idea can you send a tweet as an endorsement. Just reply to my first tweet. 
  </p>
-  Also on<a href="https://jgregorymcverry.com/notes/2020-04-06-1" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-03-31-1" rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  Also on<a href="https://jgregorymcverry.com/notes/2020-04-06-1" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-04-06-2" rel="next"><strong>Next Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-03-31-1" rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
   	</main>
   		<footer>
---
+++ notes/2020-04-06-2.php
@@ -0,0 +1,21 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-04-06 08:54:20">6<sup>th</sup> March 2020</time>
+ <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/GOIFoundation/status/1247112481002196998">@goifoundation</a></p>
+ <p class="p-name e-content">Happy Birthday Zita! You are one of my heores, an author and web advocate. You fight injustice everyday. Please take some time and enjoy your day. You serve it. 
+ </p>
+  Also on <a href="https://jgregorymcverry.com/notes/2020-04-06-2" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-04-06-1" rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
