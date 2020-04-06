c8818c599ce21586097c30217a4030712cba13b0
Greg McVerry <jgregmcverry@gmail.com>
2020-04-06 08:59:05 -0400

    add a note
    add a note

---
+++ notes/2020-04-06-3.php
@@ -0,0 +1,21 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-04-06 09:04:20">6<sup>th</sup> March 2020</time>
+ <p>In reply to <a class="u-in-reply-to" href="hhttps://twitter.com/jgmac1106/status/1247144049347383299">@jgmac1106</a></p>
+ <p class="p-name e-content">Scott Sadik and I need to get our @MozOpenLeaders program running again. We are building out the #OER network in Ghana when covid-19 hit. We are planning our remote options now. If anyone on your team wants to get involved we have plenty of server space. 
+ </p>
+  Also on <a href="https://jgregorymcverry.com/notes/2020-04-06-3" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-04-06-2" rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
