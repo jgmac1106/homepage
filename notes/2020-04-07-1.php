fe64e416741a96f6d6220e7f04c9f9cfc5bbf2d4
Greg McVerry <jgregmcverry@gmail.com>
2020-04-07 07:15:02 -0400

    Create 2020-04-07-1.php
    add note

---
+++ notes/2020-04-07-1.php
@@ -0,0 +1,22 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-04-07 07:14:20">7<sup>th</sup> March 2020</time>
+ <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/tosbourn/status/1247465742305505282">@tosbourn</a></p>
+ <p class="p-name e-content">Here is is agood source of articles <a href="https://indieweb.org/Webmention#articles">https://indieweb.org/Webmention#articles</a>. I just added webmentions to my wiki at <a href="http://longthoughts.jgregorymcverry.com/doku.php">http://longthoughts.jgregorymcverry.com/doku.php</a>. It took a webmention.io account, two lines in my header, one javascript file (css optional)
+ </p>
+  Also on <a href="https://jgregorymcverry.com/notes/2020-04-07-1" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-04-06-7 "rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  	 	<div id="webmentions"></div>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
