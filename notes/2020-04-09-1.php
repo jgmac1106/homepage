e1f41afa7011995ae50968ef45c5502a8a2355f3
Greg McVerry <jgregmcverry@gmail.com>
2020-04-09 09:21:30 -0400

    Update 2020-04-09-1.php
    write poem

--- notes/2020-04-09-1.php
+++ notes/2020-04-09-1.php
@@ -8,31 +8,31 @@
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2020-04-09 08:54:20">9<sup>th</sup> March 2020</time>
  <
- <p class="p-name e-content">
-                                            a         
-                                             dog     we  
+ <span class="p-name e-content">
+                                            Me 	     and         
+                                            my dog  we  
                                              recharging! 
                                             ever so loyal
-                                            quite loving 
-                                            quite friend 
-                                            rather large 
-                                            rather brown 
-                                          decidedly high 
-                                        canis familiaris 
-             whistling               colored, big, fond  
-           recharging rise, sharp trees, crazy friends   
-         ever so affectioned allies fleeing, restfully   
-  we recharging rise, great trees, caring supporters   
-rather loving  its amorous quakers running, restfully    
+                                            we tranverse 
+                                            our trails on
+                                            each falling 
+                                            sun when the
+                                            day is done. 
+                                        The climb begins up
+             into the trees            towering, big, fond  
+           swaying rise, quiet trees, silent friends   
+         ever so affectionate allies swaying restfully   
+  our recharging rises, to meet trees, caring supporters   
+rather loving spectators cheering our running, restfully    
  quite high   its loverlike allies working, restfully    
-              its fond friends scampering, restfully     
-              its crazy allies scampering, restfully     
-               ever so piercing trees running, forte     
-               its charmed allies running, restfully     
-               ever so last trees scampering, forte      
-               ever so friend move  positively high      
-               friend! loyal           rather large      
-              happy!   chase            trail tail       
+              supporting friends scampering, bouncing    
+              climbing, hiking, scampering, rock to rock    
+               ever so piercing the forest floors with   
+               boundless energy unleashed in every step   
+               ever so friends following well worn trails     
+               into places of known and deep truths      
+               answers without          asking know    
+             the truth  chase            the  tr      
              brown!    deep              hike soul       
             hound      got               time hike       
             move      fun                 got  had       
@@ -42,7 +42,7 @@ rather loving  its amorous quakers running, restfully
               two        and              day   end      
                we                         love   are     
                                            for           
-</p>
+</span>
   Also on <a href="https://jgregorymcverry.com/notes/2020-04-09-1" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-04-07-1 "rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
   	</main>
