7eb950afe8b8092bb99bd35774ef1609a79c21dc
Greg McVerry <jgregmcverry@gmail.com>
2020-04-09 09:00:57 -0400

    Create 2020-04-09-1.php
    publish notes

---
+++ notes/2020-04-09-1.php
@@ -0,0 +1,55 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-04-09 08:54:20">9<sup>th</sup> March 2020</time>
+ <
+ <p class="p-name e-content">
+                                            a         
+                                             dog     we  
+                                             recharging! 
+                                            ever so loyal
+                                            quite loving 
+                                            quite friend 
+                                            rather large 
+                                            rather brown 
+                                          decidedly high 
+                                        canis familiaris 
+             whistling               colored, big, fond  
+           recharging rise, sharp trees, crazy friends   
+         ever so affectioned allies fleeing, restfully   
+  we recharging rise, great trees, caring supporters   
+rather loving  its amorous quakers running, restfully    
+ quite high   its loverlike allies working, restfully    
+              its fond friends scampering, restfully     
+              its crazy allies scampering, restfully     
+               ever so piercing trees running, forte     
+               its charmed allies running, restfully     
+               ever so last trees scampering, forte      
+               ever so friend move  positively high      
+               friend! loyal           rather large      
+              happy!   chase            trail tail       
+             brown!    deep              hike soul       
+            hound      got               time hike       
+            move      fun                 got  had       
+             we        use                fun  two       
+             we        you                two  dog       
+             had        fun               can   key      
+              two        and              day   end      
+               we                         love   are     
+                                           for           
+</p>
+  Also on <a href="https://jgregorymcverry.com/notes/2020-04-09-1" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-04-07-1 "rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  	 	<div id="webmentions"></div>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
