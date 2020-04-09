2cc1df1a228c5c7e82b90deb24728d2ab44107e4
Greg McVerry <jgregmcverry@gmail.com>
2020-04-09 13:26:44 -0400

    Create 2020-04-09-2.php
    add note

---
+++ notes/2020-04-09-2.php
@@ -0,0 +1,23 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry" >
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-04-09 13:04:20">9<sup>th</sup> March 2020</time>
+ <img class="u-photo" href="https://jgregorymcverry.com/photos/20200409dogtraxcodingdawg.jpg" alt="concrete poem about dog, girl commenting that you can do neat stuff with code">
+ <p class="p-name e-content">
+      I made a poem about hanging with my dog each day for the #NaPoWriMo  Day Nine Concrete poem challenge. My dog came out fat. <a href="https://jgregorymcverry.com/notes/2020-04-09-1">https://jgregorymcverry.com/notes/2020-04-09-1</a> Luckily @dogtrax turned the source code into a comic.              
+</p>
+  Also on <a href="https://jgregorymcverry.com/notes/2020-04-09-2" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-04-09-1 "rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  	 	<div id="webmentions"></div>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
