3ef7a0124c615893813478f1443b129d63eed290
Greg McVerry <jgregmcverry@gmail.com>
2020-04-13 15:17:48 -0400

    Update shinywebsite.php
    still mucking around

--- shinywebsite.php
+++ shinywebsite.php
@@ -42,6 +42,38 @@
     
     <!-- import the webpage's javascript file -->
     <script src="/script.js" defer></script>
+    <style>
+      body {
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      background-color: #000000;
+      }
+      p {
+      position: relative;
+      font-family: Geneva, sans-serif;
+      font-size: 60px;
+      text-transform: uppercase;
+      letter-spacing: 2px;
+      background: linear-gradient(90deg, #DC143C, #fff, #000);
+      background-repeat: no-repeat;
+      background-size: 80%;
+      animation: animate 3s infinite;
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: rgba(255, 255, 255, 0);
+      }
+      @keyframes animate {
+      0% {
+      background-position: -500%;
+      }
+      100% {
+      background-position: 500%;
+      }
+      }
+    </style>
   </head>  
   <body>
     <div id="doodlebird">
