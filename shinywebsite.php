45a1621a13f888ee0bce3e26f3ed02c8a8a79708
Greg McVerry <jgregmcverry@gmail.com>
2020-04-13 16:25:32 -0400

    Update shinywebsite.php
    fix

--- shinywebsite.php
+++ shinywebsite.php
@@ -15,7 +15,27 @@
       height: 100vh;
       background-color: #000000;
       }
-      
+      p {
+      position: relative;
+      font-family: Geneva, sans-serif;
+      font-size: 60px;
+      text-transform: uppercase;
+      letter-spacing: 2px;
+      background: linear-gradient(90deg, #DC143C, #fff, #000);
+      background-repeat: no-repeat;
+      background-size: 80%;
+      animation: animate 3s infinite;
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: rgba(255, 255, 255, 0);
+      }
+      @keyframes animate {
+      0% {
+      background-position: -500%;
+      }
+      100% {
+      background-position: 500%;
+      }
+      }
     </style>
     <!-- import the webpage's stylesheet -->
     <link rel="stylesheet" href="/css/crossfade.css">
@@ -43,9 +63,11 @@
        <p>YOU</p>
      </div>
       <div class="shiny">
-       <p>Get A Website</p>
+       <p>Get A </p>
      </div>
-    
+      <div class="shiny">
+       <p>Website</p>
+     </div>
       
     </div>
 
