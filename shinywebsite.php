637f58d8fb17fd579f0855d88714a7a30f554233
Greg McVerry <jgregmcverry@gmail.com>
2020-04-13 16:14:53 -0400

    upload
    upload

--- shinywebsite.php
+++ shinywebsite.php
@@ -15,27 +15,7 @@
       height: 100vh;
       background-color: #000000;
       }
-      p {
-      position: relative;
-      font-family: Geneva, sans-serif;
-      font-size: 60px;
-      text-transform: uppercase;
-      letter-spacing: 2px;
-      background: linear-gradient(90deg, #DC143C, #fff, #000);
-      background-repeat: no-repeat;
-      background-size: 80%;
-      animation: animate 3s infinite;
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: rgba(255, 255, 255, 0);
-      }
-      @keyframes animate {
-      0% {
-      background-position: -500%;
-      }
-      100% {
-      background-position: 500%;
-      }
-      }
+      
     </style>
     <!-- import the webpage's stylesheet -->
     <link rel="stylesheet" href="/css/crossfade.css">
