96687051cb9cae51373c22b42551fd99ed3fbf49
Greg McVerry <jgregmcverry@gmail.com>
2020-05-10 07:10:43 -0400

    Update 2020-05-10-2.php
    fix spelling mistake

--- notes/2020-05-10-2.php
+++ notes/2020-05-10-2.php
@@ -9,7 +9,7 @@
 <time class="dt-published" datetime="2020-05-10 06:34:20">10<sup>th</sup>May 2020</time>
  
  <p class="p-name e-content">
-    I just published today's #poetry on #poembox "May's Winter Win" https://jgregorymcverry.com/mypoetry#audiopoems Follow with RSS or on @pocketcast #literacies #nctevillage #writingcommunity
+    I just published today's #poetry on #poembox "May's Winter Wind" https://jgregorymcverry.com/mypoetry#audiopoems Follow with RSS or on @pocketcast #literacies #nctevillage #writingcommunity
 </p>
   Also on <a href="https://jgregorymcverry.com/notes/2020-05-10-2" class="u-url" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-05-10-1 "rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
