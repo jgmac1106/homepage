15c30ab3b66ac4ab17b964b0b15964267cbd9459
Greg McVerry <jgregmcverry@gmail.com>
2020-05-11 06:38:13 -0400

    Update 2020-05-11-1.php
    fix spelling

--- notes/2020-05-11-1.php
+++ notes/2020-05-11-1.php
@@ -9,7 +9,7 @@
 <time class="dt-published" datetime="2020-05-11 06:34:20">11<sup>th</sup>May 2020</time>
  
  <p class="p-name e-content">
-    I just published today's #poetry on #poembox "Spring's Silence" https://jgregorymcverry.com/mypoetry#audiopoems The last of my #poemofpresence series on the covid19 polar vortex Follow with RSS or on @pocketcast #literacies #nctevillage #writingcommunity
+    I just published today's #poetry on #poembox "Spring's Silence" https://jgregorymcverry.com/mypoetry#audiopoems The last of my #poemosfpresence series on the covid19 polar vortex Follow with RSS or on @pocketcast #literacies #nctevillage #writingcommunity
 </p>
   Also on <a href="https://jgregorymcverry.com/notes/2020-05-11-1" class="u-url" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-05-10-2 "rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
