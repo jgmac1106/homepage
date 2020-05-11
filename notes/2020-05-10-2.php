59e100ac4ba5e71ab67133c05af5481ec1fa5162
Greg McVerry <jgregmcverry@gmail.com>
2020-05-11 06:33:49 -0400

    add a note
    add a note

--- notes/2020-05-10-2.php
+++ notes/2020-05-10-2.php
@@ -11,7 +11,7 @@
  <p class="p-name e-content">
     I just published today's #poetry on #poembox "May's Winter Wind" https://jgregorymcverry.com/mypoetry#audiopoems Follow with RSS or on @pocketcast #literacies #nctevillage #writingcommunity
 </p>
-  Also on <a href="https://jgregorymcverry.com/notes/2020-05-10-2" class="u-url" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-05-10-1 "rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  Also on <a href="https://jgregorymcverry.com/notes/2020-05-10-2" class="u-url" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2020-05-11-1 "rel="next">Next Note</a>  <a href="https://jgregorymcverry.com/notes/2020-05-10-1 "rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
   	</main>
   	 	<div id="webmentions"></div>
---
+++ notes/2020-05-11-1.php
@@ -0,0 +1,23 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry" >
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-05-11 06:34:20">11<sup>th</sup>May 2020</time>
+ 
+ <p class="p-name e-content">
+    I just published today's #poetry on #poembox "Spring's Silence" https://jgregorymcverry.com/mypoetry#audiopoems The last of my #poemofpresence series on the covid19 polar vortex Follow with RSS or on @pocketcast #literacies #nctevillage #writingcommunity
+</p>
+  Also on <a href="https://jgregorymcverry.com/notes/2020-05-11-1" class="u-url" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-05-10-2 "rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  	 	<div id="webmentions"></div>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
