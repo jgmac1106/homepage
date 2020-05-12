3d886908082788810035d68f47203415d2509536
Greg McVerry <jgregmcverry@gmail.com>
2020-05-12 06:31:04 -0400

    Update 2020-05-12-1.php
    fix spelling mistake

--- notes/2020-05-12-1.php
+++ notes/2020-05-12-1.php
@@ -9,7 +9,7 @@
 <time class="dt-published" datetime="2020-05-12 06:34:20">12<sup>th</sup>May 2020</time>
  
  <p class="p-name e-content">
-    I just published today's #poetry on #poembox "Slippages of Images" https://jgregorymcverry.com/mypoetry#audiopoems A requet by @nomadwarmachine to rhyme images "for a friend" #clmooc #writingcommunity
+    I just published today's #poetry on #poembox "Slippages of Images" https://jgregorymcverry.com/mypoetry#audiopoems A request by @nomadwarmachine to rhyme images "for a friend" #clmooc #writingcommunity
 </p>
   Also on <a href="https://jgregorymcverry.com/notes/2020-05-12-1" class="u-url" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-05-11-3 "rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
