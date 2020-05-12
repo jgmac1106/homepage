30ebbe2a591813997de14c86d087295452122cc0
Greg McVerry <jgregmcverry@gmail.com>
2020-05-12 06:25:59 -0400

    add a note
    add a note

---
+++ notes/2020-05-12-1.php
@@ -0,0 +1,23 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry" >
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-05-12 06:34:20">12<sup>th</sup>May 2020</time>
+ 
+ <p class="p-name e-content">
+    I just published today's #poetry on #poembox "Slippages of Images" https://jgregorymcverry.com/mypoetry#audiopoems A requet by @nomadwarmachine to rhyme images "for a friend" #clmooc #writingcommunity
+</p>
+  Also on <a href="https://jgregorymcverry.com/notes/2020-05-12-1" class="u-url" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-05-11-3 "rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  	 	<div id="webmentions"></div>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
