1471a36981dc800130929364fca0750d2c0eb40b
Greg McVerry <jgregmcverry@gmail.com>
2020-05-13 07:23:30 -0400

    add a post
    add a post

---
+++ canimagesrhyme.php
@@ -0,0 +1,289 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">Can Images Rhyme?</h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
+     on <time class="dt-published" datetime="2020-05-13 07:24:20">13<sup>th</sup>May 2020</time></p>
+   <small>Featured Image: <a title="Cat-erpiller" href="https://flickr.com/photos/sarah-nomadwarmachine/49734027238">"Cat-erpiller"</a> by <a href="https://flickr.com/people/sarah-nomadwarmachine">NomadWarMachine</a> is licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/2.0/">CC BY-NC-SA</a> </small>
+
+    
+    <span class="summary">
+    <p class="p-summary">A #clmooc exploration in methods of making two images sing together</p>
+    </span>
+  </div>
+<img class="featured u-featured" src="https://live.staticflickr.com/65535/49734027238_225b2a037c_z.jpg" alt="">
+    <div class="copy">
+
+  <div class="e-content">
+     <p>As part of my #edu106 class I decided to focus on Poetry as my "Teach Something." I made an <a href="https://media.southernct.edu/media/Ignite+TalkA+What+is+Poetry/1_m9ml7dbz">ignite talk </a> covering my four universal truths for life and poetry.</p>
+     <p>This lead Sarah <a href="https://twitter.com/NomadWarMachine/status/1259902543318179840">to ask </a> how can you rhyme an image?</p>
+    <blockquote class="twitter-tweet" data-conversation="none" data-cards="hidden" data-partner="tweetdeck"><p lang="en" dir="ltr">How would you rhyme images? <br><br>Asking for a &quot;friend&quot;<br><br>🤣<a href="https://twitter.com/hashtag/clmooc?src=hash&amp;ref_src=twsrc%5Etfw">#clmooc</a></p>&mdash; Sarah Honeychurch (@NomadWarMachine) <a href="https://twitter.com/NomadWarMachine/status/1259902543318179840?ref_src=twsrc%5Etfw">May 11, 2020</a></blockquote>
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+<p>I of course had to take the literal route at first and just rhyme "image"</p>
+<audio class="u-audio" src="https://jgregorymcverry.com/poetry/slippagesofimages.mp3" controls="controls"></audio>
+<p>hemorrhages of<br>
+hues in unseen<br>
+scrimmages<br>
+that<br>
+pillage<br>
+shades<br>
+of messages<br>
+mask in <br>
+envisages<br>
+of answers <br>
+gleaning<br>
+in crosstitches<br>
+of time. A<br>
+vintage shine<br>
+in slippage<br>
+of pastel<br>
+blends<br>
+slipping up <br>
+spines</p>
+<p>Kevin then <a href="https://twitter.com/dogtrax/status/1260150228101009408">chimed in</a> and took us the figurative route:</p>
+  <blockquote>With finger-paints (that’s how you rhyme an image) and colored smudges on glass, with chalk dust shadows, with nothing there, nothing at all, just the possibility of rhyme of the image, or maybe you meant literal? As in, image scrimmage, pilgrimage … I prefer the visual choices</blockquote>
+ p>To which I replied</p>
+ <blockquote>Can it be both? or must it be all of the above? Maybe answer just C, or 42. Anywhoo I say both. Bicameral rhymes of forced responses in confidence of color keeping time</blockquote>
+   <audio class="u-audio" src="https://jgregorymcverry.com/poetry/fingerpaintingsinthesky.mp3" controls="controls">
+</audio>
+
+
+<p>
+&nbsp;&nbsp;&nbsp;words unheard<br>
+in touches of<br>
+smudges as<br>
+fingers dance<br>
+both singer<br>
+and thinker<br>
+bisexual<br>
+particles<br>
+&nbsp;&nbsp;&nbsp;pouncing on partners<br>
+&nbsp;&nbsp;of opportunity<br>
+in typical<br>
+&nbsp; but invisible<br>
+lunacy of love<br>
+bouncing on hymnal<br>
+voices of hues<br>
+&nbsp;&nbsp;&nbsp;moisten alloys<br>
+and joyces<br>
+in flexible lines pf<br>
+&nbsp;&nbsp;&nbsp;visual<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;verse<br>
+<br>
+ pinnacle<br>
+of trivial<br>
+chalk dust<br>
+trails<br>
+&nbsp;&nbsp;which<br>
+unveil<br>
+&nbsp;miserable<br>
+masterpieces<br>
+in<br>
+crust<br>
+&nbsp; wrought<br>
+with distrust<br>
+as sounds<br>
+&nbsp;&nbsp;stalk<br>
+colors that<br>
+squawk in<br>
+&nbsp;&nbsp;&nbsp;voices<br>
+who trigger<br>
+finger painting<br>
+in the<br>
+&nbsp;&nbsp;&nbsp;sky</p>
+<p>Sarah then noted this inquiry had a deep import. Which would require further study verse, and gifs</p>
+<blockquote class="twitter-tweet" data-partner="tweetdeck"><p lang="en" dir="ltr">Oh. Well. You know it goes out here ... we spin paintings out of dots ... <a href="https://t.co/ZGyTyKbWid">pic.twitter.com/ZGyTyKbWid</a></p>&mdash; KevinHodgson (@dogtrax) <a href="https://twitter.com/dogtrax/status/1260166280532852736?ref_src=twsrc%5Etfw">May 12, 2020</a></blockquote>
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+<p>Sarah also shared the reasoning of her line of inquiry</p>
+<blockquote>thinking about all of what you've been saying though, and watching your video. Thinking about the root of poetry as making again as I watched you ... sometimes a semi-serious question gets a very serious (helpful) answer</blockquote>
+<p>The root of poetry really pulled on me, away from my explorations into the rhyming images. Had to get a groove on for a bit</p>
+<p>So I posted a broken palindrom poem (fixed now)</p>
+<p>
+Root of<br>
+poetry<br>
+like all<br>
+Truth<br>
+lies <br>
+in the sound<br>
+The One<br>
+a beat<br>
+eternal<br>
+<br>
+beat<br>
+eternal<br>
+<br>
+A one<br>
+the sound<br>
+<br>
+the lies, <br>
+<br>
+Truth!<br>
+<br>
+All like<br>
+poetry of Root</p>
+<p>This brought Dan Blicksenderfer into the conversation</p>
+<blockquote class="twitter-tweet" data-conversation="none" data-cards="hidden" data-partner="tweetdeck"><p lang="en" dir="ltr">I love this; it made me think of Bootsy Collins and &quot;playing on the One&quot; <br><br>The One<br>a beat<br>eternal</p>&mdash; Dan Blickensderfer (@Dan_Blick) <a href="https://twitter.com/Dan_Blick/status/1260188821351792640?ref_src=twsrc%5Etfw">May 12, 2020</a></blockquote>
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+<p>To which I replied</p>
+<p>
+Looking back my <br>
+palindrome poem<br>
+off a bit.<br>
+<br>
+That's okay<br>
+my knowledge<br>
+of "the one"<br>
+comes from<br>
+"The Other One"<br>
+dem boys always<br>
+off beat</p>
+<p>mean it<br>
+ synergy<br>
+in energy<br>
+from academic<br>
+articles down<br>
+to quirks in quarks<br>
+of our particles<br>
+gravitational waves<br>
+gyrating in mazes<br>
+of synchronized gazes<br>
+everything moves<br>
+but<br>
+no matter the beat<br>
+Always returns to the<br>
+One</p>
+<p>I did listen to a lot of Bootsy Collins yesterday. Like a lot. From "Bootzilla" to "Bootsy's Back". This may have influenced my verse as our exploration into rhyming images <a href="https://twitter.com/steelwagstaff/status/1260191531157291010">drew in</a> Steel Wagstaff</p>
+<blockquote>Basil Bunting described poetry emerging from the rhythmic grunts/breaths that ancient humans made while dancing. In a 1970 lecture he said "the arts are closely related. They have all a common ancestry. In particular music and poetry are twin sisters born of the primitive dance.</blockquote>
+<p>To which I replied</p>
+<p>dance goes<br>
+deeper than<br>
+grunts & bending<br>
+knees<br>
+What of bees?<br>
+or stars who gyrate<br>
+at frequencies<br>
+we can't see?<br>
+We all chasing the same<br>
+One<br>
+An intergalactic stereophonic<br>
+boogey oogey of poetry<br>
+in bent<br>
+sounds</p>
+<p>Told you I spent sometime listen to Boosty. Slapping interextuality all over this work, but I needed to answer Sarah's semi serious question, so I got back to work (after cranking up the bass).</p>
+<p>of course now I have to<br>
+ jam on Bootzilla<br>
+me and Bootsy run deep<br>
+in the rhinestone flow<br>
+of my arteries <br>
+"Oh yeah I am programmable,<br>
+one hit of the dial" </p>
+<p>I first turned to color theory, and wantes to include a call back to Kevin's dots. After all, maybe color, or lack there of, is how you make an image rhyme?</p>
+<p>
+  2 Contrast<br>
+&nbsp;&nbsp;these perspectives<br>
+let's compare<br>
+backgrounds in sound<br>
+dancing in color<br>
+theories<br>
+could a rhyme<br>
+be side by<br>
+side?<br>
+Analogous splashes<br>
+on sandy shores<br>
+or does a couplet<br>
+require<br>
+complementary colors<br>
+&nbsp;&nbsp;&nbsp;opposite<br>
+dots in<br>
+harmony toasting<br>
+to tones<br>
+of purples<br>
+&nbsp;&nbsp;&nbsp;purged from<br>
+snails<br>
+Not sure<br>
+Issue ain't black<br>
+and white<br>
+Guess I am neutral.</p>
+<p>Then I thought what if it is the meaning and not the color of images that rhyme. Like what rhymes with Frog? Pig or rainbow?</p>
+<p>So I turned to theories on reading images. Specifially Frank Serafini's Tripartite theory</p>
+<p>To address<br>
+how images<br>
+rhyme<br>
+be a mess to<br>
+exclude<br>
+Kress<br>
+Always enjoyed<br>
+Frank's<br>
+take<br>
+<br>
+maybe rhymes<br>
+hold court<br>
+in tripartite<br>
+&nbsp;&nbsp;&nbsp;castles of<br>
+color and<br>
+&nbsp;&nbsp;sound<br>
+rhymes found<br>
+in visual swirls<br>
+&nbsp;&nbsp;&nbsp;&nbsp;designing<br>
+semiotic seals<br>
+&nbsp;&nbsp;&nbsp;beaten to perform<br>
+<br>
+light on retinas<br>
+refactored into<br>
+&nbsp;&nbsp;&nbsp;rhyme<br>
+filtered,<br>
+&nbsp;&nbsp;&nbsp;discarded<br>
+charred and<br>
+&nbsp;&nbsp;&nbsp;scarred<br>
+<br>
+poems of<br>
+&nbsp;&nbsp;perceptiona<br>
+literal linkages<br>
+&nbsp;&nbsp;&nbsp;&nbsp;of<br>
+<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; or<br>
+<br>
+rhymes foregrounded<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;in sounds<br>
+of structure.<br>
+Semiotic turns<br>
+in metafunctions<br>
+at visual<br>
+&nbsp;&nbsp;&nbsp;&nbsp;conjunctions<br>
+naming<br>
+interpretive moves by<br>
+long toothed<br>
+jesters who<br>
+sway and swoon<br>
+Kings and Queens<br>
+<br>
+in grooves that<br>
+swim in<br>
+ideology of<br>
+social convention<br>
+constructed<br>
+&nbsp;&nbsp;&nbsp;in perceptions<br>
+of context<br>
+&nbsp;&nbsp;&nbsp;&nbsp;lways knocking<br>
+a buoy in<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;unsettled<br>
+patterns<br>
+storms of images<br>
+in rhyme</p>
+<p>The poems were not necessarily written in this order, in fact they weren't but it helped the linear shape of this post to group tagents together.</p>
+<p>Can you rhyme images? I think so. The moose out front told me the issue was closed and the owl said the answer was three.</p>
+
+      </div>
+<p><small>Also on <a class="u-syndication" href="https://indieweb.xyz/en/poetry">IndieWeb Poetry</a></small> <small>Also on <a class="u-syndication" href="https://clmoocring.jgregorymcverry.com/clmooc-planet/">CLMOOC Planet</a></small></p>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
