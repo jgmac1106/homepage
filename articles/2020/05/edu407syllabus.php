a8ff69abf75f3e96f2ebe3f3980ad57bcc557590
Greg McVerry <jgregmcverry@gmail.com>
2020-05-25 11:34:40 -0400

    add article path
    add article path

--- articles/2020/05/edu407syllabus.php
+++ articles/2020/05/edu407syllabus.php
@@ -1,6 +1,6 @@
-<?php include 'head.php'; ?>
+<?php include '../../head.php'; ?>
 </head>
-<?php include 'header.php'; ?>
+<?php include '../../header.php'; ?>
     </header>
   <main class="longreads">   
   <article class="h-entry">
