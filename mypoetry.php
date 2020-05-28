dd5c2b756838cf14cb79a78715a12e2bf1c8d462
Greg McVerry <jgregmcverry@gmail.com>
2020-05-28 09:24:02 -0400

    Update mypoetry.php
    add today's poem

--- mypoetry.php
+++ mypoetry.php
@@ -16,7 +16,7 @@
    <p class="p-summary">A boombox of bad poems</p>
    
 
-    <time class="dt-updated" datetime="2020-05-27 12:04:20">27<sup>th</sup> May 2020</time>
+    <time class="dt-updated" datetime="2020-05-28 09:24:20">28<sup>th</sup> May 2020</time>
    <img class="u-photo" src="https://jgregorymcverry.com/photos/poemboxpodcastart.jpg" style="width:200px">
    <a class="u-author h-card" href="https://jgregorymcverry.com/mypoetry#audiopoems">jgmac1106</a>
  </div>
@@ -820,6 +820,15 @@ Music <a href="https://soundcloud.com/jusbeats/7-a-m-funky-bass-sessions"> 7 A.M
 </audio>
 <data class="p-size" value="481406"></data><data class="p-duration" value="PT00H00M15S"></data><br />
 <p class="e-content"><a class="u-url" href="https://quickthoughts.jgregorymcverry.com/2020/05/27/cline-timesas-truthdiverges-falsegeographicalboundariesas-selectionsof-truthsflow-inadaptations">Show Link</a></p>
+</div>
+<div class="h-entry audiopoem">
+  <p class="p-name">Atone</p>
+  <a class="p-author" href="/"></a>
+  <time class="dt-published" datetime="2020-05-28 09:24:20">28<sup>th</sup> May 2020</time>
+ <audio class="u-audio" src="https://jgregorymcverry.com/poetry/Atone.mp3" controls="controls">
+</audio>
+<data class="p-size" value="529093"></data><data class="p-duration" value="PT00H00M16S"></data><br />
+<p class="e-content"><a class="u-url" href="https://quickthoughts.jgregorymcverry.com/2020/05/27/poemsofpresencewefill-our-poemswith-trees-and-birdsrarely">Show Link</a></p>
 </div>
 </div>
 <div class="audiopoem">
