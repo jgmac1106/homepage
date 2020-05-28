8c246340811cb804274739477bd0befd76d58f06
Greg McVerry <jgregmcverry@gmail.com>
2020-05-28 13:17:45 -0400

    Create tech4teens.php
    add a post

---
+++ tech4teens.php
@@ -0,0 +1,36 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">EDU 407 Beginning Literacies Syllabus</h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
+     on <time class="dt-published" datetime="2020-05-28 13:07:20">28<sup>th</sup> May 2020</time></p>
+    <small>Image: CfAL Logo. </small>
+
+    
+    <span class="summary">
+    <p class="p-summary">Draft Plan for the Tech4Teens Free Remote Technology Camp </p>
+    </span>
+  </div>
+<img class="featured u-featured" src="https://static.wixstatic.com/media/1f5f9e_685f03103ecf4cb698fcaedb39a1b5c6~mv2.png" alt="CfAL logo">
+    <div class="copy">
+
+  <div class="e-content">
+    <p>Concepts for Adaptive Tehnology, Bella Xiong, a Yale Computer Science student, and myself are planning a free technology camp this summer for Connecticut Teens. Students will have mentors from Yale Computer Science and students in Southern Connecticut State Education educator and librarian programs.</p>
+    <p>We can scale this poject across the state, and technically nationally quite easily. Kids needs camp. Teacher prep programs need digital field placements. We can make it happen.</p>
+    <p>More importantly we should now, more than ever, root the web in places where #literacies belong: schools and libraries. Algorithims should not determine adolescents.</p>
+    <p>If you would like to support our  Tech4Teens program please contact <a href="mailto:jricker@cfalct.org">Jennifer Ricker</a>, Executive Director of CfAL. We always need more mentors and money. If you want to learn more about fulfilling field placement requirements reach out to me.</p>
+
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRvOFD-A9sZXJPH9NeKoRkQYZ_KvvsR_qcsY-1DepzkM5bh5MgDhXPcRW2TyCWcmg/embed?start=false&loop=false&delayms=15000" frameborder="0" width="960" height="749" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+      </div>
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
