37b564f53689e9e745436f4007ea2485725d76bb
Greg McVerry <jgregmcverry@gmail.com>
2020-05-28 17:12:57 -0400

    Create 05posts.php
    add page

---
+++ articles/2020/05posts.php
@@ -0,0 +1,87 @@
+<?php include('/home/jgregory/public_html/head.php'); ?>
+</head>
+<?php include('/home/jgregory/public_html/header.php'); ?>
+</header>
+  <main class="h-feed">
+  <h2 class="p-name">My Articles</h2>
+     <div class="h-card">
+  <a class="p-name u-url" href="http://jgregorymcverry.com" rel="me">Greg McVerry</a><br>
+    </div>
+    
+<section class="card-container">
+  <article class="h-entry card">
+        <header class="card__title">
+            <h3 class="p-name">Can Images Rhyme?</h3>
+        </header>
+      <div class="authorInfo">
+         <div class="h-card"> 
+           <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
+         </div>
+        
+    <time class="dt-published" datetime="2020-05-13 07:34:20">13<sup>th</sup>May 2020</time>
+      </div>
+        <figure class="card__thumbnail">
+                <img class="featured u-featured" src="https://live.staticflickr.com/65535/49734027238_225b2a037c_z.jpg" alt="">
+         
+        </figure>
+        <div class="p-summary card__description">
+          <p>
+           A #clmooc exploration in methods of making two images sing together
+          </p>
+        </div>
+        <a  href="/canimagesrhyme" class="u-url button">Read More</a>
+    <article class="h-entry card">
+        <header class="card__title">
+            <h3 class="p-name">May's Cold Rain and Snow</h3>
+        </header>
+      <div class="authorInfo">
+         <div class="h-card"> 
+           <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
+         </div>
+        
+    <time class="dt-published" datetime="2020-05-10 08:04:20">7<sup>th</sup>May 2020</time>
+      </div>
+        <figure class="card__thumbnail">
+                <img class="featured u-featured" src="https://jgregorymcverry.com/photos/ColdMayDay.jpg" alt="">
+         
+        </figure>
+        <div class="p-summary card__description">
+          <p>
+            Three poems written in the verse of vortex of a frozen May of Covid Quarintine #poemsofpresence
+          </p>
+        </div>
+        <a  href="/coldrainandsnow" class="u-url button">Read More</a>
+
+    </article>
+    <article class="h-entry card">
+        <header class="card__title">
+            <h3 class="p-name">History of CLMOOC in Verse</h3>
+        </header>
+      <div class="authorInfo">
+         <div class="h-card"> 
+           <p>Published by <a class="u-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a></p> 
+         </div>
+        
+     <time class="dt-published" datetime="2020-05-07 09:34:20">7<sup>th</sup>May 2020</time>
+      </div>
+        <figure class="card__thumbnail">
+                <img class="featured u-featured" src="https://live.staticflickr.com/65535/49857912948_986c454bf4_z.jpg" alt="">
+         
+        </figure>
+        <div class="p-summary card__description">
+          <p>
+            A collection of poems butchering the #clmooc history as bad as the #poetry
+          </p>
+        </div>
+        <a  href="/clmoochistoryinverse" class="u-url button">Read More</a>
+
+    </article>
+ 
+    </section>
+    <a href="/articles/2020/04" rel="prev">Previous page</a>
+    </main>
+    <footer>
+<?php include('/home/jgregory/public_html/footer.php'); ?>
+  </body>
+  
+</html>
