5dc6d53977d3b5d7ef659f6c8f93021c387533bc
Greg McVerry <jgregmcverry@gmail.com>
2020-05-29 05:34:09 -0400

    Create lemonadeclub1.php
    add a post

---
+++ lemonadeclub1.php
@@ -0,0 +1,39 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+</header>
+<main class="longreads">
+<article class="h-entry">
+<h1 class="p-name">Lemonade Run Club</h1>
+<div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a>
+on <time class="dt-published" datetime="2020-05-29 05:24:20">29<sup>th</sup> May 2020</time></p>
+<small>Image: Screen shot from Trackbook of the running trail</small>
+
+<span class="summary">
+  <p class="p-summary">First run preparing for the Lemonade Club 5k Remote Challenge</p>
+</span>
+</div>
+<img class="featured u-featured" src="https://jgregorymcverry.com/photos/lemonade/May28thrun.png" alt="">
+<div class="copy">
+<div class="e-content">
+  <p>When Jess suggested we start up a The Lemonade Club 5k remote challenge I really liked the idea. I have not run a 5k since my father's 60th birthday party 5k....almost 14 years ago. Plus it sounded like a great way to stay social while we physically distance.</p>
+  <p>Yesterday I, and the boys, started training. Last year I completed a trail in the woods behind the house. Each lap takes a runner ~2,510 feet (I need to manually measure GPS a bit flaky). In 23 minutes Danny (my youngest at 7) ran three laps. Ben (9) clocked in one lap and John (11) ran one lap after we finished. Having Danny as a pacer helped. I slowed down "for him" I "totally could have kept running without the winding"...okay sure</p>
+  <ul>
+    <li>Feet Ran: 7530</li>
+    <li>Elapsed Time: 23 minutes</li>
+    <li>Miles Ran: 1.43</li>
+    <li>KM Ran: 2.30</li>
+    <li>Time: 23 minutes</li>
+    <li>Elevation (uphill) 265 ft</li>
+    <li>Elevation downhill -319 ft</li>
+  </ul>
+  <p>So it looks like we got about half way there our first attempt. Another 1.67 miles. Not too bad out of the gate. 8,817 would take 3 and 1/2 more laps. Now it sounds more daunting once I do the math. Still I think we can get there by the time the remote 5k gets scheduled.</p>
+</div>
+</article>
+</main>
+<footer>
+<?php include 'footer.php'; ?>
+</body>
+
+</html>
