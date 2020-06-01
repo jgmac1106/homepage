f72a266160ee4f7dc68a9a35e2bc5b6020725ace
Greg McVerry <jgregmcverry@gmail.com>
2020-06-01 09:19:11 -0400

    Update mypoetry.php
    fix file link

--- mypoetry.php
+++ mypoetry.php
@@ -852,7 +852,7 @@ Music <a href="https://soundcloud.com/jusbeats/7-a-m-funky-bass-sessions"> 7 A.M
   <p class="p-name">Starved Trees</p>
   <a class="p-author" href="/"></a>
   <time class="dt-published" datetime="2020-05-31 10:14:20">31<sup>st</sup> May 2020</time>
- <audio class="u-audio" src="https://jgregorymcverry.com/poetry/wicksofjustice.mp3" controls="controls">
+ <audio class="u-audio" src="https://jgregorymcverry.com/poetry/StarvedTrees.mp3" controls="controls">
 </audio>
 <data class="p-size" value="814101"></data><data class="p-duration" value="PT00H00M11S"></data><br />
 <p class="e-content"><a class="u-url" href="https://quickthoughts.jgregorymcverry.com/2020/05/31/starved-treesgrow-strongestabundance-keeps-roots-fromgrowingdeepdisconnectedwhile-treesversed">Show Link</a></p>
