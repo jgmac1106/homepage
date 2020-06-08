aec2093236a9d5c28ddb83e5f73a2f39622ad9a4
Greg McVerry <jgregmcverry@gmail.com>
2020-06-08 09:13:31 -0400

    Create 2020-06-08-1.php
    add note

---
+++ notes/2020-06-08-1.php
@@ -0,0 +1,23 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry" >
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-06-08 09:04:20">6th<sup>th</sup>June 2020</time>
+ 
+ <p class="p-name e-content">
+  Academic Writing is Disciplinary Literacies. We have oour own genres and rules. Learn to play the game and college gets easier.
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/XV4Sjxh-_Rs" frameborder="0" allowfullscreen=""></iframe> <iframe width="560" height="315" src="https://www.youtube.com/embed/IJsrADW8wao" frameborder="0" allowfullscreen=""></iframe> <iframe width="560" height="315" src="https://www.youtube.com/embed/ECCrYObflSA" frameborder="0" allowfullscreen=""></iframe> <iframe width="560" height="315" src="https://www.youtube.com/embed/viaR8N-BIhw" frameborder="0" allowfullscreen=""></iframe></p>
+  Also on <a href="https://jgregorymcverry.com/notes/2020-06-08-1" class="u-url" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-05-12-1 "rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  	 	<div id="webmentions"></div>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
