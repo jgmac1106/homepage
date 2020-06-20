a2d25df3caa9aafa63f73ac25d2c4c6e8f30fcd7
Greg McVerry <jgregmcverry@gmail.com>
2020-06-20 14:59:42 -0400

    Update 2020-06-20-1.php
    fix

--- notes/2020-06-20-1.php
+++ notes/2020-06-20-1.php
@@ -10,7 +10,7 @@
 <video class="u-video" src="https://jgregorymcverry.com/videos/mst3000.mov" controls="controls"></video>
 
  <p class="p-name e-content">
-I don't know Dave do you speak Molybdenum?
+I don't know Dave? Are you made of Molybdenum?
 </p>
   Also on <a href="https://jgregorymcverry.com/notes/2020-06-20-1" class="u-url" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-06-10-1 "rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
   	</main>
