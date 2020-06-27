3188b9c3da5d041b32faacbd348898556237f7dd
Greg McVerry <jgregmcverry@gmail.com>
2020-06-27 10:08:58 -0400

    Create launchingtech4teens.php
    add a post

---
+++ launchingtech4teens.php
@@ -0,0 +1,52 @@
+</head>
+<?php include 'header.php'; ?>
+</header>
+  <main class="longreads">   
+  <article class="h-entry">
+  
+    <h1 class="p-name">
+      ElmCity Webmakers Joins Tech4Teens Club
+    </h1>
+   
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
+     on <time class="dt-published" datetime="2020-06-27 09:44:40">27<sup>th</sup> June 27</time></p>
+    
+
+    
+    <div class="summary">
+    <p class="p-summary">We are launching Tech 4 Teens Club. An #IndieWeb club for teens to learn web design, podcasting, photography, video editing, and coding. </p>
+    </div>
+ </div>
+   <img class="featured u-featured" src="http://tech-4-teens.club/wp-content/uploads/2020/06/cropped-Screen-Shot-2020-06-24-at-10.46.28-AM.png">
+    <div class="copy">
+  <div class="e-content">
+<p>We need to recenter the web in marginalized communities, we need to ensure all youth can own their story and control the data of their narrative, but we failed. We never tried. When people laugh at our efforts to teach youth of New Haven to build their own websites racism and the colonization of the web win.</p>
+<p>People act as if facebook, instagram, etsy, and pinterest are inevitable. They are not. These massive web platforms are just a blip in the history of human literacy. 12 years. That is it. Never in human history have we concentrated so much power to how kids read and write in just a handful of companies. Never in human history have we allowed adolescents to be algorithmically defined. I am not okay with this, nor should you.</p>
+<p>At the same time we have allowed systemic racism to ensure the great riches of Silicon Valley never reach Black and Brown children. We saw it when Covid-19 hit. What schools could shift quickly to remote learning? If you got packets of white paper sent home you probably had brown skin. It was sinful. Stop saying digital divide. The lack of equity is continued subjugation. Full stop.</p>
+<p>Something magical happens when you give young children and youth websites. They take ownership of their learning. They express passion and they learn the skills that will allow them to sift through the nonsense and hate being spread online. </p>
+<p>More importantly it isn't about keeping the bad off the web, but flooding the good. The BIPOC children of greater New Haven deserve the opportunity to learn the ways of living online. We worried so much about ensuring students could read without(many of us) realizing that how we read has changed.</p>
+<p>Seven years ago, a few of us at Southern Connecticut State University, decided to do something about it. We started the <a href="https://elmcitywebmakers.com">Elm City Webmakers</a>. This club was originally part of Gear Up and based on Mozilla Clubs, a now defunct network of youth clubs.</p>
+  <img src="https://jgregorymcverry.com/photos/studentcomicstrips.com" src="student researching Trail of Tears">
+<p>While the students worked with us we design personal websites, learned to make comic strips, and designed memes. We learned about the Black Panthers and the history of New Haven. We wrote letters to the Miami Police Union when they threatened to boycott the Super Bowl over Beyonce's formation. The project culminated in students learning to make digital art and receive three credits for taking EDU 106: New Literacies: Digital Text and Tools for Lifelong learning. Dual enrollment and turning the city of New Haven into our high school campus helps to fight for equity in tech</p>
+<p>After our GearUp students graduated, many now enrolled at SCSU, Elm City Webmakers continued. This time as part of the global IndieWeb movement. We seek a person centric alternative to the corporate web that uses the web itself as a social network through open protocols. This empowers people to own their data, control their spaces online, and make better connections.</p>
+<p>The Elm City Webmakers have hosted dozens of meet ups in New Haven through the years. Over the last two years we have organized and hosted an annual two day free camp in New Haven. </p>
+<img src="https://quickthoughts.jgregorymcverry.com/file/607fc915723937cd53befbaaa908a7c2/thumb.jpg" alt="four people at IWC New Haven">
+<p>Many SCSU students have come by to build personal websites. we have helped local authors and small business owners build their personal websites, but I felt it was too late. We need to teach website building, multimedia production, and code like any other critical writing skill. Early and often.</p>
+<p>So the Elm City Webmakers reached out to the Center for Adaptive Learning (full disclosure I serve on the Board of Directors of the Literacy Coalition of Greater New Haven with the CfAL founder). We began to plan our IndieWeb meet ups as a youth program. The students would all get a free website and choose to chase their passion in one of three pathways: podcasting, video production, or photography. We had just launched in February when covid-19 hit. Everything changed. Then George Floyd was brutally murdered, and you realized nothing changed.</p>
+<p>In talking to Jennifer Ricker, the CfAL director, the two of decided this is bigger than New Haven and something had to be done. The children of New Haven got abandoned by hundreds of years of racism and slavery. Social media has grown into tools to spread hate. Facebook. Pinterest and Etsy now extract the value of artist of the Black Diaspora. If we do not act the major companies will create digital versions of the Hudson Trading Company and Anglo-Belgian India Rubber Company. We decided to further evolve the Elm City Webmakers (which will be back on campus soon) into a free remote camp for teens everywhere.</p>
+
+<p>Tech4Teens is designed to empower youth to share their passion through technology.   The program is open to students 13 and older, ideally those that have completed 7th and/or 8th grades.  Information technology students from Yale and SCSU mentor youth to ensure each student has the support needed to be successful.   Any students that do not have a working computer will be giving a “loaner laptop” before the program – and then they can return the computer after completing the program to upgrade to a nicer refurbished Chromebook! (Students that do not complete the training can still keep laptop…but won’t be able to turn it in for a nice one.)</p>
+
+<p>Students will have the option to learn how to create websites, videos, podcasts and coding projects – all based on their passion and interests.  All student projects are posted on individual websites, and will be showcased on community showcase at end of program.  Weekly prizes including cellphone powerbars, wireless earbuds, wireless speakers, and even programmable robots!</p>
+<p>This is an all volunteer, self-funded project. Name.com donated server space.CfAl has secured a few small grants, we have found interns from Yale and SCSU to serve as mentors but we could use your help. We would love to provide all participants with a new Chromebook, for example. We want to get great recording gear. If you can help as a volunteer or to make a donation please reach out to us.
+
+ 
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
