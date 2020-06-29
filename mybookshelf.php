2633c53fa8d4baa877fa455837136538c2bc6655
Greg McVerry <jgregmcverry@gmail.com>
2020-06-29 16:24:47 -0400

    trying own sheet
    tryin my own stylesheet

---
+++ mybookshelf.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>INTERTEXTrEVOLUTION</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="webmention" href="https://webmention.io/jgregorymcverry.com/webmention" />
+    <link rel="pingback" href="https://webmention.io/jgregorymcverry.com/xmlrpc" />
+    <link rel="stylesheet" href="/css/bookshelf.css">
+    <script src="/script.js" defer></script>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-9ralMzdK1QYsk4yBY680hmsb4/hJ98xK3w0TIaJ3ll4POWpWUYaA2bRjGGujGT8w" crossorigin="anonymous">
+     <link href="https://fonts.googleapis.com/css?family=Heebo|Quicksand&display=swap" rel="stylesheet"> 
+     <link href="https://fonts.googleapis.com/css?family=Creepster|Griffy&display=swap" rel="stylesheet"> 
+     <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap" rel="stylesheet"> 
+     <link rel="authorization_endpoint" href="https://indieauth.com/auth">
+<link rel="token_endpoint" href="https://tokens.indieauth.com/token">
+<link rel="microsub" href="https://aperture.p3k.io/microsub/207">
+<script src="/js/webmention.js" async></script>
+<link rel="stylesheet" href="/css/webmentions.css">
+ </head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="h-feed">
+  <h2 class="p-name">jgmac1106 Bookshelf</h2>  
+  <a class="u-author" href="/"></a>
+<div class="bookcards">
+  <div class="h-cite u-rl bookcard" href="https://quickthoughts.jgregorymcverry.com/2020/06/28/monument-poems-new-and-selected">
+    <div class="bookcard-body">
+      <div class="bookcard-front">
+      <h2 class="p-name">
+        Monument
+      </h2>
+<img class="u-photo" src="https://quickthoughts.jgregorymcverry.com/file/bdd19ed0f9cabd5569f868355e01ea4c/Screen+Shot+2020-06-28+at+7.35.33+AM.png">
+        <p>Natasha Trethewey</p>
+      </div>
+      <div class="bookcard-back e-content">
+      <p> Natasha Trethewey paints the past in brutal truths. Her collection of new and curated poems in Tretheway's skills as a historian of humanity. As she notes "Why is everything I see the past / I’ve tried to forget?"</p>
+     <p>
+     Often Trethewey shines the light of her stanza onto corners of history we have forgotten. these stories usually revolve around Mississippi where she grew up the child of biracial parents, Trethewey's mother who her stepfathered murdered, and deep seeded racism.</p>
+
+<p>Through this lens Trethweay tells the story of the help, the dock workers, those forgottten during Katrina. She also finds historical artifacts such as piantings or the Race classification system used in Mexico.</p>
+        <a href="https://quickthoughts.jgregorymcverry.com/2020/06/28/monument-poems-new-and-selected">Read Full Review</a>
+
+
+</div>
+    </div>
+  </div>
+ <div class="h-cite u-rl bookcard" href="https://quickthoughts.jgregorymcverry.com/2020/06/28/monument-poems-new-and-selected">
+    <div class="bookcard-body">
+      <div class="bookcard-front">
+      <h2 class="p-name">
+        Type Writer Rodeo
+      </h2>
+<img class="u-photo" src="https://quickthoughts.jgregorymcverry.com/file/fa9a2a43ef6689508b0f0e8a37203163/thumb.jpg">
+        <p>Jodi Egerton, David Fruchter, Kerri Ann Holt, Sean Petri</p>
+      </div>
+      <div class="bookcard-back e-content">
+    <p> I could not put this collection down, still can't. In fact I turn to typewriter rodeo when ever I need a good dose of inspiration to slap me upside the head.</p>
+
+<p>"Typewriter rodeo" tells the tale of four friends who write free improv poetry for people on typewriters. The grup heads out to events and conferences, asks for an idea and then the keys start chomping through history.</p>
+
+<p>The authros gathered their poems and organized them into collections such as "love" "family" "whimsy", and "vunerability" to name a few</p>
+        <a href="https://quickthoughts.jgregorymcverry.com/2020/06/28/monument-poems-new-and-selected">Read Full Review</a>
+
+
+</div>
+    </div>
+  </div>
+  <div class="h-cite u-rl bookcard" href="https://quickthoughts.jgregorymcverry.com/2020/05/14/fooling-with-words">
+    <div class="bookcard-body">
+      <div class="bookcard-front">
+      <h2 class="p-name">
+        Fooling with Words
+      </h2>
+<img class="u-photo" src="https://quickthoughts.jgregorymcverry.com/file/0f1550ce223e129b922d02d518d97181/615561.jpg">
+        <p>Bill Moyers</p>
+      </div>
+      <div class="bookcard-back e-content">
+  <p>Few books make my, "Do not give away list." The highest rating I can give. Moyer's "Fooling with Words", make the cut as a poets guide to writing and living.</p>
+
+<p>The book which involves a series of interviews with 11 poets at the Dodge Poetry festival would make a great gift or even a textbook in an Intro to Poetry class.</p>
+
+<p>Each chapter, one for each poet overflows with advice and learning from some of the top masters of 20th Century American poets.</p>
+
+<p>Based on who spoke at the festival some voices feel underrepresented. The age of the poets, many who lived lives three times over, also means fresh voices of Spoken Word and Hip Hop did not make the stage. Cervantes, Lamkin, Garrison, Geok-Lin Lim create space for critical voices but the book still feels quite white.</p>
+        <a href="https://quickthoughts.jgregorymcverry.com/2020/05/14/fooling-with-words">Read Full Review</a>
+
+
+</div>
+    </div>
+  </div>
+    <div class="h-cite u-rl bookcard" href="https://quickthoughts.jgregorymcverry.com/2020/05/14/fooling-with-words">
+    <div class="bookcard-body">
+      <div class="bookcard-front">
+      <h2 class="p-name">
+        Sentinel
+      </h2>
+<img class="u-photo" src="https://quickthoughts.jgregorymcverry.com/file/01b0c7d58126c00d7e0a042061c09da5/51GLyRdN9zL._SX319_BO1%2C204%2C203%2C200_.jpg">
+        <p>Robert Hunter</p>
+      </div>
+      <div class="bookcard-back e-content">
+  <p>When Hunter passed I ordered his collection Sentinel. Probably haven't seen it in twenty years and never really read the work.</p>
+
+<p>Sure Hunter published all his books and poems online, from his own website,  before people knew what online meant but I like to read poetry on the shitter.</p>
+
+<p>Right off the top you realize how different and same Hunter's poetry taste when the the words hit your tongue.</p>
+
+<p>Same cascading truths peaking from cracks in the soul that we dig from deep within the earth but the poetry more sparse.</p>
+
+<p>Hunter writes with many dichotomies and paradigms. A style, I realize, I emulate, but I never deeply studied his poetry like I have his lyrics.</p>
+        <a href="https://quickthoughts.jgregorymcverry.com/2020/05/14/fooling-with-words">Read Full Review</a>
+
+
+</div>
+    </div>
+  </div>
+</div>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
