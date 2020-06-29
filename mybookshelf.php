5b4741078d7fb095c6c015bdff788b714180ef68
Greg McVerry <jgregmcverry@gmail.com>
2020-06-29 17:06:41 -0400

    Update mybookshelf.php
    switch main to div to change layout

--- mybookshelf.php
+++ mybookshelf.php
@@ -4,7 +4,7 @@
 <?php include 'header.php'; ?>
     </header>
  
-  <main class="h-feed">
+  <div class="h-feed">
   <h2 class="p-name">jgmac1106 Bookshelf</h2>  
   <a class="u-author" href="/"></a>
 <div class="bookcards">
@@ -99,7 +99,7 @@
     </div>
   </div>
 </div>
-    </main>
+    </div>
  
 
   </body>
