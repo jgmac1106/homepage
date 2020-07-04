084a81091076396069aeb40b70c4ef317d18b212
Greg McVerry <jgregmcverry@gmail.com>
2020-07-04 10:25:49 -0400

    Update whyithread.php
    testing something

--- whyithread.php
+++ whyithread.php
@@ -11,17 +11,13 @@
    <small>Image: <a title="thread 3" href="https://flickr.com/photos/jetheriot/6289825456">"thread 3"</a> by <a href="https://flickr.com/people/jetheriot">jetheriot</a> is licensed under <a href="https://creativecommons.org/licenses/by/2.0/">CC BY</a> </small>
 
     
-    <span class="summary">
+    <div class="summary">
     <p class="p-summary">A quick summary of IRC chat logs where I explore reasons for participating in Twitter threads</p>
-    </span>
+    </div>
   </div>
 <img class="featured u-featured" src="https://live.staticflickr.com/6226/6289825456_7aaebcb6d7_z.jpg" alt="">
  <div class="e-content">
-    <div>
-      <div class="daymark">2020-07-04 <span class="tz">UTC</span></div>
-      <div id="t1593862928611300" class="h-cite line msg-message "><div class="in"><a href="https://chat.indieweb.org/2020-07-04#t1593862928611300" class="hash">#</a> <time class="dt-published" datetime="2020-07-04T11:42:08+00:00"><a href="https://chat.indieweb.org/2020-07-04/1593862928611300" class="u-url time" title="2020-07-04T11:42:08+00:00">11:42</a></time> <span class="text"><span class="nick p-author h-card"><div class="avatar"><img src="https://chat.indieweb.org/img.php?url=http%3A%2F%2Fquickthoughts.jgregorymcverry.com%2Ffile%2F6802fbcdd3fada8cb61ffb798978f72f%2Fthumb.jpg&amp;sig=baae475641689e740df11b17420b079553c0063ca72f5a3ddfc096953f6d9d05" width="20" height="20" class="u-photo"/></div><span class=""><a href="http://jgregorymcverry.com" class="author p-nickname p-name u-url" target="_blank">[jgmac1106]</a></span></span> <span class="e-content p-name">I do Twitter threads....kinda helps me get ideas to unfold...stream of consciousness like stuff</span></span></div></div>
-
-    </div>
+    test, test test
     
       </div>
 
