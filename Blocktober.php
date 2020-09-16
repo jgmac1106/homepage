3621af6ed38a887efb7ed4dd5a12ea06bbb2eef3
Greg McVerry <jgregmcverry@gmail.com>
2020-09-16 08:51:56 -0400

    Create Blocktober.php
    add a post

---
+++ Blocktober.php
@@ -0,0 +1,56 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">Join Us for Blocktober</h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
+     on <time class="dt-published" datetime="2020-09-16 11:24:20">16<sup>th</sup>September 2020</time></p>
+   <small><a title="soul sucker" href="https://flickr.com/photos/cartercomics/53317757">"soul sucker"</a> by <a href="https://flickr.com/people/cartercomics">I'm Fantastic</a> is licensed under <a href="https://creativecommons.org/licenses/by/2.0/">CC BY</a> </small>
+
+    
+    <span class="summary">
+    <p class="p-summary">Our democracy is threatened. People doomscrolling into depression. All facebook's fault</p>
+    </span>
+  </div>
+<img class="featured u-featured" src="/phots/assets/scarycomputer.jpg" alt="">
+    <div class="copy">
+
+  <div class="e-content">
+    <p>Welcome to Blocktober. The IndieWeb community asks everyone to sign off of Facebook, Instagram, and What's App, and other social networks through the month of October and past the election in the United States. May be the only way to save the soul of our world.</p>p>
+    <p>Facebook is evil and profits off of racism and hate. Facebook is evil and actively undermines democracy across the globe. Fcaebook is evil and actively profits from addictive algorithims. Facebook is evil and endangers children. Facebook is evil and attacks the open web. We need to take action. Join us for Blocktober</p>
+    <p>Four years ago a massive intelligence operation threatened our election. We will not know if the Russians tipped the scale but we do know Putin had a finger on it.</p>
+<p>Facebook had four years to fix the problem. They did nothing. Worse facebook used the same fake news to try and buy influence in Washington. They even fell on the anti-semetic trope that George Soros was taking over the world. Not fake news on Facebook, but actual Fcaebook lobbyists. Still even in 2019 facebook let hate groups buy key words on terms such as “Joseph Goebbels,” “Josef Mengele,” “Heinrich Himmler,” the neo-nazi punk band Skrewdriver and Benito Mussolini’s long-defunct National Fascist Party. Facebook is a threat to demoncracy across the globe.</p>
+<p>Where other social netowkrs have banned political ads and flag media when politicians lie Zuckerberg openly welcomes the political advertsing. He wants all of Donal Trump's money.</p>
+<p>Facebook and all of its properties deliver almost all of the disinformation worldwide. If you utilize Facebook, What's App, anor Instagram you condone profiting from hate. Your dollars go to undermining our democracy, attacking minorities, and underminining women. It must stop. Join us for Blocktober</p>
+
+  <p>Facebook manipulates children and users. In 2019 facebook paid teens $20 a month to install VPNs to allow them to spy on competitors. When facebook sold products to children who may have overspent facebook refused to refund parents and called them "whales" in internal documents. Even scarier is we now allow adolescents to be defined by an algorithim. Instagram decides what's cool, what body shaming images young women see. In 2012 facebook ran an experiment, without consent, omn 700,000 to see if their feed could change their mood. It does. Unethical science, unethical business. Facebook, which began as an app for ranking women as hot or not bemust stopped.</p>
+ 
+    <p>Facebook profits off of addiction just as any drug dealer. Do you know they keep cognitive scientists on staff to make products more addictive? In fact that is why the notification bubble turned to red...Red led to engagement. Just like hate. See facebook needs impression, arguing, anger and fake news lead to engagement. Facebook thrives on fear.</p>
+    <p>Facebook fights against the open web. It first began by removing access to write apis and siloed off the platfrom fromthe rest of the wbe. Now Instagram wants to go a step further and make it a copyright violation to link to material on the web. In fact facebook wants to make it impossible to link content or view linked content without being logged in. For a long time now facebook and Instagram used oEmbed, which according to the creator, is "an open web API standard for fetching embed code based on a URL.” </p>
+    <p>In other words facebook doesn't want the web to work by simply linking to content URLs....They NEED you logged into facebook and Instagram so they can track your every movement, every site visit, every click. That's how they profit. By spying on your every move. Facebook has decided it is too hard to spy on the open web and wants to make sure they are the only place you spend any free time. Facebook is evil.</p>
+<p>In all of human history reading and writing (barring the Church and Slave Trade) have been open to all. Literacy provided a path to liberation. Free libraries sprouted up. The web was born on open standards and made anyone a publisher. Facebook wants to destroy all this. In the last 12 years we have concentrated almost all of humanities literary interactions in the hands of one company...One company that has time and time again demonstrated they happily profit off of racism and hate, undermine democracy, prey on children, and attack the open web.</p>
+<p>We need to protect our society. We need Blocktober. So join use this October 1st through election day in the USA and quit facebook, all their properties, and any other social netowrk you feels threatens democracy.</p>
+<h3>How to Get Involved</h3>
+<ul>
+  <li>Join us for a social two screen screening of the Social Dilemma on Septermber 25th at 18:00 UTC (14:00 EDT)</li>
+  <li>Log off for the month. Call a friend. Play chess. Enjoy a month free of doomscrolling</li>
+  <li>Encourage parents and grandparents to get off of facebook. Older people, especially older white people, are much more susceptible to disinformation and they spread it faster than any other demographic</li>
+  <li>Get a blog. Share your passion</li>li>
+  <li>Join us on the IndieWeb chat to hang out.</li>
+  <li>Vote. Early. Make sure you know your state's rules</li>
+</ul>
+
+
+      </div>
+<p><small>Also on Blocktober<a class="u-syndication" href="https://indieweb.xyz/en/Blocktober">IndieWeb OER</a></small> 
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
