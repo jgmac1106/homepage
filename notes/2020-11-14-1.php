7e81523fadabb85cadfa71e388841ce36cadfdf9
Greg McVerry <jgregmcverry@gmail.com>
2020-11-14 09:36:18 -0500

    Update 2020-11-14-1.php
    fix an error

--- notes/2020-11-14-1.php
+++ notes/2020-11-14-1.php
@@ -13,7 +13,7 @@
 This was my first attempt at recording a pawn. People think I should stream while I carve. I think people are crazy. That is why I am outside carving and not talking to them. <br>
 I need to work on camera placement so you do not see the back of my hand only. 
 </p>
-  Also on <a href="https://jgregorymcverry.com/notes/2020-11-14-1" class="u-url" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-11-13-1 "rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  Also on <a href="https://jgregorymcverry.com/notes/2020-11-14-1" class="u-url" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-11-13-1" rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
   	</main>
   	 	<div id="webmentions"></div>
   		<footer>
