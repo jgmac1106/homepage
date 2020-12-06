1d449138b53c61efb86f5b46cec540c1007ec76b
Greg McVerry <jgregmcverry@gmail.com>
2020-12-05 19:44:15 -0500

    add a note
    add a note

---
+++ notes/2020-12-05-1.php
@@ -0,0 +1,24 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry" >
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2020-12-05 19:24:20">14<sup>th</sup> November 2020</time>
+<video class="u-video" controls src="https://jgregorymcverry.com/videos/Jambalaya.mp4"></video>
+
+ <p class="p-name e-content">
+This is both my first attempt at playing a song on the ukulele. Also my first time recording it. <br >
+I know a couple chords and a few scales but I a don't know any songs. I just have kinda been beating away on my uke and hoping something comes out. It ain't pretty but it is a start. 
+</p>
+  Also on <a href="https://jgregorymcverry.com/notes/2020-12-05-1" class="u-url" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-11-14-1" rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  	</main>
+  	 	<div id="webmentions"></div>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
