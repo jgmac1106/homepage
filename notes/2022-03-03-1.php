0c6068628d7ff93634b9c68e0046a070a5d594e1
Greg McVerry <jgregmcverry@gmail.com>
2022-03-03 20:13:21 -0500

    Update 2022-03-03-1.php
    removed extra poem

--- notes/2022-03-03-1.php
+++ notes/2022-03-03-1.php
@@ -9,7 +9,6 @@
 <time class="dt-published" datetime="2022-03-03 19:34:20">3<sup>rd</sup>March 2022</time>
  <p class="p-name e-content">
 &nbsp;&nbsp;#smallpoems #clmooc<br>
-<pre><br>
 &nbsp;&nbsp;Did you zoom in<br>
 to the state of the union?<br>
 <br>
@@ -83,81 +82,7 @@ Reauthorizes while<br>
 making the stacks<br>
 Hinge on Risk Based<br>
 Benjamins<br>
-&nbsp;&nbsp;#smallpoems #clmooc<br>
-<pre><br>
-&nbsp;&nbsp;Did you zoom in<br>
-to the state of the union?<br>
-<br>
-It really hurt me<br>
-to hear no h/t<br>
-to cybersecurity<br>
-Bridges and free day care<br>
-no mention of data<br>
-in the air<br>
-<br>
-now to be<br>
-fair<br>
-this must be taught in<br>
-diapers<br>
-before scary wipers<br>
-(both kinds)<br>
-<br>
-Occam's Razor to Me<br>
-protect our digital<br>
-boundaries<br>
-even<br>
-as<br>
-E. Orders thrust us<br>
-towards zero trust<br>
-hoarders stack attacks<br>
-of ransomware<br>
-<br>
-So Let us be okay<br>
-(yeah I mean help pay) and<br>
-share the burden<br>
-Head the Call!<br>
-Shields Up.<br>
-Yes ma'am can you<br>
-spare us two pence<br>
-for the cup?<br>
-<br>
-Tax Breaks and<br>
-Hand Shakes<br>
-Third Party Attestation<br>
-Shakes hands<br>
-and breaks attacks on<br>
-complex systems<br>
-While Biden maybe<br>
-hiding public discussions<br>
-from the Russians<br>
-Senate past a bill<br>
-that will have<br>
-massive<br>
-repercussions<br>
-the Portman/Peters/Putin Act<br>
-well that ain't the name to be exact<br>
-will try to light a fire by<br>
-requiring mandatory<br>
-reporting in<br>
-72 hours for those<br>
-with power<br>
-of critical<br>
-infrastructure<br>
-No more duck and<br>
-cover<br>
-(or insurance)<br>
-Also tries<br>
-to modernize<br>
-FedRAMP<br>
-I'd need a Genie Lamp<br>
-to afford that stamp<br>
-Then it turns to FISMA<br>
-Just to Hicthya<br>
-Three Times<br>
-Reauthorizes while<br>
-making the stacks<br>
-Hinge on Risk Based<br>
-Benjamins<br>
+
 
 </p>
 Also on <a href="https://jgregorymcverry.com/notes/2022-03-03-1" class="u-url" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-12-05-1" rel="prev">Previous Note</a> <a href="https://jgregorymcverry.com/notes/2022-03-0-2" rel="next">Next Note</a>    <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
