d9c508f01d6b58749a343d796d7cdb515e287a88
Greg McVerry <jgregmcverry@gmail.com>
2022-03-03 19:44:04 -0500

    Update 2022-03-03-1.php

--- notes/2022-03-03-1.php
+++ notes/2022-03-03-1.php
@@ -7,7 +7,7 @@
   	<div class="note h-entry">
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2022-03-03 19:34:20">3<sup>rd</sup>March 2022</time>
- <p class="p-name e-content">
+ <div class="p-name e-content">
 &nbsp;&nbsp;#smallpoems #clmooc
 &nbsp;&nbsp;Did you zoom in
 to the state of the union?
@@ -82,7 +82,7 @@ Reauthorizes while
 making the stacks
 Hinge on Risk Based
 Benjamins
-</p>
+</div>
 </div>
 Also on <a href="https://jgregorymcverry.com/notes/2022-03-03-1" class="u-url" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-12-05-1" rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
   	</main>
