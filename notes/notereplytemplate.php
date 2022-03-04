a1bbdf82ba6281a55771d48bb952c53bb55bdca6
Greg McVerry <jgregmcverry@gmail.com>
2022-03-03 19:50:29 -0500

    Create notereplytemplate.php

---
+++ notes/notereplytemplate.php
@@ -0,0 +1,27 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+  <main class="longreads"> 
+    <div class="notewrapper">
+  	<div class="note h-entry">
+      <a class="u-author" href="/"></a>
+
+        
+  
+      
+        <time datetime="YYYY-MM-DD TT:TT:TT" class="dt-published">YYY-MM-DD TT:TT</time>
+       <p>In reply to <a class="u-in-reply-to" href="some url" rel="prev">Genre</a> by <a class="h-card p-author" href="">author</a></p>
+      <p class="p-name e-content">
+  Also on <a href="url" class="u-url" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-11-14-1" rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+
+</div>
+  	</main>
+<div id="webmentions"></div>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
