51467c86ab608ebf7e786585f72cf3141718823f
Greg McVerry <jgregmcverry@gmail.com>
2022-03-03 20:04:55 -0500

    Update 2022-03-03-2.php

--- notes/2022-03-03-2.php
+++ notes/2022-03-03-2.php
@@ -8,7 +8,7 @@
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2022-03-03 19:52:20">3<sup>rd</sup>March 2022</time>
  <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/dogtrax/status/1499542603938902016">@dogtrax</a></p>
- <p class="p-name e-content">It was your reflections from rhizolearning which will make @nomadwarmachine happy a bit undadaesque to knock stick to just the text, not enough question in your poems if you forget to look. And what is truth if not rearranged questions?
+ <p class="p-name e-content">It was your reflections from rhizolearning which will make @nomadwarmachine happy a bit undadaesque to knock stick to just the text, not enough question in your poems if you forget to look. And what is truth if not rearranged questions??
  </p>
   Also on<a href="https://jgregorymcverry.com/notes/2022-03-03-2" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2022-03-03-2" rel="prev">Previous Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
