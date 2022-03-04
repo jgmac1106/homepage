d4ddaa2134dfbf13a9a0da3e1c8c20beddfe98aa
Greg McVerry <jgregmcverry@gmail.com>
2022-03-03 19:39:29 -0500

    Create 2022-03-03-1.php

---
+++ notes/2022-03-03-1.php
@@ -0,0 +1,95 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2022-03-03 19:34:20">3<sup>rd</sup>March 2022</time>
+ <p class="p-name e-content">
+&nbsp;&nbsp;#smallpoems #clmooc
+&nbsp;&nbsp;Did you zoom in
+to the state of the union?
+
+It really hurt me
+to hear no h/t
+to cybersecurity
+Bridges and free day care
+no mention of data
+in the air
+
+now to be
+fair
+this must be taught in
+diapers
+before scary wipers
+(both kinds)
+
+Occam's Razor to Me
+protect our digital
+boundaries
+even
+as
+E. Orders thrust us
+towards zero trust
+hoarders stack attacks
+of ransomware
+
+So Let us be okay
+(yeah I mean help pay) and
+share the burden
+Head the Call!
+Shields Up.
+Yes ma'am can you
+spare us two pence
+for the cup?
+
+Tax Breaks and
+Hand Shakes
+Third Party Attestation
+Shakes hands
+and breaks attacks on
+complex systems
+While Biden maybe
+hiding public discussions
+from the Russians
+Senate past a bill
+that will have
+massive
+repercussions
+the Portman/Peters/Putin Act
+well that ain't the name to be exact
+will try to light a fire by
+requiring mandatory
+reporting in
+72 hours for those
+with power
+of critical
+infrastructure
+No more duck and
+cover
+(or insurance)
+Also tries
+to modernize
+FedRAMP
+I'd need a Genie Lamp
+to afford that stamp
+Then it turns to FISMA
+Just to Hicthya
+Three Times
+Reauthorizes while
+making the stacks
+Hinge on Risk Based
+Benjamins
+</p>
+</div>
+Also on <a href="https://jgregorymcverry.com/notes/2022-03-03-1" class="u-url" rel="canonical"><strong>My Notes</strong></a>  <a href="https://jgregorymcverry.com/notes/2020-12-05-1" rel="prev">Previous Note</a>   <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  	</main>
+  	<div id="webmentions"></div>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
