f1ce27e82ed500a917c00a92fc38f397dd4478af
Greg McVerry <jgregmcverry@gmail.com>
2022-03-20 08:57:11 -0400

    adding a poem
    adding a poem

--- notes/2022-03-03-2.php
+++ notes/2022-03-03-2.php
@@ -10,7 +10,7 @@
  <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/dogtrax/status/1499542603938902016">@dogtrax</a></p>
  <p class="p-name e-content">It was your reflections from rhizolearning which will make @nomadwarmachine happy a bit undadaesque to knock stick to just the text, not enough question in your poems if you forget to look. And what is truth if not rearranged questions??
  </p>
-  Also on<a href="https://jgregorymcverry.com/notes/2022-03-03-2" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2022-03-03-2" rel="prev">Previous Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+  Also on<a href="https://jgregorymcverry.com/notes/2022-03-03-2" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2022-03-03-1" rel="prev">Previous Note</a> <a href="https://jgregorymcverry.com/notes/2022-03-0-2" rel="next">Next Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
   	</main>
   		<footer>
---
+++ notes/2022-03-20-1.php
@@ -0,0 +1,33 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2022-03-20 08:50:53">20<sup>th</sup>March 2022</time>
+ <p>In reply to <a class="u-in-reply-to" href="https://twitter.com/telliowkuwp/status/1505506346854195201">@telliowkuwp</a></p>
+ <p class="p-name e-content">
+   &nbsp;&nbsp;&nbsp;When given<br>
+&nbsp;&nbsp;&nbsp;charge<br>
+&nbsp;&nbsp;A<br>
+mass of pointed<br>
+potential.<br>
+We unleash<br>
+&nbsp;&nbsp;&nbsp;a greater acceleration<br>
+together<br>
+we escape the<br>
+negative gradient<br>
+&nbsp;&nbsp;&nbs;of your constant pull<br>
+#smallpoems
+ </p>
+  Also on<a href="https://jgregorymcverry.com/notes/2022-03-20-1" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2022-03-03-2" rel="prev">Previous Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
