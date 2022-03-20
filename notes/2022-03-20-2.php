96fbaf0fe525b537566e0724cc21570e3e271225
Greg McVerry <jgregmcverry@gmail.com>
2022-03-20 14:58:26 -0400

    Adding note
    Adding a note

---
+++ notes/2022-03-20-2.php
@@ -0,0 +1,23 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2022-03-20 14:27:53">20<sup>th</sup> March 2022</time>
+ <
+ <p class="p-name e-content">
+   Trying to develop a mobile workflow for publishing notes. Currently type in sublime and push using Fast Git and GitHub Actions. 
+#IndieWeb 
+ </p>
+  Also on<a href="https://jgregorymcverry.com/notes/2022-03-20-2" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2022-03-20-01" rel="prev">Previous Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
