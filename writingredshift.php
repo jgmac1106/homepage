460502259ba0834ab25cdf02f749d9df9fa85747
Greg McVerry <jgregmcverry@gmail.com>
2022-03-20 09:52:59 -0400

    Create writingredshift.php
    added backstage post

---
+++ writingredshift.php
@@ -0,0 +1,55 @@
+<?php include 'head.php'; ?>
+</head>
+<?php include 'header.php'; ?>
+    </header>
+  <main class="longreads">   
+  <article class="h-entry">
+  <h1 class="p-name">Shifting Realities: A Story of a Poem</h1>
+  <div class="pubinfo">
+  <p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">J. Gregroy McVerry</a> 
+     on <time class="dt-published" datetime="2022-03-20 09:45:43">20<sup>th</sup> March 2022</time></p>
+    <small>Image: <a title="GRB 080319B" href="http://www.nasa.gov/images/content/218810main_grb_20080320_HI.jpg">"GRB 080319B"</a> by NASA Public Domain  </small>
+
+    
+    <span class="summary">
+    <p class="p-summary">Someone asked me about the genesis of a recent poem. This is the behind the stage #clmooc #literacies</p>
+    </span>
+  </div>
+<img class="featured u-featured" src="https://upload.wikimedia.org/wikipedia/commons/3/32/GRB_080319B.jpg?20080321201146" alt="">
+    <div class="copy">
+  <div class="e-content">
+   <p>I recently published a poem <a href="https://stream.jgregorymcverry.com/2022/03/19/smallpoemsstuck-in-my.html">Redshift"</a> and someone pinged me and asked how I wrote the poem. The poem tries to capture the feeling of weight by using everything from Newton's First Law (Inertial Frame) to gamma ray burst as a metaphor for every day life.</p>
+<p>First I often write about space and gravity. When you talk about forces billions of years old at a scale of energy we can never imagine you have good material that almost approaches the mysteries of the human condition.</p>
+<p>I am also lazy. Coming up with new ideas is hard. Many of the poems I write each day map perfectly to the featured article or "on this day" feature of Wikipedia. I love this feature I could chase rabbit holes forever. Sometimes these journeys become poems.</p>
+<p>I read the following articles for the poem:</p>
+<ul>
+   <li><a href="https://en.wikipedia.org/wiki/Main_Page">Wikipedia Homepage</a></li>
+   <li><a href="https://en.wikipedia.org/wiki/Redshift">Redshift</a></li>
+      <li><a href="https://en.wikipedia.org/wiki/Gravitational_redshift">Gravitational Redshift</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Lorentz_covariance">Lorentz Covariance</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Inertial_frame">Inertial Frame of Reference</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Triangulum_Galaxy">Triangulum Galaxy</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Local_Group">Local Group</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Milky_Way">Milky Way</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Phoenix_Dwarf">Phoenix Dwarf</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Superluminous_supernova">Superluminous Supernova</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Supernova#Core_collapse">Core Collapse</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Electron_capture">Electron Capture</a></li>
+   </ul>
+   <p>As I would read I would then take notes to form the poem. In the first phase I just collect phrases I think sound cool. I look for phrases that easily have double meanings or where I can allude to everyday struggles utilizing an antithesis of paradigms.</p>
+   <img src="https://jgregorymcverry.com/photos/redshiftphoto1.jpg" alt="screenshot of journal">
+   <p>I then draft the poem. Usually in my journal. You also see all the names for the Triangulum Constellation. I thought that would make a good poem too. Babylonians called it "The Plow." It marked the start of Autumn harvest. The Chinese call the constellation "Heaven's Great General." Poem pretty much writes itself.</p>
+    <img src="https://jgregorymcverry.com/photos/redshiftphoto2.jpg" alt="screenshot of journal">
+    <p>I would never recommend poetry as a way to synthesize information about science (I would still make it a playlist assignment choice) but it really works for me. When you transform phenomenon from billions of years ago in to arranged phonemes the information you use remains sticky.</p>
+    <p>In the end I have no secret sauce to writing poetry. I read multiple sources, take notes, draft an idea, and publish a poem. Like gravity. The Writing Process is a constant that can not change without altering your frame of reference.</p>
+      </div>
+   </div>
+<p><small>Also on <a class="u-syndication" href="https://indieweb.xyz/en/poetry">IndieWeb Poetry</a></small> 
+</article>
+    </main>
+    <footer>
+<?php include 'footer.php'; ?>
+
+  </body>
+ 
+</html>
