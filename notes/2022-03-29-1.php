8cc3de314d7f3d21e62050aefa0e835e474ed785
Greg McVerry <jgregmcverry@gmail.com>
2022-03-29 20:54:56 -0400

    Update 2022-03-29-1.php
    i didn't like the old version

--- notes/2022-03-29-1.php
+++ notes/2022-03-29-1.php
@@ -9,14 +9,15 @@
 <time class="dt-published" datetime="2022-03-29 20:39:07">29<sup>th</sup> March 2022</time>
  
  <div class="p-name e-content">
+    #smallpoems<br>
    Torchlight<br>
 &nbsp;&nbsp;saddles<br>
 &nbsp;&nbsp;&nbsp;&nbsp;strike flint<br>
-in the sky<br>
+sauntering skyward<br>
  &nbsp;golden<br>
  ore, veins of light<br>
- on painted shores bashing<br>
- on bottom of our clif<br>
+ on painted shores<br>
+keeping time at cliff's edge<br>
  Weightless girth<br>
  &nbsp;&nbsp;Clinched to<br>
  unfettered<br>
@@ -36,7 +37,7 @@ in the sky<br>
   &nbsp;&nbsp;&nbsp;Need<br>
   For the Zen<br>
   of Provenance<br> 
- </div>
+ </div><br>
   Also on<a href="https://jgregorymcverry.com/notes/2022-03-29-1" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2022-03-20-02" rel="prev">Previous Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
   	</main>
