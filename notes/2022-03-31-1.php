273033c9eddd59b0d71af12704a06308bb98b660
Greg McVerry <jgregmcverry@gmail.com>
2022-03-31 20:30:59 -0400

    Create 2022-03-31-1.php
    add a poem

---
+++ notes/2022-03-31-1.php
@@ -0,0 +1,37 @@
+<?php include '../head.php'; ?>
+</head>
+ <?php include '../header.php'; ?>
+</header>  
+   <main class="longreads"> 
+  	<div class="notewrapper">
+  	<div class="note h-entry">
+<a class="u-author" href="/"></a>
+<time class="dt-published" datetime="2022-03-31 19:39:07">31<sup>st</sup> March 2022</time>
+ 
+ <div class="p-name e-content">
+    #smallpoems<br>
+  <br>
+After
+&nbsp;&nbsp;carving<br>
+crooked<br>
+&nbsp;&nbsp;&nbsp;knights<br>
+bouncing on<br>
+&nbsp;broken<br>
+mounts;<br>
+&nbsp; hills of<br>
+logic in Staunton's<br>
+&nbsp;&nbsp;Angles<br>
+grow.<br>
+Truth cut in hard<br>
+predictable<br>
+counts<br>
+ </div><br>
+  Also on<a href="https://jgregorymcverry.com/notes/2022-03-31-1" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2022-03-30-2" rel="prev">Previous Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+</div>
+  	</main>
+  		<footer>
+     <?php include '../footer.php'; ?>
+
+  </body>
+ 
+</html>
