e72d7ef5495784d6e624277631d1b87251eb7a4e
Greg McVerry <jgregmcverry@gmail.com>
2022-04-03 10:28:36 -0400

    Update 2022-04-03-1.php
    forgot hashtag

--- notes/2022-04-03-1.php
+++ notes/2022-04-03-1.php
@@ -9,6 +9,7 @@
 <time class="dt-published" datetime="2022-04-03 9:39:07">3<sup>rd</sup> April 2022</time>
  
  <div class="p-name e-content">
+    #silentsunday #clmooc
    <img class="u-photo" src="https://jgregorymcverry.com/photos/2022-04-03SilentSunday.jpg" alt="tree with growth that looks like nose">
   Also on<a href="https://jgregorymcverry.com/notes/2022-04-01-1" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2022-03-31-1" rel="prev">Previous Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
 </div>
