df20a6d620e551daea32c114e695886e1a775209
Greg McVerry <jgregmcverry@gmail.com>
2022-07-12 13:53:16 -0400

    Update latestnote.php
    publish note

--- notes/latestnote.php
+++ notes/latestnote.php
@@ -1,19 +1,19 @@
-<?php include '../head.php'; ?>
+?php include '../head.php'; ?>
 </head>
  <?php include '../header.php'; ?>
 </header>  
    <main class="longreads"> 
-  	<div class="notewrapper">
-  	<div class="note h-entry">
+   <div class="notewrapper">
+   <div class="note h-entry">
 <a class="u-author" href="/"></a>
 <time class="dt-published" datetime="2022-07-08 10:24:07">6<sup>th</sup> June 2022</time>
  <p class="p-name e-content">
  #EDU566dailyponderance What builds self-esteem? Why? How can technology help or hurt? <br><a href=""></a>
 
 </p>
-Also on<a href="https://jgregorymcverry.com/notes/2022-07-08-1" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2022-07-07-1" rel="prev">Previous Note</a>  <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
-  	</main>
-  		<footer>
+Also on<a href="https://jgregorymcverry.com/notes/2022-07-08-1" rel="canonical"><strong>My Notes</strong></a> <a href="https://jgregorymcverry.com/notes/2022-07-07-1" rel="prev">Previous Note</a> a href="https://jgregorymcverry.com/notes/2022-07-12-1" rel="next">Next Note</a> <a class="u-syndication" href="https://brid.gy/about#twitter">Published with Bridgy</a>
+   </main>
+      <footer>
      <?php include '../footer.php'; ?>
 
   </body>
