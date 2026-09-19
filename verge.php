0f2f66bc1aa629586f4e7ea5adc84628d9bfcf51
Greg McVerry <jgregmcverry@gmail.com>
2026-09-19 10:21:50 -0400

    Publish https://jgregorymcverry.com/verge

---
+++ verge.php
@@ -0,0 +1,21 @@
+<!-- publishing:managed -->
+<?php include __DIR__.'/head.php'; ?>
+</head><body><?php include __DIR__.'/header.php'; ?></header>
+<main class="longreads">
+<!-- publishing:body --><article class="note h-entry" style="grid-auto-rows:auto">
+
+<div class="pubinfo" style="grid-column:2/8"><p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">Greg McVerry</a> on <time class="dt-published" datetime="2026-09-19">2026-09-19</time> · <a class="u-url" href="https://jgregorymcverry.com/verge">Permalink</a></p></div>
+<div class="p-name e-content copy" style="grid-row:3"><div class="verse" style="white-space:pre-wrap">learned society
+dribbling aphorisms
+into broken prose
+on forgeotten farms
+roofs collapse under
+weight of sutras
+who dance to matters of 
+  indifference to peasents
+  whose poverty defines
+philosophies of the forgotten  
+</div></div>
+</article><!-- /publishing:body -->
+<div id="webmentions"></div>
+</main><footer><?php include __DIR__.'/footer.php'; ?></body></html>
