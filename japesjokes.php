6c6cadc8ee09c0fea4f5c6efa8f4b52c22b9c751
Greg McVerry <jgregmcverry@gmail.com>
2026-09-19 16:42:12 -0400

    Publish https://jgregorymcverry.com/japesjokes

---
+++ japesjokes.php
@@ -0,0 +1,30 @@
+<!-- publishing:managed -->
+<?php include __DIR__.'/head.php'; ?>
+</head><body><?php include __DIR__.'/header.php'; ?></header>
+<main class="longreads">
+<!-- publishing:body --><article class="note h-entry" style="grid-auto-rows:auto">
+
+<div class="pubinfo" style="grid-column:2/8"><p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">Greg McVerry</a> on <time class="dt-published" datetime="2026-09-19">2026-09-19</time> · <a class="u-url" href="https://jgregorymcverry.com/japesjokes">Permalink</a></p></div>
+<div class="p-name e-content copy" style="grid-row:3"><div class="verse" style="white-space:pre-wrap">Jape&#x27;s Jokes
+ and Aarons
+ paintings
+ experimenting
+  in
+hegemony
+  of
+  mind
+   recursive
+design of regression
+ to the mean
+Eliza we all know.
+the voice behind
+     the machine
+ syntax of sentences
+ in sin taxes of
+ form hunting
+ novelty&#x27;s 
+ sheen
+</div></div>
+</article><!-- /publishing:body -->
+<div id="webmentions"></div>
+</main><footer><?php include __DIR__.'/footer.php'; ?></body></html>
