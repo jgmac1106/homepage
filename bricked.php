009715b4178ebfe88b39f9ada8d2e7c0bbf9c7be
Greg McVerry <jgregmcverry@gmail.com>
2026-09-19 09:57:57 -0400

    Publish https://jgregorymcverry.com/bricked

--- bricked.php
+++ bricked.php
@@ -2,10 +2,10 @@
 <?php include __DIR__.'/head.php'; ?>
 </head><body><?php include __DIR__.'/header.php'; ?></header>
 <main class="longreads">
-<!-- publishing:body --><article class="h-entry" style="grid-auto-rows:auto">
-<h1 class="p-name">Bricked</h1>
+<!-- publishing:body --><article class="note h-entry" style="grid-auto-rows:auto">
+
 <div class="pubinfo" style="grid-column:2/8"><p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">Greg McVerry</a> on <time class="dt-published" datetime="2026-09-17">2026-09-17</time> · <a class="u-url" href="https://jgregorymcverry.com/bricked">Permalink</a></p></div>
-<div class="e-content copy" style="grid-row:3"><div class="verse" style="white-space:pre-wrap">In chimes of pain
+<div class="p-name e-content copy" style="grid-row:3"><div class="verse" style="white-space:pre-wrap">In chimes of pain
 a solace of peace bubbles up
 breath releases
 into a stolen tomorrow built
