ab52f4d7284a2316321e2952d7dcf63c60bf7afb
Greg McVerry <jgregmcverry@gmail.com>
2026-09-19 14:49:07 -0400

    Publish https://jgregorymcverry.com/grail

---
+++ grail.php
@@ -0,0 +1,30 @@
+<!-- publishing:managed -->
+<?php include __DIR__.'/head.php'; ?>
+</head><body><?php include __DIR__.'/header.php'; ?></header>
+<main class="longreads">
+<!-- publishing:body --><article class="note h-entry" style="grid-auto-rows:auto">
+
+<div class="pubinfo" style="grid-column:2/8"><p>Published by <a class="p-author h-card" href="https://jgregorymcverry.com">Greg McVerry</a> on <time class="dt-published" datetime="2026-09-19">2026-09-19</time> · <a class="u-url" href="https://jgregorymcverry.com/grail">Permalink</a></p></div>
+<div class="p-name e-content copy" style="grid-row:3"><div class="verse" style="white-space:pre-wrap">Gilgamesh
+ Ganymede and
+ Galahad
+ Drinking from
+ our Grail
+   cursing
+ The Wandering
+ in denying
+  entropic release
+  from an 
+  everlasting spring
+ tucked in magical
+  memories 
+  infinitely prolonged
+  Aligning with
+  A Nameless One
+  swirling as the Chalice
+  falls hollow
+  in a Plague of Life
+</div></div>
+</article><!-- /publishing:body -->
+<div id="webmentions"></div>
+</main><footer><?php include __DIR__.'/footer.php'; ?></body></html>
